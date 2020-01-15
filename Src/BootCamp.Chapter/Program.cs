99b5ca1a0e2fec8851fee5d3d6c1d7c5d135fd13
Mihail Fox <mihai.fologea@gmail.com>
2020-01-15 23:12:21 +0200

    my initial homework commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,32 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			int numberOfPersons = 2;
+			int currentPerson = 0;
+
+			while (currentPerson < numberOfPersons)
+			{
+				Console.Write("Name: ");
+				string name = Console.ReadLine();
+				Console.Write("Surename: ");
+				string surename = Console.ReadLine();
+				Console.Write("Age: ");
+				string years = Console.ReadLine();
+				Console.Write("Weight: ");
+				string weight = Console.ReadLine();
+				Console.Write("Height: ");
+				string heigth = Console.ReadLine();
+
+				Console.WriteLine($"{name} {surename} is {years} old, his weight is {weight} kg and his height is {heigth} cm");
+
+				double bmi = Math.Round(Convert.ToDouble(weight) / Math.Pow(Convert.ToDouble(heigth) / 100.0, 2), 2, MidpointRounding.AwayFromZero);
+				
+				Console.WriteLine($"Your BMI is: {bmi}\n");
+				
+				Console.WriteLine("press ENTER to continue...\n");
+				Console.ReadLine();
+				currentPerson++;
+			}
         }
     }
 }
