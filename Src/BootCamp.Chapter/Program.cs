de1822ca7a44ede82349c5e65933c4b59aab4b15
RoelofWobben <Charlie_lab19>
2020-01-15 22:39:04 +0100

    my homework from day1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -2,11 +2,42 @@
 
 namespace BootCamp.Chapter
 {
-    class Program
-    {
-        static void Main(string[] args)
+     static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("What is your name: ");
+                var name = Console.ReadLine();
+
+                Console.Write("What is your surname: ");
+                var surnname = Console.ReadLine();
+
+                Console.Write("What is your age: ");
+                var age = int.Parse(Console.ReadLine());
+
+                Console.Write("What is your weight in kilogram: ");
+                float weight = float.Parse(Console.ReadLine());
+
+                Console.Write("What is your height in metres: ");
+                float height = float.Parse(Console.ReadLine());
+
+                float bmi = GetBmi(weight, height);
+
+                Console.WriteLine($"{name}  {surnname}  is {age} years old, his weight is {weight} kg and his height is {height:F2} cm.");
+                Console.WriteLine($"His BMI is:  {bmi:N2}");
+                Console.WriteLine("");
+                if (i == 0)
+                {
+                    Console.WriteLine("Now for another person \n ");
+                }
+            }
+        }
+
+        private static float GetBmi(float weight, float height)
+        {
+            return weight / (float)Math.Pow(height, 2);
         }
     }
+    
 }
