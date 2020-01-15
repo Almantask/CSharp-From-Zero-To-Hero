6944ddaec026e5980cf05f4e112160c9be042bc8
EiEiO11490 <jdog11490@yahoo.com>
2020-01-15 16:27:52 -0500

    Added code from Homework 1 for Homework 2 Fusssssiiioonnn.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName = "Tom";
+            string lastName = "Jefferson";
+            Decimal prisonerAge = 19;
+            Decimal weight = 50;
+            Decimal height = 156.5m;
+            Decimal bodyMassIndex = (weight / height / height) * 10000;
+
+            Console.WriteLine(firstName + " " + lastName + ", is " + prisonerAge + " years old, " + weight + " kg, and " + height + " cm.");
+            Console.WriteLine("Prisoner BMI: " + bodyMassIndex);
+            Console.ReadLine();
+
+            string firstNameTu = "Kai";
+            string lastNameTu = "the Nigerian C# prince";
+            Decimal prisonerAgeTu = 25;
+            Decimal weightTu = 69;
+            Decimal heightTu = 420;
+            Decimal bodyMassIndexTu = (weightTu / heightTu / heightTu) * 10000;
+
+            Console.WriteLine(firstNameTu + " " + lastNameTu + ", is " + prisonerAgeTu + " years old, " + weightTu + " kg, and " + heightTu + " cm.");
+            Console.WriteLine("Prisoner BMI: " + bodyMassIndexTu);
+            Console.ReadLine();
+
         }
     }
 }
