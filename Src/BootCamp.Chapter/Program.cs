97cda6cfcec7f10027860de63b59272d69b4f65f
davinsoman <33527736+davinsoman@users.noreply.github.com>
2020-01-16 14:27:39 -0500

    Chapter 1/2 Homework
    While bulky to read, it does satisfy each requirement in the homework. The only thing that could be modified in my head is the square root variable as I know there is a simpler method to do so.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,41 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Hancock";
+            string surname = "Johnson";
+            int age = 20;
+            float weight = 81;
+            float heightcm = 180;
+            float heightm = heightcm / 100;
+            float heightsq = heightm * heightm;
+            float BMI = weight / heightsq;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Weight: " + weight + "kg");
+            Console.WriteLine("Height: " + heightcm + "cm");
+            Console.WriteLine("BMI: " + BMI);
+
+            Console.WriteLine();
+
+            string name2 = "Debra";
+            string surname2 = "Jones";
+            int age2 = 22;
+            float weight2 = 45;
+            float heightcm2 = 165;
+            float heightm2 = heightcm2 / 100;
+            float heightsq2 = heightm2 * heightm2;
+            float BMI2 = weight2 / heightsq2;
+
+            Console.WriteLine("Name: " + name2);
+            Console.WriteLine("Surname: " + surname2);
+            Console.WriteLine("Age: " + age2);
+            Console.WriteLine("Weight: " + weight2 + "kg");
+            Console.WriteLine("Height: " + heightcm2 + "cm");
+            Console.WriteLine("BMI: " + BMI2);
+
+            Console.ReadLine();
         }
     }
 }
