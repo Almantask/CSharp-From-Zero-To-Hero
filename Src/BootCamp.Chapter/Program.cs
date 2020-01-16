aed8255367fb9487529b9faf1e2ab5219973db71
mkravtz <mkravtz@outlook.com>
2020-01-16 09:06:11 -0700

    Renamed all types to "var" for consistency.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -11,7 +11,7 @@ namespace BootCamp.Chapter
             {
                 // Get name, age, weight, and height from the user.
                 Console.Write("Please enter your surname: ");
-                string name = Console.ReadLine();
+                var name = Console.ReadLine();
                 Console.Write("Please enter your age: ");
                 var age = Console.ReadLine();
                 Console.Write("Please enter your weight (in kg): ");
@@ -19,7 +19,7 @@ namespace BootCamp.Chapter
                 Console.Write("Please enter your height (in m): ");
                 var height = Console.ReadLine();
                 // Set the user data in a sentence and then write it to the console.
-                string sentence = (name + " is " + age + " " + "years old, his weight is " + weight + "kg and his height is " + height + "m.");
+                var sentence = (name + " is " + age + " " + "years old, his weight is " + weight + "kg and his height is " + height + "m.");
                 Console.WriteLine(sentence);
                 // Double height in preparation for calculating BMI.
                 var heightToDouble = Convert.ToDouble(height) * Convert.ToDouble(height);
@@ -29,7 +29,7 @@ namespace BootCamp.Chapter
                 Double Bmi = weightToDouble / heightToDouble;
                 //Convert BMI from Double to Int for better readability by the user.
                 var BmiFinal = Convert.ToInt32(Bmi);
-                string BmiOutput = ("Your estimated BMI is " + BmiFinal);
+                var BmiOutput = ("Your estimated BMI is " + BmiFinal);
                 Console.WriteLine(BmiOutput);
             }
         }
