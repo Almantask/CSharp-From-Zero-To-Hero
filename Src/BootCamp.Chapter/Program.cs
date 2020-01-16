c20bba6c80a1c6523e8c9b84b27eada943de639a
Cheeseman <bendi@outlook.hu>
2020-01-16 23:33:37 +0100

    I've modified several parts in this version, namely 1. I moved the declarations to their corresponding places and fixed some typos there too; 2. Fixed some typos in printing; 3. Modified the height input. It now asks for meter and not for centimeter. These are the parts I was unable to modify: 1.The while loop's condition (for some reason the string.ToLover() does not work for me...) 2.The last (cont = Console.ReadLine() )line. If I make it into a simple ReadLine() it causes the program's while loop to run indefinietly.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,29 +6,26 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            String name;
-            int age;
-            double height;
-            double weight;
-            String cont = "Y";
+          
+            string cont = "Y";
             while (cont.Equals("Y") || cont.Equals("y"))
             {
                 Console.Write("What is your name?");
-                name = Console.ReadLine();
+                string name = Console.ReadLine();
                 Console.Write("How old are you?");
-                age = int.Parse(Console.ReadLine());
-                Console.WriteLine("How tall are you? In cm ");
-                if (!double.TryParse(Console.ReadLine(), out height))
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("How tall are you? In meters ");
+                if (!double.TryParse(Console.ReadLine(), out double height))
                 {
                     Console.WriteLine("Something is wrong with your height");
                 }
                 Console.WriteLine("What is your weight? In kg ");
-                if (!double.TryParse(Console.ReadLine(), out weight))
+                if (!double.TryParse(Console.ReadLine(), out double weight))
                 {
                     Console.WriteLine("Something is wrong with your weight");
                 }
-                Console.WriteLine(name + " is" + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
-                Console.WriteLine("Their BMI Index is " + (weight / (height * height))); //Math.Pow(height, 2) = (height * height)
+                Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
+                Console.WriteLine("Their BMI Index is " + (weight / Math.Pow(height, 2) ) ); //Math.Pow(height, 2) = (height * height)
                 Console.Write("Do you wish to make an another round? Y/N ");
                 cont = Console.ReadLine();
             }
