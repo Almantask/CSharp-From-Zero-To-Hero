6c62a2525cb3c1118719abcf4f6e537c414e7022
escht <lauren@almost-legit.com>
2020-01-17 14:50:08 -0500

    Started over, with the suggested changes. Working to address the calculation part now.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,36 +6,38 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-           string[] firstName = { "Josie", "Daniel", "Liam" };
-            string[] lastName = { "Flores", "James", "Jones" };
-            int[] age = { 33, 34, 37 };
-            float[] weight = { 58.967f, 65.77f, 71.11f }; // in kg
-            float[] height = { 167.64f, 177.8f, 182.88f }; // in cm
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(firstName);
 
-            Console.WriteLine(firstName[0] + " " + lastName[0] + " is " + age[0] + " years old, her weight is " + weight[0] + "kg and her height is " + height[0] + " cm.");
-            Console.WriteLine(firstName[1] + " " + lastName[1] + " is " + age[1] + " years old, his weight is " + weight[1] + "kg and his height is " + height[1] + " cm.");
-            Console.WriteLine(firstName[2] + " " + lastName[2] + " is " + age[2] + " years old, his weight is " + weight[2] + "kg and his height is " + height[2] + " cm.");
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(lastName);
 
-            // Formula: weight (kg) / [height (m)]2
+            Console.Write("Age: ");
+            var age = Console.ReadLine();
+            Console.WriteLine(age);
+
+            Console.Write("Height (in cm): ");
+            float height = Console.ReadLine();
+            Console.WriteLine(height);
+
+            Console.Write("Weight (in kg): ");
+            float weight = Console.ReadLine();
+            Console.WriteLine(weight);
+
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, their weight is " + weight + "kg, their height is " + height + " cm and their BMI is ");
+
+            // BMI formula: weight (kg) / [height (m)]2
             // Calculation: [weight(kg) / height(cm) / height(cm)] x 10,000
             // cm to m --> divide by 100
 
-            var toMeters0 = (height[0] / 100);
-            var metersSquared0 = (toMeters0 * toMeters0);
-            var bmiJosie = (weight[0] / metersSquared0);
+            // my code.. this is where I am having trouble
+            // var toMeters = height / 100;
+            // var metersSquared = toMeters * toMeters;
+            // var bmi = weight / metersSquared;
+            // Console.WriteLine(bmi);
 
-            var toMeters1 = (height[1] / 100);
-            var metersSquared1 = (toMeters1 * toMeters1);
-            var bmiDaniel = (weight[1] / metersSquared1);
-
-            var toMeters2 = (height[2] / 100);
-            var metersSquared2 = (toMeters2 * toMeters2);
-            var bmiLiam = (weight[2] / metersSquared2);
-
-            Console.WriteLine(); // line break
-            Console.WriteLine(firstName[0] + "'s BMI: " + bmiJosie);
-            Console.WriteLine(firstName[1] + "'s BMI: " + bmiDaniel);
-            Console.WriteLine(firstName[2] + "'s BMI: " + bmiLiam);
         }
     }
 }
