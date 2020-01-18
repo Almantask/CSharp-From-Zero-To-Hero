88594950de88f83db406a3d91a9f157ca6787fe5
KiddX3 <unlu658@gmail.com>
2020-01-18 16:17:37 +0000

    Homework 1: Used a while loop for multiple users.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -4,9 +4,39 @@ namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //First Commit
         {
-            Console.WriteLine("Hello World!");
+            string firstname;
+            string surname;
+            int age;
+            double weight;
+            double height;
+            string ans;
+
+            do
+            {
+                Console.WriteLine("What is your Firstname?   ");
+                firstname = Console.ReadLine();
+                Console.WriteLine("What is your Surname?   ");
+                surname = Console.ReadLine();
+                Console.WriteLine("Age?");
+                age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Weight?");
+                weight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Height?");
+                height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Your firstname is " + firstname + " and surname is " + surname + " , Age is " + age + ". Your weight is " + weight + " in kg and " +
+                    "height is " + height + " in cm.");
+                Console.WriteLine("Their BMI is " + (weight / (height / 100 * height / 100)));
+
+                Console.WriteLine("Again?");
+                ans = Console.ReadLine();
+
+            } while (ans != "no");
+
+            
+
         }
     }
 }
