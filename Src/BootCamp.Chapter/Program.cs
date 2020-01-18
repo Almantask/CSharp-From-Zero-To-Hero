9992b3419f35be9ede64473ba3bd758d498f7b63
JProgg <jprog@protonmail.com>
2020-01-18 12:25:34 +0100

    Fixed assing of variables in the same line

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -8,29 +8,20 @@ namespace BootCamp.Chapter
         {
             for (int i = 0; i < 2; i++)
             {
-                string name;
                 Console.Write("Please, tell me your name: ");
-                name = Console.ReadLine();
+                string name = Console.ReadLine();
 
-                
-                string surname;
                 Console.Write("Please, tell me your surname: ");
-                surname = Console.ReadLine();
+                string surname = Console.ReadLine();
 
-                
-                int age = 0;
                 Console.Write("Please, tell me your age: ");
-                age = Convert.ToInt32(Console.ReadLine());
+                int age = Convert.ToInt32(Console.ReadLine());
 
-                
-                double weight = 0;
                 Console.Write("Please, tell me your weight: ");
-                weight = Convert.ToDouble(Console.ReadLine());
+                double weight = Convert.ToDouble(Console.ReadLine());
 
-                
-                double height = 0;
                 Console.Write("Please, tell me your height (in cm): ");
-                height = Convert.ToDouble(Console.ReadLine());
+                double height = Convert.ToDouble(Console.ReadLine());
 
                 Console.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
 
