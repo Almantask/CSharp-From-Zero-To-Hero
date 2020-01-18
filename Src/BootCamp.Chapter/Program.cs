b2dc953f728b50521afa6b7e392a5ecf09edf929
JProgg <jprog@protonmail.com>
2020-01-18 11:44:02 +0100

    Fixed: Debug to console For Loop for better reading Declared variables on more proper places

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,34 +7,37 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name;
-            string surname = "";
-            int age = 0;
-            double weight = 0;
-            double height = 0;
-
-            for (int i = 0; i <= 1; i++)
+            for (int i = 0; i < 2; i++)
             {
-                Debug.Write("Please, tell me your name: ");
+                string name;
+                Console.Write("Please, tell me your name: ");
                 name = Console.ReadLine();
 
-                Debug.Write("Please, tell me your surname: ");
+                
+                string surname;
+                Console.Write("Please, tell me your surname: ");
                 surname = Console.ReadLine();
 
-                Debug.Write("Please, tell me your age: ");
+                
+                int age = 0;
+                Console.Write("Please, tell me your age: ");
                 age = Convert.ToInt32(Console.ReadLine());
 
-                Debug.Write("Please, tell me your weight: ");
+                
+                double weight = 0;
+                Console.Write("Please, tell me your weight: ");
                 weight = Convert.ToDouble(Console.ReadLine());
 
-                Debug.Write("Please, tell me your height (in cm): ");
+                
+                double height = 0;
+                Console.Write("Please, tell me your height (in cm): ");
                 height = Convert.ToDouble(Console.ReadLine());
 
-                Debug.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
+                Console.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
 
                 double BMI = Math.Round(CalculateBMI(weight, height), 2);
 
-                Debug.WriteLine("His BMI is: {0} \n", BMI);
+                Console.WriteLine("His BMI is: {0} \n", BMI);
             }
 
         }
