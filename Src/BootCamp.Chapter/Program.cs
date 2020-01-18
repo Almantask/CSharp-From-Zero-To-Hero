25ac9d7bcdef92f7b14230a618ef117424faea1d
chatie <chatie@gmail.com>
2020-01-18 01:22:47 -0800

    Calculate BMI with fixes: 1) Declared variables as late as possible so that reader does not need to memorize each variable to save time and concentration on more important things. 2) Read data for 2 persons instead of 1. 3) Merge into CSinn Homework branch instead of Master Branch.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,27 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int num = 0; num < 2; num++)
+            {
+                Console.WriteLine("Calculate BMI for a person.");
+
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter weight: ");
+                float weight = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter height: ");
+                float height = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+                //BMI is weight (kg) / [height (m)] ^ 2
+                float bmi = (float)(weight / Math.Pow((height / 100), 2));
+                Console.WriteLine("Body-mass index (BMI) is " + bmi);
+            }
         }
     }
 }
