9044aae533d502d18511306ecf2bcb8eb19546ec
Almantas Karpavičius <almantusk@gmail.com>
2020-01-18 14:28:26 +0200

    Homework 3 tests

---
+++ Src/BootCamp.Chapter/Checks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    /// <summary>
+    /// Test class is used to test your implementation.
+    /// Each homework will have a set of steps that you will have to do.
+    /// You can name your functions however you want, but to validate your solution, place them here.
+    /// DO NOT CALL FUNCTIONS FROM TESTS CLASS
+    /// DO NOT IMPLEMENT FUNCTIONS IN TESTS CLASS
+    /// TESTS CLASS FUNCTIONS SHOULD ALL HAVE 1 LINE OF CODE
+    /// </summary>
+    public static class Checks
+    {
+        public static int PromptInt(string message)
+        {
+            // To do: call your implementation. 
+            return 0;
+        }
+
+        public static string PromptString(string message)
+        {
+            // To do: call your implementation. 
+            return "";
+        }
+
+        public static float PromptFloat(string message)
+        {
+            // To do: call your implementation. 
+            return 0;
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            // To do: call your implementation. 
+            return 0;
+        }
+    }
+}
---
+++ Tests/BootCamp.Chapter.Tests/Utils/ConsoleStub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BootCamp.Chapter.Tests.Utils
+{
+    public static class ConsoleStub
+    {
+        public static StringWriter StubConsole(string readLineReturn)
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            var input = new StringReader(readLineReturn);
+            Console.SetIn(input);
+
+            return output;
+        }
+    }
+}
