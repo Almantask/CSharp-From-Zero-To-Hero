c8fe520942f63af25d493cc3ebd328cd1eca5833
Stewart Leslie <54007059+Relem76@users.noreply.github.com>
2020-01-18 18:29:40 +0000

    Update and rename BMISLeslie to BMISLeslie.cs

---
+++ Src/BootCamp.Chapter/BMISLeslie.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace BMISLeslie
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Declare variables
+            string name;
+            string surname;
+            int age;
+            decimal height, weight, bmi;
+            
+            //First Person
+            //Console Entries and add value to variables
+            Console.WriteLine("Please enter your first name:");
+            name = (Console.ReadLine());
+            Console.WriteLine("Please Enter your surname:");
+            surname = (Console.ReadLine());
+            Console.WriteLine("Please enter your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your height in cm:");
+            height = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter your weight in kg:");
+            weight = Convert.ToDecimal(Console.ReadLine());
+
+            //Work out BMI
+            bmi = weight / ((height / 100) * (height / 100));
+
+            //Printout
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cms. His BMI is {Math.Round(bmi, 2)}.");
+
+        //2nd Person
+        //Console Entries and add value to variables
+        Console.WriteLine("Please enter your first name:");
+            name = (Console.ReadLine());
+            Console.WriteLine("Please Enter your surname:");
+            surname = (Console.ReadLine());
+            Console.WriteLine("Please enter your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your height in cm:");
+            height = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter your weight in kg:");
+            weight = Convert.ToDecimal(Console.ReadLine());
+
+            //Work out BMI
+            bmi = weight / ((height / 100) * (height / 100));
+
+            //Printout
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cms. His BMI is {Math.Round(bmi, 2)}.");
+        }
+    }
+}
