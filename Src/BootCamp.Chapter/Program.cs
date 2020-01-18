6921174dd846412aa4c19554aaabb1c78e999991
chatie <chatie@gmail.com>
2020-01-18 00:02:40 -0800

    1) Declared variables as late as possible so that reader does not need to memorize each variable to save time and concentration on more important things. 2) Read data for 2 persons instead of 1. 3) Merge into CSinn Homework branch instead of Master Branch. :-)

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,29 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name;
-            string surname;
-            int age;
-            int weight;
-            float height;
-            float bmi;
+            for (int num = 0; num < 3; num++)
+            {
+                Console.WriteLine("Calculating BMI for a person.");
 
-            Console.Write("Enter name: ");
-            name = Console.ReadLine();
-            Console.Write("Enter surname: ");
-            surname = Console.ReadLine();
-            Console.Write("Enter age: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter weight: ");
-            weight = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter height: ");
-            height = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter weight: ");
+                float weight = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter height: ");
+                float height = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+                Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
 
-            //BMI is weight (kg) / [height (m)] ^ 2
-            bmi = (float)(weight / Math.Pow((height / 100), 2));
-            Console.WriteLine("Body-mass index (BMI) is " + bmi);
+                //BMI is weight (kg) / [height (m)] ^ 2
+                float bmi = (float)(weight / Math.Pow((height / 100), 2));
+                Console.WriteLine("Body-mass index (BMI) is " + bmi);
+            }
+
         }
     }
 }
