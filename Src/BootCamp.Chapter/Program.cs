900c19aa2591d61885491df52f9bd1fb289bad76
LionThrewpy <karper360l@gmail.com>
2020-01-18 14:20:02 +0000

    Completed Homework#1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,8 +6,21 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Added another line to hello world!");
+            Console.Write("First name: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Surname: ");
+            var surName = Console.ReadLine();
+            Console.Write("Age: ");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("Weight in kilograms: ");
+            var weightInKilograms = float.Parse(Console.ReadLine());
+            Console.Write("Height in centimeters: ");
+            var heightInCentimeters = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstName} {surName} is {age} years old, his weight is {weightInKilograms} kg and his height is {heightInCentimeters} cm.");
+            
+            var bodyMassIndex = weightInKilograms / (Math.Pow(heightInCentimeters, 2));
+            Console.WriteLine($"Their body mass index is {bodyMassIndex}");
         }
     }
 }
