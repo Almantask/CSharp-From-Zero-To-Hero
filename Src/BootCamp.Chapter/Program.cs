20a500f84710d46571f74afddcaf7defde2a4f61
escht <escht@users.noreply.github.com>
2020-01-18 10:02:57 -0500

    adjusted height and weight

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,58 +9,48 @@ namespace BootCamp.Chapter
             Console.WriteLine("First User");
             Console.Write("First name: ");
             string firstName = Console.ReadLine();
-            Console.WriteLine(firstName);
 
             Console.Write("Last name: ");
             string lastName = Console.ReadLine();
-            Console.WriteLine(lastName);
 
             Console.Write("Age: ");
             var age = Console.ReadLine();
-            Console.WriteLine(age);
 
             Console.Write("Height (in cm): ");
-            var height = Console.ReadLine();
-            Console.WriteLine(height);
+            var height = float.Parse(Console.ReadLine());
 
             Console.Write("Weight (in kg): ");
-            var weight = Console.ReadLine();
-            Console.WriteLine(weight);
+            var weight = float.Parse(Console.ReadLine());
 
             Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, their weight is " + weight + "kg and their height is " + height + " cm.");
 
             var toMeters = height / 100;
             var metersSquared = toMeters * toMeters;
             var bmi = weight / metersSquared;
-            Console.WriteLine(firstName + lastName + " BMI: " + bmi);
+            Console.WriteLine(firstName + " " + lastName + " BMI: " + bmi);
 
             Console.WriteLine("Second User");
             Console.Write("First name: ");
             string firstName2 = Console.ReadLine();
-            Console.WriteLine(firstName);
 
             Console.Write("Last name: ");
             string lastName2 = Console.ReadLine();
-            Console.WriteLine(lastName2);
 
             Console.Write("Age: ");
             var age2 = Console.ReadLine();
-            Console.WriteLine(age2);
 
             Console.Write("Height (in cm): ");
-            var height2 = Console.ReadLine();
-            Console.WriteLine(height2);
+            var height2 = float.Parse(Console.ReadLine()); ;
 
             Console.Write("Weight (in kg): ");
-            var weight2 = Console.ReadLine();
-            Console.WriteLine(weight2);
+            var weight2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(firstName2 + " " + lastName2 + " is " + age2 + " years old, their weight is " + weight2 + "kg and their height is " + height2 + " cm.");
 
-            var toMeters2 = height2 / 100;
+            var toMeters2 = height / 100;
             var metersSquared2 = toMeters2 * toMeters2;
-            var bmi2 = weight2 / metersSquared2;
-            Console.WriteLine(firstName2 + lastName2 + " BMI: " + bmi2);
+            var bmi2 = weight / metersSquared2;
+            Console.WriteLine(firstName2 + " " + lastName2 + " BMI: " + bmi2);
 
         }
     }
