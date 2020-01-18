3a3f5a2b3e120f7ae0ee7b6f4f8a8bf23de51820
chatie <chatie@gmail.com>
2020-01-17 23:10:01 -0800

    Calculate BMI

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,29 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            string surname;
+            int age;
+            int weight;
+            float height;
+            float bmi;
+
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter weight: ");
+            weight = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            //BMI is weight (kg) / [height (m)] ^ 2
+            bmi = (float)(weight / Math.Pow((height / 100), 2));
+            Console.WriteLine("Body-mass index (BMI) is " + bmi);
         }
     }
 }
