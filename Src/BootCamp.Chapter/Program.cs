8030b7c58cd6633879d86acc268cbe1f93b0dea6
KiddX3 <unlu658@gmail.com>
2020-01-18 16:33:23 +0000

    Made changes as requested by ellmaster. Variable declared before line where variable is used.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,30 +6,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args) //First Commit
         {
-            string firstname;
-            string surname;
-            int age;
-            double weight;
-            double height;
-            string ans;
-
+       
             do
             {
+                string firstname;
                 Console.WriteLine("What is your Firstname?   ");
                 firstname = Console.ReadLine();
+                string surname;
                 Console.WriteLine("What is your Surname?   ");
                 surname = Console.ReadLine();
+                int age;
                 Console.WriteLine("Age?");
                 age = Convert.ToInt32(Console.ReadLine());
+                double weight;
                 Console.WriteLine("Weight?");
                 weight = Convert.ToInt32(Console.ReadLine());
+                double height;
                 Console.WriteLine("Height?");
                 height = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Your firstname is " + firstname + " and surname is " + surname + " , Age is " + age + ". Your weight is " + weight + " in kg and " +
                     "height is " + height + " in cm.");
                 Console.WriteLine("Their BMI is " + (weight / (height / 100 * height / 100)));
-
+                
+                string ans;
                 Console.WriteLine("Again?");
                 ans = Console.ReadLine();
 
