00a97a8cf0ea15253230bfd4f34a07656931a334
Jason Jarvis <blibmaster@msn.com>
2020-01-18 00:29:16 +0000

    This is my first commit, I am attempting to submit my program.cs for homework.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,67 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstname;
+            string surname;
+            int age;
+            float weight;
+            float height;
+            float bmi;
+
+            Console.WriteLine("What is your first name?");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your surname?");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your weight in kg?");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is height in cm?");
+            height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine(firstname + " " + surname + " is " + age + " years old, his weight is " + weight + "kg and his height is " + height + "cm");
+            Console.WriteLine("This makes his BMI " + weight / ((height / 100) * (height / 100)));
+
+            Console.WriteLine("What is your first name?");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your surname?");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your weight in kg?");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is height in cm?");
+            height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine(firstname + " " + surname + " is " + age + " years old, his weight is " + weight + "kg and his height is " + height + "cm");
+            Console.WriteLine("This makes his BMI " + weight / ((height / 100) * (height / 100)));
+
+            Console.WriteLine("What is your first name?");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("What is your surname?");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your weight in kg?");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is height in cm?");
+            height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine(firstname + " " + surname + " is " + age + " years old, his weight is " + weight + "kg and his height is " + height + "cm");
+            bmi = weight / ((height / 100) * (height / 100));
+            Console.WriteLine("This makes his BMI " + bmi);
         }
     }
 }
