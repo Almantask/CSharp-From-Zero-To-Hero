9604e7bf531b2c8bf56d5d56913cd90148f99ea9
KiddX3 <unlu658@gmail.com>
2020-01-18 23:30:00 +0000

    Declared and assigned variable on one line.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -11,28 +11,24 @@ namespace BootCamp.Chapter
        
             do
             {
-                string firstname;
+                
                 Console.WriteLine("What is your Firstname?   ");
-                firstname = Console.ReadLine();
-                string surname;
+                string firstname = Console.ReadLine();
                 Console.WriteLine("What is your Surname?   ");
-                surname = Console.ReadLine();
-                int age;
+                string surname = Console.ReadLine();
                 Console.WriteLine("Age?");
-                age = Convert.ToInt32(Console.ReadLine());
-                double weight;
+                int age = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Weight?");
-                weight = Convert.ToInt32(Console.ReadLine());
-                double height;
+                double weight = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Height?");
-                height = Convert.ToInt32(Console.ReadLine());
+                double height = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Your firstname is " + firstname + " and surname is " + surname + " , Age is " + age + ". Your weight is " + weight + " in kg and " +
                     "height is " + height + " in cm.");
                 Console.WriteLine("Their BMI is " + (weight / (height / 100 * height / 100)));
                 
                 
-                Console.WriteLine("Again?");
+                Console.WriteLine("Again? type no to exit");
                 ans = Console.ReadLine();
 
             } while (ans != "no");
