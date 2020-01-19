22800072f3f02c7ef5318f2babfe95d69970063a
Mihail Fox <mihai.fologea@gmail.com>
2020-01-19 16:13:17 +0200

    moved program logic to class Lesson3

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -7,19 +7,19 @@ namespace BootCamp.Chapter
     {
         public static int ConvertStringToInt(string message)
         {
-            Console.WriteLine(message);
+            Console.Write(message);
             return Convert.ToInt32(Console.ReadLine());
         }
 
         public static string ConvertStringToString(string message)
         {
-            Console.WriteLine(message);
+            Console.Write(message);
             return Console.ReadLine();
         }
 
         public static float ConvertStringToFloat(string message)
         {
-            Console.WriteLine(message);
+            Console.Write(message);
 
             return float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
@@ -27,6 +27,29 @@ namespace BootCamp.Chapter
         public static float Bmi(float weight, float heigth)
         {
             return weight / (float)Math.Pow(heigth, 2);
+
+        }
+
+        public static void Demo(int numberOfPersons = 1)
+        {
+           int currentPerson = 0;
+
+           while (currentPerson < numberOfPersons)
+            {
+                string name = ConvertStringToString("Name: ");
+                string sureName = ConvertStringToString("Surename: ");
+                int age = ConvertStringToInt("Age: ");
+                float weight = ConvertStringToFloat("Weight: ");
+                float height = ConvertStringToFloat("Height: ");
+
+                Console.WriteLine($"{name} {sureName} is {age} old, his weight is {weight}kg and his heigth is {height}m");
+
+                Console.WriteLine($"{sureName} {name}'s BMI is {Bmi(weight, height)}\n");
+
+                Console.WriteLine("press ENTER to continue...\n");
+                Console.ReadLine();
+                currentPerson++;
+            }
 
         }
     }
