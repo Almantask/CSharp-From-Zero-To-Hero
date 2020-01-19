ee459d779b74aaaa3510b6243e75d599ae088e54
Danesz <ragcsak.dancsi@gmail.com>
2020-01-19 14:49:36 +0100

    Added the second person, and changed a bit code.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,23 +6,36 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            //Tell me if I did the task wrongly, because I wasn't really sure if I had had to define them and not ask them in.
             //Read name, surename, age, weight (in kg) and height (in cm) from console
             // calculate BMI index
             Console.WriteLine("Welcome to my homework. Please, type in your full name");
-            string name = Console.ReadLine();
-            string surname = Console.ReadLine();
+            string name1 = Console.ReadLine();
             Console.WriteLine("Type in your age");
-            int age = int.Parse(Console.ReadLine());
+            int age1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Type in your weight(kg)");
-            int weight = int.Parse(Console.ReadLine());
+            double weight1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Type in your height(cm)");
-            double height = double.Parse(Console.ReadLine());
-            double heightM = Math.Pow(height / 100.0,2); //just to convert centimeters into meters, then ^2 them
-            double bmi = weight / heightM;
-            Console.WriteLine("{0} {1} is {2} years old, his weight is {3} kg and his height {4} cm.",name,surname,age,weight,height);
-            Console.WriteLine("His BMI index is: {0}", Math.Round(bmi,2));
+            double height1 = double.Parse(Console.ReadLine());
+            double heightM1 = Math.Pow(height1 / 100.0, 2); //just to convert centimeters into meters, then ^2 them
+            double bmi1 = weight1 / heightM1;
             
+            Console.WriteLine("Now, type in another person's data. Please start with fullname");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Type in your age");
+            int age2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type in your weight(kg)");
+            double weight2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type in your height(cm)");
+            double height2 = double.Parse(Console.ReadLine());
+            double heightM2 = Math.Pow(height2 / 100.0, 2); //just to convert centimeters into meters, then ^2 them
+            double bmi2 = weight2 / heightM2;
+
+            Console.WriteLine($"{name1} is {age1} years old, his weight is {weight1} kg and his height {height1} cm.");
+            Console.WriteLine("His BMI index is: {0}", Math.Round(bmi1, 2));
+            Console.WriteLine($"{name2} is {age2} years old, his weight is {weight2} kg and his height {height2} cm.");
+            Console.WriteLine("His BMI index is: {0}", Math.Round(bmi2, 2));
+        
+            Console.ReadKey();
         }
     }
 }
