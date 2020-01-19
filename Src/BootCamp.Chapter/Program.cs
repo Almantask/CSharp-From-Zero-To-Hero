457df27d7e2516f33fd232fb4542fe1fd917b221
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-01-19 13:02:21 +0100

    Repair indention.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -2,18 +2,18 @@
 {
     class Program
     {
-/// <summary>
-/// Homework 3
-/// 1) Fork https://github.com/csinn/CSharp-From-Zero-To-Hero/tree/Chapter1/Homework/3 
-/// 2) Take homework 2 code, place it in the forked branch and refactor it using functions. There should be as little duplicate code as possible(there should be functions for: 
-/// - Calculating BMI,
-/// - Prompt for input and converting it to int,
-/// - Prompt for input and converting it to string,
-/// - Prompt for input and converting it to float. 
-/// 3) Put all the function you made into the right places in Checks
-/// 4) Move all the code from main function into lesson3 class function called Demo.Call it from main function.
-/// </summary>
-/// <param name="args"></param>
+        /// <summary>
+        /// Homework 3
+        /// 1) Fork https://github.com/csinn/CSharp-From-Zero-To-Hero/tree/Chapter1/Homework/3 
+        /// 2) Take homework 2 code, place it in the forked branch and refactor it using functions. There should be as little duplicate code as possible(there should be functions for: 
+        /// - Calculating BMI,
+        /// - Prompt for input and converting it to int,
+        /// - Prompt for input and converting it to string,
+        /// - Prompt for input and converting it to float. 
+        /// 3) Put all the function you made into the right places in Checks
+        /// 4) Move all the code from main function into lesson3 class function called Demo.Call it from main function.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Lesson3.Demo();
