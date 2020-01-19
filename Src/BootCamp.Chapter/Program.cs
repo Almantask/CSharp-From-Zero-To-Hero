060a8ba0a670b5868af177a3d79d972f0c2422d4
kaiomv <kaio.manzano21@gmail.com>
2020-01-19 17:00:30 -0400

    Variables now declared as closed as used first, the second bmi calculation now uses the same variable as the first calculation

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,58 +6,50 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            int age;
-            double height, bmi, weight;
-            string name, surename;
-
             Console.WriteLine("BMI Calculator ");
             Console.WriteLine("Hello, what's the name of the person that you want to calculate the BMI? ");
-            name = Console.ReadLine();
+            string name = Console.ReadLine();
             
             Console.WriteLine("What's the surename? ");
-            surename = Console.ReadLine();
+            string surename = Console.ReadLine();
             
             Console.WriteLine("What's the Age? ");
-            age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine());
             
             Console.WriteLine("What's the height? ");
-            height = Convert.ToDouble(Console.ReadLine());
+            double height = Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine("What's the weight? ");
-            weight = Convert.ToDouble(Console.ReadLine());
+            double weight = Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
             
-            bmi = (weight / ((height / 100.0) * (height / 100.0)));
+            double bmi = (weight / ((height / 100.0) * (height / 100.0)));
 
             Console.WriteLine(name + " " + surename + " BMI is " + bmi);
             Console.ReadLine();
-
-            int age2;
-            double height2, bmi2, weight2;
-            string name2, surename2;
-
+            
             Console.WriteLine("BMI Calculator ");
             Console.WriteLine("Hello, what's the name of the person that you want to calculate the BMI? ");
-            name2 = Console.ReadLine();
+            name = Console.ReadLine();
 
             Console.WriteLine("What's the surename? ");
-            surename2 = Console.ReadLine();
+            surename = Console.ReadLine();
 
             Console.WriteLine("What's the Age? ");
-            age2 = Convert.ToInt32(Console.ReadLine());
+            age = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What's the height? ");
-            height2 = Convert.ToDouble(Console.ReadLine());
+            height = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("What's the weight? ");
-            weight2 = Convert.ToDouble(Console.ReadLine());
+            weight = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(name2 + " " + surename2 + " is " + age2 + " years old, his weight is " + weight2 + " kg and his height is " + height2 + " cm.");
+            Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
 
-            bmi2 = (weight2 / ((height2 / 100.0) * (height2 / 100.0)));
+            bmi = (weight / ((height / 100.0) * (height / 100.0)));
 
-            Console.WriteLine(name2 + " " + surename2 + " BMI is " + bmi2);
+            Console.WriteLine(name + " " + surename + " BMI is " + bmi);
             Console.ReadLine();
         }
     }
