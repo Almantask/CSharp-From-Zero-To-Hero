33bb1e040cf30b079d13a14808c787ac70918cbc
Tim <tim_k2020@outlook.com>
2020-01-20 20:17:38 +0400

    Submit Homework1&2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
 namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Weight(kg): ");
+                float weight = float.Parse(Console.ReadLine());
+
+                Console.Write("Height(cm): ");
+                float height = float.Parse(Console.ReadLine());
+
+                float bmi = weight / (float)Math.Pow(height / 100, 2);
+
+                Console.WriteLine($"{name} {surname} is {age} years old, his weight" +
+                    $"is {weight} kg and his height is {height:N1} cm.");
+                Console.WriteLine($"BMI: {bmi}\n");
+            }
+            Console.ReadKey();
         }
     }
 }
