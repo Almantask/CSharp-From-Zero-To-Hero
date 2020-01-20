43c3230eda7fdb1669e6d2a22d4803471691ac2d
pinology <59784688+pinology@users.noreply.github.com>
2020-01-20 20:52:02 +0800

    removed one-line variable declarations, moved to declaring the variable when used. i.e. string firstName; firstName = Console.ReadLine(); -> string firstName = Console.ReadLine();

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -12,38 +12,31 @@ namespace BootCamp.Chapter
             for (int i = 0; i < 2; i++)
             {
 
-                //input first name, last name
-                string firstName, lastName;
-
+            //input first name, last name
             Console.Write("Input First Name: ");
-            firstName = Console.ReadLine();
+            string firstName = Console.ReadLine();
             Console.Write("Input Last Name: ");
-            lastName = Console.ReadLine();
+            string lastName = Console.ReadLine();
 
             // input age
-            int age;
-
             Console.Write("Input age: ");
             string s = Console.ReadLine();
-            int.TryParse(s, out age);
+            int.TryParse(s, out int age);
 
             // input weight and height
-            float weight, height;
-
             Console.Write("Input weight (in kg): ");
             s = Console.ReadLine();
-            float.TryParse(s, out weight);
+            float.TryParse(s, out float weight);
 
             Console.Write("Input height (in cm): ");
             s = Console.ReadLine();
-            float.TryParse(s, out height);
+            float.TryParse(s, out float height);
             
             //print info
             Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
 
             //calculate and print bmi
             float bmi = weight / ((height / 100) * (height / 100));
-
             Console.WriteLine(firstName + " " + lastName + " has a BMI of " + bmi + ".");
 
             }
