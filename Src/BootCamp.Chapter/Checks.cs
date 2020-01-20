b5f28b0a64bac8af3598805fabbdd496c08a1cba
H.J.Holder <hjholder@alaska.edu>
2020-01-21 02:31:22 +0900

    Removed unused using statements

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter
 {
     /// <summary>
     /// Test class is used to test your implementation.
