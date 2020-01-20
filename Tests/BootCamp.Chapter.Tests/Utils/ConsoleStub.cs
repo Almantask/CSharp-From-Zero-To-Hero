3b2a213d7b07952b5c3e452cf38e3bfc61405c19
Almantas Karpavičius <almantusk@gmail.com>
2020-01-20 23:29:12 +0200

    More advanced tests for BMI Console sub to file, invalid input validation, invalid BMI validation

--- Tests/BootCamp.Chapter.Tests/Utils/ConsoleStub.cs
+++ Tests/BootCamp.Chapter.Tests/Utils/ConsoleStub.cs
@@ -7,6 +7,8 @@ namespace BootCamp.Chapter.Tests.Utils
 {
     public static class ConsoleStub
     {
+        private const string testFileExtension = "consoleStub";
+
         public static StringWriter StubConsole(string readLineReturn)
         {
             var output = new StringWriter();
@@ -16,6 +18,40 @@ namespace BootCamp.Chapter.Tests.Utils
             Console.SetIn(input);
 
             return output;
+        }
+
+        /// <summary>
+        /// Writes console output to a file called {testKey}.consoleStub
+        /// </summary>
+        public static StreamWriter StubConsole(string readLineReturn, string testKey)
+        {
+            var output = new StreamWriter($"{testKey}.{testFileExtension}");
+            Console.SetOut(output);
+
+            var input = new StringReader(readLineReturn);
+            Console.SetIn(input);
+
+            return output;
+        }
+
+        /// <summary>
+        /// To be used only if <see cref="StreamWriter"/> for stub console was used.
+        /// Read file of console input by testKey.
+        /// </summary>
+        public static string ReadAllText(string testKey)
+        {
+            var output = File.ReadAllText($"{testKey}.{testFileExtension}");
+
+            return output;
+        }
+
+        /// <summary>
+        /// To be used only if <see cref="StreamWriter"/> for stub console was used.
+        /// Deletes the file for console using testKey.
+        /// </summary>
+        public static void Cleanup(string testKey)
+        {
+            File.Delete($"{testKey}.{testFileExtension}");
         }
     }
 }
