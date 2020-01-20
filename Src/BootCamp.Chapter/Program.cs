784cfb975b83e8d202dd9815e38ece865885fe3d
Dan C <dconway0421@gmail.com>
2020-01-19 22:25:05 -0500

    corrected issues per pull request

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BootCamp.Chapter
 {
@@ -24,13 +25,14 @@ namespace BootCamp.Chapter
                 Console.WriteLine("How much do you weight in pounds?");
                 weight = int.Parse(Console.ReadLine());
                 
-                float calculateBMI = 703 * weight / (height * height);
+                float calculateBMI = 703f * weight / (height * height);
 
-                Console.WriteLine($"{name} is {age} years old, weighs {weight} pounds and is {height} inches tall. \nWith a weight of {weight} pounds and height of {height} inches, {name} has a BMI of {calculateBMI}.");
+                Console.WriteLine($"{name} is {age} years old, weighs {weight} pounds and is {height} inches tall. ");
+                Console.WriteLine($"\nWith a weight of {weight} pounds and height of {height} inches, {name} has a BMI of {calculateBMI}.");
 
                 
                 Console.WriteLine("\nWould you like the check the BMI of someone else? (y/n)");
-                askAgain = Console.ReadLine().ToLower();
+                askAgain = Console.ReadLine().ToLower(CultureInfo.InvariantCulture);
                     while (askAgain != "n" && askAgain != "y")
                     {
                         Console.WriteLine($"Invalid answer; you entered {askAgain} please enter y or n.");
