08647066b0333b7413d894b2c4c408188b0d2fa8
SilentKingdom13 <59810236+SilentKingdom13@users.noreply.github.com>
2020-01-20 19:52:37 +0000

    Updated to read from 2 people in the console.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-        // Homework Lesson 1 & 2 - SilentKingdom
+            // Homework Lesson 1 & 2 - SilentKingdom
             // BMI Calculator for 2 people
 
             // First person = variable1
@@ -27,20 +27,23 @@ namespace BootCamp.Chapter
             float sum1 = weight1 / height_s1;
             Console.WriteLine(name1 + " has a BMI of " + sum1 + ".");
 
-            // Comparison to Tom Jefferson
-            Console.WriteLine("\nWe'll compare " + name1 + "'s BMI with that of a previous subject:\n");
 
             // Second person = variable2
-            const string name2 = "Tom Jefferson";
-            const int age2 = 19;
-            const float weight2 = 50f;
-            const float height2 = 156.5f;
-            Console.WriteLine(name2 + " is " + age2 + ", and weighs " + weight2 + "kg at a height of " + height2 + "cm.\n");
+            Console.Write("\n\nWhat is your name? ");
+            string name2 = Console.ReadLine();
+            Console.Write("And how old are you? ");
+            int age2 = int.Parse(Console.ReadLine());
+            Console.Write("How much do you weigh (kg)? ");
+            float weight2 = float.Parse(Console.ReadLine());
+            Console.Write("How tall are you? (cm) ");
+            float height2 = float.Parse(Console.ReadLine());
+            // Testing string + variable layouts
+            Console.WriteLine("\n{0} is {1}, and weighs {2}kg at a height of {3}cm.\n", name2, age2, weight2, height2);
 
             // Their BMI
-            const float heightConvert2 = height2 / 100;
-            const float height_s2 = heightConvert2 * heightConvert2;
-            const float sum2 = weight2 / height_s2;
+            float heightConvert2 = height2 / 100;
+            float height_s2 = heightConvert2 * heightConvert2;
+            float sum2 = weight2 / height_s2;
             Console.WriteLine(name2 + " has a BMI of " + sum2 + ".");
         }
     }
