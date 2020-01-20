42ae82e479379f503a4f2f82094571d04fdfa426
Max <hellhunter15@hotmail.com>
2020-01-20 03:17:34 +0100

    Console Reads name age weight and hight from console and prints it back together with a BMI.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,8 +6,23 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            // Read name, surename, age, weight (in kg) and height (in cm) from console.
+            Console.Write("Please tell me your first name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please tell me your surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Please tell me your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Please tell me your weight in Kg: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.Write("Please tell me your height in cm: ");
+            double height = double.Parse(Console.ReadLine());
+            // Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm .");
+            // BMI = kg/m2
+            double heightInMeters = height / 100;
+            double bmi = weight / (heightInMeters * heightInMeters);
+            Console.WriteLine($"Your BMI is {bmi}");
         }
     }
 }
