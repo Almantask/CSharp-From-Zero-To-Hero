39a7ee17064cda951c641d8a19719b6c884c3776
Emb3rlight <carl.tenko@gmail.com>
2020-01-21 10:14:08 +0000

    Homework 3 for review

--- Src/BootCamp.Chapter/Lession3.cs
+++ Src/BootCamp.Chapter/Lession3.cs
@@ -9,55 +9,53 @@ namespace BootCamp.Chapter
         
         public static void Demo()
         {
-            GatherUserInput();
-            CalcBMI(weight, height);
-            PrintConsole(firstname, secondname, age, weightsingle, heightsingle, BMI);
-        }
-        public static void GatherUserInput()
-        {
+            bool backtotop = true;
+            while(backtotop)
+            { 
             //User data input
-            Console.WriteLine("What is your first name?");
-            var firstname = Console.ReadLine();
+            var firstname = MessageString("What is your first name ?");
+            var secondname = MessageString("What is your second name ? ");
+            var age = MessageFloat("What is your age?");
+            var weight = MessageFloat("What is your weight in kg?");
+            var height = MessageFloat("What is your height in Meters?");
+            float BMI = CalcBMI(weight, height);
+           
+            //Calc and return BMI
+            CalcBMI(weight, height);
 
-            Console.WriteLine("What is your second name?");
-            var secondname = Console.ReadLine();
-
-            Console.WriteLine("What is your age?");
-            var age = Console.ReadLine();
-
-            Console.WriteLine("What is your weight in kg?");
-            var weight = Console.ReadLine();
-
-            Console.WriteLine("What is your height in Meters?");
-            var height = Console.ReadLine();
-
-        }
-        public static void CalcBMI(int weight, int height)
-        {
-            //Convert to Singles
-            var weightsingle = Convert.ToSingle(weight);
-            var heightsingle = Convert.ToSingle(height);
-
-            //Calc for BMI
-            var Calc1 = weightsingle / heightsingle;
-            var BMI = Calc1 / heightsingle;
-        }
-        static void PrintConsole(string firstname, string secondname, int age, float weightsingle, float heightsingle, float BMI)
-        {
             //Display output to user
-            Console.WriteLine(firstname + " " + secondname + "is " + age + " years old, his weight is " + weightsingle + " kg and his height is " + heightsingle + " cm ");
+            Console.WriteLine(firstname + " " + secondname + "is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm ");
             Console.WriteLine("Your BMI is " + BMI);
 
+            //Continue
+            Console.WriteLine("Would you like to do another person Y/N?");
+            var option = Console.ReadLine();
+            if ((option) == "N" || (option) == "n")
+            {
+            backtotop = false;
+            }
+
+            }
         }
+        public static string MessageString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static float MessageFloat(string message)
+        {
+            return Convert.ToSingle(MessageString(message));
+        }
+        public static float CalcBMI(float weight, float height)
+        {
+                //Convert to Singles
+                var weightsingle = Convert.ToSingle(weight);
+                var heightsingle = Convert.ToSingle(height);
 
-
-
-    }
-
-
-
-
-            
-
+                //Calc for BMI
+                var Calc1 = weightsingle / heightsingle;
+                return Calc1 / heightsingle;
+        }
+    }     
 }
      
