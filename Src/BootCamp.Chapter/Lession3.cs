2ec8d7d4b803f499b11523053faafd406a565e33
Emb3rlight <carl.tenko@gmail.com>
2020-01-21 00:59:36 +0000

    50% done with errors

--- Src/BootCamp.Chapter/Lession3.cs
+++ Src/BootCamp.Chapter/Lession3.cs
@@ -6,60 +6,58 @@ namespace BootCamp.Chapter
 {
     class Lession3
     {
-        static void Main(string[] args)
+        
+        public static void Demo()
         {
-
-            //Set variable
-            var backtotop = true;
-
-
-            //Start While loop
-            while (backtotop)
-
-            {
-
-                //USer data input
-
-                Console.WriteLine("What is your first name?");
-                var firstname = Console.ReadLine();
-
-                Console.WriteLine("What is your second name?");
-                var secondname = Console.ReadLine();
-
-                Console.WriteLine("What is your age?");
-                var age = Console.ReadLine();
-
-                Console.WriteLine("What is your weight in kg?");
-                var weight = Console.ReadLine();
-
-                Console.WriteLine("What is your height in Meters?");
-                var height = Console.ReadLine();
-
-
-                //Convert to Singles
-                var weightsingle = Convert.ToSingle(weight);
-                var heightsingle = Convert.ToSingle(height);
-
-
-                //Calc for BMI
-                var Calc1 = weightsingle / heightsingle;
-                var BMI = Calc1 / heightsingle;
-
-
-                //Display output to user
-                Console.WriteLine(firstname + " " + secondname + "is " + age + " years old, his weight is " + weightsingle + " kg and his height is " + heightsingle + " cm ");
-                Console.WriteLine("Your BMI is " + BMI);
-
-                //Continue
-                Console.WriteLine("Would you like to do another person Y/N?");
-                var option = Console.ReadLine();
-                if ((option) == "N" || (option) == "n")
-                {
-                    backtotop = false;
-                }
-
-            }
+            GatherUserInput();
+            CalcBMI(weight, height);
+            PrintConsole(firstname, secondname, age, weightsingle, heightsingle, BMI);
         }
-    }
-}
+        public static void GatherUserInput()
+        {
+            //User data input
+            Console.WriteLine("What is your first name?");
+            var firstname = Console.ReadLine();
 
+            Console.WriteLine("What is your second name?");
+            var secondname = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What is your weight in kg?");
+            var weight = Console.ReadLine();
+
+            Console.WriteLine("What is your height in Meters?");
+            var height = Console.ReadLine();
+
+        }
+        public static void CalcBMI(int weight, int height)
+        {
+            //Convert to Singles
+            var weightsingle = Convert.ToSingle(weight);
+            var heightsingle = Convert.ToSingle(height);
+
+            //Calc for BMI
+            var Calc1 = weightsingle / heightsingle;
+            var BMI = Calc1 / heightsingle;
+        }
+        static void PrintConsole(string firstname, string secondname, int age, float weightsingle, float heightsingle, float BMI)
+        {
+            //Display output to user
+            Console.WriteLine(firstname + " " + secondname + "is " + age + " years old, his weight is " + weightsingle + " kg and his height is " + heightsingle + " cm ");
+            Console.WriteLine("Your BMI is " + BMI);
+
+        }
+
+
+
+    }
+
+
+
+
+            
+
+}
+     
