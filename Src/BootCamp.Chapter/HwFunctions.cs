d3cd25eb0ff19eefa9921551da2fcfbe4a908c12
mkravtz <mkravtz@outlook.com>
2020-01-20 20:37:08 -0700

    Originally went off of the instructions from the Twitch chat. I've updated it to match the Discord homework instructions.

--- Src/BootCamp.Chapter/HwFunctions.cs
+++ Src/BootCamp.Chapter/HwFunctions.cs
@@ -2,8 +2,22 @@
 
 namespace BootCamp.Chapter
 {
-    public class Lesson3
+public class Lesson3
     {
+        public static void Demo()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                var surname = Lesson3.PromptString("Please enter your surname: ");
+                var age = Lesson3.PromptInt("Please enter your age: ");
+                var weight = Lesson3.PromptFloat("Please enter your weight (in kg): ");
+                var height = Lesson3.PromptFloat("Please enter your height (in m): ");
+                Console.WriteLine(surname + " is " + age + " " + "years old, his weight is " + weight + "kg and his height is " + height + "m.");
+
+                var Bmi = Lesson3.CalculateBmi(weight, height);
+                Console.WriteLine("Your estimated BMI is " + Bmi);
+            }
+        }
         public static string PromptString(string message)
         {
             Console.Write(message);
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -8,17 +8,10 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 2; i++)
             {
-                var surname = Lesson3.PromptString("Please enter your surname: ");
-                var age = Lesson3.PromptInt("Please enter your age: ");
-                var weight = Lesson3.PromptFloat("Please enter your weight (in kg): ");
-                var height = Lesson3.PromptFloat("Please enter your height (in m): ");
-                Console.WriteLine(surname + " is " + age + " " + "years old, his weight is " + weight + "kg and his height is " + height + "m.");
-                
-                var Bmi = Lesson3.CalculateBmi(weight, height);
-                Console.WriteLine("Your estimated BMI is " + Bmi);
+                Lesson3.Demo();
             }
+
         }
     }
 }
