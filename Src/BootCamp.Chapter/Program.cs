71aa5020e5d6f866e4f55fcb4331ae8f1ac2d1f3
pinology <59784688+pinology@users.noreply.github.com>
2020-01-22 00:00:41 +0800

    homework 1 code in the correct branch .-.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,14 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Console = System.Console;
 
 namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        public static void Main()
+            {
+
+            //run twice
+
+            for (int i = 0; i < 2; i++)
+            {
+
+            //input first name, last name
+            Console.Write("Input First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Input Last Name: ");
+            string lastName = Console.ReadLine();
+
+            // input age
+            Console.Write("Input age: ");
+            string s = Console.ReadLine();
+            int.TryParse(s, out int age);
+
+            // input weight and height
+            Console.Write("Input weight (in kg): ");
+            s = Console.ReadLine();
+            float.TryParse(s, out float weight);
+
+            Console.Write("Input height (in cm): ");
+            s = Console.ReadLine();
+            float.TryParse(s, out float height);
+            
+            //print info
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            //calculate and print bmi
+            float bmi = weight / ((height / 100) * (height / 100));
+            Console.WriteLine(firstName + " " + lastName + " has a BMI of " + bmi + ".");
+
+            }
         }
+
     }
 }
