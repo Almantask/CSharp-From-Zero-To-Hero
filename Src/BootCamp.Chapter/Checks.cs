41a3c36f53d0a0120f35fc88779fc561e42cdcf7
Ujabaja <davinsoman@yahoo.com>
2020-01-22 00:20:37 -0500

    Completion of Homework 3

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -17,25 +17,25 @@ namespace BootCamp.Chapter
         public static int PromptInt(string message)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.PromptInt(message);
         }
 
         public static string PromptString(string message)
         {
             // To do: call your implementation. 
-            return "";
+            return Lesson3.PromptString(message);
         }
 
         public static float PromptFloat(string message)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.PromptFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.CalculateBMI(weight, height);
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Console = System.Console;
+
+namespace BootCamp.Chapter
+{
+    public class Lesson3
+    {
+        public static void Demo()
+        {
+            Console.WriteLine("Please enter your data" + Environment.NewLine);
+            do
+            {
+                var name = PromptString("What is your first name?");
+                var surname = PromptString("What is your last name?");
+                var age = PromptInt("What is your age");
+                var weight = PromptFloat("What is your weight in kilograms");
+                var heightCm = PromptFloat("What is your height in centimeters");
+                var BMI = CalculateBMI(weight, heightCm / 100);
+
+                Console.WriteLine();
+                Console.WriteLine("Name: " + name + " " + surname);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Weight: " + weight + "kg");
+                Console.WriteLine("Height: " + heightCm / 100 + "m");
+                Console.WriteLine("BMI: " + BMI);
+
+
+                Console.WriteLine(Environment.NewLine + "Continue with a new set of inputs? " +
+                "Press Y to continue or press any other button to exit the program" + Environment.NewLine);
+
+                string answer = Console.ReadLine().ToLowerInvariant();
+                if (answer == "y")
+                {
+                    Console.WriteLine(Environment.NewLine + "Please enter your data" + Environment.NewLine);
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine + "Thank you. Good Bye");
+                    break;
+                }
+            }
+            while (true);
+
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float CalculateBMI(float weight, float heightM)
+        {
+            return weight / (heightM * heightM);
+
+        }
+    }
+}
+
