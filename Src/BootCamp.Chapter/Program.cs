86ad0b66b98199c8da43db56846c5a8818d3da70
Carina Rodriguez <rodcar.daisy@outlook.com>
2020-01-21 22:36:31 -0800

    Re wrote code for homework 1. Still having issues with BMI formula. BMI fromula was commented out.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,52 +7,27 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			{//Line that will ask user for their name.
+				Console.Write("Input your full name: ");
+				string name = Console.ReadLine();
+				Console.WriteLine("Hello again, " + name);
+				
+				//Line that will ask user for their age. 
+				Console.Write(name + ", what is your age? ");
+				string age = Console.ReadLine();
 
+				//Line that reads users weight and height.
+				Console.Write(name + ", what is your weight in kgs? ");
+				string weight = Console.ReadLine();
+				Console.Write(name+", what is your height in cm? ");
+				string height = Console.ReadLine();
 
-			{
-				//Test for push function 
-				// test comment recording chnages test 
-				// test 2 
-				//CONTROL + F5
-				//Need to read Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
-				const string CarinaName = "Carina";
+				Console.WriteLine(name + " is " + age + " years old, his weight is " + weight +
+					" kgs and his height is " + height + " cm.");
 
-				const string CarinaSurename = "Rod";
-				const int CarinaAge = 24;
-				const double CarinaWeight = 63.5;
-				const double CarinaHeight = 162.56;
-				//Why does ¨/¨double weight work in the BMI formula
-				const double CarinaBMI = (CarinaWeight / CarinaHeight/CarinaHeight) *10000;
-
-				var greeting = CarinaName + " " + CarinaSurename + " is " +
-					CarinaAge + " years old, her weight is " +
-					CarinaWeight + " kgs and her height is " + CarinaHeight + " cm.";
-
-				Console.WriteLine(greeting);
-				Console.WriteLine(CarinaBMI + " BMI");
-
-				const string Name = "Alex";
-
-				const string alexSurename = "Pez";
-				const int alexAge = 27;
-				const double alexWeight = 69.5f;
-				const double alexHeight = 175.56f;
-				const double AlexBMI = (alexWeight / alexHeight/alexHeight)*10000 ;
-			
-				//need help with the fraction calculation. 
-				//I know 10000 does not belong there but it makes it work. 
-
-				var alexGreeting = Name + " " + alexSurename + " is " +
-					alexAge + " years old, her weight is " +
-					alexWeight + " kgs and her height is " + alexHeight + " cm.";
-
-				Console.WriteLine(alexGreeting);
-				Console.WriteLine(AlexBMI + " BMI");
-				//TEST FOR CHANGE MARK
-
-
-
-
+				//Formula for user BMI. 
+				//var BMI = height / weight / weight;
+				//Console.WriteLine(name + " BMI is " + BMI);
 			}
 
 		}
