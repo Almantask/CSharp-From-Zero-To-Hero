70fbc960239166aa0e742ab130af8ac946d0a770
wajeht <zombyard@gmail.com>
2020-01-22 00:12:20 -0600

    homework for chapter 1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "";
+            string sureName = "";
+            int age = 0;
+            float weight = 0f;
+            float height = 0f;
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Surename: ");
+            sureName = Console.ReadLine();
+
+            Console.Write("Age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight: ");
+            weight = int.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " is " + age + " years old. " + name + " weight is " + weight + "kg. and height is " + height + ".");
+
+            var bmi = weight / (height * height);
+            Console.WriteLine(bmi);
+
         }
     }
+    
 }
