4d44de55f26a5bd3e1db5a6b5c7e8fbe96232eb6
LunarKitsune <47466235+LunarKitsune@users.noreply.github.com>
2020-01-21 20:23:32 -0500

    Renamed File Checks to be capital

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -1,25 +1,6 @@
-﻿/*
- *                      ********This is a test class********
- * 
- * This class should not be changed. This tests each function for the BMI. I did
- * have to do a little cheating in looking at what this would look like, but eventually
- * I will self practice in how I think on TDD before creating a base project and running
- * it as to have full familiarity with TDD and how it helps in the long run with coding.
- * 
- * For notes on my own understanding:
- *  Each function in a test class needs a method that mimics the method being
- *  used in the program. This would be a test method. Initially this is created
- *  with nothing set in the program yet(thus it fails)
- *  
- *  Once something is created the method is tested until it passes with correct
- *  and expected results. 
-*/
-
-using System;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter
 {
-    class checks
+    class Checks
     {
         public static int GetIntegerPrompt(string message)
         {
@@ -44,7 +25,7 @@ namespace BootCamp.Chapter
 
         public static float GetFloatCalculation(float height, float weight)
         {
-            var result = Lesson3.GetBMI(height, weight);
+            var result = Lesson3.GetBmi(height, weight);
 
             return weight;
         }
--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -1,18 +1,4 @@
-﻿/*
- * ********Lesson Notes for me here********
- * Requirements for this bootcamp lesson(lesson 3)
- *  1. Need to have refactored code to recieve inputs
- *  2. Need to gather the following pieces of information:
- *      -Name(string}
- *      -Age(int)
- *      -Height(Float)[This will be in Meters]
- *      -Weight(Float)[This will be in Killograms]
-*/
-
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Globalization;
+﻿using System;
 
 namespace BootCamp.Chapter
 {
@@ -33,13 +19,13 @@ namespace BootCamp.Chapter
                 personHeight = GetFloat("What is your height?: ");
                 personWeight = GetFloat("What is your weight?: ");
 
-                bmi = GetBMI(personHeight, personWeight);
+                bmi = GetBmi(personHeight, personWeight);
 
                 Console.WriteLine($"{name} is {age} years old with a height of {personHeight} Meters and a weight of {personWeight} Killograms. This persons BMI is: {bmi}\n");
             }
         }
 
-        public static float GetBMI(float height, float weight)
+        public static float GetBmi(float height, float weight)
         {
             return height / (float)Math.Pow(weight, 2);
         }
