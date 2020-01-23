085a55c271fb93f79e3b4caad4dd4f2056d20dee
kaiomv <kaio.manzano21@gmail.com>
2020-01-23 14:52:38 -0400

    Lesson3.cs now read data for 2 users, messages printed adjusted

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -6,14 +6,23 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            string name = PrintAndReadString("What is your name?");
-            string surename = PrintAndReadString("What is your surename?");
-            int age = PrintAndReadInt("What is your age?");
-            float height = PrintAndReadFloat("What is your height in meters?");
-            float weight = PrintAndReadFloat("What is your weight in kg?");
+            string name = PrintAndReadString("Hello, what's the name of the person that you want to calculate the BMI?");
+            string surename = PrintAndReadString("What's the surename?");
+            int age = PrintAndReadInt("What's the age?");
+            float height = PrintAndReadFloat("What's the height in meters?");
+            float weight = PrintAndReadFloat("What's the weight in kg?");
 
             Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " m ");
-            Console.WriteLine("Your BMI is " + BmiCalc(weight, height));
+            Console.WriteLine(name + " " + surename + "'s BMI is " + BmiCalc(weight, height));
+
+            name = PrintAndReadString("Hello, what's the name of the person that you want to calculate the BMI?");
+            surename = PrintAndReadString("What's the surename?");
+            age = PrintAndReadInt("What's the age?");
+            height = PrintAndReadFloat("What's the height in meters?");
+            weight = PrintAndReadFloat("What's the weight in kg?");
+
+            Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " m ");
+            Console.WriteLine(name + " " + surename + "'s BMI is " + BmiCalc(weight, height));
 
             Console.ReadLine();
         }
