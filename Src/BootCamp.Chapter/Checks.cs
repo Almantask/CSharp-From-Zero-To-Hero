c1487034d66d0d58f574fd5fa8488dbcf671a756
H.J.Holder <hjholder@alaska.edu>
2020-01-23 12:16:23 +0900

    Had to delete old fork and redo fork and this is just transfered over from homeowork 3

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter
 {
     /// <summary>
     /// Test class is used to test your implementation.
@@ -16,26 +12,26 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+
+            return Lesson3.ConvertToInt(message);
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            return Lesson3.ConvertToString(message);
         }
+
+
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            return Lesson3.ConvertToFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            return Lesson3.CalculateBmi(weight, height);
+
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Console = System.Console;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+
+        public static void Demo()
+        {
+            bool moreEntries = true;
+
+            while (moreEntries)
+            {
+                UserInfo();
+                string convertedString = ConvertToString("\nEnter something to be converted to String");
+                Console.WriteLine($"Your converted string is \"{convertedString}\"");
+
+                int convertedInt = ConvertToInt("\nEnter a number to be converted to Int, no '.'");
+                Console.WriteLine($"Your converted int is \"{convertedInt}\"");
+
+                float convertedFloat = ConvertToFloat("\nEnter something to be converted to a float. Only numbers.");
+                Console.WriteLine($"Your converted float is \"{convertedFloat}\"");
+
+                moreEntries = AddAnotherPerson();
+            }
+
+
+        }
+
+        public static bool AddAnotherPerson()
+        {
+            bool moreEntries = true;
+
+            Console.WriteLine("\nWould you like to enter another person? " +
+                                                           "Press Y to enter another or any other button to exit.");
+
+            string checkAnswer = Console.ReadLine().ToLowerInvariant();
+
+            if (checkAnswer != "y")
+            {
+                Console.WriteLine("You chose not to enter another user. Thank you, goodbye.");
+                moreEntries = false;
+            }
+
+            return moreEntries;
+
+        }
+
+
+        public static void UserInfo()
+        {
+
+            Console.WriteLine("Please enter your first name.");
+            string firstName = CheckStringForLettersOnly();
+
+
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = CheckStringForLettersOnly();
+
+
+
+            Console.WriteLine("Please enter your age.");
+            //I went with float for age because maybe someone likes to put percent of new age such as 7.5 years old...
+            float age = float.Parse(CheckStringForDigitsOnly());
+
+
+
+            Console.WriteLine("Please enter your weight in kg.");
+            float weightInKg = float.Parse(CheckStringForDigitsOnly());
+
+
+            Console.WriteLine("Please enter your height in meters.");
+            float height = float.Parse(CheckStringForDigitsOnly());
+
+
+            Console.WriteLine("Thank you for your information. You entered:");
+
+            Console.WriteLine($"\n``` {firstName} {lastName} is {age} years old, your weight is {weightInKg} kg and" +
+                              $" your height is {height} cm. ```");
+
+
+            CalculateBmi(weightInKg, height);
+        }
+
+
+        //Make sure string has only letters
+        public static string CheckStringForLettersOnly()
+        {
+            bool stop = true;
+            string testString = "";
+
+            while (stop)
+            {
+                testString = Console.ReadLine();
+
+                if (!IsAllLetters(testString))
+                {
+                    Console.WriteLine("You entereted that wrong. Please try again.\n");
+                }
+
+                else
+                {
+                    stop = false;
+                }
+            }
+
+            return testString;
+        }
+
+        //check each character in string to see if it has any numbers
+        public static bool IsAllLetters(string testForLetters)
+        {
+            foreach (char c in testForLetters)
+            {
+
+                if (!Char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //Make sure string has only numbers
+        public static string CheckStringForDigitsOnly()
+        {
+            bool stop = true;
+            string testDigits = "";
+
+            while (stop)
+            {
+                testDigits = Console.ReadLine();
+
+                if (!IsAllDigits(testDigits))
+                {
+                    Console.WriteLine("You entereted that wrong. Please try again.\n");
+                }
+                else
+                {
+                    stop = false;
+                }
+            }
+
+            return testDigits;
+        }
+
+        //Make sure string is only numbers also allowed to have '.'
+        public static bool IsAllDigits(string testForNumbers)
+        {
+            foreach (char c in testForNumbers)
+            {
+                if (!Char.IsDigit(c) && c != '.')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static float CalculateBmi(float weightInKg, float height)
+        {
+            float bmi = weightInKg / (height * height);
+
+            Console.WriteLine($"\nYour BMI based on your heigh of {height}m and your weight of {weightInKg}Kg " +
+                             $"is {bmi} kg/(m*m)");
+            return bmi;
+        }
+
+        //For Checks
+        public static int ConvertToInt(string message)
+        {
+            Console.WriteLine(message);
+
+            int toAnInt = int.Parse(Console.ReadLine());
+            return toAnInt;
+        }
+
+
+        public static string ConvertToString(string message)
+        {
+            Console.WriteLine(message);
+            string readInString = Console.ReadLine();
+
+            return readInString;
+        }
+
+
+        public static float ConvertToFloat(string message)
+        {
+            Console.WriteLine(message);
+            float toAFloat = float.Parse(Console.ReadLine());
+
+            return toAFloat;
+        }
+
+
+    }
+}
