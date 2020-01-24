b6166628bd70d515de0cab849bb0427c2fcf264d
telefunk <telewebb@gmail.com>
2020-01-24 18:48:43 -0500

    Homework code for assignment 1+2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter person " + (i+1) + " first name:");
+                string fName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + "'s last name:");
+                string lName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s weight in kg:");
+                double weight = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s height in cm:");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(fName + " " + lName + "is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.");
+
+                double BMI = weight / Math.Pow(height / 100.0, 2);
+
+                Console.WriteLine(fName + " " + lName + "'s BMI is " + BMI);
+
+            }
         }
     }
 }
