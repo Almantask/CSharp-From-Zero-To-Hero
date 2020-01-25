1078aab3098e73b770d0d1e8cf628723c196376a
ShadowEnder <groundon777@msn.com>
2020-01-24 22:26:50 -0500

    Copied code from Lesson 3 and moved ResponseValidation to its own function.

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,22 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            return Lesson4.PromptInt(message);
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            return PromptString(message);
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            return PromptFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            return CalculateBmi(weight, height);
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson4.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson4
+    {
+        public static void Demo()
+        {
+            bool calculateAgain = true;
+
+            while (calculateAgain)
+            {
+                Console.WriteLine("BMI Calculator");
+
+                string name = PromptString("Enter your name: ");
+                int age = PromptInt("Enter your age: ");
+                float weight = PromptFloat("Enter your weight(kg): ");
+                float height = PromptFloat("Enter your height(cm): ");
+
+                Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+                float BMI = CalculateBmi(weight, height / 100);
+
+                Console.WriteLine("Your BMI is " + BMI);
+
+                WeightType(BMI);
+
+                calculateAgain = ResponseValidation();
+            }
+
+            Console.WriteLine("Exiting...");
+
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        public static int PromptInt(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.Write(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            return weight / (height * height);
+        }
+
+        public static void WeightType(float BMI)
+        {
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("You are Underweight.");
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                Console.WriteLine("You have a Normal Weight.");
+            }
+            else if (BMI >= 25 && BMI < 30)
+            {
+                Console.WriteLine("You are Overweight.");
+            }
+            else if (BMI >= 30)
+            {
+                Console.WriteLine("You are Obese.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. Please try again.");
+            }
+        }
+
+        public static bool ResponseValidation()
+        {
+            Console.WriteLine("Do you want to calculate another persoon's BMI? (y/n)");
+            string continueTheFight = Console.ReadLine();
+            while (continueTheFight != "y" && continueTheFight != "n")
+            {
+                Console.WriteLine("That is not a valid response.");
+                Console.WriteLine("Do you want to calculate another persoon's BMI? (y/n)");
+                continueTheFight = Console.ReadLine();
+            }
+
+            if (continueTheFight == "n")
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
