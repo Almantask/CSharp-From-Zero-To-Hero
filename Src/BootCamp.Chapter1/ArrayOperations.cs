6592a966a9d2f3954e5a9b8dd029abbda98aa8a5
Almantas Karpavičius <almantusk@gmail.com>
2020-01-25 13:19:41 +0200

    Homework 5 assignment

---
+++ Src/BootCamp.Chapter1/ArrayOperations.cs
@@ -0,0 +1,96 @@
+﻿namespace BootCamp.Chapter1
+{
+    public static class ArrayOperations
+    {
+        /// <summary>
+        /// Sort the array in ascending order.
+        /// If array empty or null- don't do anything.
+        /// </summary>
+        /// <param name="array">Input array in a random order.</param>
+        public static void Sort(int[] array)
+        {
+            // ToDo: implement.
+        }
+
+        /// <summary>
+        /// Reverse the array elements, first being last and so on.
+        /// If array empty or null- don't do anything.
+        /// </summary>
+        /// <param name="array">Input array in a random order.</param>
+        public static void Reverse(int[] array)
+        {
+            // ToDo: implement.
+        }
+
+        /// <summary>
+        /// Remove last element in array.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <returns>A new array with the last element removed. If an array is empty or null, returns input array.</returns>
+        public static int[] RemoveLast(int[] array)
+        {
+            // ToDo: implement.
+            return array;
+        }
+
+        /// <summary>
+        /// Remove first element in array.
+        /// </summary>
+        /// <returns>A new array with the first element removed. If an array is empty or null, returns input array.</returns>
+        public static int[] RemoveFirst(int[] array)
+        {
+            // ToDo: implement.
+            return array;
+        }
+
+        /// <summary>
+        /// Removes array element at given index.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <param name="index">Index at which array element should be removed.</param>
+        /// <returns>A new array with element removed at a given index. If an array is empty or null, returns input array.</returns>
+        public static int[] RemoveAt(int[] array, int index)
+        {
+            // ToDo: implement.
+            return array;
+        }
+
+        /// <summary>
+        /// Inserts a new array element at the start.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <param name="number">Number to be added.</param>
+        /// <param name="index">Index at which array element should be added.</param>
+        /// <returns>A new array with element added at a given index. If an array is empty or null, returns input array.</returns>
+        public static int[] InsertFirst(int[] array, int number, int index)
+        {
+            // ToDo: implement.
+            return array;
+        }
+
+        /// <summary>
+        /// Inserts a new array element at the end.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <param name="number">Number to be added.</param>
+        /// <returns>A new array with element added in the end of array. If an array is empty or null, returns input array.</returns>
+        public static int[] InsertLast(int[] array, int number)
+        {
+            // ToDo: implement.
+            return array;
+        }
+
+        /// <summary>
+        /// Inserts a new array element at the specified index.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <param name="number">Number to be added.</param>
+        /// <param name="index">Index at which array element should be added.</param>
+        /// <returns>A new array with element inserted at a given index. If an array is empty or null, returns input array.</returns>
+        public static int[] InsertAt(int[] array, int number, int index)
+        {
+            // ToDo: implement.
+            return array;
+        }
+    }
+}
--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
