51d0e47a840155a3eef9c3a09de59342b12aaf7c
ShadowEnder <groundon777@msn.com>
2020-01-25 22:44:07 -0500

    Back to WriteLine instead of Write, added a space at the end of 'not a valid number' error message and at the beginning of 'name cannot be empty' error message.

--- Src/BootCamp.Chapter/Lesson4.cs
+++ Src/BootCamp.Chapter/Lesson4.cs
@@ -39,7 +39,7 @@ namespace BootCamp.Chapter
 
             if (string.IsNullOrEmpty(input))
             {
-                Console.WriteLine("Name cannot be empty.");
+                Console.WriteLine(" Name cannot be empty.");
                 return "-";
             }
 
@@ -58,7 +58,7 @@ namespace BootCamp.Chapter
 
             if(!int.TryParse(input, out int output))
             {
-                Console.Write("\"" + input + "\" is not a valid number.");
+                Console.WriteLine("\"" + input + "\" is not a valid number. ");
                 return -1;
             }
             return output;
@@ -76,7 +76,7 @@ namespace BootCamp.Chapter
 
             if (!float.TryParse(input, out float output))
             {
-                Console.Write("\"" + input + "\" is not a valid number.");
+                Console.WriteLine("\"" + input + "\" is not a valid number. ");
                 return -1;
             }
 
