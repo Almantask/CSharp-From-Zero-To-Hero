1769820cca1829bee0e80c42b461d398f6d0e60f
Mihail Fox <mihai.fologea@gmail.com>
2020-01-26 18:06:25 +0200

    Implemented Reverse() method

--- Src/BootCamp.Chapter1/ArrayOperations.cs
+++ Src/BootCamp.Chapter1/ArrayOperations.cs
@@ -34,7 +34,15 @@
         /// <param name="array">Input array in a random order.</param>
         public static void Reverse(int[] array)
         {
-            // ToDo: implement.
+            if (array != null && array.Length != 0)
+            {
+                for (int i = 0; i < array.Length / 2; i++)
+                {
+                    int temp = array[i];
+                    array[i] = array[array.Length - i - 1];
+                    array[array.Length - i - 1] = temp;
+                }
+            }
         }
 
         /// <summary>
