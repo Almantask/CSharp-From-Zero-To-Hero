2e7535c30effbc10f372f9135e4a328387c2722a
escht <escht@users.noreply.github.com>
2020-01-27 00:04:09 -0500

    added error messaging

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -1,8 +1,4 @@
-﻿
-
-
-
-using System;
+﻿using System;
 namespace BootCamp.Chapter
 {
     public class Lesson3
@@ -16,27 +12,15 @@ namespace BootCamp.Chapter
 
         }
 
-
         public static void EnterUserData()
         {
 
             string firstName = Lesson3.PromptUserString("First name: ");
-            Console.WriteLine();
-
             string lastName = Lesson3.PromptUserString("Last name: ");
-            Console.WriteLine();
-
             int age = Lesson3.PromptUserInt("Age: ");
-            Console.WriteLine();
-
-            float weight = Lesson3.PromptUserFloat("Weight (in kg): ");
-            Console.WriteLine();
-
             float height = Lesson3.PromptUserFloat("Height (in m): ");
-            Console.WriteLine();
-
+            float weight = Lesson3.PromptUserFloat("Weight (in kg): ");                      
             float bmi = Lesson3.CalcBmi(weight, height);
-            Console.WriteLine();
 
             Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, their weight is " + weight + "kg and their height is " + height + "m. Their BMI is " + bmi + ".");
             Console.WriteLine();
@@ -44,16 +28,12 @@ namespace BootCamp.Chapter
 
 
         public static string PromptUserString(string message)
-            // Empty string for name -> return "-".
-            // Print error message "Name cannot be empty." in a new line.
         {
             Console.WriteLine(message);
             string name = Console.ReadLine();
-
             if (string.IsNullOrEmpty(name))
             {
-                
-                Console.Write("Name cannot be empty.");
+                Console.Write("Name cannot be empty.\n");
                 return "-";
             }
 
@@ -62,48 +42,49 @@ namespace BootCamp.Chapter
 
         public static int PromptUserInt(string message)
         { 
-            //Input is not a number -> return -1.
-            // Input is a number but 0 -> return 0.
-            // Input is valid -> return input.
+            // Input is not a number -> return -1.
             Console.WriteLine(message);
             int num = int.Parse(Console.ReadLine());
-
-            //if (num != Int32.TryParse())
-            //{
-            //    return -1;
-            //}
-            //else if (num == 0)
-            //{
-            //    return 0;
-            //}
-            //else
-            //{
-            //    return num;
-            //}
-
-
-            //if (num == 0)
-            //{
-            //    return 0;
-            //}
+            if (num == 0)
+                {
+                    return 0;
+                }
 
             return num;
         }
-
-        
-
 
         public static float PromptUserFloat(string message)
         {
             Console.WriteLine(message);
             float measurments = float.Parse(Console.ReadLine());
+            if (measurments >= 0)
+                {
+                    return -1;
+                }
+
             return measurments;
         }
 
         public static float CalcBmi(float weight, float height)
         {
+            
+            if (weight <= 0)
+                {
+                    Console.Write("Failed calculating BMI. Reason:\n");
+                    Console.Write("Weight cannot be equal or less than zero, but was" + weight + "\n");
+               
+                }
+
+            if (height <= 0)
+                {
+                    Console.Write("Failed calculating BMI. Reason:\n");
+                    Console.Write("Height cannot be equal or less than zero, but was" + height + "\n");
+               
+                }
+
             float bmi = weight / (height * height);
             return bmi;
+
+            }
         }
     }
-}
