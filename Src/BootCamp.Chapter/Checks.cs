690aee599cb10cc9e3cb034197791265db375235
Carter Welke <cwill365@outlook.com>
2020-01-26 16:39:12 -0800

    Added my functions to Checks.cs

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,26 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            //call my implementation
+            return Program.getInt(message);
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            //call my implementation
+            return Program.getString(message);
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            //call my implementation
+            return Program.getFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            //call my implementation
+            return Program.CalculateBmi(weight, height);
         }
     }
 }
