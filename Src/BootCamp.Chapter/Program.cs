314d7ab8b1e876fa632e59864f444390bb14024b
telefunk <telewebb@gmail.com>
2020-01-27 17:16:25 -0500

    Copied and past code from homework 1+2 into homework 3. Initial commit.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter person " + (i + 1) + " first name:");
+                string fName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + "'s last name:");
+                string lName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s weight in kg:");
+                float weight = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s height in cm:");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine(fName + " " + lName + "is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.");
+
+                float bmi = weight / ((height / 100) * (height / 100));
+
+                Console.WriteLine(fName + " " + lName + "'s BMI is " + bmi);
+
+            }
         }
     }
 }
