912c30fb9162a6dcdb185e3141e1c63ab169d3bd
Carter Welke <cwill365@outlook.com>
2020-01-26 16:40:28 -0800

    implemented functions in Program.cs and called Lesson3.Demo();

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            Lesson3.Demo();
+        }
+        public static int getInt(string message)
+        {
+            Console.Write(message);
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static string getString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static float getFloat(string message)
+        {
+            Console.Write(message);
+            return (float)Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            return weight / (height * height);
         }
     }
 }
