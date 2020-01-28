ccef3c218fca30063bc1d76426242665b1762249
telefunk <telewebb@gmail.com>
2020-01-27 19:01:14 -0500

    Rearanged code so that the work is happening in a seperate cs file.

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter person " + (i + 1) + " first name:");
+                string fName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + "'s last name:");
+                string lName = Console.ReadLine();
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s weight in kg:");
+                float weight = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter " + fName + " " + lName + "'s height in cm:");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine(fName + " " + lName + "is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.");
+
+                float bmi = weight / ((height / 100) * (height / 100));
+
+                Console.WriteLine(fName + " " + lName + "'s BMI is " + bmi);
+            }
+    }
+}
