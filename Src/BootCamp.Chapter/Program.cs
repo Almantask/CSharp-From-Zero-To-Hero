6ba2b2e89a49f41710f583751f507eaca8e9a807
joshmond20 <joshmnd8@gmail.com>
2020-01-29 23:44:17 +0000

    Initial commit.
    Added a user interface to the application to gather and display information from the user, this includes: firstname, surname, age, weight and height.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter first name: ");
+
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter age: ");
+            var age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter weight (in kg): ");
+            var weight = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter weight (in cm): ");
+            var height = Double.Parse(Console.ReadLine());
+
+            var output = $"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height} cm.";
+
+            Console.WriteLine(output);
+            
         }
     }
 }
