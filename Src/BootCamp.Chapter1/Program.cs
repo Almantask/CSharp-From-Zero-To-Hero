e3a8e9ab6dd4a58723736df28fd6e772e6d0c2c2
H.J.Holder <hjholder@alaska.edu>
2020-01-29 16:20:33 +0900

    Removed commented out code

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 
 namespace BootCamp.Chapter1
 {
@@ -6,22 +6,7 @@ namespace BootCamp.Chapter1
     {
         static void Main(string[] args)
         {
-           // int[] myArray = new int[] { 4, 8, 0, 1, 9, 3, 7, 2, 88, 54, 11, 18, 100, 6 };
-              int[] myArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9  };
-            //{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int[] testArray = new int[] { 0, 1 };
-           // int[] nullArray = new int[] { };
 
-
-            //ArrayOperations.Sort(myArray);
-            ArrayOperations.Reverse(myArray);
-            //ArrayOperations.RemoveLast(myArray);
-            //ArrayOperations.RemoveFirst(myArray);
-            //ArrayOperations.RemoveAt(myArray, 6);
-            ////ArrayOperations.InsertFirst(nullArray, 3);
-            //ArrayOperations.InsertFirst(testArray, 10);
-            //ArrayOperations.InsertLast(testArray, 10001);
-            //ArrayOperations.InsertAt(testArray, 1337, 1);
         }
     }
 }
