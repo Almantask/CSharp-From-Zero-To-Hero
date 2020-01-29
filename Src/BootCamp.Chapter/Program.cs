64b99df6ac2da9d5dc1735f77cfad6f2532b3fed
Almantas Karpavičius <almantusk@gmail.com>
2020-01-29 02:12:04 +0200

    Secret message hw

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bootcamp.Secret;
 
 namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mumbles = new List<Task>();
+
+            for (var index = 0; index < 14; index++)
+            {
+                Aysnchromaguss.Mumble();
+            }
+
+            await Task.Delay(1000);
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("What's the passcode?");
+            
+            var passphrase = Console.ReadLine();
+            Aysnchromaguss.CheckPassphrase(passphrase);
         }
     }
 }
---
+++ Src/Bootcamp.Secret/Aysnchromaguss.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.Secret
+{
+    public static class Aysnchromaguss
+    {
+        private static readonly Random _rnd = new Random();
+        public static int WordIndex;
+
+        private readonly static string[] _secretMessage =
+        {
+            "A", "minore", "ad", "maius",
+            "a", "solis", "ortu", "usque", "ad", "occasum",
+            "ab", "uno", "disce", "omnes"
+
+        };
+
+        public static async Task Mumble()
+        {
+            var word = _secretMessage[WordIndex];
+            WordIndex = (WordIndex + 1) % _secretMessage.Length;
+            var delay = _rnd.Next(100);
+            await Task.Delay(delay);
+
+            Console.Write($"{word} ");
+        }
+
+        public static void CheckPassphrase(string guess)
+        {
+            const string passphrase = "a minore ad maius" +
+                                      "A solis ortu usque" +
+                                      "ad occasum ab uno disce omnes";
+
+            if (passphrase == guess)
+            {
+                Console.WriteLine("from the smaller to the greater");
+                Console.WriteLine("from sunrise to sunset");
+                Console.WriteLine("from one, learn all");
+            }
+        }
+    }
+}
