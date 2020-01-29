711bb0d167a3f1d8ea99c5dc46bbac5f707c35ed
Jason Howard <jasonlhoward@gmail.com>
2020-01-29 09:00:49 -0700

    Removed extraneous comments and made output one line.

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -8,32 +8,15 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            //Console.Write("Enter your first name: ");
-            //    string firstName = Console.ReadLine();
             string firstName = PromptString("Enter your first name: ");
-
-            //Console.Write("Enter your last name: ");
-            //    string lastName = Console.ReadLine();
             string lastName = PromptString("Enter your last name: ");
-
-            //Console.Write("Enter your age: ");
-            //    string age = Console.ReadLine();
             int age = PromptInt("Enter your age: ");
-
-            //Console.Write("Enter your weight (in kg): ");
-            //    float weight = float.Parse(Console.ReadLine());
             float weight = PromptFloat("Enter your weight (in kg): ");
-
-            //Console.Write("Enter your height (in cm): ");
-            //    float height = float.Parse(Console.ReadLine());
             float height = PromptFloat("Enter your height (in meters): ");
-
-            //float bmi = (weight / ((height * height) / 100)) * 100;
             float bmi = CalculateBmi(weight, height);
 
-            Console.WriteLine("{0} {1} is {2} years old, his weight is {3} kg and his height is {4} meters.", firstName, lastName, age, weight, height);
-            Console.WriteLine("His BMI is {0}.", bmi);
-        }
+            Console.WriteLine("{0} {1} is {2} years old, he has a BMI of {3} with a weight of {4} kg and a height of {5} meters.", firstName, lastName, age, bmi, weight, height);
+         }
 
         public static int PromptInt(string message)
         {
