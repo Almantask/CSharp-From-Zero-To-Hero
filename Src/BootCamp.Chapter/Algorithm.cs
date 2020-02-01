ef0ac63be7184d593eedce8c2ada33265200ffac
ellmaster <mc_pseudo-software@yahoo.de>
2020-02-01 01:14:39 +0100

    Optional homework included.
    Optional homework can be solved by anyone who solved part 1 and 2.
    May have minor issues, my little testing worked fine though.

---
+++ Src/BootCamp.Chapter/Algorithm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bootcamp.Optional;
+
+namespace BootCamp.Chapter
+{
+    internal class Algorithm
+    {
+        /// <summary>
+        /// This is used for testing only. Feel free to put test cases in.
+        /// </summary>
+        internal static void OptionalTestCases()
+        {
+            // int swapCount = 0;
+            // Sort(new int[0], ref swapCount);
+
+            // This will print your array like "3, 2, 4, 1".
+            Utility.PrintArray(new int[] { 3, 2, 4, 1 });
+        }
+
+        /// <summary>
+        /// This method will sort an array. Example: 3, 2, 4, 1 -> 1, 2, 3, 4
+        /// </summary>
+        /// <param name="array">The input array.</param>
+        /// <param name="swapCount">The amount of swaps needed to this point. Use Utility.Swap() for swapping!</param>
+        /// <returns>The sorted array.</returns>
+        internal static int[] Sort(int[] array, ref int swapCount)
+        {
+            if (array.Length > 2)
+            {
+                Split(array, out int[] array1, out int[] array2);
+                return Merge(Sort(array1, ref swapCount), Sort(array2, ref swapCount));
+            }
+
+            if (array[0] < array[1])
+            {
+                // You need to use my Swap function for solving the challange.
+                // It does nothing else than just swapping the two elements of index1 and index2 of the array.
+                Utility.Swap(array, 0, 1, ref swapCount);
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// This function splits an array in two arrays.
+        /// </summary>
+        /// <param name="array">The input array.</param>
+        /// <param name="array1">The (smaller) left side.</param>
+        /// <param name="array2">The right side.</param>
+        internal static void Split(int[] array, out int[] array1, out int[] array2)
+        {
+            array1 = new int[array.Length / 2];
+            array2 = new int[array.Length / 2];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = array[i];
+                array2[i] = array[i + array1.Length];
+            }
+
+        }
+
+        /// <summary>
+        /// This function merges two arrays into one.
+        /// </summary>
+        /// <param name="array1">The left side.</param>
+        /// <param name="array2">The right side.</param>
+        /// <returns>The combination of left and right.</returns>
+        internal static int[] Merge(int[] array1, int[] array2)
+        {
+            int[] array = new int[array1.Length + array2.Length];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array[i] = array1[i];
+            }
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array[i + array2.Length] = array2[i];
+            }
+
+            return array;
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Bootcamp.Optional;
 using Bootcamp.SecretP1;
 using Bootcamp.SecretP2;
 
@@ -9,6 +10,36 @@ namespace BootCamp.Chapter
     class Program
     {
         static async Task Main(string[] args)
+        {
+            // Put two // in front of the await to comment out Part1And2 once you solved it.
+            // Make sure to note down the secret passcode before. You will need it!
+            await Part1And2();
+            OptionalPart();
+        }
+
+        // You are not supposed to change anything but "runTests = true" comment here.
+        private static void OptionalPart()
+        {
+            bool runTests = false;
+
+            // Uncomment the next line to run your tests only.
+            // runTests = true;
+
+            if (runTests)
+            {
+                // Runs your tests.
+                Algorithm.OptionalTestCases();
+            }
+            else
+            {
+                // You are not supposed to use Google.
+                // You are supposed to write code in the Algorithm.cs file and only there.
+                Dialog.StartChallange(Algorithm.Sort, Algorithm.Split, Algorithm.Merge);
+            }
+        }
+
+        // You are not supposed to change anything in this function.
+        private static async Task Part1And2()
         {
             Console.WriteLine("Debug this correctly to move on to part 2 of this homework.");
             Console.WriteLine();
@@ -45,9 +76,15 @@ namespace BootCamp.Chapter
             Console.WriteLine();
             Console.WriteLine("-------------------------------");
             Console.WriteLine("What's the passcode?");
-            
+
             var passphrase = Console.ReadLine();
             Aysnchromaguss.CheckPassphrase(passphrase);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("If you didn't note the passcode, you should do it now!");
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
         }
     }
 }
