17e99624a53b4da34c1e78e6616aafe001137ff4
MrPepino <vermang@outlook.be>
2020-02-01 22:56:43 +0100

    Hard reset changes during lesson and added code from homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,32 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName;
+            string lastName;
+            int age;
+            double weight;
+            double height;
+
+            Console.Write("Enter a first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter a last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter an age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a weight: ");
+            weight = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter a height: ");
+            height = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // introduce person
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is "
+                            + weight + " kg and his height is " + height + " cm.");
+
+            // calculate BMI
+            height = height / 100.0;
+            double bmi = weight / (height * height);
+
+            Console.WriteLine("BMI: " + bmi.ToString("0.##"));
         }
     }
 }
