3723039c82924378896c059e4a2fe162d5e51e59
ellmaster <mc_pseudo-software@yahoo.de>
2020-02-02 12:53:23 +0100

    Added Swap and IsNullOrEmpty functions.

--- Src/BootCamp.Chapter1/ArrayOperations.cs
+++ Src/BootCamp.Chapter1/ArrayOperations.cs
@@ -91,5 +91,17 @@
             // ToDo: implement.
             return array;
         }
+
+        private static void Swap(int[] array, int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+
+        private static bool IsNullOrEmpty(int[] array)
+        {
+            return array == null || array.Length == 0;
+        }
     }
 }
