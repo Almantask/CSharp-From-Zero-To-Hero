fe60ce657a1970b4c050d7bc1a93bb9591d5bc39
MrPepino <vermang@outlook.be>
2020-02-02 12:50:29 +0100

    Added person 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            // input person 1
             Console.Write("Enter a first name: ");
             string firstName = Console.ReadLine();
             Console.Write("Enter a last name: ");
@@ -18,13 +19,37 @@ namespace BootCamp.Chapter
             Console.Write("Enter a height: ");
             double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            // introduce person
-            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is "
-                            + weight + " kg and his height is " + height + " cm.");
+            // introduce person 1
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his/her weight is "
+                            + weight + " kg and his/her height is " + height + " cm.");
 
-            // calculate BMI
+            // calculate BMI person 1
             double heightInMeters = height / 100.0;
             double bmi = weight / (heightInMeters * heightInMeters);
+
+            Console.WriteLine("BMI: " + Math.Round(bmi, 2));
+
+
+
+            // input person 2
+            Console.Write("Enter a first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter a last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter an age: ");
+            age = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter a weight: ");
+            weight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter a height: ");
+            height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // introduce person 2
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his/her weight is "
+                            + weight + " kg and his/her height is " + height + " cm.");
+
+            // calculate BMI person 2
+            heightInMeters = height / 100.0;
+            bmi = weight / (heightInMeters * heightInMeters);
 
             Console.WriteLine("BMI: " + Math.Round(bmi, 2));
         }
