be391e8009c9d0cfc704b1a4abde2b125e565de1
MrPepino <vermang@outlook.be>
2020-02-02 12:47:07 +0100

    Declared variables as late as possible, replaced Convert with Parse, declared variable for height in meters, used Math.Round instead for rounding

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,32 +7,26 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            int age;
-            double weight;
-            double height;
-
             Console.Write("Enter a first name: ");
-            firstName = Console.ReadLine();
+            string firstName = Console.ReadLine();
             Console.Write("Enter a last name: ");
-            lastName = Console.ReadLine();
+            string lastName = Console.ReadLine();
             Console.Write("Enter an age: ");
-            age = Convert.ToInt32(Console.ReadLine());
+            int age = Int32.Parse(Console.ReadLine());
             Console.Write("Enter a weight: ");
-            weight = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double weight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Enter a height: ");
-            height = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             // introduce person
             Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is "
                             + weight + " kg and his height is " + height + " cm.");
 
             // calculate BMI
-            height = height / 100.0;
-            double bmi = weight / (height * height);
+            double heightInMeters = height / 100.0;
+            double bmi = weight / (heightInMeters * heightInMeters);
 
-            Console.WriteLine("BMI: " + bmi.ToString("0.##"));
+            Console.WriteLine("BMI: " + Math.Round(bmi, 2));
         }
     }
 }
