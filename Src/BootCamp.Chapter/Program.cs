4541e9ea93440a29ea7d2113b5b9eb45bb4f6995
RoelofWobben <Charlie_lab19>
2020-02-03 21:30:10 +0100

    solved almost the main function

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
@@ -6,9 +8,28 @@
         {
             // Print each of the statistical output using Text Table with padding 3:
             // - FindHighestBalanceEver
+
+            Console.WriteLine("The richest person ever was : ");
+            var answer = BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
             // - FindPersonWithBiggestLoss
+
+            //Console.WriteLine("The person with the biggest loss was  : ");
+            //answer = BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances);
+            //Console.WriteLine(TextTable.Build(answer, 3));
+
             // - FindRichestPerson
+
+            Console.WriteLine("The richest person at this moment is : ");
+            answer = BalanceStats.FindRichestPerson(PeoplesBalances.Balances);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
             // - FindMostPoorPerson
+
+            Console.WriteLine("The poorest person at this moment is : ");
+            answer = BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances);
+            Console.WriteLine(TextTable.Build(answer, 3));
         }
     }
 }
