e3a8af2d2b94e6fab622031494622b85815d825f
pinology <59784688+pinology@users.noreply.github.com>
2020-02-03 10:11:34 +0800

    removed comments in checks class

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -4,14 +4,6 @@ using System.Text;
 
 namespace BootCamp.Chapter
 {
-    /// <summary>
-    /// Test class is used to test your implementation.
-    /// Each homework will have a set of steps that you will have to do.
-    /// You can name your functions however you want, but to validate your solution, place them here.
-    /// DO NOT CALL FUNCTIONS FROM TESTS CLASS
-    /// DO NOT IMPLEMENT FUNCTIONS IN TESTS CLASS
-    /// TESTS CLASS FUNCTIONS SHOULD ALL HAVE 1 LINE OF CODE
-    /// </summary>
     public static class Checks
     {
         public static int PromptInt(string message)
@@ -21,13 +13,11 @@ namespace BootCamp.Chapter
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
             return Lesson4.PromptString(message);
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
             return Lesson4.PromptFloat(message);
         }
 
