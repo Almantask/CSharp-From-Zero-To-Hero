40a6b2958676daf3c0d23daa8662599ddfd8d25d
MrPepino <vermang@outlook.be>
2020-02-03 20:19:55 +0100

    Refactored to PromptPersonData function and 2 calls

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -7,28 +7,22 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            // input person 1
+            PromptPersonData();
+            PromptPersonData();
+        }
+
+        private static void PromptPersonData()
+        {
+            // input
             string firstName = PromptString("Enter first name: ");
             string lastName = PromptString("Enter last name: ");
             int age = PromptInt("Enter age: ");
             float weight = PromptFloat("Enter weight: ");
             float height = PromptFloat("Enter height: ");
 
-            // output person 1
+            // output
             Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his/her weight is " + weight + " kg and his/her height is " + height + " cm.");
             float bmi = CalculateBmi(weight, height / 100);
-            Console.WriteLine(firstName + " " + lastName + "'s BMI: " + Math.Round(bmi, 2));
-
-            // input person 2
-            firstName = PromptString("Enter first name: ");
-            lastName = PromptString("Enter last name: ");
-            age = PromptInt("Enter age: ");
-            weight = PromptFloat("Enter weight: ");
-            height = PromptFloat("Enter height: ");
-
-            // output person 2
-            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his/her weight is " + weight + " kg and his/her height is " + height + " cm.");
-            bmi = CalculateBmi(weight, height / 100);
             Console.WriteLine(firstName + " " + lastName + "'s BMI: " + Math.Round(bmi, 2));
         }
 
