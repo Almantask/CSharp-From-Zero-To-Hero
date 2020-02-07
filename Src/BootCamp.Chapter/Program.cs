a2ea3da4616d17d8dbffdfc696944b020e1b4d83
RoelofWobben <Charlie_lab19>
2020-02-07 21:39:49 +0100

    Solved part1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -11,6 +11,17 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            // part1 
+            var contents = File.ReadAllLines(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input/Balances.corrupted");
+            foreach (var line in contents)
+            {
+                var repairedText = line.ToString().Replace("_", "");
+                repairedText += Environment.NewLine; 
+                File.AppendAllText(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input/Balances.repaired", repairedText);
+
+            }
+            
+                    
         }
     }
 }
