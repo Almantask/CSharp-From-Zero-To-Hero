39859bb201c28e998e8732cdd0555708369414cf
chatie <chatie@gmail.com>
2020-02-07 23:27:53 -0800

    Homework 8 (Files and Errors)
    Part 1:
    Something went wrong with people and their balances! Balances file got corrupted. Balances.corrupted file has '_' all over the place and it needs to be restored in order to be used properly. Help the finance department restore their broken file. The example file of how it should look like restored is Balances.txt. All the input files are in \Input directory.
    
    Part 2:
    Use logic from homework 7 and find the same: richest, poorest and historic losses.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,22 +1,70 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
     public class Program
     {
+        //Corrupted balance file from finance department store.
+        private const string CorruptedBalanceFile = @"..\..\..\Input\Balances.corrupted";
+        //Fixed balance file to help finance department store.
+        private const string FixedBalanceFile = @"..\..\..\Input\Balances.fixed";
+        //Dollar sign.
+        private const char DollarSign = '€';
+
         static void Main(string[] args)
         {
-            // Using FindHighestBalanceEver, print the statistical output using Text Table with padding 3.
-            Console.WriteLine(TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances), 3));
+            var corruptedBalanceLines = File.ReadAllLines(CorruptedBalanceFile);
+            var fixedBalanceLines = new string[corruptedBalanceLines.Length];
 
-            // Using FindPersonWithBiggestLoss, print the statistical output using Text Table with padding 3.
-            Console.WriteLine(TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances), 3));
+            FixCorruptedBalanceFile(corruptedBalanceLines, fixedBalanceLines);
+            DisplayStasticalData(CreateBalanceLinesForTextTable(fixedBalanceLines));
+        }
 
-            // Using FindRichestPerson, print the statistical output using Text Table with padding 3.
-            Console.WriteLine(TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances), 3));
+        private static string[] CreateBalanceLinesForTextTable(string[] fixedBalanceLines)
+        {
+            var balanceLinesForTextTable = new string[fixedBalanceLines.Length];
+            var balanceSeperator = "," + DollarSign + " ";
 
-            // Using FindMostPoorPerson, print the statistical output using Text Table with padding 3.
-            Console.WriteLine(TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances), 3));
+            for (int i = 0; i < fixedBalanceLines.Length; i++)
+            {
+                var balances = fixedBalanceLines[i].Split(balanceSeperator);
+                var balanceLine = new StringBuilder(balances[0]);
+
+                for (int j = 1; j < balances.Length; j++)
+                {
+                    balanceLine.Append(", ");
+                    balanceLine.Append(balances[j].Replace(",", "."));
+                }
+
+                balanceLinesForTextTable[i] = balanceLine.ToString();
+            }
+
+            return balanceLinesForTextTable;
+        }
+
+        private static void FixCorruptedBalanceFile(string[] corruptedBalanceLines, string[] fixedBalanceLines)
+        {
+            for (var i = 0; i < corruptedBalanceLines.Length; i++)
+            {
+                fixedBalanceLines[i] = corruptedBalanceLines[i].Replace("_", "");
+            }
+
+            File.WriteAllLines(FixedBalanceFile, fixedBalanceLines);
+        }
+
+        private static void DisplayStasticalData(string[] peopleBalances)
+        {
+            //Find richest and print the statistical output using Text Table with padding 3.
+            Console.WriteLine(TextTable.Build(BalanceStats.FindRichestPerson(peopleBalances), 3));
+
+            //Find poorest and print the statistical output using Text Table with padding 3.
+            Console.WriteLine(TextTable.Build(BalanceStats.FindMostPoorPerson(peopleBalances), 3));
+
+            //Find historic losses and print the statistical output using Text Table with padding 3.
+            Console.WriteLine(TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(peopleBalances), 3));
         }
     }
+
 }
