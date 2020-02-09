49df4a75c51229a7f4c2dde25a87b6b474ce488f
Almantas Karpavičius <almantusk@gmail.com>
2020-02-09 21:51:43 +0200

    Lesson 9 done

---
+++ Src/BootCamp.Chapter/Emojis.cs
@@ -0,0 +1,14 @@
+﻿namespace BootCamp.Chapter
+{
+    public static class Emojis
+    {
+        public const string Smile = "\u263A";
+        public const string Warning = "\u26A0";
+        public const string Stop = "\u26D4";
+        public const string Skull = "\u2620";
+        public const string HappyChill = "\u263A";
+        public const string VerySad = "\u2639";
+        public const string PirateFlag = "\uFE0F";
+        public const string GayFlag = "\u26A7";
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Text;
+using static BootCamp.Chapter.Emojis;
 
 namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            var emojis = $"{Stop}{Skull}{Smile}{GayFlag}{PirateFlag}{Warning}{HappyChill}{VerySad}";
+
+            // Encode
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(emojis);
+            // Decode
+            char[] asciiChars = Encoding.ASCII.GetChars(asciiBytes);
+            string asciiString = new string(asciiChars);
+
+            byte[] unicodeBytes = Encoding.Unicode.GetBytes(emojis);
+
+            var asciiBytesString = ToHexedString(asciiBytes);
+            var unicodeBytesString = ToHexedString(unicodeBytes);
+
+            Console.WriteLine($"Ascii: {asciiString}");
+            Console.WriteLine($"Unicode: {emojis}");
+
+            int a = 0;
+
+            Console.WriteLine($"Ascii: {asciiBytesString}");
+            Console.WriteLine($"Unicode: {unicodeBytesString}");
+
+            Console.ReadLine();
+        }
+
+        private static string ToHexedString(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (var b in bytes)
+            {
+                sb.Append($"{b:X} ");
+            }
+
+            return sb.ToString();
         }
     }
 }
