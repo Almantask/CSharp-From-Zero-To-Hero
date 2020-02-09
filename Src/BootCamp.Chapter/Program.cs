58d4ecc54b09c492b3670280f18639b12ce5277b
RoelofWobben <Charlie_lab19>
2020-02-09 11:47:55 +0100

    first ry ro solve part2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -13,15 +13,62 @@ namespace BootCamp.Chapter
         {
             // part1 
             var contents = File.ReadAllLines(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input/Balances.corrupted");
-            foreach (var line in contents)
+            for (int i = 0; i < contents.Length; i++)
             {
-                var repairedText = line.ToString().Replace("_", "");
-                repairedText += Environment.NewLine; 
-                File.AppendAllText(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input/Balances.repaired", repairedText);
-
+                var repairedText = contents[i].ToString().Replace("_", "");
+                repairedText += Environment.NewLine;
+                if (i == 0)
+                {
+                    File.WriteAllText(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances2.txt", repairedText);
+                }
+                else
+                {
+                    File.AppendAllText(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances2.txt", repairedText);
+                }
+                
             }
             
-                    
+
+            // part2
+
+            contents = File.ReadAllLines(@"C:\Users\roelof\source\repos\RoelofWobben\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input/Balances.txt");
+
+            // Print each of the statistical output using Text Table with padding 3:
+            // - FindHighestBalanceEver
+
+            Console.WriteLine("The richest person ever was : ");
+            var answer = BalanceStats.FindHighestBalanceEver(contents);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
+            // - FindPersonWithBiggestLoss
+
+            Console.WriteLine("The person with the biggest loss was  : ");
+            answer = BalanceStats.FindPersonWithBiggestLoss(contents);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
+            // - FindRichestPerson
+
+            Console.WriteLine("The richest person at this moment is : ");
+            answer = BalanceStats.FindRichestPerson(contents);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
+            // - FindMostPoorPerson
+
+            Console.WriteLine("The poorest person at this moment is : ");
+            answer = BalanceStats.FindMostPoorPerson(contents);
+            Console.WriteLine(TextTable.Build(answer, 3));
+
+
         }
     }
 }
+
+
+
+/* 
+
+Mirabelle Quinnell,€ 73,44,€ 220,32,€ -36,72,€ 293,76,€ -76,50,€ -284,58
+Mirabelle Quinnell,€ 99,66,€ -81,54,€ 223,48,€ -108,72,€ -72,48,€ -87,58,
+
+ 
+*/
--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
@@ -34,7 +37,74 @@
         /// </summary>
         public static string Build(string message, int padding)
         {
-            return "";
+            if (string.IsNullOrEmpty(message))
+            {
+                return "";
+            }
+            // make a array so I can find the longest word. 
+
+            var arrayOfLines = message.Split(Environment.NewLine);
+
+            // Find length of longest word 
+            var lengthLongestWord = GetLongestWordLength(arrayOfLines);
+
+            // print table 
+
+            var table = new StringBuilder();
+            AddTopOrBottomLineToTable(padding, lengthLongestWord, table);
+            AddEmptyLinesToTable(padding, lengthLongestWord, table);
+            AddTextToTable(padding, arrayOfLines, lengthLongestWord, table);
+            AddEmptyLinesToTable(padding, lengthLongestWord, table);
+            AddTopOrBottomLineToTable(padding, lengthLongestWord, table);
+            return table.ToString();
+
+        }
+
+        private static void AddTextToTable(int padding, string[] arrayOfLines, int lengthLongestWord, StringBuilder table)
+        {
+            foreach (var word in arrayOfLines)
+            {
+                table.Append("|");
+                table.Append(String.Empty.PadRight(padding, ' '));
+                table.Append(word);
+                table.Append(String.Empty.PadRight(lengthLongestWord - word.Length + padding, ' '));
+                table.Append("|");
+                table.Append(Environment.NewLine);
+            }
+        }
+
+        private static int GetLongestWordLength(string[] arrayOfLines)
+        {
+            var lengthLongestWord = 0;
+            foreach (var word in arrayOfLines)
+            {
+                if (word.Length > lengthLongestWord)
+                {
+                    lengthLongestWord = word.Length;
+                }
+            }
+
+            return lengthLongestWord;
+        }
+
+        private static void AddEmptyLinesToTable(int padding, int lengthLongestWord, StringBuilder table)
+        {
+            for (int i = 0; i < padding; i++)
+            {
+                table.Append("|");
+                table.Append($"{ String.Empty.PadRight(lengthLongestWord + 2 * padding, ' ')}");
+                table.Append("|");
+                table.Append($"{Environment.NewLine}");
+            }
+
+        }
+
+        private static void AddTopOrBottomLineToTable(int padding, int lengthLongestWord, StringBuilder table)
+        {
+            table.Append("+");
+            table.Append($"{String.Empty.PadRight(lengthLongestWord + 2 * padding, '-')}");
+            table.Append("+");
+            table.Append($"{Environment.NewLine}");
         }
     }
 }
