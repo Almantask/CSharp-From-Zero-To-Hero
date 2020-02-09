a2cb672ad3a782c4c1ae383e5413cfb57169d339
Mihail Fox <mihai.fologea@gmail.com>
2020-02-09 04:44:16 +0200

    Created a demo for the four main methods.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace BootCamp.Chapter
 {
@@ -11,6 +6,25 @@ namespace BootCamp.Chapter
     {
         private static void Main(string[] args)
         {
+            // Print each of the statistical output using Text Table with padding 3:
+            // - FindHighestBalanceEver
+            // - FindPersonWithBiggestLoss
+            // - FindRichestPerson
+            // - FindMostPoorPerson
+
+            const int padding = 3;
+
+            var highestBalanceEver = TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances), padding);
+            Console.WriteLine(highestBalanceEver);
+
+            var personWithBiggestLoss = TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances), padding);
+            Console.WriteLine(personWithBiggestLoss);
+
+            var richestperson = TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances), padding);
+            Console.WriteLine(richestperson);
+
+            var poorestPerson = TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances), padding);
+            Console.WriteLine(poorestPerson);
         }
     }
 }
