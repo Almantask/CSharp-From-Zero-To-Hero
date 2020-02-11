df70121dbb65bdbdb18dc3017dd0fbd3c0c80d7d
Mihail Fox <mihai.fologea@gmail.com>
2020-02-11 19:39:12 +0200

    I removed "using static" for BalanceStats and FilesUtility to make code more clear.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Text;
-using static BootCamp.Chapter.BalanceStats;
-using static BootCamp.Chapter.FilesUtility;
 using static BootCamp.Chapter.TextTable;
 using static System.Console;
 
@@ -15,26 +13,27 @@ namespace BootCamp.Chapter
 
         private static void Demo()
         {
-            DefaultCurrencySymbol = "€";
-            DefaultCurrencyLocation = 1;
+            // set currency symbol and position for BlanceStats.FormatCurrency method
+            BalanceStats.DefaultCurrencySymbol = "€";
+            BalanceStats.DefaultCurrencySymbolPosition = 1;
 
             const int padding = 3;
             OutputEncoding = Encoding.UTF8;
 
             // runs statistics methods on Ballances.corrupted
 
-            var repairedPeopeAndBalances = MakeBalancesParsable();
+            var repairedPeopeAndBalances = FilesUtility.MakeBalancesParsable();
 
-            WriteLine(Build(FindHighestBalanceEver(repairedPeopeAndBalances), padding));
+            WriteLine(Build(BalanceStats.FindHighestBalanceEver(repairedPeopeAndBalances), padding));
 
-            WriteLine(Build(FindMostPoorPerson(repairedPeopeAndBalances), padding));
+            WriteLine(Build(BalanceStats.FindMostPoorPerson(repairedPeopeAndBalances), padding));
 
-            WriteLine(Build(FindPersonWithBiggestLoss(repairedPeopeAndBalances), padding));
+            WriteLine(Build(BalanceStats.FindPersonWithBiggestLoss(repairedPeopeAndBalances), padding));
 
-            WriteLine(Build(FindRichestPerson(repairedPeopeAndBalances), padding));
+            WriteLine(Build(BalanceStats.FindRichestPerson(repairedPeopeAndBalances), padding));
 
             // Creates Balances.repaired
-            RepairPeopleAndBalances(CoruptedFile, RepairedFile);
+            FilesUtility.RepairPeopleAndBalances(FilesUtility.CoruptedFile, FilesUtility.RepairedFile);
         }
     }
 }
