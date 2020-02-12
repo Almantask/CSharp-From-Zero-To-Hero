9d21ac7971e65166932249018973c4b55c50cfb6
Chris Alvarez <cjalvarez@live.com>
2020-02-11 23:02:59 -0600

    Initial commit of HW 7 to my remote repository

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TextTable.Build("Hello", 0);
+            TextTable.Build1($"Hello{Environment.NewLine}World!", 0);
+            
             // Print each of the statistical output using Text Table with padding 3:
             // - FindHighestBalanceEver
             // - FindPersonWithBiggestLoss
--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
@@ -11,13 +14,35 @@
            +-----+
            |Hello|
            +-----+
-           
+        */
+
+        public static string Build(string message, int padding)
+        {
+            var sb = new StringBuilder();
+            Console.WriteLine(sb.Append($"+-----+{Environment.NewLine}|{ message}|{Environment.NewLine}+-----+"));
+            //Console.WriteLine(sb.Append($"|{message}|"));
+            //Console.WriteLine($"+-----+");
+            return sb.ToString();
+        }
+
+
+        /*
          Input: $"Hello{Environment.NewLine}World!", 0
            +------+
            |Hello |
            |World!|
            +------+
-           
+         */
+        public static string Build1(string message, int padding)
+        {
+            var sb = new StringBuilder();
+            //Console.WriteLine(sb.Append($"+-----+{Environment.NewLine}|{message}|{Environment.NewLine}+-----+"));
+            Console.WriteLine($"|{message}|");
+            return sb.ToString();
+        }
+
+
+        /*
          Input: "Hello", 1
            +-------+
            |       |
@@ -26,15 +51,19 @@
            +-------+
 
          */
+        public static string Build2(string message, int padding)
+        {
+            return "";
+        }
 
         /// <summary>
         /// Build a table for given message with given padding.
         /// Padding means how many spaces will a message be wrapped with.
         /// Table itself is made of: "+-" symbols. 
         /// </summary>
-        public static string Build(string message, int padding)
-        {
-            return "";
-        }
+        //public static string Build(string message, int padding)
+        //{
+        //    return "";
+        //}
     }
 }
