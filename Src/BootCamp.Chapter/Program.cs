f851f0512f241b1f1038fc5f9797bc17dba820c3
Mihail Fox <mihai.fologea@gmail.com>
2020-02-14 19:00:19 +0200

    added code for demonstrating CaesarCipher methods.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -4,18 +4,40 @@ namespace BootCamp.Chapter
 {
     internal static class Program
     {
-        private const int cipherKey = 20;
-        private const string message = "Good evening, Infidels! Hahahaha!";
+        private const int cipherKey = 10;
+        private const string message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
         private static void Main(string[] args)
         {
+            Console.WriteLine($"Caesar cipher demo");
+            Console.WriteLine($"Message: {message} | key: {cipherKey}{Environment.NewLine}");
             var encryptedMessage = CaesarCipher.Encrypt(message, cipherKey);
-            Console.WriteLine(encryptedMessage);
+            Console.WriteLine($"Encryption result: {encryptedMessage}");
 
             var decryptedMessage = CaesarCipher.Decrypt(encryptedMessage, cipherKey);
-            Console.WriteLine(decryptedMessage);
+            Console.WriteLine($"Decryption result: {decryptedMessage}");
+            Console.WriteLine();
 
-            CaesarCipher.AnalyseFrequency(encryptedMessage);
+            Console.WriteLine($"Testing frequency attack...{Environment.NewLine}");
+            var posibleKeys = CaesarCipher.AnalyseFrequency(encryptedMessage);
+
+            Console.WriteLine($"{posibleKeys.Length} possible key found.{Environment.NewLine}");
+            Console.WriteLine($"Decryption results: {Environment.NewLine}");
+
+            foreach (var key in posibleKeys)
+            {
+                var crackedMessage = CaesarCipher.Decrypt(encryptedMessage, key);
+
+                if (key == cipherKey)
+                {
+                    Console.WriteLine("Decryption key found!");
+                    Console.WriteLine($"Key = {key} - {crackedMessage}");
+
+                    break;
+                }
+                Console.WriteLine($"Key = {key} - {crackedMessage}");
+                Console.WriteLine();
+            }
         }
     }
 }
