fb6f9fa0afbe1f994ea30d7187ae2845148b7195
Mihail Fox <mihai.fologea@gmail.com>
2020-02-16 03:02:58 +0200

    added utility class for conversion mehtods.

---
+++ Src/BootCamp.Chapter/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public static class Utils
+    {
+        public static int ConvertToInt(string input)
+        {
+            int.TryParse(input, out int value);
+            return value;
+        }
+
+        public static bool IsNumeric(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+
+        public static bool IsStringValid(string inputString)
+        {
+            return !string.IsNullOrEmpty(inputString) && !string.IsNullOrWhiteSpace(inputString);
+        }
+    }
+}
