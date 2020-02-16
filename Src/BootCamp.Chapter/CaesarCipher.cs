def22f2dc01aa14d81b4536ab331ec2cf03e0e99
RoelofWobben <Charlie_lab19>
2020-02-16 18:18:41 +0100

    solved HW10

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,34 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            var output = ""; 
+
+            if (String.IsNullOrEmpty(message))
+            {
+                return message; 
+            }
+
+
+            foreach (char ch in message)
+            {
+                output += DecryptOneChar(ch, shift); 
+            }
+
+            return output; 
+        }
+
+        private static char DecryptOneChar(char ch, byte shift)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + shift) - offset) % 26) + offset);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            return Encrypt(message, Convert.ToByte(26 - shift)); 
         }
     }
 }
