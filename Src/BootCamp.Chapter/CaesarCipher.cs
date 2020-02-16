14955cf6d85fb16fec6ad88cdb84f5b8fd160893
Almantas Karpavičius <almantusk@gmail.com>
2020-02-16 14:33:10 +0200

    Lesson 10 tests done

---
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    /// <summary>
+    /// Caesar cipher which supports all ASCII character (256 in total)
+    /// </summary>
+    public static class CaesarCipher
+    {
+        public static string Encrypt(string message, byte shift)
+        {
+            return message;
+        }
+
+        public static string Decrypt(string message, byte shift)
+        {
+            return message;
+        }
+    }
+}
