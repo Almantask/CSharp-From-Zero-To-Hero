3ccdb32e75cfac3de3437700b11d80795480c7f2
Nevin <Nevin.Danielson@NEVIND-T480S>
2020-02-15 20:56:55 -0600

    decimal and binary conversions

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,19 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            Conversions.Demo();
+
         }
     }
 }
+/*Part 1
+Make a program which has 2 conversion modes: 
+From decimal to binary
+From binary to decimal
+
+Part 2
+Use your WASD(both capital and non capital) keys to print arrows
+W- ↥
+A- ↤
+S- ↧
+D- ↦*/
