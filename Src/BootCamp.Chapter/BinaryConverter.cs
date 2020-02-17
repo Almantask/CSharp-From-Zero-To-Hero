157c300622afbf55ad903731616308baa66982cb
Tomas <akavickistomas@Gmail.com>
2020-02-17 20:25:38 +0000

    Part 1 working

--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -8,12 +8,29 @@ namespace BootCamp.Chapter
     {
         public static long ToInteger(string binary)
         {
+
             return 0;
         }
 
         public static string ToBinary(long number)
         {
-            return "";
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                sb.Append(number % 2);
+                number /= 2;
+            } while (number != 0);            
+
+            return ReverseString(sb.ToString());
+        }
+        public static string ReverseString(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                sb.Append(text[i]);
+            }
+            return sb.ToString();
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
@@ -7,6 +8,21 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Console.WriteLine(BinaryConverter.ToBinary(5));
+
+            var foo = "0101";
+            byte[] bytes = Encoding.Unicode.GetBytes(foo);
+            var bar = Encoding.ASCII.GetString(bytes);
+           
+            Console.WriteLine(bar);
+            
+         //   Console.WriteLine(aaa);
+          //  Console.WriteLine(1/2);
+
+            //  Console.WriteLine(foo = foo % 2);
+
+
         }
     }
 }
