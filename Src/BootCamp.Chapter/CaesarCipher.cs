1076ddf23df2bb218b20d3267ebe1fef6df7e6da
RoelofWobben <Charlie_lab19>
2020-02-17 21:27:45 +0100

    so0me changes requeted by Kai

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -11,36 +10,38 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            
-
             if (String.IsNullOrEmpty(message))
             {
-                return message; 
+                return message;
             }
 
-            var sb = new StringBuilder(); 
+            var sb = new StringBuilder();
             foreach (char ch in message)
             {
-                sb.Append(DecryptOneChar(ch, shift)); 
+                sb.Append(DecryptOneChar(ch, shift));
             }
 
-            return sb.ToString(); 
+            return sb.ToString();
         }
 
         private static char DecryptOneChar(char ch, byte shift)
         {
+            const int numberOfCharsInAlphabet = 26;
+            const char upperA = 'A';
+            const char lowerA = 'a'; 
+
             if (!char.IsLetter(ch))
             {
                 return ch;
             }
 
-            char offset = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch + shift) - offset) % 26) + offset);
+            char offset = char.IsUpper(ch) ? upperA : lowerA;
+            return (char)((((ch + shift) - offset) % numberOfCharsInAlphabet) + offset);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return Encrypt(message, Convert.ToByte(26 - shift)); 
+            return Encrypt(message, Convert.ToByte(26 - shift));
         }
     }
 }
