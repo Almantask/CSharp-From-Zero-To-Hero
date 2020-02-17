f88f431abe2133af3a28e5050c3fe580d0a1afca
chatie <chatie@gmail.com>
2020-02-17 15:50:12 -0800

    Homework 10 - Random Div and Mod Implement Caesar cipher: https://en.wikipedia.org/wiki/Caesar_cipher It needs to support both encryption and decryption

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,80 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (string.IsNullOrEmpty(message))
+            {
+                return message;
+            }
+
+            return TransformMessage(message, shift);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            if (string.IsNullOrEmpty(message))
+            {
+                return message;
+            }
+
+            return TransformMessage(message, shift * -1);
+        }
+
+        private static string TransformMessage(string message, int shift)
+        {
+            var cipheredMessage = new StringBuilder();
+
+            for (var i = 0; i < message.Length; i++)
+            {
+                cipheredMessage.Append(GetNewLetter(message[i], shift));
+            }
+
+            return cipheredMessage.ToString();
+        }
+
+        private static char GetNewLetter(char letter, int shift)
+        {
+            if (!isLetter(letter))
+            {
+                return letter;
+            }
+
+            var letterPosition = GetLetterPosition(letter);
+
+            var newLetterPosition = (letterPosition + shift) % 26;
+            if (newLetterPosition < 0)
+            {
+                newLetterPosition += 26;
+            }
+
+            return IsUpper(letter) ? (char)(newLetterPosition + 'A') : (char)(newLetterPosition + 'a');
+        }
+
+        private static bool isLetter(char letter)
+        {
+            if ((letter >= 'a' && letter <= 'z') ||
+                (letter >= 'A' && letter <= 'Z'))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static int GetLetterPosition(char letter)
+        {
+            if (IsUpper(letter))
+            {
+                return letter - 'A';
+            }
+            return letter - 'a';
+        }
+
+        private static bool IsUpper(char letter)
+        {
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
