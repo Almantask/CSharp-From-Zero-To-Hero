8371d99e2adb36a4410e5c20c9bf47446751f34b
Nevin <Nevin.Danielson@NEVIND-T480S>
2020-02-18 13:00:11 -0600

    trying to resolve Git conflicts

---
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public static class BinaryConverter
+    {
+        public static long ToInteger(string binary)
+        {
+            return Conversions.ConvertBinaryToDecimal();
+        }
+
+        public static string ToBinary(long number)
+        {
+            return Conversions.ConvertDecimalToBinary(); ;
+        }
+    }
+}
