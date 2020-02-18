92c8525708c6d29da5dd4f9ab6f50f5a50344ae4
Tomas <akavickistomas@Gmail.com>
2020-02-18 22:29:12 +0000

    Encryption and Decryption working, passes all tests

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,38 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (message == null)
+            {
+                return null;
+            }
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            char[] chars = Encoding.ASCII.GetChars(bytes);
+
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var incrementedChar = chars[i] + shift;
+                encryptedMessage.Append((char)incrementedChar);
+            }
+            return encryptedMessage.ToString();
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            if (message == null)
+            {
+                return null;
+            }
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            char[] chars = Encoding.ASCII.GetChars(bytes);
+
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var decryptedChar = chars[i] - shift;
+                decryptedMessage.Append((char)decryptedChar);
+            }
+            return decryptedMessage.ToString();
         }
     }
 }
