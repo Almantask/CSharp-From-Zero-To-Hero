dbbb471c28903d7a8326d794417f758245eaca80
Tomas <akavickistomas@Gmail.com>
2020-02-18 21:24:33 +0000

    Part 1 fully working

--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -8,10 +8,43 @@ namespace BootCamp.Chapter
     {
         public static long ToInteger(string binary)
         {
+            if (String.IsNullOrEmpty(binary))
+            {
+                return 0;
+            }
 
-            return 0;
+            if (!IsBinary(binary))
+            {
+                throw new InvalidBinaryNumberException(binary);
+            }
+
+            var number = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                number *= 2;
+                number += int.Parse(binary[i].ToString());
+            }
+            return number;
         }
 
+        public static bool IsBinary(string binary)
+        {
+            for (int i = 0; i < binary.Length; i++)
+            {
+                try
+                {
+                    var number = int.Parse(binary[i].ToString());
+                    if (number != 0 && number != 1)
+                    {
+                        return false;
+                    }
+                }catch
+                {
+                    throw new InvalidBinaryNumberException(binary);
+                }                
+            }
+            return true;
+        }
         public static string ToBinary(long number)
         {
             StringBuilder sb = new StringBuilder();
