02f153299954b68e97dc89e2b4cff71148705025
Aurora <adrianbard@gmail.com>
2020-02-19 18:31:15 -0100

    First commit

---
+++ Src/BootCamp.Chapter/Lesson10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson10
+    {
+        public static string EncryptCypher(string message, int shift)
+        {
+            if (shift < 0)
+            {
+                shift = Math.Abs(shift + 95);
+            }
+
+            var messageBytes = Encoding.ASCII.GetBytes(message);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                var currentCharByte = messageBytes[i];
+                byte newByte = Convert.ToByte((((currentCharByte - 32) + shift) % 95) + 32);
+                messageBytes[i] = newByte;
+            }
+
+            var newMessage = Encoding.ASCII.GetString(messageBytes);
+
+            return newMessage;
+
+        }
+
+        public static string DecryptCypher(string message, int shift)
+        {
+            return EncryptCypher(message, -shift);
+        }
+    }
+}
