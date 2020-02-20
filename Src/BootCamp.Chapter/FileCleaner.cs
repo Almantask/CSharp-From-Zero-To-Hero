5b8424026adf68f9f8936d924392ed9de76cfdc5
Mihail Fox <mihai.fologea@gmail.com>
2020-02-20 22:02:56 +0200

    implement 'Clean' method and 'FileCleaner' constructor.

--- Src/BootCamp.Chapter/FileCleaner.cs
+++ Src/BootCamp.Chapter/FileCleaner.cs
@@ -5,16 +5,51 @@ using System.Text;
 
 namespace BootCamp.Chapter
 {
-    public static class FileCleaner
+    public class FileCleaner
     {
-        /// <summary>
-        /// Cleans up dirtyFileName 
-        /// </summary>
-        /// <param name="dirtyFile">Dirty file with "_" placed in random places.</param>
-        /// <param name="cleanedFile">Cleaned up file without any "_".</param>
-        public static void Clean(string dirtyFile, string cleanedFile)
+        private const string _corruptionChar = "_";
+        private const string _emptyChar = "";
+        private readonly string _dirtyFile;
+        private readonly string _cleanedFile;
+
+        public FileCleaner(string dirtyFile, string cleanedFile)
         {
-            File.WriteAllText(cleanedFile, "a");
+            if (File.Exists(cleanedFile))
+            {
+                File.Delete(cleanedFile);
+            }
+            if (File.Exists(dirtyFile))
+            {
+                _dirtyFile = dirtyFile;
+                _cleanedFile = cleanedFile;
+            }
+            else
+            {
+                Console.WriteLine($"{dirtyFile} does not exist!");
+            }
+        }
+
+        public void Clean()
+        {
+            string line;
+
+            try
+            {
+                using StreamReader reader = new StreamReader(_dirtyFile);
+                using StreamWriter writer = new StreamWriter(_cleanedFile);
+                while ((line = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine(line.Replace(_corruptionChar, _emptyChar));
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"The file was not found: '{ex}'");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"The file could not be opened: '{ex}'");
+            }
         }
     }
 }
