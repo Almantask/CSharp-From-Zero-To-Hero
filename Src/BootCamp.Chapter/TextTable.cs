3e98755e47fb97b3c5478ff2dce1c0f1242108f3
Mihail Fox <mihai.fologea@gmail.com>
2020-02-20 23:17:08 +0200

    implement method to determine size of the longest word in message.

--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,10 +1,17 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
     /// </summary>
-    public static class TextTable
+    public class TextTable
     {
+        private readonly string _message;
+        private readonly string _padding;
+
+        private readonly string divider = Environment.NewLine;
+
         /*
 
          Input: "Hello", 0
@@ -32,9 +39,23 @@
         /// Padding means how many spaces will a message be wrapped with.
         /// Table itself is made of: "+-" symbols. 
         /// </summary>
-        public static string Build(string message, int padding)
+        public string Build(string message, int padding)
         {
             return "";
+        }
+
+        private int FindLongestWord(string message)
+        {
+            int size = 0;
+            foreach(string word in message.Split(divider))
+            {
+                if(word.Length > size)
+                {
+                    size = word.Length;
+                }
+            }
+
+            return size;
         }
     }
 }
