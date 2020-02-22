f5646b7c3c6ab44c7b0a76d59757142efff9d580
Mihail Fox <mihai.fologea@gmail.com>
2020-02-22 16:15:49 +0200

    implement Test class

---
+++ Src/BootCamp.Chapter/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public static class Test
+    {
+        private static readonly NumberFormatInfo numberFormatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
+        public static bool IsStringValid(string inputString)
+        {
+            return !string.IsNullOrEmpty(inputString) && !string.IsNullOrWhiteSpace(inputString);
+        }
+
+        public static decimal ConvertToDecimal(string input)
+        {
+            decimal.TryParse(input, NumberStyles.Currency, numberFormatInfo, out decimal value);
+
+            return value;
+        }
+    }
+}
