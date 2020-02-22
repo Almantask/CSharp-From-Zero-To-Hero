d7112064b0ec355caea1cb905d4e4aec5477663c
Mihail Fox <mihai.fologea@gmail.com>
2020-02-22 16:15:30 +0200

    implement StringOps class.

---
+++ Src/BootCamp.Chapter/StringOps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public static class StringOps
+    {
+        public const string InvalidMessage = "N/A.";
+        public const string IsTheRichestPerson = "is the richest person";
+        public const string AreTheRichestPeople = "are the richest people";
+        public const string HadTheMostMoneyEver = "had the most money ever";
+        public const string HaveTheLeastMoney = "have the least money";
+        public const string HasTheLeastMoney = "has the least money";
+        public const string LostTheMostMoney = "lost the most money";
+
+        public static string FormatAndCommas(Account[] validPeople)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < validPeople.Length; i++)
+            {
+                sb.Append(validPeople[i].GetName());
+                if (i + 2 < validPeople.Length)
+                {
+                    sb.Append(", ");
+                }
+                else if (i + 1 < validPeople.Length)
+                {
+                    sb.Append(" and ");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string FormatCurrency(decimal value, string currencySymbol)
+        {
+            const char negativeSymbol = '-';
+
+            var formatedCurrency = new StringBuilder();
+
+            if (value < 0)
+            {
+                value *= -1;
+                formatedCurrency.Append(negativeSymbol).Append(currencySymbol).Append(value);
+                return formatedCurrency.ToString();
+            }
+
+            formatedCurrency.Append(currencySymbol).Append(value);
+            return formatedCurrency.ToString();
+        }
+    }
+}
