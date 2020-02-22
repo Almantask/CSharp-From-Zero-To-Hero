3f8ef9758d74082bad945764819f77a52299c3ce
Mihail Fox <mihai.fologea@gmail.com>
2020-02-23 00:27:28 +0200

    remove test code.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -5,20 +5,9 @@ namespace BootCamp.Chapter
 {
     internal static class Program
     {
-        private static string[] MyBalances => new[]
-        {
-            "Tom, 1, 2, 3, 4",
-            "Katherine, 1, 2, 3, 4",
-            "Bill, 1, 2, 3, 4",
-            "Catie, 1, 2, 3, 4"
-        };
-
         private static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            //Console.WriteLine("");
-            BalanceStats balanceStats = new BalanceStats(MyBalances);
-            Console.WriteLine(balanceStats.FindHighestBalanceEver());
+            Console.WriteLine("");
         }
     }
 }
