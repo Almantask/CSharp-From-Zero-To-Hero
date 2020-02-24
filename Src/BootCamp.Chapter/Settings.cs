c3bfd998b291f0af2d41d46477c525fbbad8d003
Mihail Fox <mihai.fologea@gmail.com>
2020-02-24 22:06:43 +0200

    change fields to readonly.

--- Src/BootCamp.Chapter/Settings.cs
+++ Src/BootCamp.Chapter/Settings.cs
@@ -4,11 +4,11 @@ namespace BootCamp.Chapter
 {
     public static class Settings
     {
-        public static NumberFormatInfo numberFormatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
-        public static CultureInfo cultureInfo = new CultureInfo("en-GB");
-        public static char divider = ',';
-        public static string currency = "\u00A4";
-        public static string corruptionChar = "_";
-        public static string emptyChar = "";
+        public static readonly NumberFormatInfo numberFormatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
+        public static readonly CultureInfo cultureInfo = new CultureInfo("en-GB");
+        public static readonly char divider = ',';
+        public static readonly string currency = "\u00A4";
+        public static readonly string corruptionChar = "_";
+        public static readonly string emptyChar = "";
     }
 }
