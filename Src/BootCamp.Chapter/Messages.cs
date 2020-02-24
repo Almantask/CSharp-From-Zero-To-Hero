4621a88ef5ea24f1943029f627b5e2ef8ac4e6fa
Mihail Fox <mihai.fologea@gmail.com>
2020-02-24 19:19:43 +0200

    move messages to their own class.

---
+++ Src/BootCamp.Chapter/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public static class Messages
+    {
+        public static readonly string InvalidMessage = "N/A.";
+        public static readonly string IsTheRichestPerson = "is the richest person";
+        public static readonly string AreTheRichestPeople = "are the richest people";
+        public static readonly string HadTheMostMoneyEver = "had the most money ever";
+        public static readonly string HaveTheLeastMoney = "have the least money";
+        public static readonly string HasTheLeastMoney = "has the least money";
+        public static readonly string LostTheMostMoney = "lost the most money";
+    }
+}
--- Src/BootCamp.Chapter/StringOps.cs
+++ Src/BootCamp.Chapter/StringOps.cs
@@ -4,14 +4,6 @@ namespace BootCamp.Chapter
 {
     public static class StringOps
     {
-        public static readonly string InvalidMessage = "N/A.";
-        public static readonly string IsTheRichestPerson = "is the richest person";
-        public static readonly string AreTheRichestPeople = "are the richest people";
-        public static readonly string HadTheMostMoneyEver = "had the most money ever";
-        public static readonly string HaveTheLeastMoney = "have the least money";
-        public static readonly string HasTheLeastMoney = "has the least money";
-        public static readonly string LostTheMostMoney = "lost the most money";
-
         public static string FormatAndCommas(Account[] validPeople)
         {
             var sb = new StringBuilder();
