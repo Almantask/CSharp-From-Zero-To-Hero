185842654925acdb87c7cc15a55ef9fcf5a0e421
Stefan Andersson <stefan@saoa.se>
2020-02-25 21:53:31 +0100

    Added Two More Lines

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,8 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
