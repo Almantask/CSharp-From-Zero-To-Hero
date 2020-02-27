e830a969f37f23a9e9d309766cc9be674fb5a7a0
Aurora <adrianbard@gmail.com>
2020-02-27 17:00:38 -0100

    Moved all logic to CaesarCipher.cs and changed constants to PascalCase.

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -1,18 +1,48 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Caesar cipher which supports all ASCII character (256 in total)
     /// </summary>
     public static class CaesarCipher
     {
+        const int CypherLength = 95; // number of characters before it loops to the start
+        const int Unusable = 32; //the first 32 chars of ascii are not printable
+
+
         public static string Encrypt(string message, byte shift)
         {
-            return Lesson10.Cypher(message, shift);
+            return Cypher(message, shift);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return Lesson10.DecryptCypher(message, shift);
+            shift = (byte) Math.Abs(CypherLength - shift);
+
+            return Cypher(message, shift);
+        }
+
+        static string Cypher(string message, int shift)
+        {
+            if (message == null)
+            {
+                return null;
+            }
+            var messageBytes = Encoding.ASCII.GetBytes(message);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                var currentCharByte = messageBytes[i];
+                byte newByte = Convert.ToByte(((currentCharByte - Unusable + shift) % CypherLength) + Unusable);
+                messageBytes[i] = newByte;
+            }
+
+            var newMessage = Encoding.ASCII.GetString(messageBytes);
+
+            return newMessage;
+
         }
     }
 }
