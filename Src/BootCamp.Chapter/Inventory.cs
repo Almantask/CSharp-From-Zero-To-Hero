99246041898da697fc1ec932cd778c7d292da8e2
Tomas <akavickistomas@Gmail.com>
2020-02-27 21:50:24 +0000

    Inventory is  working

--- Src/BootCamp.Chapter/Inventory.cs
+++ Src/BootCamp.Chapter/Inventory.cs
@@ -2,10 +2,10 @@
 {
     public class Inventory
     {
-        private Item[] _items;
+        public Item[] _items;
         public Item[] GetItems()
         {
-            return new Item[0];
+            return _items;
         }
 
         public Inventory()
@@ -15,12 +15,34 @@
 
         public Item[] GetItems(string name)
         {
-            return new Item[0];
+            Item[] items = new Item[0];
+            for (int i = 0; i < _items.Length; i++)
+            {
+                if (_items[i].GetName().Equals(name))
+                {
+                    items = AddItemTo(items, _items[i]);
+                }
+            }
+            return items;
         }
 
         public void AddItem(Item item)
         {
+            _items = AddItemTo(_items, item);
+        }
 
+        public Item[] AddItemTo(Item[] items, Item item)
+        {
+            var copyOfInventory = new Item[items.Length];
+            items.CopyTo(copyOfInventory, 0);
+            items = new Item[items.Length + 1];
+
+            for (int i = 0; i < copyOfInventory.Length; i++)
+            {
+                items[i] = copyOfInventory[i];
+            }
+            items[^1] = item;
+            return items;
         }
 
         /// <summary>
@@ -29,7 +51,45 @@
         /// </summary>
         public void RemoveItem(Item item)
         {
+            if (!Contains(item))
+            {
+                return;
+            }
+            var keepInScope = false;
+            var amendedArray = new Item[_items.Length - 1];
 
+            for (int i = 0; i < _items.Length - 1; i++)
+            {
+                // Once same item will be found
+                // It will iterate only in this scope
+                if (AreTheSame(_items[i], item) || keepInScope)
+                {
+                    keepInScope = true;
+                    amendedArray[i] = _items[i + 1];
+                    continue;
+                }
+                amendedArray[i] = _items[i];
+            }
+            _items = amendedArray;
+        }
+
+        public bool Contains(Item item)
+        {
+            for (int i = 0; i < _items.Length; i++)
+            {
+                if(AreTheSame(_items[i], item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool AreTheSame(Item item1, Item item2)
+        {
+            var isNameEqual = item1.GetName().Equals(item2.GetName());
+            var isPriceEqual = item1.GetPrice().Equals(item2.GetPrice());
+            return isNameEqual && isPriceEqual;
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var inv = new Inventory();
+            var item1 = new Item("kelnis", 1, 1);
+            var item2 = new Item("kelnis7", 2, 1);
+            //// Item[] foo = new Item[] { new Item("kelnis", 2, 1), new Item("kelnis", 3, 1), new Item("kelnis", 4, 1) };
+            //Item[] foo = new Item[0];
+            //Console.WriteLine(foo.Length);
+            //foo = new Item[foo.Length + 1];
+            //Console.WriteLine(foo.Length);
+            //foo[^1] = item1;
+            //Console.WriteLine(foo.Length);
 
+            inv.AddItem(item1);
+            inv.AddItem(new Item("kelnis", 2, 1));
+            inv.AddItem(new Item("kelnis2", 3, 1));
+            inv.AddItem(new Item("kelniss", 4, 1));
+
+            //  inv.RemoveItem(item2);
+            // var foo =  RemoveAt(inv.GetItems(), 3);
+
+            int i = 0;
+            foreach (var item in inv.GetItems("kelnis"))
+            {
+                Console.WriteLine(i + item.GetName());
+                i++;
+            }
+
+            PrintInventory(inv);
+
+        }
+        public static Item[] RemoveAt(Item[] array, int index)
+        {
+            var amendedArray = new Item[array.Length - 1];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i >= index)
+                {
+                    amendedArray[i] = array[i + 1];
+                    continue;
+                }
+                amendedArray[i] = array[i];
+            }
+            return amendedArray;
+        }
+
+        static void PrintInventory(Inventory inv)
+        {
+            foreach (var item in inv.GetItems())
+            {
+                System.Console.WriteLine(item.GetName() + "   " + item.GetPrice());
+            }
         }
     }
 }
