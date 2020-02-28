cdccaa76f974ea7b9f086a2eeb53072f84057720
telefunk <telewebb@gmail.com>
2020-02-28 09:38:50 -0500

    Inittial commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //test change
             // Print each of the statistical output using Text Table with padding 3:
             // - FindHighestBalanceEver
             // - FindPersonWithBiggestLoss
