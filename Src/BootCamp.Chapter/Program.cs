0c2d3bd04711c0a1b0c932e5f8256d1f51f3a701
Tomas <akavickistomas@Gmail.com>
2020-02-29 18:20:39 +0000

    Removed duplicate code...

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace BootCamp.Chapter
+﻿using System.IO;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var file = "./Input/b.txt";
+         //   var foo = File.ReadAllText(file);   
+
+
+          //  System.Console.WriteLine(FileCleaner.IsFileClean(foo));
+            FileCleaner.Clean(file, "./Input/ttft.txt");
+
+         //   System.Console.WriteLine(foo);
         }
     }
 }
