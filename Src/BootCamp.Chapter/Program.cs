900fab1f4c5be25f71fb6c4691bb265e07392198
Stefan Andersson <stefan@saoa.se>
2020-03-06 21:36:02 +0100

    Okie.. removed.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,9 +6,6 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
 
             Program.HomeworkOne();
 
