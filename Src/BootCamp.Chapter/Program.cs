60624b7685ac3f8022e0d00ac887a0fed081d8c1
Mahdi Bur <60852105+Mahdi0123Copen@users.noreply.github.com>
2020-03-08 00:22:39 +0100

    Update Program.cs

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,15 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Mahdi";
+            int age = 29;
+            double weight = 80.9;
+            float height = 166.6f;
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg and his height is {height} cm. ");
+            //BMI calculations
+            double bmi = weight * 703 / height / height;
+            Console.WriteLine(bmi);
+            //Code
         }
     }
 }
