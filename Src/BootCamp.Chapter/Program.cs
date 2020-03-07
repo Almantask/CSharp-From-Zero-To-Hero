742109629a45b725d095792e1e32865104e25c85
Mahdi Bur <60852105+Mahdi0123Copen@users.noreply.github.com>
2020-03-08 00:43:12 +0100

    Have added const to variables

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,10 +6,10 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name = "Mahdi";
-            int age = 29;
-            double weight = 80.9;
-            float height = 166.6f;
+            const string name = "Mahdi";
+            const int age = 29;
+            const double weight = 80.9;
+            const float height = 166.6f;
             Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg and his height is {height} cm. ");
             //BMI calculations
             double bmi = weight * 703 / height / height;
