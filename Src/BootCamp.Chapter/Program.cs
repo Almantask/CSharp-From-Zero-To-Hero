c740a5bef1e9853f9732baead3d46fce222de7ed
Mihail Fox <mihai.fologea@gmail.com>
2020-03-10 02:37:36 +0200

    add demo code to Main().

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Logger.SetTarget(LogTarget.Console);
+            Logger.Log($"Program start...{DateTime.Now}");
 
+            var people = AddSampleData();
+            PrintResults(people);
+
+            Console.ReadLine();
+            Logger.Log($"Program exit...{DateTime.Now}");
+        }
+
+        private static void PrintResults(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Logger.Log($"{person.GetFirstName()} {person.GetLastName()} is {person.GetAge()} old,");
+                Logger.Log($"measures {person.GetHeigth()}m, weights {person.GetWeight()}Kg");
+                Logger.Log($"and has BMI of {Bmi.Calculate(person)}");
+            }
+        }
+
+        private static List<Person> AddSampleData()
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Fox", "Mihail", 36, 83, 1.76f),
+                new Person("Schwarzenegger", "Arnold ", 73, 120, 1.88f)
+            };
+
+            return people;
         }
     }
 }
