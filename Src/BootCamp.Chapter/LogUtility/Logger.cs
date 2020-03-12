9d061fc4c54c8ecb7e2a5866994808a7c3a9f7e9
chatie <chatie@gmail.com>
2020-03-12 15:06:40 -0700

    1) Made  a method called SetLogOption to set the logger itself based on the option. 2) Eliminated a swtich statement that was being used to select which option to use.

--- Src/BootCamp.Chapter/LogUtility/Logger.cs
+++ Src/BootCamp.Chapter/LogUtility/Logger.cs
@@ -12,26 +12,15 @@ namespace BootCamp.Chapter
             File.Delete(LogPath);
         }
 
-        private const int ConsoleLogger = 1;
-        private const int FileLogger = 2;
-        private readonly int LogOption = ConsoleLogger; //Set to either ConsoleLogger or FileLogger
-        private ILogger _logger;
+        private ILogger SetLogOption()
+        {
+            //return new ConsoleLogger();
+            return new FileLogger(LogPath);
+        }
 
         public void Log(string msg)
         {
-            switch (LogOption)
-            {
-                case ConsoleLogger:
-                    _logger = new ConsoleLogger();
-                    break;
-                case FileLogger:
-                    _logger = new FileLogger(LogPath);
-                    break;
-                default:
-                    throw new InvalidArgumentException(LogOption);
-            }
-
-            _logger.Log(msg);
+            SetLogOption().Log(msg);
         }
     }
 }
