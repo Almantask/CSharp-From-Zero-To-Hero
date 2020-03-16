8d56f1be430d76faad26cfb29c604147a9403b81
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-03-16 14:34:23 +0100

    Add a soultion for Part 1 Homework

--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
@@ -26,6 +29,9 @@
            +-------+
 
          */
+        private static string _corner = "+";
+        private static string _horizontalBorder = "-";
+        private static string _verticalBorder = "|";
 
         /// <summary>
         /// Build a table for given message with given padding.
@@ -34,7 +40,75 @@
         /// </summary>
         public static string Build(string message, int padding)
         {
-            return "";
+            if (string.IsNullOrEmpty(message))
+            {
+                return "";
+            }
+
+            var wordArray = message.Split(Environment.NewLine);
+
+            int maxLength = MaxLength(wordArray);
+            int messageWidth =  maxLength + 2 * padding;
+
+            StringBuilder textTable = new StringBuilder();
+
+            AddHorizontalLine(textTable, messageWidth);
+            AddEmptyLines(textTable, maxLength, padding);
+            AddLinesWithWords(wordArray, textTable, maxLength, padding);
+            AddEmptyLines(textTable, maxLength, padding);
+            AddHorizontalLine(textTable, messageWidth);
+
+            return textTable.ToString();
+        }
+
+        private static void AddLinesWithWords(string[] wordArray, StringBuilder textTable, int maxLength, int padding)
+        {
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                textTable.Append(_verticalBorder).Append(String.Empty.PadRight(padding));
+
+                textTable.Append($"{wordArray[i].PadRight(maxLength)}");
+
+                textTable.Append(String.Empty.PadRight(padding)).Append(_verticalBorder);
+
+                textTable.Append(Environment.NewLine);
+            }
+        }
+
+        private static void AddEmptyLines(StringBuilder textTable, int maxLength, int padding)
+        {
+            for (int j = 0; j < padding; j++)
+            {
+                textTable.Append(_verticalBorder);
+                textTable.Append(String.Empty.PadRight(maxLength + 2 * padding));
+                textTable.Append(_verticalBorder);
+                textTable.Append(Environment.NewLine);
+            }
+        }
+
+        private static void AddHorizontalLine(StringBuilder textTable, int width)
+        {
+            textTable.Append(_corner);
+            for (int i = 1; i <= width; i++)
+            {
+                textTable.Append(_horizontalBorder);
+            }
+            textTable.Append(_corner);
+            textTable.Append(Environment.NewLine);
+        }
+
+        private static int MaxLength(string[] wordArray)
+        {
+            int currentMax = -1;
+
+            foreach (var item in wordArray)
+            {
+                if (item.Length > currentMax)
+                {
+                    currentMax = item.Length;
+                }
+            }
+            return currentMax;
         }
     }
 }
