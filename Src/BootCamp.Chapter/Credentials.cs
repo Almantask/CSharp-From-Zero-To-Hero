d7e6aaec8673b35a818e1bcefb78025371451fcf
Mihai Fologea <mihai.fologea@gmail.com>
2020-03-17 23:16:12 +0200

    implement 'Credentials' class; implement 'UserAllreadyExistsException' exception; add 'credentials.db'.

---
+++ Src/BootCamp.Chapter/Credentials.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Credentials
+    {
+        private readonly string credentialsFile = "credentials.db";
+
+        public bool FindUser(User user)
+        {
+            string line;
+            using StreamReader reader = new StreamReader(credentialsFile);
+            bool found = false;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var isValid = User.TryParse(line, out User credentials);
+                if (isValid && user.Equals(credentials))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            reader.Close();
+            return found;
+        }
+
+        public void AddUser(User user)
+        {
+            if (FindUser(user))
+            {
+                throw new UserAllreadyExistsException("User allready exists!");
+            }
+
+            using StreamWriter writer = new StreamWriter(credentialsFile, true);
+            writer.WriteLine($"{user.Name},{user.Password}");
+            writer.Close();
+        }
+    }
+}
---
+++ Src/BootCamp.Chapter/Exceptions/UserAllreadyExistsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class UserAllreadyExistsException : Exception
+    {
+        public UserAllreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public UserAllreadyExistsException()
+        {
+        }
+
+        public UserAllreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
