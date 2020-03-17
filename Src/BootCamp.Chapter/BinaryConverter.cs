55ed262b138d53b37f4491a50f8715f9252c60e4
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-03-17 13:22:14 +0100

    Add a soultion for Part 1 Homework 9

--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -8,12 +8,50 @@ namespace BootCamp.Chapter
     {
         public static long ToInteger(string binary)
         {
-            return 0;
+            if (String.IsNullOrEmpty(binary))
+            {
+                return 0;
+            }
+            try
+            {
+                int integerNumber = Convert.ToInt32(binary, 2);
+                return integerNumber;
+            }
+            catch
+            {
+                throw new InvalidBinaryNumberException(binary);
+            }
+        }
+
+        private static bool IsBinary(string binary)
+        {
+            foreach (var item in binary)
+            {
+                if (item != '0' || item != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public static string ToBinary(long number)
         {
-            return "";
+            Stack<int> myBinaryStack = new Stack<int>();
+            while (number / 2 != 0)
+            {
+                var binaryNumber = number % 2;
+                number /= 2;
+                myBinaryStack.Push((int)binaryNumber);
+            }
+            myBinaryStack.Push((int)number);
+
+            StringBuilder sbBinary = new StringBuilder();
+            foreach (var item in myBinaryStack)
+            {
+                sbBinary.Append(item);
+            }
+            return sbBinary.ToString();
         }
     }
 }
