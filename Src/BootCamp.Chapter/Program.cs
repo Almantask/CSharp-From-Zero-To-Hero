0929ad2c81940fd4fb11594e5f4548c4af00f6b3
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-03-22 01:42:45 +0100

    Change code look in Program.cs

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,15 +6,11 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            // Print each of the statistical output using Text Table with padding 3:
-            // - FindHighestBalanceEver
-            // - FindPersonWithBiggestLoss
-            // - FindRichestPerson
-            // - FindMostPoorPerson
-            Console.WriteLine(TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances),3));
-            Console.WriteLine(TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances),3));
-            Console.WriteLine(TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances),3));
-            Console.WriteLine(TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances),3));
+            Console.Write(TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances),3));
+            Console.Write(TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances),3));
+            Console.Write(TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances),3));
+            Console.Write(TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances),3));
+
             Console.ReadKey();
         }
     }
