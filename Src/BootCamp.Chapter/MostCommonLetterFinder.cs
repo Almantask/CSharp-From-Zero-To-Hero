9195b89d917babdeca47a2046d56ef0a902bdb23
Roelof Wobben <r.wobben@home.nl>
2020-03-27 22:38:11 +0100

    solved HW5

--- Src/BootCamp.Chapter/MostCommonLetterFinder.cs
+++ Src/BootCamp.Chapter/MostCommonLetterFinder.cs
@@ -1,10 +1,48 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections.Generic;
+
+namespace BootCamp.Chapter
 {
     public static class MostCommonLetterFinder
     {
         public static char Find(string sentence)
         {
-            return 'z';
+            Dictionary<char, int> histogram = new Dictionary<char, int>(); 
+            if (string.IsNullOrEmpty(sentence))
+            {
+                throw new ArgumentNullException(); 
+            }
+
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                throw new ArgumentNullException(); 
+            }
+
+            foreach (var character in sentence)
+            {
+                if (histogram.ContainsKey(character))
+                {
+                    histogram[character] += 1;
+                }
+                else
+                {
+                    histogram[character] = 1; 
+                }
+                ; 
+            }
+
+
+            var max = default(KeyValuePair<char, int>);
+            foreach (var item in histogram)
+            {
+                if (item.Value > max.Value)
+                {
+                    max = item; 
+                }
+            }
+
+            return max.Key; 
+
         }
     }
 }
--- Src/BootCamp.Chapter/ToggleableGridJagged.cs
+++ Src/BootCamp.Chapter/ToggleableGridJagged.cs
@@ -5,14 +5,48 @@ namespace BootCamp.Chapter
     public class ToggleableGridJagged : IToggleableGrid
     {
         private readonly IGridClearer _gridClearer;
+        private readonly bool[][] _toggles;
 
         public ToggleableGridJagged(bool[][] toggles, IGridClearer gridClearer)
         {
             _gridClearer = gridClearer;
+            _toggles = toggles;
         }
 
         public void Toggle(int x, int y)
         {
+            var current = _toggles[x][y];
+            _toggles[x][y] = !current;
+            Redraw(_toggles);
+        }
+
+        private void Redraw(bool[][] toggles)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            var numberOfRows = toggles.GetLength(0);
+            var numberOfCols = numberOfRows;
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfCols; j++)
+                {
+                    if (toggles[i][j])
+                    {
+                        Console.Write("\u25A0");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                if (i < numberOfRows - 1)
+                {
+                    Console.Write($"{Environment.NewLine}");
+                }
+
+
+            }
         }
     }
 }
