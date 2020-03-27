e8e705b3b2e032dfc17c60dd7c4e2c164a47fabb
Roelof Wobben <r.wobben@home.nl>
2020-03-27 19:29:50 +0100

    trying to make the 2th grid working

--- Src/BootCamp.Chapter/ToggleableGrid2d.cs
+++ Src/BootCamp.Chapter/ToggleableGrid2d.cs
@@ -4,12 +4,44 @@ namespace BootCamp.Chapter
 {
     public class ToggleableGrid2d : IToggleableGrid
     {
+        private readonly bool[][] _toggles;
+
         public ToggleableGrid2d(bool[][] toggles)
         {
+            _toggles = toggles; 
         }
 
         public void Toggle(int x, int y)
         {
+            var current = _toggles[x][y];
+            _toggles[x][y] = !current;
+            Redraw(_toggles); 
+        }
+
+        private void Redraw(bool[][] toggles)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            var numberOfRows = toggles.Length;
+            var numberOfCols = toggles.GetLength(0);
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfCols; j++)
+                {
+                    if (toggles[i][j])
+                    {
+                        Console.Write("\u25A0");
+                    }
+                   
+                }
+
+                if (i < numberOfRows - 1)
+                {
+                    Console.Write($"{Environment.NewLine}");
+                }
+                
+                 
+            }
         }
     }
 }
