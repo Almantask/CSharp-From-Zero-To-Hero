d894a5330a1f2cf8102b5050dce7c024a794a21e
dave94punk <david.katona@hotmail.com>
2020-04-01 13:48:38 +0200

    First commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,8 +6,29 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter the number of individuals you would like to calculate the BMI of: ");
+            int numberOfIndividuals = Convert.ToInt32(Console.ReadLine());
+
+            while (numberOfIndividuals != 0)
+            {
+                Console.Write("Please enter the individual's first name: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Please enter the individual's last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Please enter the individual's age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter the individual's weight (in kg): ");
+                float weight = float.Parse(Console.ReadLine());
+                Console.Write("Please enter the individual's height (in cm): ");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, their weight is {weight} kg and their height is {height} cm.");
+
+                float heightInMeters = height / 100f;
+                float bodyMassIndex = weight / (MathF.Pow(heightInMeters, 2f)); //BMI equals to kg/m2 hence the number 2
+                Console.WriteLine($"{firstName} {lastName}'s BMI is: {bodyMassIndex}");
+
+                numberOfIndividuals--;
+            }
         }
     }
 }
