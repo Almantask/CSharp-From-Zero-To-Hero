b6548bb5fbc7013bbf7d75b47330f851fccbf1a3
TheBleydel <thebleydel@gmail.com>
2020-04-05 15:05:15 +0200

    Added solution for Homework 1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,32 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Homework 1 and 2 
+            //1
+            string name, surename;
+            int age;
+            double weight, height, heightMeters, bmi;
+
+            Console.WriteLine("What's your name ?");
+            name = Console.ReadLine();
+            Console.WriteLine("What's your name ?");
+            surename = Console.ReadLine();
+
+            Console.WriteLine("What's your age ?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your weight (in kg) ?");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What's your height (in cm) ?");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            //2
+
+            heightMeters = height / 100;
+            bmi = weight / (heightMeters * heightMeters);
+            bmi = Math.Round(bmi, 2);
+
+            Console.WriteLine("{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm", name, surename, age, weight, height);
+            Console.WriteLine("BMI is {0}", bmi);
         }
     }
 }
