7c61b856bf1b926644dabe5a6c3edfaac09b719a
Jere <jeretreaper@gmail.com>
2020-04-07 00:59:10 -0400

    Added new line "this is Jere"

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,7 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.writeLine("This is Jere!");
         }
     }
 }
