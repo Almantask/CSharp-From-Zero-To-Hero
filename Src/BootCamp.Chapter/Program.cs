6a8373ff928b0f3dd83a9c23b961ee2ff6879806
blu3jay707@outlook.com <blu3jay707@outlook.com>
2020-04-07 01:09:49 -0500

    Finished homework to calculate BMI

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,36 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi! I will calculate your BMI. I just need to ask a few questions. What is your full name?");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much do you weigh in kg?");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your height in cm?");
+            double heightcm = Convert.ToDouble(Console.ReadLine());
+            double heightm = heightcm / 100;
+            double heightsq = heightm * heightm;
+            Console.WriteLine("Great! Now I will calculate your BMI.");
+            Console.WriteLine("Your name is " + fullname + ", you are " + age + " years old, you weigh " + weight + " kgs, and you are " + heightcm + " cm tall. your BMI is " + weight / heightsq + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
