613da4efd6652c525206e27bc101815762f1760b
Max <hellhunter15@hotmail.com>
2020-04-08 05:31:37 +0200

    initializing 2d array done.

--- Src/BootCamp.Chapter/ToggleableGrid2d.cs
+++ Src/BootCamp.Chapter/ToggleableGrid2d.cs
@@ -3,14 +3,21 @@
     public class ToggleableGrid2D : IToggleableGrid
     {
         private readonly IGridClearer _gridClearer;
+        Grid2d toggelsgrid;
 
         public ToggleableGrid2D(bool[,] toggles, IGridClearer gridClearer)
         {
             _gridClearer = gridClearer;
+
         }
 
         public void Toggle(int x, int y)
         {
+        }
+
+        private void OutOfBoundsCheck(int x, int y)
+        {
+
         }
     }
 }
