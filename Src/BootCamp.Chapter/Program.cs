a5bd5c9d652299d31d349c02f0d8324d8bb40de8
shareni <anubis.firepoi@gmail.com>
2020-04-09 17:42:31 +0200

    Finished homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,39 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello first user! What's your name?");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Great, and what's your surname?");
+            string surname1 = Console.ReadLine();
+            Console.WriteLine("Amazing, and how old are you?");
+            string age1 = Console.ReadLine();
+            Console.WriteLine("Cool, and what's your weight?");
+            string weight1 = Console.ReadLine();
+            float w1 = float.Parse(weight1);
+            Console.WriteLine("Groovie, and how tall are you?");
+            string height1 = Console.ReadLine();
+            float h1 = float.Parse(height1);
+            float bmi1 = (w1 / h1 / h1) * 10000;
+
+            Console.WriteLine(name1 + " " + surname1 + " is " + age1 + " years old, his weight is " + weight1 + " kg and his height is " + height1 + " cm.");
+            Console.WriteLine("Based on this information, your BMI is " + bmi1);
+
+            Console.WriteLine("Hello second user! What's your name?");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Great, and what's your surname?");
+            string surname2 = Console.ReadLine();
+            Console.WriteLine("Amazing, and how old are you?");
+            string age2 = Console.ReadLine();
+            Console.WriteLine("Cool, and what's your weight?");
+            string weight2 = Console.ReadLine();
+            float w2 = float.Parse(weight2);
+            Console.WriteLine("Groovie, and how tall are you?");
+            string height2 = Console.ReadLine();
+            float h2 = float.Parse(height2);
+            float bmi2 = (w2 / h2 / h2) * 10000;
+
+            Console.WriteLine(name2 + " " + surname2 + " is " + age2 + " years old, his weight is " + weight2 + " kg and his height is " + height2 + " cm.");
+            Console.WriteLine("Based on this information, your BMI is " + bmi2);
         }
     }
 }
