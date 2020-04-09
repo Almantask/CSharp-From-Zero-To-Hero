0a606806d256ec89c69dc07cceaabc46586dbe49
Michal Hajdo <smephey@gmail.com>
2020-04-09 12:35:43 +0100

    Adding solution for homework 1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your first name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What's your surname?");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("How much do you weight in kg?");
+            var weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("How tall are you in cm?");
+            var height = float.Parse(Console.ReadLine());
+
+            float heightInMetres = height / 100;
+
+            float bodyMassIndex = weight / (heightInMetres * heightInMetres);
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + "kg and his height is " + height + " cm");
+
+            Console.WriteLine("Their BMI is: " + bodyMassIndex);
         }
     }
 }
