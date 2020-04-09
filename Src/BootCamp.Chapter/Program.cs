9691ca57f78aca97c553342ec4d1de4911d682fd
Max <hellhunter15@hotmail.com>
2020-04-09 04:56:52 +0200

    2d Grid now toggles.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,8 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-
+            ToggleableGrid2D grid = new ToggleableGrid2D(new bool[2, 5]);
+            grid.Toggle(1, 4);
         }
     }
 }
--- Src/BootCamp.Chapter/ToggleableGrid2d.cs
+++ Src/BootCamp.Chapter/ToggleableGrid2d.cs
@@ -1,23 +1,58 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public class ToggleableGrid2D : IToggleableGrid
     {
         private readonly IGridClearer _gridClearer;
-        Grid2d toggelsgrid;
+        bool[,] grid;
 
-        public ToggleableGrid2D(bool[,] toggles, IGridClearer gridClearer)
+        public ToggleableGrid2D(bool[,] toggles)//, IGridClearer gridClearer
         {
-            _gridClearer = gridClearer;
+            //_gridClearer = gridClearer;
+            grid = toggles;
 
         }
 
         public void Toggle(int x, int y)
         {
+            OutOfBoundsCheck(x, y);
+            grid[x, y] = !grid[x, y];
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int k = 0; k < grid.GetLength(1); k++)
+                {
+                    if (grid[i, k])
+                    {
+                        Console.Write("■");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                if (i != grid.GetLength(0) -1)
+                {
+                    Console.WriteLine();
+                }
+            }
         }
 
         private void OutOfBoundsCheck(int x, int y)
         {
-
+            if (x < 0 || x >= grid.GetLength(0))
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if ( y < 0 || y >= grid.GetLength(1))
+            {
+                throw new IndexOutOfRangeException();
+            }
         }
     }
 }
