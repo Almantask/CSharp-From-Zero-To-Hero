9f24a4b9835608e22888b073a17ef65e8b93cdf1
MikeChisowda <mikechisowda@hotmail.com>
2020-04-09 20:25:33 +0200

    Cleared format.

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,88 +16,22 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            //Input
-            Console.WriteLine(message);
-            string number = Console.ReadLine();
-
-            //Check if the string is empty
-            if (string.IsNullOrEmpty(number)) return 0;
-
-            //Check if the string is a number
-            if (!int.TryParse(number, out var wholeNumber))
-            {
-                Console.WriteLine(number + " is not a valid number.");
-                return -1;
-            }
-
-            return wholeNumber;
+            return Program.PromptInt(message);
         }
 
         public static string PromptString(string message)
-        {
-            //Input
-            Console.WriteLine(message);
-            string text = Console.ReadLine();
-
-            //Check if the string is empty
-            if (string.IsNullOrEmpty(text))
-            {
-                Console.WriteLine("Name cannot be empty.");
-                return "-";
-            }
-
-            return text;
+        { 
+            return Program.PromptString(message);
         }
 
         public static float PromptFloat(string message)
         {
-            //Input
-            Console.WriteLine(message);
-            string number = Console.ReadLine();
-
-            //Check if the string is empty
-            if (string.IsNullOrEmpty(number)) return 0;
-
-            //Check if the string is a number
-            if (!float.TryParse(number, out float floatNumber))
-            {
-                Console.WriteLine(number + " is not a valid number.");
-                return -1;
-            }
-
-            return floatNumber;
+            return Program.PromptFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            string caseFail = "Failed calculating BMI. Reason: ";
-            string case1 = "Height cannot be equal or less than zero, but was " + height + ".";
-            string case2 = "Weight cannot be equal or less than zero, but was " + weight + ".";
-
-            //Check if values are correct
-            if (height <= 0 && weight <= 0)
-            {
-                Console.WriteLine(caseFail);
-                Console.WriteLine(case1);
-                Console.WriteLine(case2);
-                return -1;
-            }
-            else if (height <= 0)
-            {
-                Console.WriteLine(caseFail);
-                Console.WriteLine(case1);
-                return -1;
-            }
-            else if (weight <= 0)
-            {
-                Console.WriteLine(caseFail);
-                Console.WriteLine(case2);
-                return -1;
-            }
-
-            float bmi = weight / (height * height);
-
-            return bmi;
+            return Program.CalculateBMI(weight, height);
         }
     }
 }
