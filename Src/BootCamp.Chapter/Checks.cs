64394eb8d806ab974d642608ad5758a5c00321cc
Michal Hajdo <smephey@gmail.com>
2020-04-09 15:22:35 +0100

    Refactored Lesson 1 into its own class, refactored the solution to use functions.

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,26 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            // To do: call your implementation
+            return Lesson3.GetInt(message);
         }
 
         public static string PromptString(string message)
         {
             // To do: call your implementation. 
-            return "";
+            return Lesson3.GetString(message);
         }
 
         public static float PromptFloat(string message)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.GetFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.CalculateBMI(height, weight);
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+
+        public static void Demo()
+        {
+            string name = GetString("What's your first name?");
+            string surname = GetString("What's your surname?");
+            int age = GetInt("How old are you?");
+            float weight = GetFloat("How much do you weight in kg?");
+            float height = GetFloat("How tall are you in cm?");
+            float mHeight = ConvertToMetres(height);
+            float bodyMassIndex = CalculateBMI(mHeight, weight);
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + "kg and his height is " + height + " cm");
+
+            Console.WriteLine("Their BMI is: " + bodyMassIndex);
+        }
+
+        public static string GetString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int GetInt(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float GetFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float GetHeight(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float CalculateBMI(float height, float weight)
+        {
+            return weight / (height * height);
+        }
+
+        private static float ConvertToMetres(float height)
+        {
+            return height / 100;
+        }
+
+    }
+}
