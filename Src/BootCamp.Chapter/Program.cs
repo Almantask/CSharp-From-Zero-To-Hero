046cc448054998d47e4a0b26114e128cad41478f
B.Martinsons <martinsonsb@gmail.com>
2020-04-10 22:13:05 +0300

    Made changes according to the requested changes in pull request.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,29 +9,8 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            // Need to refactor the code I made in Lesson 2.
-            // Use functions to achieve DRY (as little duplicate code as possible).
-
-            // Name input
-            string name = PromptString("Enter your name: ");
-
-            // Surname input
-            string surname = PromptString("Enter your surname: ");
-
-            // Age input
-            int age = PromptInt("Enter your age: ");
-
-            // Weight input
-            float weight = PromptFloat("Enter your weight(in kg): ");
-
-            // Height input
-            float height = PromptFloat("Enter your height(in cm): ");
-
-            // BMI calculation
-            float bmi = CalculateBMI(weight, (height/100));
-
-            // User profile output
-            PrintUserProfile(name, surname, age, weight, height, bmi);
+            AskUserQuestionsAndUserPrintProfile();
+            AskUserQuestionsAndUserPrintProfile();
         }
 
         public static string PromptString(string message)
@@ -69,6 +48,18 @@ namespace BootCamp.Chapter
                 height,
                 string.Format("{0:F1}", bmi)
                 );
+        }
+
+        static void AskUserQuestionsAndUserPrintProfile()
+        {
+            string name = PromptString("Enter your name: ");
+            string surname = PromptString("Enter your surname: ");
+            int age = PromptInt("Enter your age: ");
+            float weight = PromptFloat("Enter your weight(in kg): ");
+            float height = PromptFloat("Enter your height(in cm): ");
+            float bmi = CalculateBMI(weight, (height / 100));
+
+            PrintUserProfile(name, surname, age, weight, height, bmi);
         }
     }
 }
