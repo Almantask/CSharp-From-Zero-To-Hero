40533173317163ed31055da918a82c15d4130d39
Jay Sampson <jahnell5@gmail.com>
2020-04-12 19:31:53 -0400

    Switched the variable and function names Deleted the redundant function

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,25 +9,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            PersonOneBMI();
-            PersonTwoBMI();
+            PrintBMI();
+            PrintBMI();
         }
             
-         static string Name(string askName)
+         static string AskName(string name)
         {
-            Console.WriteLine(askName);
+            Console.WriteLine(name);
             return Console.ReadLine();           
         }
    
-          static int Age(string askAge)
+          static int AskAge(string age)
         {
-            Console.WriteLine(askAge);
+            Console.WriteLine(age);
             return Int32.Parse(Console.ReadLine());       
         }
 
-         static float Measurements(string askMeasurements)
+         static float AskMeasurements(string Measurements)
         {
-            Console.WriteLine(askMeasurements);
+            Console.WriteLine(Measurements);
             return float.Parse(Console.ReadLine());           
         }       
 
@@ -44,28 +44,16 @@ namespace BootCamp.Chapter
             return bodyMassIndex;
         }
                
-         static void PersonOneBMI()
+         static void PrintBMI()
         {
-            string name1 = Name("What is your name?");
-            int age1 = Age("How old are you?");
-            float weight1 = Measurements("What is your weight in kg?");
-            float height1 = Measurements("What is your height in cm?");
+            string name1 = AskName("What is the patient's name?");
+            int age1 = AskAge("How old are they?");
+            float weight1 = AskMeasurements("What is their weight in kg?");
+            float height1 = AskMeasurements("What is their height in cm?");
             var bmi = CalculateBodyMassIndex(height1, weight1);
-            Console.WriteLine(name1 + " is " + age1 + " years old, your weight is " + weight1 + " kg and your height is " + height1 + " cm.");
+            Console.WriteLine(name1 + " is " + age1 + " years old, their weight is " + weight1 + " kg and their height is " + height1 + " cm.");
             Print(name1, bmi);
         }
-
-        static void PersonTwoBMI()
-        {
-            string name2 = Name("What is your friend's name?");
-            int age2 = Age("How old are they?");
-            float weight2 = Measurements("What is their weight in kg?");
-            float height2 = Measurements("What is their height in cm?");
-            var bmi = CalculateBodyMassIndex(height2, weight2);
-            Console.WriteLine(name2 + " is " + age2 + " years old, your weight is " + weight2 + " kg and your height is " + height2 + " cm.");
-            Print(name2, bmi);
-        }
-
     }
 }
         
