e00f5bbc26a6e92c534950a81ef0046b59e461c0
Jean Royce <8617825+jeanroyce@users.noreply.github.com>
2020-04-11 22:29:16 -0300

    Fixed auto test

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -12,24 +12,34 @@ namespace BootCamp.Chapter
             // Greetings
             Console.WriteLine("Hello! Im a BMI Calculator, to calculate I need some info.");
 
+            // Get Info from user
             string name = PromptString("First name: ");
             string surname = PromptString("Surname: ");
             int age = PromptInt("Age: ");
             float weight = PromptFloat("Weight(Kg): ");
             float height = PromptFloat("Height(cm): ");
+
+            // Convert height(Cm) to Meter
             float heightM = height / 100;
 
             // Calculate BMI
-            float bmi = CalculateBmi(heightM, weight);
-            Console.WriteLine(bmi);
+            float bmi = CalculateBmi(weight, heightM);
 
+            // Print Info on console
             Console.WriteLine($"{name} {surname} is {age} year old, his weight is {weight} kg and his height is {height} cm.\n" +
-                    $"IBM: {bmi}");
+                    $"BMI: {bmi}");
 
-
+            // Ask if user want to do the test again
+            Console.WriteLine("Would you like to try again?");
+            string response = Console.ReadLine();
+            if (response.ToLower() == "yes" || response.ToLower() == "y")
+            {
+                Console.Clear();
+                Demo();
+            }
         }
 
-        public static float CalculateBmi(float height, float weight)
+        public static float CalculateBmi(float weight, float height)
         {
             float bmi = weight / (height * height);
             return bmi;
@@ -45,7 +55,6 @@ namespace BootCamp.Chapter
         {
             Console.Write(message);
             return int.Parse(Console.ReadLine());
-
         }
 
         public static string PromptString(string message)
