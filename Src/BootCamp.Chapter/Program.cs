3533e5bf274bb3e31c611d7ae0b87ce4150caeff
kclaka <kennyigbechi@gmail.com>
2020-04-12 00:32:27 -0700

    Assignment Done

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Enter Age: ");
+            if (!int.TryParse(Console.ReadLine(), out int age))
+            {
+                Console.WriteLine("Age Should be a Number");
+            }
+
+            Console.WriteLine("Enter Weight: ");
+            if(!int.TryParse(Console.ReadLine(), out int weight))
+            {
+                Console.WriteLine("Weight must be a Number");
+            }
+
+            Console.WriteLine("Enter Height: ");
+            if(!double.TryParse(Console.ReadLine(), out double height))
+            {
+                Console.WriteLine("Height Must be a Number");
+            }
+
+            Console.WriteLine($"{name} {surname} is {age} old, his weight is " +
+                $"{weight}KG and his height is {height}cm");
+
+            Console.WriteLine($"BMI is {height / weight}");
+
         }
     }
 }
