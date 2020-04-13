1d77919baa968adeda37f8eb0c89b6216b089c8a
Trollermaner <li.william.mail@gmail.com>
2020-04-13 14:21:40 -0400

    Homework 1 part 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,16 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
+            string Name1 = "Tom Jefferson";
+            int Age1 = 19;
+            int Weight1 = 50;
+            float Height1 = 156.5f;
+            float Height1v2 = Height1 / 100;
+            float Height1v3 = Height1v2 * Height1v2;
+            float Bmi1 = Weight1 / Height1v3;
+
+            Console.WriteLine(Name1 + " is " + Age1 + " years old, his weight is " + Weight1 + " kg, his height is " + Height1 + " cm and his bmi is " + Bmi1 + ".");
         }
     }
 }
