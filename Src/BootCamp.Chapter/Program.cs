e58c4a4d28d74b279390d6c858cf6e24ca957d2d
joshuajz <35657686+joshuajz@users.noreply.github.com>
2020-04-12 21:04:56 -0400

    Finsihed

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,18 +6,56 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name1 = "Tom Jefferson";
-            int age1 = 19;
-            int weight1 = 50;
-            float height1 = 156.5f;
-            float bmi1 = weight1 / height1 / height1 * 10000;
-            Console.WriteLine($"My name is {name1} and I am {age1} years old, my weight is {weight1} kg, I'm {height1} meters tall and my BMI is {bmi1}");
-            string name2 = "Tom Holland";
-            int age2 = 21;
-            int weight2 = 80;
-            float height2 = 145.9f;
-            float bmi2 = weight2 / height2 / height2 * 10000;
-            Console.WriteLine($"My name is {name2} and I am {age2} years old, my weight is {weight2} kg, I'm {height2} meters tall and my BMI is {bmi2}");
+            // Person 1:
+            // Gathering Input:
+            // Name
+            Console.WriteLine("Enter Name: ");
+            string nm1 = Console.ReadLine();
+
+            // Age
+            Console.WriteLine("Enter Age: ");
+            int age1 = int.Parse(Console.ReadLine());
+        
+            // Weight
+            Console.WriteLine("Enter Weight: ");
+            int weight1 = int.Parse(Console.ReadLine());
+
+            // Height
+            Console.WriteLine("Enter Height: ");
+            float height1 = float.Parse(Console.ReadLine());
+
+            // Calculating BMI
+            float bmi1 = weight1 / (height1 * height1) * 10000;
+
+            
+            // Person 2:
+            // Gathering Input:
+            // Name
+            Console.WriteLine("Enter Name: ");
+            string nm2 = Console.ReadLine();
+
+            // Age
+            Console.WriteLine("Enter Age: ");
+            int age2 = int.Parse(Console.ReadLine());
+
+            // Weight
+            Console.WriteLine("Enter Weight: ");
+            int weight2 = int.Parse(Console.ReadLine());
+
+            // Height
+            Console.WriteLine("Enter Height: ");
+            float height2 = float.Parse(Console.ReadLine());
+
+            // Calculating BMI
+            float bmi2 = weight1 / (height1 * height1) * 10000;
+
+
+            // Outputting Results:
+            // Person 1:
+            Console.WriteLine($"My name is {nm1} and I am {age1} years old, my weight is {weight1} kg, I'm {height1} meters tall and my BMI is {bmi1}");
+            
+            // Person 2:
+            Console.WriteLine($"My name is {nm2} and I am {age2} years old, my weight is {weight2} kg, I'm {height2} meters tall and my BMI is {bmi2}");
         }
     }
 }
