86625352fec6151308302efc777c3c1de13baed1
Roelof Wobben <r.wobben@home.nl>
2020-04-14 13:52:03 +0200

    made a class Transaction with as properties : ShopName, City, Street, soldIten, TimeWhenSold and the TotalPrice

---
+++ Src/BootCamp.Chapter/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Transaction
+    {
+        public string ShopName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string SoldItem { get; set; }
+        public DateTime TimeWhenSold { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
