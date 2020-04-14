49fccbb67ed8872a36eb81b816547bf92b54d609
Gary Pearce <flebberflep@gmail.com>
2020-04-15 00:49:19 +0100

    Created 'GetInput' fuction to take inputs from user Created prompts to gather information needed for the exercise. Used console to write information about the user and do a basic BMI calculation.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,22 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fName = GetInput("Enter your first name: ");
+            string lName = GetInput("Enter your last name: ");
+            int age = int.Parse(GetInput("Enter your age: "));
+            float height = float.Parse(GetInput("Enter your height (in cm): "));
+            float weight = float.Parse(GetInput("Enter your weight (in kg): "));
+            float bmi = weight / (height * height /10000);
+            Console.WriteLine("{0} {1} is {2} years old, weighs {3} kg and is {4} cm tall.", fName, lName, age, weight, height);
+            Console.WriteLine("{0}'s BMI is {1}.", fName, bmi);
+
+            Console.ReadKey();
+        }
+        public static string GetInput(string a)
+        {
+            Console.Write(a);
+            var b = Console.ReadLine();
+            return b;
         }
     }
 }
