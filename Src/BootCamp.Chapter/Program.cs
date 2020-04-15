13b5c7d630adbee3e6a841a1cb83b4b3e0138aa1
Gary Pearce <flebberflep@gmail.com>
2020-04-15 21:52:28 +0100

    Renamed all variables to make code more clear. Changed string formatting for readability Added second person processing. Added whitespace between parts of code that read info, parts that write info and parts that pause processing.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,22 +6,38 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string fName = GetInput("Enter your first name: ");
-            string lName = GetInput("Enter your last name: ");
-            int age = int.Parse(GetInput("Enter your age: "));
-            float height = float.Parse(GetInput("Enter your height (in cm): "));
-            float weight = float.Parse(GetInput("Enter your weight (in kg): "));
-            float bmi = weight / (height * height /10000);  // division to place decimal where it would be expected for a BMI measurement.
-            Console.WriteLine("{0} {1} is {2} years old, weighs {3} kg and is {4} cm tall.", fName, lName, age, weight, height);
-            Console.WriteLine("{0}'s BMI is {1}.", fName, bmi);
+            //First person
+            string firstName1 = GetInput("Enter your first name: ");
+            string lastName1 = GetInput("Enter your last name: ");
+            int age1 = int.Parse(GetInput("Enter your age: "));
+            float height1 = float.Parse(GetInput("Enter your height (in cm): "));
+            float weight1 = float.Parse(GetInput("Enter your weight (in kg): "));
+            float bmi1 = weight1 / (height1 * height1 /10000);  // division to place decimal where it would be expected for a BMI measurement.
 
-            Console.ReadKey();
+            Console.WriteLine($"{firstName1} {lastName1} is {age1} years old, weighs {weight1} kg and is {height1} cm tall."); 
+            Console.WriteLine($"{firstName1}'s BMI is {bmi1}.");
+
+            Console.ReadKey(); //Pause for patient to check details.
+            Console.Clear(); //Doctor-Patient Confidentiality.
+
+            //Second Person
+            string firstName2 = GetInput("Enter your first name: ");
+            string lastName2 = GetInput("Enter your last name: ");
+            int age2 = int.Parse(GetInput("Enter your age: "));
+            float height2 = float.Parse(GetInput("Enter your height (in cm): "));
+            float weight2 = float.Parse(GetInput("Enter your weight (in kg): "));
+            float bmi2 = weight2 / (height2 * height2 / 10000);
+
+            Console.WriteLine($"{firstName2} {lastName2} is {age2} years old, weighs {weight2} kg and is {height2} cm tall.");
+            Console.WriteLine($"{firstName2}'s BMI is {bmi2}.");
+
+            Console.ReadKey(); //Pause for patient again.
         }
-        static string GetInput(string a) // Getting started early on method blocks!
+        static string GetInput(string prompt)
         {
-            Console.Write(a);
-            var b = Console.ReadLine();
-            return b;
+            Console.Write(prompt);
+            var input = Console.ReadLine();
+            return input;
         }
     }
 }
