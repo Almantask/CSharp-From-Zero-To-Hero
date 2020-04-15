57210a331e8de4c2d7b12bc088fb780e3cac2b66
Almantas Karpavičius <almantusk@gmail.com>
2020-04-15 17:47:17 +0300

    Lesson template updated

---
+++ Src/BootCamp.Chapter/Examples/Example1/Example1Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter.Examples.Example1
+{
+    public static class Example1Demo
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace BootCamp.Chapter
+﻿using BootCamp.Chapter.Examples.Example1;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Example1Demo.Run();
         }
-
-        
     }
 }
