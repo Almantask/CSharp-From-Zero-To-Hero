0a17c3ab44a2b36dd41e48167ee78751ac337c23
Max <hellhunter15@hotmail.com>
2020-04-15 02:07:24 +0200

    Added a new Method for getting the most common letter in a string using a dictionary.

--- Src/BootCamp.Chapter/MostCommonLetterFinder.cs
+++ Src/BootCamp.Chapter/MostCommonLetterFinder.cs
@@ -9,8 +9,46 @@ namespace BootCamp.Chapter
         {
             if (string.IsNullOrWhiteSpace(sentence))
             {
-                throw new ArgumentNullException();
+                throw new ArgumentNullException("The sentence you gave was empty.");
             }
+
+            return GetMostCommonLetterDic(sentence);
+        }
+
+        private static char GetMostCommonLetterDic(string sentence)
+        {
+            Dictionary<char, int> lettersDic = new Dictionary<char, int>();
+
+            foreach (char letter in sentence)
+            {
+                if (lettersDic.ContainsKey(letter))
+                {
+                    lettersDic[letter] += 1;
+                }
+                else
+                {
+                    lettersDic.Add(letter, 1);
+                }
+            }
+            char mostCommonLetter = ' ';
+            int number = 0;
+            foreach (KeyValuePair<char, int> key in lettersDic)
+            {
+                if (key.Value > number)
+                {
+                    mostCommonLetter = key.Key;
+                    number = key.Value;
+                }
+            }
+            return mostCommonLetter;
+        }
+
+        private static char GetMostCommonLetter(string sentence)
+        {
+            int mostCommonLetterCount = 0;
+            char mostCommonLetter = ' ';
+            int nextLetterCount = 0;
+            char nextLetter = ' ';
 
             List<char> sentenceList = new List<char>();
             foreach (char character in sentence)
@@ -19,26 +57,16 @@ namespace BootCamp.Chapter
             }
             sentenceList.Sort();
 
-            return GetMostCommonLetter(sentence, sentenceList);
-        }
-
-        private static char GetMostCommonLetter(string originalSentence, List<char> sortedSentenceList)
-        {
-            int mostCommonLetterCount = 0;
-            char mostCommonLetter = ' ';
-            int nextLetterCount = 0;
-            char nextLetter = ' ';
-
-            foreach (char c in sortedSentenceList)
+            foreach (char c in sentenceList)
             {
                 if (c == nextLetter)
                 {
                     nextLetterCount++;
-                    if (c == sortedSentenceList[sortedSentenceList.Count - 1])
+                    if (c == sentenceList[sentenceList.Count - 1])
                     {
                         if (nextLetterCount == mostCommonLetterCount)
                         {
-                            mostCommonLetter = FirstCharFound(originalSentence, mostCommonLetter, nextLetter);
+                            mostCommonLetter = FirstCharFound(sentence, mostCommonLetter, nextLetter);
                         }
                         else if (nextLetterCount > mostCommonLetterCount)
                         {
@@ -51,7 +79,7 @@ namespace BootCamp.Chapter
                 {
                     if (nextLetterCount == mostCommonLetterCount)
                     {
-                        mostCommonLetter = FirstCharFound(originalSentence, mostCommonLetter, nextLetter);
+                        mostCommonLetter = FirstCharFound(sentence, mostCommonLetter, nextLetter);
                     }
                     else if (nextLetterCount > mostCommonLetterCount)
                     {
@@ -60,7 +88,7 @@ namespace BootCamp.Chapter
                     }
                     nextLetterCount = 1;
                     nextLetter = c;
-                    if (c == sortedSentenceList[sortedSentenceList.Count - 1] && nextLetterCount > mostCommonLetterCount)
+                    if (c == sentenceList[sentenceList.Count - 1] && nextLetterCount > mostCommonLetterCount)
                     {
                         mostCommonLetterCount = nextLetterCount;
                         mostCommonLetter = nextLetter;
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
