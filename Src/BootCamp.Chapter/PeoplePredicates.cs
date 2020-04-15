1dd48e0ffc6008cbff7275d31abee329180d6de8
Max <hellhunter15@hotmail.com>
2020-04-15 03:20:44 +0200

    Person Class finished; Can now get age from a person.

--- Src/BootCamp.Chapter/PeoplePredicates.cs
+++ Src/BootCamp.Chapter/PeoplePredicates.cs
@@ -10,20 +10,20 @@ namespace BootCamp.Chapter
         /// a) over 18, who do not live in UK, whose surename does not contain letter 'a'.
         /// </summary>
         /// <returns></returns>
-        public static bool IsA(Person person) => person.Age >= 18 && person.Town != "UK" && person.Surname.ToLower().Contains('a');
+        public static bool IsA(Person person) => person.GetAge() >= 18 && person.Country != "UK" && person.SurName.ToLower().Contains('a');
 
         /// <summary>
         /// b) under 18,  who do not live in UK, whose surename does not contain letter 'a'.  
         /// </summary>
         /// <param name="person"></param>
         /// <returns></returns>
-        public static bool IsB(Person person) => person.Age < 18 && person.Town != "UK" && !person.Surname.ToLower().Contains('a');
+        public static bool IsB(Person person) => person.GetAge() < 18 && person.Country != "UK" && !person.SurName.ToLower().Contains('a');
 
         /// <summary>
         /// c) who do not live in UK, whose surename and name does not contain letter 'a'.  
         /// </summary>
         /// <param name="person"></param>
         /// <returns></returns>
-        public static bool IsC(Person person) => person.Town != "UK" && person.Surname.ToLower().Contains('a') && !person.FirstName.ToLower().Contains('a');
+        public static bool IsC(Person person) => person.Country != "UK" && person.SurName.ToLower().Contains('a') && !person.FirstName.ToLower().Contains('a');
     }
 }
--- Src/BootCamp.Chapter/Person.cs
+++ Src/BootCamp.Chapter/Person.cs
@@ -1,11 +1,53 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public class Person
     {
-        // add missing properties
-        public int Age { get; }
-        public string Town { get; }
-        public string Surname { get; }
+        //TODO: add missing properties
+        // name,sureName,birthday,gender,country,email,streetAddress
+        // Chrisy,Frise,4/24/1946,Male,Philippines,cfrise0@europa.eu,62895 Troy Parkway
         public string FirstName { get; }
+        public string SurName { get; }
+        public string Birthday { get; }
+        public string Gender { get; }
+        public string Country { get; }
+        public string Email { get; }
+        public string StreetAddress { get; }
+
+        public Person(string name, string surName, string birthday, string gender, string country, string email, string streetAddress)
+        {
+            FirstName = name;
+            SurName = surName;
+            Birthday = birthday;
+            Gender = gender;
+            Country = country;
+            Email = email;
+            StreetAddress = streetAddress;
+        }
+
+        public int GetAge()
+        {
+            string now = DateTime.Now.ToString().Split(' ')[0];
+            int day = int.Parse(now.Split('/')[0]);
+            int month = int.Parse(now.Split('/')[1]);
+            int year = int.Parse(now.Split('/')[2]);
+
+            int birthDay = int.Parse(Birthday.Split('/')[1]);
+            int birthMonth = int.Parse(Birthday.Split('/')[0]);
+            int birthYear = int.Parse(Birthday.Split('/')[2]);
+
+            int age = year - birthYear;
+            if (birthMonth > month)
+            {
+                age--;
+            }
+            else if (birthMonth == month && birthDay > day)
+            {
+                age--;
+            }
+            return age;
+        }
+
     }
 }
