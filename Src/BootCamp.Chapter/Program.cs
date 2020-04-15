099d692107bb01398a5ec8df611f5d787b795489
Gary Pearce <flebberflep@gmail.com>
2020-04-15 19:26:14 +0100

    Refactored code from Homework 1 & 2 into seperate method blocks in Lesson3 Class file.
    Put all methods into the checks file and ran tests, all passed.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,22 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string fName = GetInput("Enter your first name: ");
-            string lName = GetInput("Enter your last name: ");
-            int age = int.Parse(GetInput("Enter your age: "));
-            float height = float.Parse(GetInput("Enter your height (in cm): "));
-            float weight = float.Parse(GetInput("Enter your weight (in kg): "));
-            float bmi = weight / (height * height /10000);  // division to place decimal where it would be expected for a BMI measurement.
-            Console.WriteLine("{0} {1} is {2} years old, weighs {3} kg and is {4} cm tall.", fName, lName, age, weight, height);
-            Console.WriteLine("{0}'s BMI is {1}.", fName, bmi);
-
-            Console.ReadKey();
-        }
-        static string GetInput(string a) // Getting started early on method blocks!
-        {
-            Console.Write(a);
-            var b = Console.ReadLine();
-            return b;
+            Lesson3.Demo();
         }
     }
 }
