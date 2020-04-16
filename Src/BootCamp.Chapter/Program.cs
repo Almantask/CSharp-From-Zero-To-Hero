aa2c09ab1683f639ef094dcf3615f72e15ef9c4c
COMV99 <63080440+COMV99@users.noreply.github.com>
2020-04-16 01:43:13 -0600

    Add files via upload

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
-namespace BootCamp.Chapter
+namespace BootCamp.Chapter1
 {
     class Program
     {
+        //test comment
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 21;       // in years
+            float weight = 58.9f;   //in kg
+            float height = 167.6f;  //in cm
+            string myName = "ComV99";
+            var bmi = weight / ((height * height) / 1000);
+
+            Console.WriteLine(myName + " is " + age +
+                   " years old, his weight is " + weight +
+                   " kg and his height is " + height + "cm.");
+            Console.WriteLine("His BMI is " + bmi);
+
+            age = 2;
+            weight = 12;
+            height = 60;
+            string otherName = "Todd";
+            bmi = weight / ((height * height) / 1000);
+
+            Console.WriteLine(otherName + " is " + age +
+                   " years old, his weight is " + weight +
+                   " kg and his height is " + height + "cm.");
+            Console.WriteLine("His BMI is " + bmi);
+
         }
     }
 }
