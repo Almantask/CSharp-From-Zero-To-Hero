3c3c27ac8a999d03bde189f7a9d6cb860dd8656b
Trollermaner <li.william.mail@gmail.com>
2020-04-16 11:59:22 -0400

    removed the 1's after the variables

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -15,20 +15,20 @@ namespace BootCamp.Chapter
             Console.WriteLine("Person #" + number+ ":" );
 
             Console.Write("Full Name: ");
-            string name1 = Console.ReadLine();
+            string name = Console.ReadLine();
 
             Console.Write("Age: ");
-            int age1 = int.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
 
             Console.Write("Weight (kg): ");
-            float weight1 = float.Parse(Console.ReadLine());
+            float weight = float.Parse(Console.ReadLine());
 
             Console.Write("Height (cm): ");
-            float height1 = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
 
-            float bmi1 = weight1 / (height1 * height1) * 10000;
+            float bmi = weight / (height * height) * 10000;
 
-            Console.WriteLine(name1 + " is " + age1 + " years old, his weight is " + weight1 + " kg, his height is " + height1 + " and his bmi is " + bmi1 + ".");
+            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg, his height is " + height + " and his bmi is " + bmi + ".");
 
             Console.WriteLine(" ");
 
