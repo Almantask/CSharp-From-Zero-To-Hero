fa3be98e1325129cbf8bc543be4b3ab0eb09496b
Trollermaner <li.william.mail@gmail.com>
2020-04-17 15:31:57 -0400

    saving

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,38 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            CalculateBmi(1);
+
+            CalculateBmi(2);
+        }
+        private static void CalculateBmi(int number)
+        {
+            Console.WriteLine("Person #" + number + ":");
+
+            Console.Write("Full Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight (kg): ");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.Write("Height (cm): ");
+            float height = float.Parse(Console.ReadLine());
+
+            float bmi = weight / (height * height) * 10000;
+
+            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg, his height is " + height + " and his bmi is " + bmi + ".");
+
+            Console.WriteLine(" ");
+
+        }
+
+        static void ConvertToInt(int height) 
+        {
+
+
         }
     }
 }
