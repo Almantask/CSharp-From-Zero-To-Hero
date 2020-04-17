cbf6dd72e43e7de26b55f2b5fade8206fa59f16a
Bosco_2019 <bitcoinbosco@protonmail.com>
2020-04-17 18:47:07 +0100

    My submission for Chapter 1 homework 1and2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Can this method work for both Tom Jefferson and the second person?
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            int ageNumber = Convert.ToInt32(age);
+
+            Console.WriteLine("How much do you weigh in kg?");
+            var weight = Console.ReadLine();
+            double weightKg = Convert.ToDouble(weight);
+
+            Console.WriteLine("How tall are you in cm?");
+            var height = Console.ReadLine();
+            double heightcm = Convert.ToDouble(height);
+
+            double heightMeters = heightcm / 100.0;
+            double heightMetersSquared = heightMeters * heightMeters;
+            double bmi = weightKg / heightMetersSquared;
+
+            Console.WriteLine("Thank you for using my BMI calculator," + " " + (name) + " " + "your BMI is" + " " + (bmi));
         }
     }
 }
