7523994a7559b57009859914fba1214c629c1f62
Mihail Fox <mihai.fologea@gmail.com>
2020-04-19 22:41:23 +0300

    implement string extensions 'IsValid' and 'ToCammelCase'.

---
+++ Src/BootCamp.Chapter/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BootCamp.Chapter
+{
+    public static class StringExtensions
+    {
+        public static bool IsValid(this string input)
+        {
+            return !string.IsNullOrWhiteSpace(input) || !string.IsNullOrEmpty(input);
+        }
+
+        public static string ToCamelCase(this string input)
+        {
+            if (!input.IsValid())
+            {
+                throw new ArgumentException("input cannot be null or empty");
+            }
+
+            var charArray = input.ToCharArray();
+            charArray[0] = char.ToLowerInvariant(charArray[0]);
+
+            return new string(charArray);
+        }
+    }
+}
