6461e76aae0e73a45bc28a48a276da96d631c32f
Jean Royce <8617825+jeanroyce@users.noreply.github.com>
2020-04-19 05:43:29 -0300

    Chapter1/Homework/10

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,12 +12,44 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            return ApplyAlgorithm(message, shift);
         }
 
+        
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            return ApplyAlgorithm(message, -shift);
+        }
+
+        private static string ApplyAlgorithm(string message, int shift)
+        {
+            if (message == "") return "";
+            if (message == null) return null;
+
+            // ISO-8859-1 for one byte per letter for most of all. AsciiEx like.
+            // UTF-8 fail in portuguese letters(like é, à, ç), unicode things go wrong.
+            var encoding = Encoding.GetEncoding("ISO-8859-1");
+            var messageToBytes = encoding.GetBytes(message);
+
+            for (var i = 0; i < messageToBytes.Length; i++)
+            {
+                if (messageToBytes[i] == 32) continue;
+                var newByte = messageToBytes[i] + shift;
+                if (newByte < 0) newByte = 256 + newByte;
+                if (newByte > 256) newByte %= 256;
+
+                try
+                {
+                    messageToBytes[i] = Convert.ToByte((newByte));
+                }
+                catch
+                {
+                    Console.WriteLine($"Unexpected error while converting {newByte} to byte.");
+                    throw;
+                }
+            }
+
+            return encoding.GetString(messageToBytes);
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            var message =
+                "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+            var message2 =
+                "Frase. Frase é todo enunciado de sentido completo, podendo ser formada por uma só palavra ou por várias, podendo ter verbos ou não.";
+
+            var encrypted = CaesarCipher.Encrypt(message, 10);
+            var decrypted = CaesarCipher.Decrypt(encrypted, 10);
+            Console.WriteLine(encrypted);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(decrypted);
+            Console.WriteLine("==============================================================================");
+            
+            var encrypted2 = CaesarCipher.Encrypt(message2, 230);
+            var decrypted2 = CaesarCipher.Decrypt(encrypted2, 230);
+            Console.WriteLine(encrypted2);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(decrypted2);
+            
         }
     }
 }
