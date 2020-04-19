2995104f30c68b84d403de661be036ea9a12679b
draksa <draksa.zg@hotmail.com>
2020-04-19 15:52:10 +0200

    Part 1 homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,19 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var name = Console.ReadLine();
+            Console.Write("Please input first name: ");
+            var surname = Console.ReadLine();
+            Console.Write("Please input last name: ");
+            var age = Console.ReadLine();
+            Console.Write("Please input age: ");
+            var weight = Console.ReadLine();
+            Console.Write("Please input weight: ");
+            var height = Console.ReadLine();
+            Console.Write("Please input height: ");
+
+            Console.WriteLine(name +" "+ surname + " is "+ age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
 
         }
     }
