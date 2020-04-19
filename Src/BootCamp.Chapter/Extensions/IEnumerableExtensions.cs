e2cbf74168b2ec9fc653010b4eb2462655cefd8f
Mihail Fox <mihai.fologea@gmail.com>
2020-04-19 22:44:49 +0300

    implement enumerable extensions 'ToStringFormated' and 'Print'.

---
+++ Src/BootCamp.Chapter/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static string ToStringFormated<T>(this IEnumerable<T> enumerableInput)
+        {
+            if (enumerableInput is null)
+            {
+                throw new ArgumentException(nameof(enumerableInput), $"{enumerableInput} can not bet null or empty");
+            }
+
+            var sb = new StringBuilder();
+            foreach (var item in enumerableInput)
+            {
+                var itemString = item.ToString();
+                {
+                    if (itemString.IsValid())
+                    {
+                        sb.Append(item).Append(",");
+                    }
+                }
+            }
+
+            if (sb.Length != 0)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            return sb.ToString();
+        }
+
+        public static void Print<T>(this IEnumerable<T> enumerableInput)
+        {
+            if (enumerableInput is null)
+            {
+                throw new ArgumentException(nameof(enumerableInput), $"{enumerableInput} can not bet null or empty");
+            }
+
+            Console.WriteLine(enumerableInput.ToStringFormated());
+        }
+    }
+}
