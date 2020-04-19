bffca114c0418ac743b90e3e5ef0d829cd986e8e
draksa <draksa.zg@hotmail.com>
2020-04-19 15:56:53 +0200

    Fixed order

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,19 +6,19 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+         Console.Write("Please input first name: ");
+         var name = Console.ReadLine();
+         Console.Write("Please input last name: ");
+         var surname = Console.ReadLine();
+         Console.Write("Please input age: ");
+         var age = Console.ReadLine();
+         Console.Write("Please input weight: ");
+         var weight = Console.ReadLine();
+         Console.Write("Please input height: ");
+         var height = Console.ReadLine();
+         
 
-            var name = Console.ReadLine();
-            Console.Write("Please input first name: ");
-            var surname = Console.ReadLine();
-            Console.Write("Please input last name: ");
-            var age = Console.ReadLine();
-            Console.Write("Please input age: ");
-            var weight = Console.ReadLine();
-            Console.Write("Please input weight: ");
-            var height = Console.ReadLine();
-            Console.Write("Please input height: ");
-
-            Console.WriteLine(name +" "+ surname + " is "+ age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+         Console.WriteLine(name +" "+ surname + " is "+ age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
 
         }
     }
