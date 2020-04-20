1588ed335f7a3800e2a05d5f6348c5c2e23d61e3
Max <hellhunter15@hotmail.com>
2020-04-20 02:05:10 +0200

    changed a comment.

--- Src/BootCamp.Chapter/ContactsCenter.cs
+++ Src/BootCamp.Chapter/ContactsCenter.cs
@@ -46,7 +46,7 @@ namespace BootCamp.Chapter
         /// <returns></returns>
         public List<Person> Filter(Predicate<Person> predicate)
         {
-            // MY COMMENT  predicate doesnt do anything untill called, just like delegate.
+            // predicate doesnt do anything untill called, just like delegate.
             // Assing what it must do first, for example with a lambda.
             //predicate = p => p.Age > 18;
             var people = new List<Person>();
