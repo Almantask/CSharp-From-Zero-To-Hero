13dd5c8824fc382f723b5932fff26018415aeecb
Mihail Fox <mihai.fologea@gmail.com>
2020-04-20 04:09:49 +0300

    remove test code.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -4,16 +4,10 @@ using BootCamp.Chapter.Csv;
 
 namespace BootCamp.Chapter
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            var testWriter = new CsvWriter("TestData/write_test.csv", CsvDelimiter.Comma, true);
-            var testReader = new CsvReader("Input/Transactions.csv", CsvDelimiter.Comma, true);
-
-            var readerRows = testReader.ReadAllRows();
-
-            testWriter.WriteRows(readerRows.Skip(100), testReader.Header);
         }
     }
 }
