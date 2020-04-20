a30a59f9347a7495d6e010b41ac7a4516202972a
Max <hellhunter15@hotmail.com>
2020-04-20 04:42:24 +0200

    made TODO list

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,16 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-
+            /*
+            //TODO. Create an extension method to shuffle elements inside a collection.
+            //TODO Make your own LINQ method! Create a method SnapFingers which works like LINQ methods, takes a predicate and removes exactly half of all the elements in the collection. If it's not even number, it should remove 1 less.
+            For example:
+            if it's 2, it removes 1;
+            if it's 4, it removes 2;
+            but if it's 3, it removes 1.
+            //TODO Create any collection of any elements you want and do a demo for LINQ:
+            Any Count Order Sets Union Intersection Subtraction
+            */
         }
     }
 }
