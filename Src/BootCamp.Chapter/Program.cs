272419248ae794673acabd264314751e117b0d5a
Mihai Fologea <mihai.fologea@gmail.com>
2020-04-20 16:25:42 +0300

    csv test code.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using BootCamp.Chapter.Csv;
 
 namespace BootCamp.Chapter
@@ -8,6 +9,19 @@ namespace BootCamp.Chapter
     {
         public static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var testReader = new CsvReader("Input/Transactions.csv", CsvDelimiter.Comma, true);
+            var testWriter = new CsvWriter("test_write.csv", CsvDelimiter.Pipe, true);
+
+            var rows = testReader.ReadAllRows();
+
+            //foreach (var row in rows)
+            //{
+            //    row.Print(CsvDelimiter.Pipe);
+            //}
+
+            testWriter.WriteRows(rows.Skip(500));
         }
     }
 }
