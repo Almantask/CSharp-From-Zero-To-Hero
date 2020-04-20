9e5268419947c99b732ab0085268f7ae98d55f53
draksa <draksa.zg@hotmail.com>
2020-04-20 02:18:23 +0200

    Reduced number of lines at the end.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -20,14 +20,14 @@ namespace BootCamp.Chapter
             Console.Write("Please input height in cm: ");
             float heightCm1 = float.Parse(Console.ReadLine());
 
-            //Result output
+            //First person result output
             Console.Write(name1 + " " + surname1 + " is " + age1 + " years old," );
             Console.WriteLine(" his weight is " + weightKg1 + " kg and his height is " + heightCm1 + " cm.");
 
-            //Convert cm to m for BMI
+            //First person convert cm to m for BMI
             float heightM1 = heightCm1/100;
 
-            //BMI (body-mass index) calculator
+            //First person BMI (body-mass index) calculator
             float bmi1 = weightKg1 / (heightM1 * heightM1);
             Console.WriteLine(name1 + "'s body-mass index (BMI) is " + bmi1);
 
@@ -43,20 +43,17 @@ namespace BootCamp.Chapter
             Console.Write("Please input height in cm: ");
             float heightCm2 = float.Parse(Console.ReadLine());
 
-            //Result output
+            //Second person result output
             Console.Write(name2 + " " + surname2 + " is " + age2 + " years old,");
             Console.WriteLine(" his weight is " + weightKg2 + " kg and his height is " + heightCm2 + " cm.");
 
-            //Convert cm to m for BMI
+            //Second person convert cm to m for BMI
             float heightM2 = heightCm2 / 100;
 
-            //BMI (body-mass index) calculator
+            //Second person BMI (body-mass index) calculator
             float bmi2 = weightKg2 / (heightM2 * heightM2);
             Console.WriteLine(name2 + "'s body-mass index (BMI) is " + bmi2);
-
-
-
-
+            
         }
     }
 }
