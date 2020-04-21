2fbb6620681c2916eb8454709694c82fe95f54c4
Jere <jeretreaper@gmail.com>
2020-04-21 00:43:12 -0400

    updated sort with code.  cleaned up comments & unneeded captialization.

--- Src/BootCamp.Chapter1/ArrayOperations.cs
+++ Src/BootCamp.Chapter1/ArrayOperations.cs
@@ -2,50 +2,40 @@
 {
     public static class ArrayOperations
     {
-        /// <summary>
-        /// Sort the array in ascending order.
-        /// If array empty or null- don't do anything.
-        /// </summary>
-        /// <param name="array">Input array in a random order.</param>
         public static void Sort(int[] array)
         {
             if (array == null || array.Length == 0)
             {
                 return;
             }
-            int[] result = new int[array.Length];
+            int result = 0;
             for (int i = 0; i < array.Length - 1; i++)
             {
-                result[i] = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        result = array[i];
+                        array[i] = array[j];
+                        array[j] = result;
+                    }
+                }
             }
-            //return result;
         }
-
-        /// <summary>
-        /// Reverse the array elements, first being last and so on.
-        /// If array empty or null- don't do anything.
-        /// </summary>
-        /// <param name="array">Input array in a random order.</param>
         public static void Reverse(int[] array)
         {
             if (array == null || array.Length == 0)
             {
                 return;
             }
-            int RevArray = array.Length;
-            for (int i = 0; i < RevArray / 2; i++)
+            int revArray = array.Length;
+            for (int i = 0; i < revArray / 2; i++)
             {
                 int temp = array[i];
-                array[i] = array[RevArray - i - 1];
-                array[RevArray - i - 1] = temp;
+                array[i] = array[revArray - i - 1];
+                array[revArray - i - 1] = temp;
             }
         }
-
-        /// <summary>
-        /// Remove last element in array.
-        /// </summary>
-        /// <param name="array">Input array.</param>
-        /// <returns>A new array with the last element removed. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveLast(int[] array)
         {
             if (array == null || array.Length == 0)
@@ -54,11 +44,6 @@
             }
             return RemoveAt(array, array.Length -1 );
         }
-
-        /// <summary>
-        /// Remove first element in array.
-        /// </summary>
-        /// <returns>A new array with the first element removed. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveFirst(int[] array)
         {
             if (array == null || array.Length == 0)
@@ -67,13 +52,6 @@
             }
             return RemoveAt(array, 0);
         }
-
-        /// <summary>
-        /// Removes array element at given index.
-        /// </summary>
-        /// <param name="array">Input array.</param>
-        /// <param name="index">Index at which array element should be removed.</param>
-        /// <returns>A new array with element removed at a given index. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveAt(int[] array, int index)
         {
             if (array == null || array.Length == 0)
@@ -84,7 +62,7 @@
             {
                 return array;
             }
-            var Array2 = new int[array.Length - 1];
+            var array2 = new int[array.Length - 1];
             int j = 0;
             for (int i = 0; i < array.Length - 1; i++)
             {
@@ -92,18 +70,11 @@
                 {
                     j++;
                 }
-                Array2[i] = array[j];
+                array2[i] = array[j];
                 j++;
             }
-            return Array2;
+            return array2;
         }
-
-        /// <summary>
-        /// Inserts a new array element at the start.
-        /// </summary>
-        /// <param name="array">Input array.</param>
-        /// <param name="number">Number to be added.</param>
-        /// <returns>A new array with element added at a given index. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertFirst(int[] array, int number)
         {
             if (array == null)
@@ -115,13 +86,6 @@
             int[] First2 = InsertAt(array, number, 0);
             return First2;
         }
-
-        /// <summary>
-        /// Inserts a new array element at the end.
-        /// </summary>
-        /// <param name="array">Input array.</param>
-        /// <param name="number">Number to be added.</param>
-        /// <returns>A new array with element added in the end of array. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertLast(int[] array, int number)
         {
             if (array == null)
@@ -133,14 +97,6 @@
             int[] Last2 = InsertAt(array, number, array.Length);
             return Last2;
         }
-
-        /// <summary>
-        /// Inserts a new array element at the specified index.
-        /// </summary>
-        /// <param name="array">Input array.</param>
-        /// <param name="number">Number to be added.</param>
-        /// <param name="index">Index at which array element should be added.</param>
-        /// <returns>A new array with element inserted at a given index. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertAt(int[] array, int number, int index)
         {
             if (array == null)
