c1ebd1806c82115d8ea160a2788881f7699fd3bf
Michel Potter <michelwesley_lima@hotmail.com>
2020-04-21 17:27:06 -0300

    change variables declaration position

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,33 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name;
-            string age;
-            string weight;
-            float Weight;
-            string height;
-            float Height;
-            float bmi;
-
             Console.WriteLine("Body-Mass Index (BMI) Calculator");
             Console.WriteLine("");
 
             // Person 01
 
             Console.WriteLine("What's your name?");
-            name = Console.ReadLine();
+            string name = Console.ReadLine();
             Console.WriteLine("How old are you?");
-            age = Console.ReadLine();
+            string age = Console.ReadLine();
             Console.WriteLine("What's your weight? (in kg)");
-            weight = Console.ReadLine();
-            Weight = float.Parse(weight);
+            string weight = Console.ReadLine();
+            float Weight = float.Parse(weight);
             Console.WriteLine("What's your height? (in cm)");
-            height = Console.ReadLine();
-            Height = float.Parse(height);
+            string height = Console.ReadLine();
+            float Height = float.Parse(height);
 
             Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg ans his height is {height} cm.");
 
-            bmi = Weight / (Height * Height / 10000);
+            float bmi = Weight / (Height * Height / 10000);
             Console.WriteLine($"Your BMI is {bmi}");
             Console.WriteLine("");
 
