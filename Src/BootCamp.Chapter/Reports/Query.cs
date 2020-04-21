f3bbd2c2a6551d9102965c3b4b04ff6b17643d2a
Mihai Fologea <mihai.fologea@gmail.com>
2020-04-21 23:05:27 +0300

    implement query by shop name.

---
+++ Src/BootCamp.Chapter/Reports/Query.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BootCamp.Chapter.Csv;
+
+namespace BootCamp.Chapter
+{
+    public static class Query
+    {
+        public static void Shop(List<Transaction> transactions, string name)
+        {
+            var shopFileName = name + ".csv";
+            var shopHeader = new CsvRow { "City", "Street", "Item", "DateTime", "Price" };
+
+            var shops = from transaction in transactions
+                        where transaction.Shop.Name == name
+                        select new CsvRow
+                        {   transaction.Shop.Address.City,
+                            transaction.Shop.Address.Street,
+                            transaction.Item.Name,
+                            transaction.DateTime.ToString(Culture.Output.DateTimeFormat.FullDateTimePattern),
+                            transaction.Item.Price.ToString("C",Culture.Output.NumberFormat).AddQuotes()
+                        };
+
+            var shopWriter = new CsvWriter(shopFileName, CsvDelimiter.Comma, true);
+
+            shopWriter.WriteAllRows(shops, shopHeader);
+        }
+    }
+}
