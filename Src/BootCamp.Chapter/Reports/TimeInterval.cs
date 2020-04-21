1b27a5a06e7417862ae42616cdd03babf42b2128
Mihai Fologea <mihai.fologea@gmail.com>
2020-04-22 02:59:21 +0300

    change members type to 'TimeSpan'; add compare and equality methods.

--- Src/BootCamp.Chapter/Reports/TimeInterval.cs
+++ Src/BootCamp.Chapter/Reports/TimeInterval.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace BootCamp.Chapter
 {
-    public class TimeInterval
+    public class TimeInterval : IEquatable<TimeInterval>
     {
-        private readonly DateTime Start;
-        private readonly DateTime End;
+        public TimeSpan Start { get; }
+        public TimeSpan End { get; }
 
-        public TimeInterval(DateTime start, DateTime end)
+        public TimeInterval(TimeSpan start, TimeSpan end)
         {
             Start = start;
             End = end;
@@ -23,9 +23,36 @@ namespace BootCamp.Chapter
             }
         }
 
-        public bool Contains(DateTime moment)
+        public bool Contains(TimeSpan moment)
         {
             return moment >= Start && moment <= End;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TimeInterval);
+        }
+
+        public bool Equals(TimeInterval other)
+        {
+            return other != null &&
+                   Start.Equals(other.Start) &&
+                   End.Equals(other.End);
+        }
+
+        public static bool operator ==(TimeInterval left, TimeInterval right)
+        {
+            return EqualityComparer<TimeInterval>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(TimeInterval left, TimeInterval right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Start, End);
         }
     }
 }
