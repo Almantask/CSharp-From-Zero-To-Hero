a9f23b2fe5adc1e37362ad3d8470d926770cc08b
Mihai Fologea <mihai.fologea@gmail.com>
2020-04-21 23:43:15 +0300

    implement 'TimeInterval' class.

---
+++ Src/BootCamp.Chapter/Reports/TimeInterval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class TimeInterval
+    {
+        private readonly DateTime Start;
+        private readonly DateTime End;
+
+        public TimeInterval(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public TimeSpan TotalTime
+        {
+            get
+            {
+                return End - Start;
+            }
+        }
+
+        public bool Contains(DateTime moment)
+        {
+            return moment >= Start && moment <= End;
+        }
+    }
+}
