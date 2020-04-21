73a0fcc9745aabf99422a7b5577b2dba289293ca
joshuajz <35657686+joshuajz@users.noreply.github.com>
2020-04-21 13:52:34 -0400

    Homework 3

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -17,25 +17,25 @@ namespace BootCamp.Chapter
         public static int PromptInt(string message)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.IntInput(message);
         }
 
         public static string PromptString(string message)
         {
             // To do: call your implementation. 
-            return "";
+            return Lesson3.StringInput(message);
         }
 
         public static float PromptFloat(string message)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.FloatInput(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.BMICalculation(weight, height);
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            Console.WriteLine("Temp, not sure what to do here yet");
+        }
+        
+        // Takes the weight and height and returns BMI
+        public static float BMICalculation(float weight, float height)
+        {
+            return weight / (height * height);
+        }
+
+        // Prompts the user for an input, converts it to an integer and returns it
+        public static int IntInput(string message)
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            return int.Parse(input);
+        }
+
+        // Prompts the user for a string and returns it (default is a string)
+        public static string StringInput(string message)
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            return input;
+        }
+
+        // Prompts the user for an input, converts it and returns a float
+        public static float FloatInput(string message)
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            return float.Parse(input);
+        }
+    }
+}
