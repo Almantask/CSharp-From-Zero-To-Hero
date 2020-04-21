85e939b5b57ba18fc31a22e3d4f4efe0bff73780
Michel Potter <michelwesley_lima@hotmail.com>
2020-04-21 09:42:32 -0300

    adapt code for another person

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,27 +6,52 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            float Weight = 0;
-            float Height = 0;
+            string name;
+            string age;
+            string weight;
+            float Weight;
+            string height;
+            float Height;
+            float bmi;
 
             Console.WriteLine("Body-Mass Index (BMI) Calculator");
-            
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            string age = Console.ReadLine();
-            Console.WriteLine("What's your weight? (in kg)");
-            string weight = Console.ReadLine();
-            Console.WriteLine("What's your height? (in cm)");
-            string height = Console.ReadLine();
+            Console.WriteLine("");
 
+            // Person 01
+
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+            Console.WriteLine("What's your weight? (in kg)");
+            weight = Console.ReadLine();
             Weight = float.Parse(weight);
+            Console.WriteLine("What's your height? (in cm)");
+            height = Console.ReadLine();
             Height = float.Parse(height);
 
             Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg ans his height is {height} cm.");
 
-            float bmi = Weight / (Height * Height / 10000);
+            bmi = Weight / (Height * Height / 10000);
+            Console.WriteLine($"Your BMI is {bmi}");
+            Console.WriteLine("");
 
+            // Person 02
+
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+            Console.WriteLine("What's your weight? (in kg)");
+            weight = Console.ReadLine();
+            Weight = float.Parse(weight);
+            Console.WriteLine("What's your height? (in cm)");
+            height = Console.ReadLine();
+            Height = float.Parse(height);
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg ans his height is {height} cm.");
+
+            bmi = Weight / (Height * Height / 10000);
             Console.WriteLine($"Your BMI is {bmi}");
         }
     }
