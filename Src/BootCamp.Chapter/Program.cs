a573a18a38501e76b3d4a84ac5fdfd9b93306ab6
Michel Potter <michelwesley_lima@hotmail.com>
2020-04-21 09:13:36 -0300

    finish homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,28 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float Weight = 0;
+            float Height = 0;
+
+            Console.WriteLine("Body-Mass Index (BMI) Calculator");
+            
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What's your weight? (in kg)");
+            string weight = Console.ReadLine();
+            Console.WriteLine("What's your height? (in cm)");
+            string height = Console.ReadLine();
+
+            Weight = float.Parse(weight);
+            Height = float.Parse(height);
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg ans his height is {height} cm.");
+
+            float bmi = Weight / (Height * Height / 10000);
+
+            Console.WriteLine($"Your BMI is {bmi}");
         }
     }
 }
