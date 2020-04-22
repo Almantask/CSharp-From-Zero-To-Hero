031d742ac3b4fc7678f5a054547aa3291fa5358c
John Lumtao(Sea Raider) <arseniolumtao1324@gmail.com>
2020-04-22 14:08:33 +0800

    Put the prompts for information into a function and called it twice in the Demo

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -14,19 +14,10 @@ namespace BootCamp.Chapter
     /// </summary>
     public static class Checks
     {
-        
         public static int PromptInt(string message)
         {
-            Console.WriteLine(message);
-            var input = Console.ReadLine();
-            bool isValidAge = string.IsNullOrEmpty(input);
-            if (isValidAge) return 0;
-            isValidAge = int.TryParse(input, out int age);
-            if (!isValidAge || age < 0)
-            {
-            Console.Write($"{(char)34}{input}{(char)34} is not a valid number.");
-            return -1;
-            }
+            Console.Write(message);
+            int age = int.Parse(Console.ReadLine());
             return age;
         }
 
@@ -34,44 +25,19 @@ namespace BootCamp.Chapter
         {
             Console.Write(message);
             string promptname = Console.ReadLine();
-            bool isValidName = string.IsNullOrEmpty(promptname);
-            if (isValidName)
-            {
-                Console.Write("Name cannot be empty.");
-                return "-";
-            }
             return promptname;
         }
 
         public static float PromptFloat(string message)
         {
             Console.Write(message);
-            var input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input)) return 0;
-            var isFloat = int.TryParse(input, out var promptInfo);
-            if (!isFloat) Console.WriteLine($"{input} is not a valid number.");
-            return promptInfo;
+            float promptinfo = float.Parse(Console.ReadLine());
+            return promptinfo;
         }
 
-        public static float CalculateBmi(float weightkg, float heightcm)
+        public static float CalculateBmi(float weightkg, float heightm)
         {
-            if (weightkg <= 0 && heightcm <= 0)
-            {
-                Console.WriteLine("Failed calculating BMI. Reason:" + Environment.NewLine + $"Weight cannot be equal or less than zero, but was {weightkg}.");
-                Console.WriteLine($"Height cannot be less than zero, but was {heightcm}.");
-            }
-            else if (heightcm > 0 && weightkg <= 0)
-            {
-                Console.WriteLine("Failed calculating BMI. Reason:" + Environment.NewLine + $"Weight cannot be equal or less than zero, but was {weightkg}.");
-            }
-            else if (weightkg > 0 && heightcm <= 0)
-            {
-                Console.WriteLine("Failed calculating BMI. Reason:" + Environment.NewLine + $"Height cannot be equal or less than zero, but was {heightcm}.");
-            }
-
-            float heightm = heightcm / 100.0f;
             float bmi = weightkg / (heightm * heightm);
-
             return bmi;
         }
     }
--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -8,16 +8,8 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            string name = PromptString("Enter your Name: ");
-            string surname = PromptString("Enter your Surname: ");
-            int age = PromptInt("Enter your age: ");
-            float weightkg = PromptFloat("Enter your weight(kg): ");
-            float heightcm = PromptFloat("Enter your height(cm): ");
-            float heightm = heightcm / 100;
-            float bmi = CalculateBmi(weightkg, heightm);
-            //Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
-            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weightkg + " kg and his height is " + heightcm + " cm. His BMI is " + bmi);
-            Console.WriteLine();
+            ProcessPersonBMI();
+            ProcessPersonBMI();
         }
 
          public static int PromptInt(string message)
@@ -47,5 +39,18 @@ namespace BootCamp.Chapter
             return bmi;
         }
 
+        public static void ProcessPersonBMI()
+        {
+            string name = PromptString("Enter your Name: ");
+            string surname = PromptString("Enter your Surname: ");
+            int age = PromptInt("Enter your age: ");
+            float weightkg = PromptFloat("Enter your weight(kg): ");
+            float heightcm = PromptFloat("Enter your height(cm): ");
+            float heightm = heightcm / 100;
+            float bmi = CalculateBmi(weightkg, heightm);
+            //Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weightkg + " kg and his height is " + heightcm + " cm. His BMI is " + bmi);
+            Console.WriteLine();
+        }
     }
 }
