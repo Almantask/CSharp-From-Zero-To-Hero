6903e59e3341a66a797ba26fa505e6bc0296211d
dave94punk <david.katona@hotmail.com>
2020-04-22 21:38:42 +0200

    Added FileUtilities.cs to contain helper functions when working with balance files such as file validation,  balance checking, and person validations.

---
+++ Src/BootCamp.Chapter/FileUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class FileUtilities
+    {
+        //This class will implement functions for file validation, balance analysis, etc.
+        //ToDo: Implement...
+    }
+}
