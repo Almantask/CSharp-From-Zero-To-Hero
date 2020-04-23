9da08cdaddc56cf68bb0a1a24ae2ab297b89498e
lukas-ostrovskis <lukas.ostrovskis1@gmail.com>
2020-04-23 09:46:30 +0300

    Homework 1&2: Calculating BMI

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,38 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            string surname;
+            int age;
+            double weight;
+            double heightCM, heightM;
+
+            double BMI;
+
+            for(int i = 0; i < 2; i++)
+            {
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Surname: ");
+                surname = Console.ReadLine();
+
+                Console.Write("Age: ");
+                age = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Weight(kg): ");
+                weight = Double.Parse(Console.ReadLine());
+
+                Console.Write("Height(cm): ");
+                heightCM = Double.Parse(Console.ReadLine());
+                heightM = heightCM / 100.0;
+
+                Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + heightCM + " cm.");
+
+                BMI = weight / (heightM * heightM);
+                Console.WriteLine("His Body-Mass Index(BMI) is " + BMI);
+            }
+
         }
     }
 }
