fe8bdf15533f8918fb4d5d0f0f27c238c7b026e9
lukas-ostrovskis <lukas.ostrovskis1@gmail.com>
2020-04-23 09:35:38 +0300

    Revert "Second "Hello World""
    This reverts commit b8cf2f05fb122ba00f9dbf13a207955547854ce3.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,7 +7,6 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
