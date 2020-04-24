573488091b80e9a54539feec2cdb889d1f160f31
dave94punk <david.katona@hotmail.com>
2020-04-24 17:17:47 +0200

    Implemented MakeContentParsable() to return an array of strings that can be used to parse balances.

--- Src/BootCamp.Chapter/FileUtilities.cs
+++ Src/BootCamp.Chapter/FileUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -8,5 +9,23 @@ namespace BootCamp.Chapter
     {
         //This class will implement functions for file validation, balance analysis, etc.
         //ToDo: Implement...
+        public static string[] MakeContentParsable(string inputFilePath, string stringToReplace)
+        {
+            if (File.Exists(inputFilePath))
+            {
+                var inputLines = File.ReadAllLines(inputFilePath);
+                var parsableLines = new string[inputLines.Length];
+
+                for (int i = 0; i < inputLines.Length; i++)
+                {
+                    var parsableLine = inputLines[i].Replace(stringToReplace, "");
+                    parsableLines[i] = parsableLine;
+                }
+
+                return parsableLines;
+            }
+
+            return new string[] { "" };
+        }
     }
 }
