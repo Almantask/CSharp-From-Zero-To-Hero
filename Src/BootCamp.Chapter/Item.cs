06fe66b19288bc3dc51f1c21c9577c8c5d174402
Max <hellhunter15@hotmail.com>
2020-04-25 05:28:05 +0200

    Made Item Class for LINQ demo.

---
+++ Src/BootCamp.Chapter/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Item
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public double Weight { get; }
+
+        public Item(string name, decimal price, double weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+    }
+}
