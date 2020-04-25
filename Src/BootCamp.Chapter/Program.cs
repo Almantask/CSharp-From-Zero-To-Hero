5d991eafc8f84c2a368601d456b9e6ee573b2c33
Hannah Myers <hannahmyers16@hotmail.co.uk>
2020-04-25 13:22:51 +0100

    Placed my solution into the program file.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int age;
+            double weight;
+            double height;
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                Console.WriteLine("Name: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Age: ");
+                age = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Weight in kg: ");
+                weight = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Height in cm: ");
+                height = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.");
+
+                double heightInMetres = height / 100.0;
+
+                double bmi = weight / (heightInMetres * heightInMetres);
+                Console.WriteLine(name + "'s BMI is " + bmi);
+            }
         }
     }
 }
