fdb80b0381754eb92da5daaae426a714cb4ceba1
Max <hellhunter15@hotmail.com>
2020-04-26 03:07:13 +0200

    Formatted Program better.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,9 +6,11 @@ namespace BootCamp.Chapter
     {
         public static void Main()
         {
-            /* REMOVE COMMENT LINE TO START ANY OF THE DEMO's!
-             */
+            
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // REMOVE COMMENT // TO START ANY OF THE DEMO's!
+
             //Tests.ShuffleTest(10);
             //Tests.SnapFingersTest();
             //Demos.AnyDemo();
