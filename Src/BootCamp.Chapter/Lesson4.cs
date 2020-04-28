76c41f487ada01ef3e2eaf550fd602a1c7b618c1
Michel Potter <michelwesley_lima@hotmail.com>
2020-04-28 20:53:30 -0300

    add validation to user input

--- Src/BootCamp.Chapter/Lesson4.cs
+++ Src/BootCamp.Chapter/Lesson4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace BootCamp.Chapter
 {
@@ -18,32 +19,61 @@ namespace BootCamp.Chapter
 
         private static void ProcessPersonBMI()
         {
-            string name = PromptString("What's your name?");
-            int age = PromptInt("How old are you?");
-            float height = PromptFloat("What's your height? (in m)");
-            float weight = PromptFloat("What's your weight? (in kg)");
+            string name = PromptString("Name:");
+            int age = PromptInt("Age:");
+            float height = PromptFloat("Height (in m):");
+            float weight = PromptFloat("Weight (in kg):");
 
-            Console.WriteLine($"{name} is {age} years old, his weight is {weight}kg and his height is {height}m. ");
+            Console.WriteLine($"{name} is {age} years old, has {weight}kg and {height}m. ");
             Console.WriteLine($"His BMI is {CalculateBMI(weight, height)}");
             Console.WriteLine("");
         }
 
         public static string PromptString(string message)
         {
-            Console.WriteLine(message);
-            return Console.ReadLine();
+            Console.Write(message + " ");
+            string answer = Console.ReadLine();
+
+            if (answer == "")
+            {
+                Console.WriteLine($"Name cannot be empty. \n");
+                return PromptString(message);
+            }
+
+            Console.Write("\n");
+            return answer;   
         }
 
         public static int PromptInt(string message)
         {
-            Console.WriteLine(message);
-            return Int32.Parse(Console.ReadLine());
+            Console.Write(message + " ");
+            int answer = 0;
+            bool isLetter = !Int32.TryParse(Console.ReadLine(), out answer);
+
+            if (isLetter || answer <= 0)
+            {
+                Console.WriteLine($"Answer must be a number greater than 0. \n");
+                return PromptInt(message);
+            } 
+           
+            Console.Write("\n");
+            return answer;
         }
 
         public static float PromptFloat(string message)
         {
-            Console.WriteLine(message);
-            return float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write(message + " ");
+            string answer = Console.ReadLine().Replace(",", ".");
+            Regex floatRule = new Regex(@"^(?:\d{1,2})?(?:\.\d{1,2})?$");
+            
+            if (!floatRule.IsMatch(answer) || answer == "0")
+            {
+                Console.WriteLine("Answer must be a number greater than 0. \n");
+                return PromptFloat(message);
+            }
+
+            Console.Write("\n");
+            return float.Parse(answer, CultureInfo.InvariantCulture);
         }
 
         public static float CalculateBMI(float weight, float height)
