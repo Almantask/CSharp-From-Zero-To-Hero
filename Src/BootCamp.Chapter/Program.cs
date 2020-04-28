4639483f190feca20069cfe3c0722bee17723173
Trollermaner <li.william.mail@gmail.com>
2020-04-27 23:12:46 -0400

    finished almost everything, still need to make a function to make errors for floats

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,83 +9,124 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.Write("Age: ");
-            var input = Console.ReadLine();
-            var variable = PromptInt(input);
+            var name = PrintString();
+            Console.WriteLine(name);
 
-            if (variable == -1) Console.WriteLine(input + " is not a number");
-            else if (variable == 0) Console.WriteLine("Age cannot be empty");
+            var age = PrintInt("Age: ", "Age");
+            Console.WriteLine(age);
+
+            float weight = PrintInt("Weight (kg): ", "Weight");
+            Console.WriteLine(weight);
+
+            float height = PrintInt("Height (m): ", "Height");
+            Console.WriteLine(height);
+           
+            var bmi = CalculateBmi(name, age, weight, height);
+        }
+        //Error messages for strings
+        static string ErrorString(string input)
+        {
+            var str = PromptString(input);
+            var error = "Name cannot be empty";
+
+            if (str == "-") return error;
+
             else
             {
-                Console.WriteLine(variable);
+                return null;
             }
         }
-
-        static float CalculateBmi(float weight,float height)
-        {   
-            return weight / height / height;
-            
-
-        }
-        public static void Print(int number)
-        {
-            Console.WriteLine("Person #" + number + ":");
-
-            //input
-            var name = PromptString("Full Name: ");
-
-            var age = PromptInt("Age: ");
-
-            var weight = PromptFloat("Weight (kg): ");
-
-            var height = PromptFloat("Height (m): ");
-
-
-            //output
-            Console.WriteLine(" ");
-            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg, his height is " + height + " and his bmi is " + CalculateBmi(weight,height) + ".");
-            Console.WriteLine(" ");
-        }
-
-
+        //Returns "-" if the input is empty
         public static string PromptString(string input)
         {
             if (string.IsNullOrEmpty(input))
             {
                 return "-";
             }
-            else return null;
+            else return input;
         }
-
-        static void PrintString(string message)
+        //returns error message if there is an error and returns null if there is no error
+        static string PrintString()
         {
-            Console.Write(message);
+            Console.Write("Full Name: ");
             var input = Console.ReadLine();
-            var name = PromptString(input);
-
-            if (name == "-") Console.WriteLine("");
-        }
-
-        public static float PromptFloat(string message)
-        {
-            Console.Write(message);
-            var input = Console.ReadLine();
-            return float.Parse(input);
-
-        }
-        static void PrintInt(string message, string error)
-        {
-            Console.Write(message);
-            var input = Console.ReadLine();
-            var variable = PromptInt(input);
-
-            if (variable == -1) Console.WriteLine(input + " is not a number");
-            else if (variable == 0) Console.WriteLine(error + " cannot be empty");
+            var error = ErrorString(input);
+            
+            if (error == null) return null;
             else
             {
-                Console.WriteLine(variable);
+                return error;
             }
         }
+        //Returns error message if there is an error, if there isn't it returns null
+        static string PrintInt(string message, string errorName)
+        {
+            Console.Write(message);
+            var input = Console.ReadLine();
+            var error = ErrorInt(errorName, input);
+            if (error == null) return null;
+            else
+            {
+                return error;
+            }
+        }
+        //error messages for int
+        static string ErrorInt(string error, string input)
+        {
+            var variable = PromptInt(input);
+            var error1 = input + " is not a number";
+            var error2 = error + " cannot be empty";
+
+            if (variable == -1) return error1;
+            else if (variable == 0) return error2;
+            else
+            {
+                return null;
+            }
+        }
+        //checks if any of the variables needed to calculate bmi is less than 0
+        static float BmiError(float weight, float height)
+        {
+            if (weight <= 0) return -0;
+            if (height <= 0) return -1;
+            if (height >= 0) return 0;
+            else if (weight >= 0) return 0;
+            else
+            {
+                return -2;
+            }
+        }
+
+        //returns error messages for height and weight
+        static string BmiErroMessages(float weight, float height)
+        {
+            var errorMsg = "Failed calculating BMI. Reason: ";
+            var error = BmiError(weight, height);
+            var errorMsg1 = errorMsg + "Weight cannot be equal or less than zero, but was " + weight;
+            var errorMsg2 = errorMsg + "Height cannot be equal or less than zero, but was " + height;
+
+            if (error == -0) return errorMsg1;
+            else if (error == -1) return errorMsg2;
+            else if (error == -2) return errorMsg1 + "\n" + errorMsg2;
+            else
+            {
+                return null;
+            }
+        }
+        static string CalculateBmi(string name, string age, float weight, float height)
+        {
+            var bmi = weight / height / height;
+            var error = BmiErroMessages(weight, height);
+            var printBmi = name + " is " + age + " years old, his weight is " + weight + " kg, his height is " + height + " and his bmi is " + bmi;
+
+            if (error == null) return printBmi ;
+            else
+            {
+                return error;
+            }
+        }
+        
+        //returns -1 if input is not a number, returns 0 if it is empty and returns output if there isn't a problem
         public static int PromptInt(string input)
         {
            
