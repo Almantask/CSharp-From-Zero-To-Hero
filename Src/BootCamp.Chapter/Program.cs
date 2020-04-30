686690a71041a3a48611a78f422094c3c6dc9a6e
Miguel Gomez <ozlarus@outlook.com>
2020-04-30 14:50:41 +0800

    Well, i just modified it to be more efficient so i used the same variables for the two persons and made a loop on it. It takes the first data input and prints it, then it clears the values and starts to the second person. After the second person the looper value will be 3 so the program ends.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string name = "Tom Jefferson";
+            int age;
+            int looper = 1;
+            float weight, mheight;
+
+            do
+            {
+                Console.WriteLine("Enter Name");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Enter Age");
+                age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Weight");
+                weight = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Height");
+                mheight = float.Parse(Console.ReadLine());
+
+                float BMI1 = weight / (mheight * mheight);
+
+                Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + "kg and his height is " + mheight + " cm.");
+                Console.WriteLine(name + "'s BMI is " + BMI1);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                looper++;
+            } while (looper < 3);
         }
     }
 }
