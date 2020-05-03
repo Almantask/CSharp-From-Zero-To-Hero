3500eb4c4c2b638c999825afec6fe98f0b1c1880
Mihail Fox <mihai.fologea@gmail.com>
2020-05-03 22:53:46 +0300

    add method for enumerating an object properties.

---
+++ Src/BootCamp.Chapter/Csv/CsvUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BootCamp.Chapter.Csv
+{
+    public static class CsvUtil
+    {
+        public static List<string> GetPropertiesName(object inputObject)
+        {
+            List<string> propertyList = new List<string>();
+
+            if (inputObject != null)
+            {
+                propertyList.AddRange(from property in inputObject.GetType().GetProperties()
+                                      select property.Name.ToCamelCase());
+            }
+
+            return propertyList;
+        }
+    }
+}
