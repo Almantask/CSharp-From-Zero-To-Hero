97761bb32e399a524d5f58773b9ec25dfbf6bf2d
Arthur <arthur.schevey@gmail.com>
2020-05-03 13:46:10 -0400

    Added comments and a bit more organization

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            // Obtaining information
             Console.Write("First Name: ");
             string person1Name = Console.ReadLine();
 
@@ -21,10 +22,14 @@ namespace BootCamp.Chapter
             Console.Write("Height: ");
             float person1Height = float.Parse(Console.ReadLine());
 
+            // Repeat information
             Console.WriteLine("\n" + person1Name + " " + person1Surname + " is " + person1Age + " years old, his weight is " + person1Weight + " kg and his height is " + person1Height + " cm.");
+
+            // BMI calculation
             float BMI1 = person1Weight / ((person1Height / 100) * (person1Height / 100));
             Console.WriteLine("BMI for " + person1Name + ": " + BMI1 + "\n");
 
+            // Obtain information from second person
             Console.Write("First Name: ");
             string person2Name = Console.ReadLine();
 
@@ -40,7 +45,10 @@ namespace BootCamp.Chapter
             Console.Write("Height: ");
             float person2Height = float.Parse(Console.ReadLine());
 
+            // Repeat information from second person
             Console.WriteLine("\n" + person2Name + " " + person2Surname + " is " + person2Age + " years old, his weight is " + person2Weight + " kg and his height is " + person2Height + " cm.");
+          
+            // Calculate BMI for second person
             float BMI2 = person2Weight / ((person2Height / 100) * (person2Height / 100));
             Console.WriteLine("BMI for " + person2Name + ": " + BMI2);
         }
