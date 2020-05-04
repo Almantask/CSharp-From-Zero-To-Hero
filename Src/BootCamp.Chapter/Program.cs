ab0d42d980cbe595a0be5339d9ca480e0c898138
Arthur <arthur.schevey@gmail.com>
2020-05-04 17:39:05 -0400

    Utilized functions and cleaner code

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,55 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            BMI();
+            BMI();
+        }
+
+        private static void BMI()
+        {
             // Obtaining information
             Console.Write("First Name: ");
-            string person1Name = Console.ReadLine();
+            string Name = Console.ReadLine();
 
             Console.Write("Last Name: ");
-            string person1Surname = Console.ReadLine();
+            string Surname = Console.ReadLine();
 
             Console.Write("Age: ");
-            int person1Age = int.Parse(Console.ReadLine());
+            int Age = int.Parse(Console.ReadLine());
 
             Console.Write("Weight: ");
-            float person1Weight = float.Parse(Console.ReadLine());
+            float Weight = float.Parse(Console.ReadLine());
 
             Console.Write("Height: ");
-            float person1Height = float.Parse(Console.ReadLine());
+            float Height = float.Parse(Console.ReadLine());
 
             // Repeat information
-            Console.WriteLine("\n" + person1Name + " " + person1Surname + " is " + person1Age + " years old, his weight is " + person1Weight + " kg and his height is " + person1Height + " cm.");
+            Console.WriteLine("\n" + Name + " " + Surname + " is " + Age + " years old, his weight is " + Weight + " kg and his height is " + Height + " cm.");
 
             // BMI calculation
-            float BMI1 = person1Weight / ((person1Height / 100) * (person1Height / 100));
-            Console.WriteLine("BMI for " + person1Name + ": " + BMI1 + "\n");
-
-
-
-
-
-            // Obtain information from second person
-            Console.Write("First Name: ");
-            string person2Name = Console.ReadLine();
-
-            Console.Write("Last Name: ");
-            string person2Surname = Console.ReadLine();
-
-            Console.Write("Age: ");
-            int person2Age = int.Parse(Console.ReadLine());
-
-            Console.Write("Weight: ");
-            float person2Weight = float.Parse(Console.ReadLine());
-
-            Console.Write("Height: ");
-            float person2Height = float.Parse(Console.ReadLine());
-
-            // Repeat information from second person
-            Console.WriteLine("\n" + person2Name + " " + person2Surname + " is " + person2Age + " years old, his weight is " + person2Weight + " kg and his height is " + person2Height + " cm.");
-          
-            // Calculate BMI for second person
-            float BMI2 = person2Weight / ((person2Height / 100) * (person2Height / 100));
-            Console.WriteLine("BMI for " + person2Name + ": " + BMI2);
+            float BMI = Weight / ((Height / 100) * (Height / 100));
+            Console.WriteLine("BMI for " + Name + ": " + BMI + "\n");
         }
     }
 }
