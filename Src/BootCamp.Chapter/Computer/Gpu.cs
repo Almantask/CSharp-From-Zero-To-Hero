af3d52b8a4e0b059119fbe6f9ce9e5aa301d2070
dave94punk <david.katona@hotmail.com>
2020-05-05 16:36:23 +0200

    Implemented Gpu class.

--- Src/BootCamp.Chapter/Computer/Gpu.cs
+++ Src/BootCamp.Chapter/Computer/Gpu.cs
@@ -1,6 +1,50 @@
 ﻿namespace BootCamp.Chapter.Computer
 {
-    public class Gpu
+    public class Gpu : Component
     {
+        private string _chipsetManufacturer;
+
+        public string GetChipsetManufacturer()
+        {
+            return _chipsetManufacturer;
+        }
+
+        private string _memoryType;
+
+        public string GetMemoryType()
+        {
+            return _memoryType;
+        }
+
+        private int _memorySize;
+
+        public int GetMemorySize()
+        {
+            return _memorySize;
+        }
+
+        private int _memoryClock;
+
+        public int GetMemoryClock()
+        {
+            return _memoryClock;
+        }
+
+        private int _gpuClock;
+
+        public int GetGpuClock()
+        {
+            return _gpuClock;
+        }
+
+
+        public Gpu(string id, string manufacturer, string modelName, string chipsetManufacturer, string memoryType, int memorySize, int memoryClock, int gpuClock) : base (id, manufacturer, modelName)
+        {
+            _chipsetManufacturer = chipsetManufacturer;
+            _memoryType = memoryType;
+            _memorySize = memorySize;
+            _memoryClock = memoryClock;
+            _gpuClock = gpuClock;
+        }
     }
 }
