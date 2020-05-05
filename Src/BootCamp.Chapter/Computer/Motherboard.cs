e78e54fbe780893bb22ffd31369aaf3c331b82c7
dave94punk <david.katona@hotmail.com>
2020-05-05 17:13:43 +0200

    Implemented Motherboard class.

--- Src/BootCamp.Chapter/Computer/Motherboard.cs
+++ Src/BootCamp.Chapter/Computer/Motherboard.cs
@@ -1,6 +1,33 @@
 ﻿namespace BootCamp.Chapter.Computer
 {
-    public class Motherboard
+    public class Motherboard : Component
     {
+        private string _chipset;
+
+        public string GetChipset()
+        {
+            return _chipset;
+        }
+
+        private string _chipsetManufacturer;
+
+        public string GetChipsetManufacturer()
+        {
+            return _chipsetManufacturer;
+        }
+
+        private string _socket;
+
+        public string GetSocket()
+        {
+            return _socket;
+        }
+
+        public Motherboard(string id, string manufacturer, string modelName, string chipset, string chipsetManufacturer, string socket) : base (id, manufacturer, modelName)
+        {
+            _chipset = chipset;
+            _chipsetManufacturer = chipset;
+            _socket = socket;
+        }
     }
 }
