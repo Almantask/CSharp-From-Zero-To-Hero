b4e89bb64cf2f3a07b7222d045713d0c4fbf06c0
dave94punk <david.katona@hotmail.com>
2020-05-05 17:39:48 +0200

    Implemented Body class.

--- Src/BootCamp.Chapter/Computer/Body.cs
+++ Src/BootCamp.Chapter/Computer/Body.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace BootCamp.Chapter.Computer
 {
-    public class Body
+    public class Body : Component
     {
+        private CaseDesign _design;
+
+        public CaseDesign GetDesign()
+        {
+            return _design;
+        }
+
+        public Body(string id, string manufacturer, string modelName, CaseDesign design) : base (id, manufacturer, modelName)
+        {
+            _design = design;
+        }
+    }
+
+    public enum CaseDesign
+    {
+        ATX,
+        EATX,
+        Micro_ATX,
+        Mini_DTX,
+        Mini_ITX
     }
 }
