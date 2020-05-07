29190996607a9c32c768daed76ecd633424486d0
marlon-malheiros <30882393+marlon-malheiros@users.noreply.github.com>
2020-05-07 19:26:38 -0300

    Added my code of homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 2; i++)
+            {
+                string name;
+                string surename;
+                int age;
+                double weight;
+                double height;
+
+                Console.Write("Enter the person's first name: ");
+                name = Console.ReadLine();
+                Console.Write("Enter the person's surename: ");
+                surename = Console.ReadLine();
+                Console.Write("Enter the person's age: ");
+                age = int.Parse(Console.ReadLine());
+                Console.Write("Enter the person's weight in kg: ");
+                weight = double.Parse(Console.ReadLine());
+                Console.Write("Enter the person's height in cm: ");
+                height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(name + " " + surename + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+                Console.WriteLine(name + " " + surename + "BMI is: " + (weight) / ((height / 100) * (height / 100)));
+                Console.WriteLine();
+            }            
         }
     }
 }
