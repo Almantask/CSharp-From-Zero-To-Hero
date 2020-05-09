e4163874ee22e9145afb8ff80fdbe5413752cea6
kalek <ahmedelakehal@gmai.com>
2020-05-10 00:03:28 +0100

    WIP

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,50 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // List holding all objects
+            var allPeople = new List<Person>();
+
+            while (true)
+            {
+                // Takes in the user's choice to either enter a new set of details to calculate the BMI of or to view all current entries.
+                Console.WriteLine("Please enter 'newbmi' to calculate a new person's BMI or enter 'view' to see all current entries: ");
+                string currentChoice = Console.ReadLine();
+
+                // Checks if the user's choice is empty, if so skips the rest of the loop and tries again.
+                if (string.IsNullOrWhiteSpace(currentChoice))
+                {
+                    continue;
+                }
+
+                // Checks if the user chose to calculate a new BMI or view all existing entries.
+                if (currentChoice.Equals("newbmi", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Takes in all the values necessary for a new entry
+                    Console.WriteLine("Please enter the person's full name: ");
+                    var fullName = Console.ReadLine();
+
+                    Console.WriteLine("Plesae enter the person's age: ");
+                    var age = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Plesae enter the person's weight in kilograms: ");
+                    var weight = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Plesae enter the person's height in centimetres: ");
+                    var height = Convert.ToDouble(Console.ReadLine());
+
+                    var time = DateTime.Now;
+
+                    // Adds all the properties to an object
+                    var currentEntry = new Person(fullName, age, weight, height, time);
+
+                    // Adds the current object to a list
+                    allPeople.Add(currentEntry);
+                } 
+                else if (currentChoice.Equals("view", StringComparison.OrdinalIgnoreCase))
+                {
+                    // TODO add report
+                }
+            }
         }
     }
 }
---
+++ Src/BootCamp.Chapter/person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Person
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double BMI
+        {
+            get
+            {
+                return (Weight / (Height * Height));
+            }
+        }
+        public DateTime Time { get; set; }
+
+
+        public Person(string fullName, int age, double weight, double height, DateTime time)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+            this.Weight = weight;
+            this.Height = height;
+            this.Time = time;
+        }
+
+        // TODO add ToString overide
+    }
+}
