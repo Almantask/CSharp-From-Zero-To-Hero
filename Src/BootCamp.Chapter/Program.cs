225d1ca623f677cf85a71f1e55c282de37fcef69
darwinjks <LiveAkwatsu@hotmail.com>
2020-05-11 07:10:09 -0400

    Fixed problems with hardcode.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace BootCamp.Chapter
+namespace Homework1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            {
+                //Introducction
+                Console.WriteLine("introduce the following information in order to calculate your BMI");
+
+                //Subject data
+                Console.Write("Full name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Age: ");
+                float age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Weight in Kg: ");
+                double weight = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Height in Cm: ");
+                double height = double.Parse(Console.ReadLine());
+                
+                //Calculated BMI
+                double heightconvertion = height / 100;
+                double totalBMI = weight / (heightconvertion * heightconvertion);
+
+                //Result
+                Console.WriteLine(name + " is " + age + " years old with a weight of " + weight + " and a height of " + height + " your total BMI is: " + (Math.Round(totalBMI, 1)));
+
+            }
+
         }
     }
 }
+
