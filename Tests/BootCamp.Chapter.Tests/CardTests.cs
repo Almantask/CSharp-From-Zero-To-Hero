5f87b8a75b21743cc872c7655354f506c5c3730f
Roelof Wobben <r.wobben@home.nl>
2020-05-13 18:52:52 +0200

    made 2 test to test the new and toString function of the Card class

---
+++ Tests/BootCamp.Chapter.Tests/CardTests.cs
@@ -0,0 +1,31 @@
+﻿using BootCamp.Chapter.Gambling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BootCamp.Chapter.Tests
+{
+    public class CardTests
+    {
+        [Fact]
+        public void New_Given_SuitAndRank_Sets_SuiteAndRank()
+        {
+            var expected = new Card(Card.Suites.Hearts, Card.Ranks.Two); 
+
+            var card = new Card(Card.Suites.Hearts, Card.Ranks.Two);
+
+            Assert.Equal(expected, card); 
+        }
+
+        [Fact]
+        public void ToString_Given_FalseSuitAndRank_Returns_String()
+        {
+            var expected = "Two of Hearts"; 
+
+            var card = new Card(Card.Suites.Hearts, Card.Ranks.Two).ToString();
+
+            Assert.Equal(expected, card);
+        }
+    }
+}
