15984ce22fc13f8504c5ab22b81d69533c7adc05
MatthewHole23 <matthew.hole23@gmail.com>
2020-05-14 20:27:19 +0100

    Revert "First Commit"
    This reverts commit a7a6be3e3b8480bc2a9b296505aa9456cdfb0ce4.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Statistics statistics = new Statistics();
-            statistics.CalculateStats();
-            statistics.DisplayStats();                    
         }
     }
 }
