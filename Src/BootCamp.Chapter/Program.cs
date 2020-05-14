ca8d2ea598b49f049c56213d939078da40d53fdb
OrangeCluster <caoell@gmail.com>
2020-05-14 17:13:07 -0400

    Homework Finished

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int age;
+            double weight;
+            double height;
+            double BMI;
+            Console.WriteLine("BMI Calculator");
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight in kg: ");
+            weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your height in cm: ");
+            height = double.Parse(Console.ReadLine());
+            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+            height = height / 100;
+            BMI = weight / Math.Pow(height, 2);
+            Console.WriteLine("Your BMI is: " + Math.Round(BMI, 2));
         }
     }
 }
