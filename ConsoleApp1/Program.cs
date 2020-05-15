febe7003ef55589f2dcaf79b93a4b5c57e805ce0
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-05-15 23:19:37 +0300

    asd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");asdasdasd
+
         }
     }
 }
