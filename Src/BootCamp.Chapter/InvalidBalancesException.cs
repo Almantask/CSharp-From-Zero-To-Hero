2748d4a69670529687dfe7f7f76c56dd57c636d0
Matthew Hole <matthew.hole23@gmail.com>
2020-05-15 21:35:40 +0100

    Added missing 'InvalidBalancesException.cs' functionality

---
+++ Src/BootCamp.Chapter/InvalidBalancesException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class InvalidBalancesException : Exception
+    {
+        private readonly string _msg;
+        public InvalidBalancesException(string error)
+        {
+            _msg = error;
+        }
+        public override string Message
+        {
+            get
+            {
+                return _msg;
+            }
+
+        }
+    }
+}
