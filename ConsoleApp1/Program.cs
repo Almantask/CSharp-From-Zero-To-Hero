76ee084a35d5515a28bea28a805e9398be3a8c03
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-05-15 23:20:57 +0300

    Homeowork

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");asdasdasd
+            string firstName = "Mert Efe";
+            string surName = "Isikgor";
+            int age = 24;
+            double weight = 85;
+            double height = 1.81;
+            double BMI = weight / (height * height);
+
+            string name = "Cagan";
+            string lastName = "Satir";
+            int ageOf = 24;
+            double caganWeight = 75;
+            double caganHeight = 1.78;
+            double caganBMI = caganWeight / (caganHeight * caganHeight);
+
+
+            Console.WriteLine(firstName + " is " + age + " years old. And his last name is " + surName + ". His BMI Score is: " + BMI + ".");
+            Console.WriteLine("And the friend of " + firstName + " which is his name " + name + " " + lastName + ". His Age and BMI Scores are: " + ageOf + " and " + caganBMI + ".");
 
         }
     }
