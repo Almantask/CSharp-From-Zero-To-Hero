57c2aded8e639f572347dc281e494b2f0999b5d9
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-05-17 16:29:22 +0300

    Homeowkr

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Person
+    {
+        private readonly string _name;
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void Talk(string message)
+        {
+
+        }
+
+
+        
+
+        public Person(string name
+           )
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
