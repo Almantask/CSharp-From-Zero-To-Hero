83e19cba4f9c5b19f8626f18796ae206e578ffd5
Hernan Di Tano <hditano@outlook.com>
2020-05-19 03:31:48 -0300

    Added a few inputs Added Bmi Calculation

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,12 +6,23 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type  your First name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Type your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Type your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type your Weight");
+            float weight = float.Parse(Console.ReadLine()); 
+            Console.WriteLine("Type your Height");
+            double height = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(firstName +" " + lastName +" is " + age +" years old, his weight is " + weight + " and his height is " + height);
+
+            double bmi = Convert.ToDouble(weight / (height * height)) * 1000;
+
+
+            Console.WriteLine("Your BMI is: "+ bmi);
 
         }
     }
