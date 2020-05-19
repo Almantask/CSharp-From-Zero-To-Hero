099d7d93bed9b281b320b84c18c8b124f5c79c4e
Roelof Wobben <r.wobben@home.nl>
2020-05-19 15:18:40 +0200

    make test for the PokerHand class

---
+++ Tests/BootCamp.Chapter.Tests/PokerHandsTests.cs
@@ -0,0 +1,37 @@
+﻿using BootCamp.Chapter.Gambling;
+using BootCamp.Chapter.Gambling.Poker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BootCamp.Chapter.Tests
+{
+    public class PokerHandsTests
+    {
+        [Fact]
+        public void AddCards_Given_Empty_List_And_A_List_With_Elements_Returns_A_List_With_Elements()
+        {
+            var cardToBeAdded = new Card(Card.Suites.Clubs, Card.Ranks.Four);
+            var hand = new PokerHand();
+            var expected = new List<Card>() { new Card(Card.Suites.Clubs, Card.Ranks.Four) };
+
+            hand.AddCards(cardToBeAdded);
+            var actual = hand.Reveal();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddCards_Given_List_And_Card_Null_Throws_ArgumentNullException()
+        {
+            var hand = new PokerHand();
+
+            Action action = () => hand.AddCards(null);
+
+            Assert.Throws<ArgumentNullException>(action); 
+
+        }
+
+    }
+}
