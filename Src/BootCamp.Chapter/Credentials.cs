44a64cc9726824fb1ea30353b642755d603eda02
dave94punk <david.katona@hotmail.com>
2020-05-19 19:05:04 +0200

    Changed Credentials to readonly struct and implemented IEquatable<Credentials>.

--- Src/BootCamp.Chapter/Credentials.cs
+++ Src/BootCamp.Chapter/Credentials.cs
@@ -1,13 +1,19 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootCamp.Chapter
 {
     // TODO: make a struct and add validation and other needed methods (if needed)
-    public class Credentials
+    public readonly struct Credentials : IEquatable<Credentials>
     {
-        public string Username;
-        public string Password;
+        public readonly string Username;
+        public readonly string Password;
 
         public Credentials(string username, string password)
         {
+            if (String.IsNullOrWhiteSpace(username) || String.IsNullOrWhiteSpace(password))
+                throw new ArgumentException($"{nameof(username)} or {nameof(password)} is not valid.");
+
             Username = username;
             Password = password;
         }
@@ -17,6 +23,21 @@
         {
             credentials = default;
             return false;
+        }
+
+        public bool Equals([AllowNull] Credentials other)
+        {
+            return Username == other.Username && Password == other.Password;
+        }
+
+        public static bool operator ==(Credentials leftExpression, Credentials rightExpression)
+        {
+            return leftExpression.Equals(rightExpression);
+        }
+
+        public static bool operator !=(Credentials leftExpression, Credentials rightExpression)
+        {
+            return !(leftExpression == rightExpression);
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Credentials credentialsOne = new Credentials("David", "HJk1##88ba");
+            Credentials credentialsTwo = new Credentials("Adam", "Kl6627");
+            Credentials credentialsThree = new Credentials("David", "HJk1##88ba");
 
+            Console.WriteLine(credentialsOne != credentialsTwo);
         }
     }
 }
