49eadf7378e1861c0e1ef450b2e02fe43adf4b41
Roelof Wobben <r.wobben@home.nl>
2020-05-20 07:35:33 +0200

    Made test for the == method

---
+++ Tests/BootCamp.Chapter.Tests/PokerComboTests.cs
@@ -0,0 +1,77 @@
+﻿using BootCamp.Chapter.Gambling.Poker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BootCamp.Chapter.Tests
+{
+    public class PokerComboTests
+    {
+        [Fact]
+        public void Smaller__Given_Set1_Is_Smaller_Set2_Returns_True()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.Flush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 12);
+
+            var actual = set1 < set2;
+
+            Assert.True(actual); 
+        }
+
+        [Fact]
+        public void Smaller__Given_Set1_Is_Equal_Set2_But_Set1_Lower_Score_Returns_True()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 12);
+
+            var actual = set1 < set2;
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Smaller__Given_Set1_Is_Greater_Set2_Returns_False()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.Flush, 12);
+
+            var actual = set1 < set2;
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Equal__Given_Set1_Is_Equal_To__Set2_Returns_True()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+
+            var actual = set1 == set2;
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Equal__Given_Set1_IsDifferent_Set_To__Set2_Returns_False()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.Pair, 1);
+
+            var actual = set1 == set2;
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Equal__Given_Set1_IsDifferent_Score_To__Set2_Returns_False()
+        {
+            var set1 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 1);
+            var set2 = new PokerCombo(PokerCombo.Sets.RoyalFlush, 2);
+
+            var actual = set1 == set2;
+
+            Assert.False(actual);
+        }
+    }
+}
