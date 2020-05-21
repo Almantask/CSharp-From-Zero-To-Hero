712c2ac1c5ae1a4878d222551bdcbc4fc8396921
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-05-21 22:03:39 +0300

    Homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,7 +9,7 @@ namespace BootCamp.Chapter
             string firstName = "Mert Efe";
             string surName = "Isikgor";
             int age = 24;
-            double weight = 85;
+            double weight = 85; 
             double height = 1.81;
             double BMI = weight / (height * height);
 
