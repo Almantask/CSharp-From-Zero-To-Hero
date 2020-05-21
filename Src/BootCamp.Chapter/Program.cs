ac1d4cf83bf9d7b0d57d19f88e66009dabb0588f
Roelof Wobben <r.wobben@home.nl>
2020-05-21 10:38:41 +0200

    make a test for poker which is failing

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using BootCamp.Chapter.Gambling;
+using BootCamp.Chapter.Gambling.Poker;
+using System;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+             
         }
+
     }
 }
---
+++ Tests/BootCamp.Chapter.Tests/PokerTests.cs
@@ -0,0 +1,41 @@
+﻿using BootCamp.Chapter.Gambling;
+using BootCamp.Chapter.Gambling.Poker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BootCamp.Chapter.Tests
+{
+    public class PokerTests
+    {
+        [Fact]
+        public void GetWinners_Given_List_Players_Returns_Winnner()
+        {
+            var player1 = new Player("player1", new PokerHand());
+            player1.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Ace));
+            player1.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.King));
+            player1.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Queen));
+            player1.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Jack));
+            player1.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Ten));
+
+            var player2 = new Player("player2", new PokerHand());
+            player2.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Six));
+            player2.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Five));
+            player2.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Four));
+            player2.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Three));
+            player2.Hand.AddCards(new Card(Card.Suites.Clubs, Card.Ranks.Two));
+
+            var game = new Poker();
+            game.AddPlayer(player1);
+            game.AddPlayer(player2);
+
+            var expected = new List<Player> { player1 }; 
+            
+            var actual = game.GetWinners();
+
+            Assert.Equal(expected, actual); 
+
+        }
+    }
+}
