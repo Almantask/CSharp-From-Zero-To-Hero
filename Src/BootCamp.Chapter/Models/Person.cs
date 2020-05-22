52d5e872b462592d41009eb2b0e545dda0ba8118
Christoffer Stolpestad <christoffer.stolpestad@intility.no>
2020-05-22 08:30:34 +0200

    added data from Homework 7

---
+++ Src/BootCamp.Chapter/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BootCamp.Chapter.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<Single> Balances { get; set; }
+    }
+}
