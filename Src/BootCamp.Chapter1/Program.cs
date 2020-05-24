598112c0633adfc79256d403ffb196bee0a5b561
Michel Potter <michelwesley_lima@hotmail.com>
2020-05-24 10:56:00 -0300

    remove code used for testing

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -1,43 +1,10 @@
-﻿using System;
-
-namespace BootCamp.Chapter1
+﻿namespace BootCamp.Chapter1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[0];
-            OutputArr(arr);
-
-            ArrayOperations.Sort(arr);
-            OutputArr(arr);
-
-            ArrayOperations.Reverse(arr);
-            OutputArr(arr);
-
-            OutputArr(ArrayOperations.RemoveLast(arr));
-
-            OutputArr(ArrayOperations.RemoveFirst(arr));
-
-            OutputArr(ArrayOperations.RemoveAt(arr, 3));
-
-            OutputArr(ArrayOperations.InsertFirst(arr, 3));
-
-            OutputArr(ArrayOperations.InsertLast(arr, 7));
-
-            OutputArr(ArrayOperations.InsertAt(arr, 9, 3));
         }
-
-        static void OutputArr(int[] arr)
-        {
-            Console.Write("[ ");
-            foreach (int num in arr)
-            {
-                Console.Write($"{num} ");
-            }
-            Console.Write("]\n");
-        }
-
 
     }
 }
