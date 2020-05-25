b1431d0917c748edf8747e2c1da04a0f9ced763b
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-05-25 15:07:34 +0300

    Done BinaryConverter Class. Pass all the tests.

--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -8,12 +9,58 @@ namespace BootCamp.Chapter
     {
         public static long ToInteger(string binary)
         {
-            return 0;
+            if (string.IsNullOrEmpty(binary))
+                return 0;
+            
+            if (!binary.Contains("0") && !binary.Contains("1"))
+                throw new InvalidBinaryNumberException(binary);
+
+            double number = 0;
+            char[] binaryNumbers = binary.ToCharArray();
+
+            for (int i = binaryNumbers.Length -1; i >= 0; i--)
+            {
+                int a = (binaryNumbers.Length - (i + 1));
+                double power = Math.Pow(2, a);
+
+                number +=Char.GetNumericValue(binaryNumbers[i]) * power;
+            }
+
+            return long.Parse(number.ToString());
         }
 
         public static string ToBinary(long number)
         {
-            return "";
+            string binaryValue = "";
+
+            if (number == 0)
+            {
+                return "0";
+            }
+
+            if (number == 1)
+            {
+                binaryValue = "1" + binaryValue;
+                number -= 1;
+            }
+
+            while (number >= 0)
+            {
+                if (number % 2 == 1)
+                {
+                    binaryValue = "1" + binaryValue;
+                    number -= 1;
+                    number /= 2;
+                }
+
+                if (number == 0)
+                    return binaryValue;
+
+                binaryValue = "0" + binaryValue;
+                number /= 2;
+            }
+            //return Convert.ToString(Convert.ToInt32(number, 16), 2);
+            return binaryValue;
         }
     }
 }
