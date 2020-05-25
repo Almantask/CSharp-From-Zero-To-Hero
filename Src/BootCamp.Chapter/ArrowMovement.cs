dd765e25d4b6308f6c1a6c7513c527b3922be304
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-05-25 15:18:29 +0300

    Done GetIndicator method. Pass all the tests.

--- Src/BootCamp.Chapter/ArrowMovement.cs
+++ Src/BootCamp.Chapter/ArrowMovement.cs
@@ -18,7 +18,31 @@ namespace BootCamp.Chapter
         /// <returns>One of the arrow characters. '↥' by default.</returns>
         public static char GetIndicator(char symbol)
         {
-            return '-';
+            switch (symbol)
+            {
+                case 'W':
+                    return '\u21a5';
+                case 'w':
+                    return '\u21a5';
+                case 'A':
+                    return '\u21A4';
+                case 'a':
+                    return '\u21A4';
+                case 'D':
+                    return '\u21A6';
+                case 'd':
+                    return '\u21A6';
+                case 'S':
+                    return '\u21A7';
+                case 's':
+                    return '\u21A7';
+                case 'C':
+                    return '\u21a5';
+                case 'c':
+                    return '\u21a5';
+            }
+
+            return 'a';
         }
     }
 }
