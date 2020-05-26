abc61b6971ce26376e9051c989e8c5ac9da50e7a
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-05-26 17:31:10 +0300

    Done Inventory class methods. Added one more method for delete array item.

--- Src/BootCamp.Chapter/Inventory.cs
+++ Src/BootCamp.Chapter/Inventory.cs
@@ -1,11 +1,13 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public class Inventory
     {
         private Item[] _items;
         public Item[] GetItems()
         {
-            return new Item[0];
+            return _items;
         }
 
         public Inventory()
@@ -15,12 +17,33 @@
 
         public Item[] GetItems(string name)
         {
-            return new Item[0];
+            Item[] allItems = GetItems();
+            Item[] filteredItems = new Item[0];
+
+            foreach (Item Item in allItems)
+            {
+                if (Item.GetName() == name)
+                {
+                    if (filteredItems.Length > 0)
+                    {
+                        var temp = (Item[]) filteredItems.Clone();
+                        filteredItems = new Item[filteredItems.Length + 1];
+                        temp.CopyTo(filteredItems, 0);
+                    }
+                    filteredItems[^1] = Item;
+                }
+            }
+            return filteredItems;
         }
 
         public void AddItem(Item item)
         {
+            if (item == null) return;
 
+            Item[] temp = GetItems();
+            _items = new Item[temp.Length + 1];
+            temp.CopyTo(_items, 0);
+            _items[^1] = item;
         }
 
         /// <summary>
@@ -29,7 +52,39 @@
         /// </summary>
         public void RemoveItem(Item item)
         {
+            if (item == null) return;
 
+            var allItems = GetItems();
+
+            for (int i = 0; i < allItems.Length; i++)
+            {
+                if (allItems[i] == item)
+                {
+                    DeleteIndexShiftElements(allItems, i);
+                }
+            }
+        }
+
+        private void DeleteIndexShiftElements(Object[] array, int index)
+        {
+            if (array == null || index == null) return;
+            if ((array.Length - index + 1) < 0) throw new InvalidOperationException("Wrong index or array!");
+            if (array.Length == 1)
+            {
+                array[0] = null;
+                return;
+            }
+
+            Object[] temp = new object[array.Length - 1];
+            Array.ConstrainedCopy(array, 0, temp, 0, array.Length-1);
+
+            for (int i = index; i < array.Length; i++)
+            {
+                temp[i] = array[i + 1];
+            }
+
+            array = new object[array.Length -1];
+            array = temp;
         }
     }
 }
