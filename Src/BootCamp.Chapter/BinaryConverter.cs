cdebfeab882a189d91f50927a8e2f4a9e72f763e
Christoffer Stolpestad <christoffer.stolpestad@intility.no>
2020-05-26 20:36:13 +0200

    ToInt done

--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -8,12 +9,41 @@ namespace BootCamp.Chapter
     {
         public static long ToInteger(string binary)
         {
-            return 0;
+            if (!IsValidBinaryNumber(binary))
+                return 0;
+
+            int intResult = 0;
+            var j = 0; 
+
+            for (int i = binary.Length - 1; i >= 0; i--, j++)
+            {
+                intResult = intResult + (int)Char.GetNumericValue(binary[i]) * (int)(MathF.Pow(2, j));
+            }
+
+            return intResult;
         }
 
         public static string ToBinary(long number)
         {
             return "";
         }
+
+        private static bool IsValidBinaryNumber(string binary)
+        {
+            bool isValid = true;
+            var validNumbers = new string[] { "0", "1" };
+            if (string.IsNullOrEmpty(binary))
+                return false;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (!(validNumbers.Contains(binary[i].ToString())))
+                    throw new InvalidBinaryNumberException(binary[i].ToString());
+            }
+
+            return isValid;
+        }
+
+        
     }
 }
