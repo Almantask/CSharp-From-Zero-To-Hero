18254e95162058a56a72b18d8628dd88dfbf01c6
dave94punk <david.katona@hotmail.com>
2020-05-26 17:57:14 +0200

    Slight changes.

--- Src/BootCamp.Chapter/Item.cs
+++ Src/BootCamp.Chapter/Item.cs
@@ -12,7 +12,7 @@ namespace BootCamp.Chapter
         {
             Name = name ?? throw new ArgumentNullException($"{nameof(Name)} cannot be null.");
             Price = price > 0 ? price : throw new ArgumentException($"{nameof(Price)} cannot be less than or equal to 0.");
-            Weight = weight > 0 ? weight : throw new ArgumentException($"{nameof(Weight)} cannot be less than 0.");
+            Weight = weight >= 0 ? weight : throw new ArgumentException($"{nameof(Weight)} cannot be less than 0.");
         }
     }
 }
