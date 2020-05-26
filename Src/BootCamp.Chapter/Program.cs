a5aeac208f3c3f875cca7ee5022ad1582f6192c1
Colby Whidden <51108569+ColbyWhidden@users.noreply.github.com>
2020-05-25 21:02:24 -0500

    Added user input and return to start functionality

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -23,10 +23,35 @@ namespace BootCamp.Chapter
 
             Console.Write("Hello world!" + System.Environment.NewLine);
 
-            Console.WriteLine("Tom's age is " + tAge + "and his height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
-            Console.WriteLine("Colby's age is " + cAge + "and his height is " + cHeight + " and his weight is " + cWeight + " BMI is: " + cBmi);
+            Console.WriteLine("Tom's age is " + tAge + " and his height is " + tHeight + " and his weight is " + tWeight + " BMI is: " + tBmi);
+            Console.WriteLine("Colby's age is " + cAge + " and his height is " + cHeight + " and his weight is " + cWeight + " BMI is: " + cBmi);
             Console.WriteLine("Blakes's age is " + bAge + " and his height is " + bHeight + " and his weight is " + bWeight + " BMI is: " + bBmi);
+
+            Console.WriteLine("Press any key to continue...");
+
             Console.ReadLine();
+            Console.Clear();
+
+        Start:
+
+            Console.WriteLine("Please enter your name. ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello " + yourName);
+
+            Console.WriteLine("Please enter your age now. ");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your height now. ");
+            double yourHeight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight now. ");
+            int yourWeight = Convert.ToInt32(Console.ReadLine());
+            double yourBMI = yourWeight / yourHeight;
+            Console.WriteLine("Alright " + yourName + ", " + "you are " + yourAge + " years old, and your BMI based off your provided measurements is " + yourBMI);
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadLine();
+            Console.Clear();
+            goto Start;
         }
     }
 }
