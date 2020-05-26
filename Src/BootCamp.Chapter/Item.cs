e306b42022d84149e87b1a1dd5b41a64d2886ea4
dave94punk <david.katona@hotmail.com>
2020-05-26 17:52:14 +0200

    Refactored Item.cs to use null-related operators and other kinds of validation.

--- Src/BootCamp.Chapter/Item.cs
+++ Src/BootCamp.Chapter/Item.cs
@@ -1,4 +1,6 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public class Item
     {
@@ -8,9 +10,9 @@
 
         public Item(string name, decimal price, float weight)
         {
-            Name = name;
-            Price = price;
-            Weight = weight;
+            Name = name ?? throw new ArgumentNullException($"{nameof(Name)} cannot be null.");
+            Price = price <= 0 ? price : throw new ArgumentException($"{nameof(Price)} cannot be less than or equal to 0.");
+            Weight = weight < 0 ? weight : throw new ArgumentException($"{nameof(Weight)} cannot be less then 0.");
         }
     }
 }
