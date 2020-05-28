c71414c647d3c538ab5ba4faced96821c739f297
Christoffer Stolpestad <christoffer.stolpestad@intility.no>
2020-05-28 20:52:13 +0200

    create random string done

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,26 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine(GenerateRandomString(100));
+        }
+
+        public static string GenerateRandomString(int characters)
+        {
+            Random random = new Random();
+            var bytes = new byte[characters];
+            for (int i = 0; i < characters; i++)
+            {
+                switch (random.Next(0, 2))
+                {
+                    case 0:
+                        bytes[i] = (byte)random.Next(65, 91);
+                        break;
+                    case 1:
+                        bytes[i] = (byte)random.Next(97, 123);
+                        break;
+                }
+            }
+            return new string(ASCIIEncoding.ASCII.GetChars(bytes));
         }
     }
 }
