6fc55a661bdf9a71367ab8fecb1e99817e38c5b2
Christoffer Stolpestad <christoffer.stolpestad@intility.no>
2020-05-28 20:29:54 +0200

    Caesar cipher done

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,30 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (message == null)
+                return message;
+
+            var byteArray = ASCIIEncoding.ASCII.GetBytes(message.ToCharArray());
+            for (int i = 0; i < message.Length; i++)
+            {
+                byteArray[i] += shift;
+            }
+
+            return new string(Encoding.ASCII.GetChars(byteArray));
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            if (message == null)
+                return message;
+
+            var byteArray = ASCIIEncoding.ASCII.GetBytes(message.ToCharArray());
+            for (int i = 0; i < message.Length; i++)
+            {
+                byteArray[i] -= shift;
+            }
+
+            return new string(Encoding.ASCII.GetChars(byteArray));
         }
     }
 }
