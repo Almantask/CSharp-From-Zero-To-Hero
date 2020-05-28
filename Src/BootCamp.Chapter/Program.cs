ce09e52d81dc79ebe5016ca3cf095b3ddbd62928
MatthewWu <mwu1879@rcs-k12.us>
2020-05-28 13:46:13 -0400

    1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,20 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter in your full name");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Enter in your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("Enter in your height");
+            string height = Console.ReadLine();
+            Console.WriteLine("Enter in your weight.");
+            string weight = Console.ReadLine();
+
+            double heighttwo = (double.Parse(height) * .01);
+            double bmi = double.Parse(weight) / (heighttwo * heighttwo);
+
+            Console.WriteLine(myName + " is " + int.Parse(age) + " years old, his height is " + double.Parse(height) + "cm, and his weight is " + double.Parse(weight) + "kg.");
+            Console.WriteLine("BMi is: " + bmi);
         }
     }
 }
