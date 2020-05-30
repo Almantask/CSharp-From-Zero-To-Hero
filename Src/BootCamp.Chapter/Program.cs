1d6d37dbaf2b6878fa4989013310e8b3716cbd1c
Arthur <arthur.schevey@gmail.com>
2020-05-30 16:14:10 -0400

    Reworked version. Previous was incorrect.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,8 +7,50 @@ namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //  obtain information from user
+            var name = PromptString("Name: ");
+            var surname = PromptString("Surname: ");
+            var age = PromptInt("Age: ");
+            var weight = PromptFloat("Weight: ");
+            var height = PromptFloat("Height: ");
+
+            // print information back to user
+            Console.WriteLine("\n" + name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+            
+            // calculated BMI using function and store it in "determinedBmi" variable
+            var determinedBmi = CalculateBmi(weight, height);
+            Console.WriteLine("BMI for " + name + ": " + determinedBmi + "\n");
+
+     
+            Console.ReadKey();
+
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        
+        public static int PromptInt(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.Write(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            float determinedBmi = weight / ((height / 100) * (height / 100));
+            return determinedBmi;
         }
     }
 }
+ 
