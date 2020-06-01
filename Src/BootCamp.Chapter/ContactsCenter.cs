f2b2e334d9d35cbef835fc9ceba7fce4977be092
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-06-01 23:08:16 +0300

    Getting people from csv to list done.

--- Src/BootCamp.Chapter/ContactsCenter.cs
+++ Src/BootCamp.Chapter/ContactsCenter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -10,7 +12,47 @@ namespace BootCamp.Chapter
 
         public ContactsCenter(string peopleFile)
         {
-            // load people
+            _people = new List<Person>();
+            LoadList(peopleFile);
+        }
+
+        private void LoadList(string peopleFile)
+        {
+            if (string.IsNullOrEmpty(peopleFile)) throw new ArgumentNullException();
+            using (StreamReader streamReader = new StreamReader(peopleFile))
+            {
+                bool firstIndexFlag = true;
+                while (!streamReader.EndOfStream)
+                {
+                    if (firstIndexFlag)
+                    {
+                        //we want to skip first row cause it's header.
+                        //This looks weird but I couldn't find diferent way to do it.
+                        streamReader.ReadLine();
+                        firstIndexFlag = false;
+                        continue;
+                    }
+
+                    string[] personInfo = streamReader.ReadLine().Split(',');
+
+                    GetPersonInfoAndAddPersonList(personInfo);
+                }
+            }
+        }
+
+        private void GetPersonInfoAndAddPersonList(string[] personInfo)
+        {
+            Person person = new Person();
+
+            person.Name = personInfo[0];
+            person.Surname = personInfo[1];
+            person.BirthDate = formatAndParseDateTime(personInfo[2]);
+            person.Gender = personInfo[3];
+            person.Country = personInfo[4];
+            person.Email = personInfo[5];
+            person.StreetAddress = personInfo[6];
+
+            _people.Add(person);
         }
 
         /// <summary>
@@ -22,6 +64,14 @@ namespace BootCamp.Chapter
             var people = new List<Person>();
             // ToDo: implement applying filter.
             return people;
+        }
+
+        public DateTime formatAndParseDateTime(string readedDateTime)
+        {
+            string[] dateTimeValues = readedDateTime.Split('/');
+            DateTime dateTime = new DateTime(int.Parse(dateTimeValues[2]), int.Parse(dateTimeValues[0]),
+                int.Parse(dateTimeValues[1]));
+            return dateTime;
         }
     }
 }
--- Src/BootCamp.Chapter/Person.cs
+++ Src/BootCamp.Chapter/Person.cs
@@ -1,7 +1,16 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public class Person
     {
         // add missing properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
     }
 }
