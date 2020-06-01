865b84477eaa443504efb30eeee029ed92db7db6
Manuel Bermudo Maeso <manuel.bermudo.maeso@edag-ps.com>
2020-06-01 12:31:18 +0200

    added required changes

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,37 +7,34 @@ namespace BootCamp.Chapter
 
         static void Main(string[] args)
         {
-            string name = "Tom";
-            string surname = "Jefferson";
-            int age = 19;
-            double weight = 50;
-            double height = 156.5;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Introduce a name:");
+                string name = Console.ReadLine();
 
-            double bmi = weight / Math.Pow(height / 100.0, 2);
+                Console.WriteLine("Introduce a surname:");
+                string surname = Console.ReadLine();
 
-            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm");
-            Console.WriteLine($"BMI of {name} {surname} is {bmi}");
+                Console.WriteLine("Introduce an age:");
+                int age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Introduce a name:");
-            name = Console.ReadLine();
+                Console.WriteLine("Introduce a weight (kg):");
+                double weight = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Introduce a surname:");
-            surname = Console.ReadLine();
+                Console.WriteLine("Introduce a height (cm):");
+                double height = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Introduce an age:");
-            age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
 
-            Console.WriteLine("Introduce a weight (kg):");
-            weight = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Introduce a height (cm):");
-            height = double.Parse(Console.ReadLine());
+                double bmi = weight / Math.Pow(height / 100.0, 2);
 
-            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm");
+                Console.WriteLine($"BMI of {name} {surname} is {bmi}.");
 
-            bmi = weight / Math.Pow(height / 100.0, 2);
-
-            Console.WriteLine($"BMI of {name} {surname} is {bmi}");
+                if (i == 0)
+                {
+                    Console.WriteLine("Now do it a second time!");
+                }
+            }            
         }
 
     }
