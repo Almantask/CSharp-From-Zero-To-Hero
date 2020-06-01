852413c24b8a13388e46a5c56ef55b93da66fa51
MatthewWu <mwu1879@rcs-k12.us>
2020-06-01 17:14:46 -0400

    a

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -14,28 +14,100 @@ namespace BootCamp.Chapter
     /// </summary>
     public static class Checks
     {
+
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            int number;
+            var input = Console.ReadLine();
+            var numberCheck = int.TryParse(input, out number);
+            if (!numberCheck)
+            {
+                Console.WriteLine(input + " is not a valid number");
+                return -1;
+            }
+
+            if (int.Parse(input) == 0)
+            {
+                return 0;
+            }
+            if (numberCheck)
+            {
+                return int.Parse(input);
+            }
+            return number;
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            Console.WriteLine(message);
+            String input = Console.ReadLine();
+            var stringCheck = string.IsNullOrEmpty(input);
+            if (stringCheck)
+            {
+                Console.WriteLine("name cannot be empty");
+                return "-";
+            }
+            else
+            {
+                return input;
+            }
+
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
-        }
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            float number;
+            var floatCheck = float.TryParse(input, out number);
+            if (!floatCheck)
+            {
+                Console.WriteLine(number);
+                Console.WriteLine(input + " is not a valid number");
+                return -1;
+            }
+            if (float.Parse(input) == 0)
+            {
+                return 0;
+            }
+            if (floatCheck)
+            {
+                return float.Parse(input);
+            }
 
+            return number;
+        }
+        public static void printError(string y, float x)
+        {
+
+            Console.WriteLine("Failed calculating BMI. Reason: " + y + " cannot be equal or less than zero, but was " + x);
+
+        }
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            if (weight <= 0 && height <= 0)
+            {
+                printError("Weight", weight);
+                printError("Height", height);
+                return -1;
+            }
+            else if (weight <= 0)
+            {
+                printError("Weight", weight);
+                return -1;
+            }
+            else if (height <= 0)
+            {
+                printError("Height", height);
+                return -1;
+            }
+            else
+            {
+                return weight / (height * height);
+            }
+
+
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,117 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+
+            CalculateBMI();
+            CalculateBMI();
+
+        }
+        public static void CalculateBMI()
+        {
+            String myName = PromptString("Enter in your full name");
+            int age = PromptInt("Enter your age");
+            float height = PromptFloat("Enter your height");
+            float weight = PromptFloat("Enter your weight");
+            float bmi = calculateBmi(weight, height);
+            Console.WriteLine(myName + " is " + age + " years old, his height is " + height + "cm, and his weight is " + weight + "kg.");
+            Console.WriteLine("BMi is: " + bmi);
+        }
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            int number;
+            var input = Console.ReadLine();
+            var numberCheck = int.TryParse(input, out number);
+            if (!numberCheck)
+            {
+                Console.WriteLine(input + " is not a valid number");
+                return -1;
+            }
+
+            if (int.Parse(input) == 0)
+            {
+                return 0;
+            }
+            if (numberCheck)
+            {
+                return int.Parse(input);
+            }
+            return number;
+        }
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            String input = Console.ReadLine();
+            var stringCheck = string.IsNullOrEmpty(input);
+            if (stringCheck)
+            {
+                Console.WriteLine("name cannot be empty");
+                return "-";
+            }
+            else
+            {
+                return input;
+            }
+
+        }
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            float number;
+            var floatCheck = float.TryParse(input, out number);
+            if (!floatCheck)
+            {
+                Console.WriteLine(number);
+                Console.WriteLine(input + " is not a valid number");
+                return -1;
+            }
+            if (float.Parse(input) == 0)
+            {
+                return 0;
+            }
+            if(floatCheck)
+            {
+                return float.Parse(input);
+            }
+
+            return number;
+        }
+        public static void printError(string y, float x)
+        {
+
+            Console.WriteLine("Failed calculating BMI. Reason: " + y + " cannot be equal or less than zero, but was " + x);
+
+        }
+        public static float calculateBmi(float weight, float height)
+        {
+
+            if (weight <= 0 && height <= 0)
+            {
+                printError("Weight", weight);
+                printError("Height", height);
+                return -1;
+            }
+            else if (weight <= 0)
+            {
+                printError("Weight", weight);
+                return -1;
+            }
+            else if (height <= 0)
+            {
+                printError("Height", height);
+                return -1;
+            }
+            else
+            {
+                return weight / (height * height);
+            }
+
         }
     }
+
 }
+
+
+
+
