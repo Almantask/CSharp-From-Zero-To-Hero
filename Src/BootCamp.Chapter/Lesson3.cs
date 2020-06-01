06213f02f58a97bb9fc26ab0190d6c2ea2576a29
Manuel Bermudo Maeso <manuel.bermudo.maeso@edag-ps.com>
2020-06-01 12:41:57 +0200

    added required changes

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -7,15 +7,29 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            string name = StringPrompt("Please introduce a name:");
-            string surname = StringPrompt("Please introduce surname:");
-            int age = IntPrompt("Please introduce age:");
-            float weight = FloatPrompt("Please introduce weight (kg):");
-            float height = FloatPrompt("Please introduce height (m):");
+            for (int i = 0; i < 2; i++)
+            {
+                string name = StringPrompt("Please introduce a name:");
+                string surname = StringPrompt("Please introduce surname:");
+                int age = IntPrompt("Please introduce age:");
+                float weight = FloatPrompt("Please introduce weight (kg):");
+                float height = FloatPrompt("Please introduce height (m):");
 
-            Console.WriteLine($"{name} {surname} is {age} years old, weight is {weight} kg and height is {height} m.");
+                Console.WriteLine(PromptPersonData(name, surname, age, weight, height));
 
-            Console.WriteLine($"BMI of {name} {surname} is {CalculateBMI(weight, height)}.");
+                Console.WriteLine(PromptPersonBMI(name, surname, weight, height));
+
+                if (i == 0)
+                {
+                    Console.WriteLine("Now let's do it a second time!");
+                }
+            }
+
+        }
+
+        private static string PromptPersonData(string name, string surname, int age, float weight, float height)
+        {
+            return $"{name} {surname} is {age} years old, weight is {weight} kg and height is {height} m.";
         }
 
         public static float FloatPrompt(string message)
@@ -40,5 +54,11 @@ namespace BootCamp.Chapter
         {
             return weight / (height * height);
         }
+
+        public static string PromptPersonBMI(string name, string surname, float weight, float height)
+        {
+           return $"BMI of {name} {surname} is {CalculateBMI(weight, height)}.";
+        }
+
     }
 }
