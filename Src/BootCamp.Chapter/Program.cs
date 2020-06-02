4e3df0acd8dd3f245903850a0b05f8e3c2caae10
James Ferguson <jimmychargin08@gmail.com>
2020-06-03 09:41:30 +1000

    Removed Console.WriteLine("Hello World"); Added code to display name, age, height and weight.
    Calculates BMI.
    
    Added range indicator for BMI because as my first code I wanted to see if I was able to do that.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace BootCamp.Chapter
+namespace BootCamp
 {
-    class Program
+    class BMI1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Name = "James Ferguson";
+            int Age = 30;
+            var Sex = "his";
+
+            double Height = 195.0;
+            var heightMetres = Height / 100.0;
+            var heightMetresMultiplied = heightMetres * heightMetres;
+            double Weight = 96.4;
+            double BMI = Weight / heightMetresMultiplied;
+            BMI = Convert.ToInt32(BMI);
+
+            Console.WriteLine($"{Name} is {Age} years old, {Sex} weight is {Weight} kg and {Sex} height is {Height}cm");
+
+            if (BMI < 18.5)
+            { Console.WriteLine($"Your BMI is {BMI}. Which is Underweight. Go have a piece of cake :("); }
+            if (BMI >= 18.5 & BMI <= 24.9)
+            { Console.WriteLine($"Your BMI is {BMI}. Which is Normal. Good stuff just a small piece of cake should do :)"); }
+            if (BMI >= 25 & BMI <= 29.9)
+            { Console.WriteLine($"Your BMI is {BMI}. Which is Overweight. Steady on with that next piece of cake mate!"); }
+            if (BMI > 30.0)
+            { Console.WriteLine($"Your BMI is {BMI}. Which is Obese. listen you ate the cake already there is no second cake!!!"); }
         }
     }
 }
