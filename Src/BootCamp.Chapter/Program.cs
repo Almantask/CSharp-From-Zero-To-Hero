4f247fd9331df42a932ca012eff917fe5eac3d48
BuildTools <unconfigured@null.spigotmc.org>
2020-06-11 22:22:07 -0400

    Completed homework for Chapters 1 and 2 (BMI code).

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,19 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter weight (in kg):");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height (in cm):");
+            float height = float.Parse(Console.ReadLine());
+
+            float bmi = weight / ((height / 100) * (height / 100));
+
+            Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
+            Console.WriteLine("Their BMI is: " + bmi);
         }
     }
 }
