1f4f43c50e79ac3f9a905fd2ec9d21f653050124
BuildTools <unconfigured@null.spigotmc.org>
2020-06-12 02:04:16 -0400

    Copied code from Homework 1And2 into Homework3. This is the code before any refactoring has been done.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Console = System.Console;
 
 namespace BootCamp.Chapter
 {
@@ -9,6 +6,19 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter weight (in kg):");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height (in cm):");
+            float height = float.Parse(Console.ReadLine());
+
+            float bmi = weight / ((height / 100) * (height / 100));
+
+            Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
+            Console.WriteLine("Their BMI is: " + bmi);
         }
     }
 }
