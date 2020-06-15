a921bfcae7e3b9e644a90c17f87fbb8b4d74fe04
itsjustkevin <kevinjchisholm@gmail.com>
2020-06-15 23:59:50 +0900

    Homework 1 and 2.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,21 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            var surname = Console.ReadLine();
+            Console.WriteLine("how old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What is your weight in kg?");
+            var weight = Double.Parse(Console.ReadLine());
+            Console.WriteLine("what is your height in in?");
+            var height = Double.Parse(Console.ReadLine());
+
+            double heightSquared = Math.Pow(height, 2);
+            double Bmi = weight / heightSquared;
+
+            Console.WriteLine($"{name} at a height of {height}in and a weight of {weight}kg, your BMI is {Bmi}");
         }
     }
 }
