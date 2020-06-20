c11e51d69b1d80e67c2eb70fefb0727afa126bbf
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 20:52:01 -0400

    Completed some of the Lesson 3 Homeowrk (except it's not fkn working yet)

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        static string name = GetString("Please enter name:");
+        static int age = GetInt("Please enter age:");
+        static float weight = GetFloat("Please enter weight (in kg):");
+        static float height = GetFloat("Please enter height (in cm):");
+        static float bmi = GetBMI();
+
+
+        public static void Demo()
+        {
+
+            Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
+            Console.WriteLine("Their BMI is: " + bmi);
+        }
+
+        public static string GetString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static int GetInt(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static float GetFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
+
+        public static float GetBMI()
+        {
+            return (float)Math.Round(weight / ((height / 100f) * (height / 100f)), 2);
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,19 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter age:");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter weight (in kg):");
-            float weight = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter height (in cm):");
-            float height = float.Parse(Console.ReadLine());
-
-            float bmi = weight / ((height / 100) * (height / 100));
-
-            Console.WriteLine(name + " is " + age + " years old, their weight is " + weight + " kg, and their height is " + height + " cm.");
-            Console.WriteLine("Their BMI is: " + bmi);
+            Lesson3.Demo();
         }
     }
 }
