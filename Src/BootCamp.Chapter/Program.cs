e7e31111c91a2d3a6191dcc0ecd6cc86aad5f7f8
Laura <Laura@DESKTOP-DIU19H8>
2020-06-28 18:41:01 +0300

    Corrected code with methods

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,30 +6,53 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Person();
-            Person();
+            PrintPersonInformation();
+            PrintPersonInformation();
         }
-
-        static void Person()
+        static string ReadName()
         {
             Console.WriteLine("Enter name");
             string name = Console.ReadLine();
+            return name;
+        }
+        static int ReadAge()
+        {
             Console.WriteLine("Enter age ");
             string age = Console.ReadLine();
-            int.Parse(age);
-            Console.WriteLine("Enter weight ");
+            int realAge = int.Parse(age);
+            return realAge;
+        }
+        public static double ReadWeight()
+        {
+            Console.WriteLine("Enter weight in kg ");
             string weight = Console.ReadLine();
-            double weightInDouble = double.Parse(weight);
+            double weightInKilograms = double.Parse(weight);
+            return weightInKilograms;
+        }
+        static double ReadHeight()
+        {
             Console.WriteLine("Enter height in cm ");
             string height = Console.ReadLine();
-            double heightInDouble = double.Parse(height);
-            double heightInMeters = heightInDouble / 100.0;
-
-            double BMI = weightInDouble / (heightInMeters * heightInMeters);
+            double heightInCentemeters = double.Parse(height);
+            return heightInCentemeters;
+        }
+        static double CalculateBMI(double weight, double height)
+        {
+            double heightInMeters = height / 100.0;
+            double BMI = weight / (heightInMeters * heightInMeters);
+            return BMI;
+        }
+        static void PrintPersonInformation()
+        {
+            string name = ReadName();
+            int age = ReadAge();
+            double weight = ReadWeight();
+            double height = ReadHeight();
 
             Console.WriteLine("You have entered:");
             Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
-            Console.WriteLine("BMI: " + BMI);
+            Console.WriteLine("BMI: " + CalculateBMI(weight, height));
         }
     }
 }
+
