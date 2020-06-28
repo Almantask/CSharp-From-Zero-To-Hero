eb715a5bbe89dc0dfdf785bf1839e23b1ffca601
Laura <Laura@DESKTOP-DIU19H8>
2020-06-28 13:17:51 +0300

    Reading from console info and calculating BMI from provided information

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter age ");
+            string age = Console.ReadLine();
+            int.Parse(age);
+            Console.WriteLine("Enter weight ");
+            string weight = Console.ReadLine();
+            double weightInDouble = double.Parse(weight);
+            Console.WriteLine("Enter height in cm ");
+            string height = Console.ReadLine();
+            double heightInDouble = double.Parse(height);
+            double heightInMeters = heightInDouble / 100.0;
+
+            double BMI = weightInDouble / (heightInMeters * heightInMeters);
+
+            Console.WriteLine("You have entered:");
+            Console.WriteLine(name+" is " + age+ " years old, his weight is " + weight+" kg and his height is "+height+" cm.");
+            Console.WriteLine("BMI: " + BMI);
         }
     }
 }
