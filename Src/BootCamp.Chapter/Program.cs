eb99f6dfd2303f4a027b5ae95f58bd45b87a36ea
Aga Kesik <aga.k396@gmail.com>
2020-07-09 14:56:13 +0200

    Added person 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Person 1:");
             Console.Write("Please provide your name: ");
             string name = Console.ReadLine();
             Console.Write("Please provide your surname: ");
@@ -24,6 +25,24 @@ namespace BootCamp.Chapter
             double bmi = weight / (heightInMeters * heightInMeters);
             Console.WriteLine("Their BMI is: " + bmi);
 
+            // Person 2
+            Console.WriteLine("Person 2:");
+            Console.Write("Please provide your name: ");
+            name = Console.ReadLine();
+            Console.Write("Please provide your surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Please provide your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Please provide your weigth in kg: ");
+            weight = int.Parse(Console.ReadLine());
+            Console.Write("Please provide your height in cm: ");
+            height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, their weight is " + weight + " kg and their height is " + height + "cm.");
+
+            heightInMeters = height / 100;
+            bmi = weight / (heightInMeters * heightInMeters);
+            Console.WriteLine("Their BMI is: " + bmi);
         }
     }
 }
