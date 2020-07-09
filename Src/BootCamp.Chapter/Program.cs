5463c9a3c9ee8f36c529cd2ffbf11fa54b26e3bf
Aga Kesik <aga.k396@gmail.com>
2020-07-09 14:27:24 +0200

    Chapter1/Homework/1and2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please provide your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please provide your surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Please provide your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Please provide your weigth in kg: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.Write("Please provide your height in cm: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, their weight is " + weight + " kg and their height is " + height + "cm.");
+
+            // BMI is (weight in kg)/(height square in m)
+            double heightInMeters = height / 100;
+            double bmi = weight / (heightInMeters * heightInMeters);
+            Console.WriteLine("Their BMI is: " + bmi);
+
         }
     }
 }
