d4e5453554897ee2b7d39c1121ce30daea9725ba
Shujat Khalid <khalidshujat@gmail.com>
2020-07-10 01:07:53 +0100

    Mostly completed homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,27 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What is your weight in kg");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your height in cm");
+            float height = float.Parse(Console.ReadLine());
+
+            float mSquared = (height / 100) * (height / 100);
+            float bmi = weight / mSquared;
+
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            Console.WriteLine("BMI = " + bmi);
         }
     }
 }
