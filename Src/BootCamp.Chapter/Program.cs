a5ce115a96ddfd9f3d3aee5aa587ccd49a573bf3
Shujat Khalid <khalidshujat@gmail.com>
2020-07-10 09:21:22 +0100

    Completed Homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BootCamp.Chapter
 {
@@ -6,27 +7,42 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
-            
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
+            static void bmiCalculator() {
+                Console.WriteLine("What is your first name?");
+                string firstName = Console.ReadLine();
 
-            Console.WriteLine("What is your age?");
-            var age = Console.ReadLine();
+                Console.WriteLine("What is your last name?");
+                string lastName = Console.ReadLine();
 
-            Console.WriteLine("What is your weight in kg");
-            float weight = float.Parse(Console.ReadLine());
+                Console.WriteLine("What is your age?");
+                var age = Console.ReadLine();
 
-            Console.WriteLine("What is your height in cm");
-            float height = float.Parse(Console.ReadLine());
+                Console.WriteLine("What is your weight in kg");
+                float weight = float.Parse(Console.ReadLine());
 
-            float mSquared = (height / 100) * (height / 100);
-            float bmi = weight / mSquared;
+                Console.WriteLine("What is your height in cm");
+                float height = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+                float mSquared = (height / 100) * (height / 100);
+                float bmi = weight / mSquared;
 
-            Console.WriteLine("BMI = " + bmi);
+                Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+                Console.WriteLine("BMI = " + bmi);
+            }
+
+            bmiCalculator();
+
+            Console.WriteLine("Do you want to continue? Y/N");
+            var answer = Console.ReadLine();
+            if (answer == "Y")
+            {
+                bmiCalculator();
+            }
+            else
+            {
+                Console.WriteLine("The End");
+            }
         }
     }
 }
