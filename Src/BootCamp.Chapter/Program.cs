048308fe0519b6d154a06cb7769114d19ca4800d
Enoch Robokoff <enochrobokoff@gmail.com>
2020-07-15 11:09:55 -0500

    Program for homework 1 from chapter 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -4,9 +4,35 @@ namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Enter weight (lbs): ");
+            float weight = float.Parse(Console.ReadLine());
+            Console.Write("Enter height (in): ");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n{name} is {age} years old. Their weight is {weight}lbs and height is {height}in.");
+            float calcBMI = (703 * weight) / (height * height);
+            Console.WriteLine($"{name}'s BMI is {calcBMI}.\n");
+
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Enter weight (lbs): ");
+            weight = float.Parse(Console.ReadLine());
+            Console.Write("Enter height (in): ");
+            height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n{name} is {age} years old. Their weight is {weight}lbs and height is {height}in.");
+            calcBMI = (703 * weight) / (height * height);
+            Console.WriteLine($"{name}'s BMI is {calcBMI}.\n");
+
+            Console.ReadKey();
         }
     }
 }
