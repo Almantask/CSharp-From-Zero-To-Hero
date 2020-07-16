4cf854ea1abfa08c3a918fc971e111a5f4d42fb8
Aga Kesik <aga.k396@gmail.com>
2020-07-16 13:43:52 +0200

    Create working function on BalanceStats

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,16 @@
             // - FindPersonWithBiggestLoss
             // - FindRichestPerson
             // - FindMostPoorPerson
+            var peopleBalances = PeoplesBalances.Balances;
+            string highestEver = BalanceStats.FindHighestBalanceEver(peopleBalances);
+            System.Console.WriteLine(highestEver);
+            string biggestLoss = BalanceStats.FindPersonWithBiggestLoss(peopleBalances);
+            System.Console.WriteLine(biggestLoss);
+            string richest = BalanceStats.FindRichestPerson(peopleBalances);
+            System.Console.WriteLine(richest);
+            string poorest = BalanceStats.FindMostPoorPerson(peopleBalances);
+            System.Console.WriteLine(poorest);
+
         }
     }
 }
