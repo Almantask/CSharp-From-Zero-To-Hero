09b3b922a1844a53367849a45f5344e257906839
Aga Kesik <aga.k396@gmail.com>
2020-07-16 14:47:19 +0200

    Print all operation on balances using text tables

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,15 +9,19 @@
             // - FindPersonWithBiggestLoss
             // - FindRichestPerson
             // - FindMostPoorPerson
-            System.Console.WriteLine("--------------------------");
-            string table = TextTable.Build("Hello{Environment.NewLine}World!", 0);
-            System.Console.WriteLine(table);
-            System.Console.WriteLine("--------------------------");
-            table = TextTable.Build("Hello{Environment.NewLine}World!", 1);
-            System.Console.WriteLine(table);
-            System.Console.WriteLine("--------------------------");
-            table = TextTable.Build("Hello{Environment.NewLine}World!", 2);
-            System.Console.WriteLine(table);
+            var peopleBalances = PeoplesBalances.Balances;
+
+            string highestEver = TextTable.Build(BalanceStats.FindHighestBalanceEver(peopleBalances), 3);
+            System.Console.WriteLine(highestEver);
+
+            string biggestLoss = TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(peopleBalances), 3);
+            System.Console.WriteLine(biggestLoss);
+
+            string richest = TextTable.Build(BalanceStats.FindRichestPerson(peopleBalances), 3);
+            System.Console.WriteLine(richest);
+
+            string poorest = TextTable.Build(BalanceStats.FindMostPoorPerson(peopleBalances), 3);
+            System.Console.WriteLine(poorest);
         }
     }
 }
