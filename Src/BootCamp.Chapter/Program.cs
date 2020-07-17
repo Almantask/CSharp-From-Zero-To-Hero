68cc9d2a2f9c6453fc418c362d793ae19757ceb9
Tailor <kralle37@gmail.com>
2020-07-17 14:15:19 +0200

    First draft.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Please enter your weight (in kg): ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Please enter your lenght (in cm): ");
+            double length = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (length * length/10000);
+
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg " +
+                $"and his height is {length} cm.");
+            Console.WriteLine($"{name} {surname}'s BMI is: {bmi:F1}");
+
+            Console.ReadLine();
         }
+
     }
 }
