bcce9c97db08971747d20807164b78605e4f5924
Zacc806 <66484094+Zacc806@users.noreply.github.com>
2020-07-19 00:44:21 +0300

    Update Program.cs

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Globalization;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input Name: ");
+            string name = Console.ReadLine();
+            string title = name;
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            title = textInfo.ToTitleCase(title); 
+
+            Console.Write("Input Surname: ");
+            string surname = Console.ReadLine();
+            string surtitle = surname;
+            surtitle = textInfo.ToTitleCase(surtitle); 
+
+            Console.Write("Input Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Input Weight (in kg): ");
+            var weight = double.Parse(Console.ReadLine());
+            Console.Write("Input Height (in cm): ");
+            var height = double.Parse(Console.ReadLine());
+
+            var BMI = weight / ((height*0.01)*(height*0.01));
+            BMI = (float) BMI;
+
+            Console.WriteLine($"{title} {surtitle} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Console.WriteLine($"Also the BMI is {BMI}.");
+
         }
     }
 }
