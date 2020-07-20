4159f138ad9baa082eea7b12c636ed18f4f7dd0e
Yossie21 <janis.jaunzems2005@gmail.com>
2020-07-20 17:29:55 +0300

    First Chapter, First Lesson - Homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int weight = int.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            double bmi = weight/((height/100) * (height / 100));
+
+            Console.WriteLine("His BMI is " + bmi +".");
+
+            // Up - first person, down - second.
+
+            string secondName = Console.ReadLine();
+            string secondSurname = Console.ReadLine();
+            int secondAge = int.Parse(Console.ReadLine());
+            int secondWeight = int.Parse(Console.ReadLine());
+            double secondHeight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(secondName + " " + secondSurname + " is " + secondAge + " years old, his weight is " + secondWeight + " kg and his height is " + secondHeight + " cm.");
+
+            double secondBmi = secondWeight / ((secondHeight / 100) * (secondHeight / 100));
+
+            Console.WriteLine("His BMI is " + secondBmi + ".");
+
+            Console.ReadLine();
+
         }
     }
 }
