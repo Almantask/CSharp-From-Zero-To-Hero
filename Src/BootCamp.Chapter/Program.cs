ac2a78443e02e21734a35282ab8c9e8b14420049
hyp <james.t.mundy@gmail.com>
2020-07-20 17:43:06 -0400

    Chapter 1 Homework copied into a fork of the CSInn branch.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int myAge = 35;
+            int rAge = 68;
+            string myFirstName = "James";
+            string myLastName = "Mundy";
+            string rFirstName = "Rose";
+            string rLastName = "Mundy";
+            double myWeight = 111.584;
+            double myHeight = 185.42000000000002;
+            double rWeight = 80;
+            double rHeight = 150;
+            double BMIFormula = myWeight / (myHeight * myHeight) * 703;
+            double rBMIForuma = rWeight / (rHeight * rHeight) * 703;
+
+            Console.WriteLine(myFirstName + " " + myLastName + " is " + myAge + " years old, his weight is " + myWeight +
+                " kg and his height is " + myHeight + ". " + myFirstName + "'s BMI is " + BMIFormula);
+
+            Console.WriteLine(rFirstName + " " + rLastName + " is " + rAge + " years old, his weight is " + rWeight +
+                " kg and her height is " + rHeight + ". " + rFirstName + "'s BMI is " + rBMIForuma);
+
         }
     }
 }
