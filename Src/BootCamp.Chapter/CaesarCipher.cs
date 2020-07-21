d6d61afc4a72afcc365964d5b28b2278dd7801b8
Aga Kesik <aga.k396@gmail.com>
2020-07-21 14:17:49 +0200

    Create CaesarCipher methods for encrypting and decrrypting

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,31 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (String.IsNullOrEmpty(message)) return message;
+
+            var encryptedMessage = new StringBuilder();
+
+            for (int i = 0; i <= message.Length - 1; i++)
+            {
+                char encryptedCharacter = Convert.ToChar(message[i] + shift);
+                encryptedMessage.Append(encryptedCharacter);
+            }
+            return encryptedMessage.ToString();
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+
+            if (String.IsNullOrEmpty(message)) return message;
+
+            var decryptedMessage = new StringBuilder();
+
+            for (int i = 0; i <= message.Length - 1; i++)
+            {
+                char decryptedCharacter = Convert.ToChar(message[i] - shift);
+                decryptedMessage.Append(decryptedCharacter);
+            }
+            return decryptedMessage.ToString();
         }
     }
 }
