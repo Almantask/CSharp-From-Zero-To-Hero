d565d6bd0fcce84117b8feef23e1b1c14523e1ea
Aga Kesik <aga.k396@gmail.com>
2020-07-28 11:46:47 +0200

    Move all logic from main to a seperate class

---
+++ Src/BootCamp.Chapter/LoggedProgram.cs
@@ -0,0 +1,33 @@
+﻿using BootCamp.Chapter.Logger;
+using System;
+
+namespace BootCamp.Chapter
+{
+    public static class LoggedProgram
+    {
+        public static void Run()
+        {
+            var logger = ChooseLogger();
+            logger.LogMessage("Starting program.");
+            logger.LogMessage("Program shutting down.");
+        }
+
+        private static ILogger ChooseLogger()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please choose logger option: 'a' - console; 'b' - file");
+                string loggerChoice = Console.ReadLine();
+                if (loggerChoice == "a")
+                {
+                    return new ConsoleLogger();
+                }
+                else if (loggerChoice == "b")
+                {
+                    return new FileLogger();
+                }
+                Console.WriteLine("Please choose: 'a' - console; 'b' - file");
+            }
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,27 +7,9 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            var logger = ChooseLogger();
-            logger.LogMessage("Starting program.");
-            logger.LogMessage("Program shutting down.");
+            LoggedProgram.Run();
         }
 
-        static public ILogger ChooseLogger()
-        {
-            while (true)
-            {
-                Console.WriteLine("Please choose logger option: 'a' - console; 'b' - file");
-                string loggerChoice = Console.ReadLine();
-                if (loggerChoice == "a")
-                {
-                    return new ConsoleLogger();
-                }
-                else if (loggerChoice == "b")
-                {
-                    return new FileLogger();
-                }
-                Console.WriteLine("Please choose: 'a' - console; 'b' - file");
-            }
-        }
+        
     }
 }
