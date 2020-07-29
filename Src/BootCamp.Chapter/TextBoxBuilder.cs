f5fc7c1f4907f0742cb2b512b87e4f468b72d9c4
MatthewHole23 <matthew.hole23@gmail.com>
2020-07-29 16:49:13 +0100

    Changes made as per review comments - Implementation of State Machine still remaining.

---
+++ Src/BootCamp.Chapter/TextBoxBuilder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class TextBoxBuilder
+    {
+        private readonly string _message;
+        private readonly TextTableAttribute _textTableAttributes;
+        private readonly int _messageLength;
+        private readonly int _numberOfLines;
+        private readonly int _numberOfMainLine;
+        private readonly string _paddingString;
+        private StringBuilder sb = new StringBuilder();
+        private const int DivideByTwo = 2;
+        private const int AddOne = 1;
+
+        public TextBoxBuilder(TextTableAttribute textTableAttributes, string message)
+        {
+            _textTableAttributes = textTableAttributes;
+            _message = message;
+            _messageLength = _message.Length + (_textTableAttributes.Padding * DivideByTwo);
+            _numberOfLines = 3 + (_textTableAttributes.Padding * DivideByTwo);
+            _numberOfMainLine = (_numberOfLines + AddOne) / DivideByTwo;
+            _paddingString = new string(' ', _textTableAttributes.Padding);
+        }
+
+        public string BuildTextBox()
+        {
+            string topAndBottom = BuildTopAndBottomLines();
+            string mainLine = BuildMainLine();
+            string mainLineWithPadding = BuildMainLineWithPadding();
+            string paddingLine = BuildPaddingLine();
+
+            for (int i = 1; i < _numberOfLines + AddOne; i++)
+            {
+                if (i == 1 || i == _numberOfLines)
+                {
+                    sb.AppendLine(topAndBottom);
+                }
+                else if (i != _numberOfMainLine && _textTableAttributes.Padding != 0)
+                {
+                    sb.AppendLine(paddingLine);
+                }
+                else if (i == _numberOfMainLine && _textTableAttributes.Padding == 0)
+                {
+                    sb.AppendLine(mainLine);
+                }
+                else if (i == _numberOfMainLine && _textTableAttributes.Padding != 0)
+                {
+                    sb.AppendLine(mainLineWithPadding);
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+        private string BuildTopAndBottomLines()
+        {
+            return $"{_textTableAttributes.Corner}{new String(_textTableAttributes.Horizontal, _messageLength)}{_textTableAttributes.Corner}";
+        }
+
+        private string BuildMainLine()
+        {
+            return $"{_textTableAttributes.Vertical}{_message}{_textTableAttributes.Vertical}";
+        }
+
+        private string BuildMainLineWithPadding()
+        {
+            return $"{_textTableAttributes.Vertical}{_paddingString}{_message}{_paddingString}{_textTableAttributes.Vertical}";
+        }
+
+        private string BuildPaddingLine()
+        {
+            return $"{_textTableAttributes.Vertical}{new string(' ', _messageLength)}{_textTableAttributes.Vertical}";
+        }
+    }
+}
--- Src/BootCamp.Chapter/TextBoxPrinter.cs
+++ Src/BootCamp.Chapter/TextBoxPrinter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Mime;
-using System.Text;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter
 {
     public static class TextBoxPrinter
     {
@@ -12,47 +7,9 @@ namespace BootCamp.Chapter
             var getTextTableAttribute = AttributesGetter.GetClassAttribute<TextTableAttribute>(obj.GetType());
             if (getTextTableAttribute == null) return obj.ToString();
 
-            var table = GenerateTable(getTextTableAttribute, obj.ToString());
-            Console.WriteLine(table);
+            TextBoxBuilder textBoxBuilder = new TextBoxBuilder(getTextTableAttribute, obj.ToString());
 
-            return GenerateTable(getTextTableAttribute, obj.ToString());
-        }
-
-        private static string GenerateTable(TextTableAttribute textTableAttributes, string message)
-        {
-            int messageLength = message.Length + (textTableAttributes.Padding * 2);
-            int numberOfLines = 3 + (textTableAttributes.Padding * 2);
-            int numberOfMainLine = (numberOfLines + 1) / 2;
-            string paddingString = new string(' ', textTableAttributes.Padding);
-            StringBuilder sb = new StringBuilder();
-
-            string topAndBottom =
-                $"{textTableAttributes.Corner}{new String(textTableAttributes.Horizontal, messageLength)}{textTableAttributes.Corner}";
-            string mainLine = $"{textTableAttributes.Vertical}{message}{textTableAttributes.Vertical}";
-            string mainLineWithPadding = $"{textTableAttributes.Vertical}{paddingString}{message}{paddingString}{textTableAttributes.Vertical}";
-            string paddingLine =
-                $"{textTableAttributes.Vertical}{new string(' ', messageLength)}{textTableAttributes.Vertical}";
-
-            for (int i = 1; i < numberOfLines+1; i++)
-            {
-                if (i == 1 || i == numberOfLines)
-                {
-                    sb.AppendLine(topAndBottom);
-                }
-                else if (i != numberOfMainLine && textTableAttributes.Padding != 0)
-                {
-                    sb.AppendLine(paddingLine);
-                }
-                else if (i == numberOfMainLine && textTableAttributes.Padding == 0)
-                {
-                    sb.AppendLine(mainLine);
-                }
-                else if (i == numberOfMainLine && textTableAttributes.Padding != 0)
-                {
-                    sb.AppendLine(mainLineWithPadding);
-                }
-            }
-            return sb.ToString().Trim();
+            return textBoxBuilder.BuildTextBox();
         }
     }
 }
