517d8af3ec6e5b54ae53d50a90688973db82b87c
Laura <Laura@DESKTOP-DIU19H8>
2020-07-30 18:21:00 +0300

    added program

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,8 +9,18 @@ namespace BootCamp.Chapter
 {
     class Program
     {
+        const string cleanedFile = @"Input\Balances.clean";
+
         static void Main(string[] args)
         {
+            var insideFile = File.ReadAllText(cleanedFile);
+            var peopleAndBalances = insideFile.Split(Environment.NewLine);
+
+
+            Console.WriteLine(Checks.Build(Checks.FindHighestBalanceEver(peopleAndBalances), 3));
+            Console.WriteLine(Checks.Build(Checks.FindMostPoorPerson(peopleAndBalances), 3));
+            Console.WriteLine(Checks.Build(Checks.FindPersonWithBiggestLoss(peopleAndBalances), 3));
+            Console.WriteLine(Checks.Build(Checks.FindRichestPerson(peopleAndBalances), 3));
         }
     }
 }
