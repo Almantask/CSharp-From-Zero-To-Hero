8d7baea133d8e905573d02f958fe7b75694f84ab
Rickolajten <svenstaviken@hotmail.com>
2020-07-31 22:24:21 +0200

    Wrote the code for Tom Jeffersson and his BMI.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace BootCamp.Chapter
+namespace BootCamp.Chapter1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Tom Jefferson";
+            var age = 19;
+            var weight = 50;
+            var height = 156.5;
+
+            var bmi = 50 / (1.565 * 1.565);
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Console.WriteLine($"{name}'s BMI is {bmi}");
         }
     }
 }
