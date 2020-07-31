862995b2309a2bde0e09ea4a42b0089a63ddc576
Aga Kesik <aga.k396@gmail.com>
2020-07-31 11:15:42 +0200

    Finished all task with passing tests

--- Src/BootCamp.Chapter/Credentials.cs
+++ Src/BootCamp.Chapter/Credentials.cs
@@ -1,22 +1,64 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     // TODO: make a struct and add validation and other needed methods (if needed)
     public class Credentials
     {
-        public string Username;
-        public string Password;
+        public readonly string Username;
+        public readonly string Password;
 
         public Credentials(string username, string password)
         {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentException("You must provide username");
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("You must provide password");
+            }
             Username = username;
-            Password = password;
+            var encodedPassword = new EncodedPassword(password);
+            Password = encodedPassword.PasswordAfterEncoding;
         }
 
         // TODO: Implement properly.
         public static bool TryParse(string input, out Credentials credentials)
         {
             credentials = default;
-            return false;
+            if (string.IsNullOrEmpty(input)) return false;
+
+            var CredentialsParts = input.Split(",");
+            if (CredentialsParts.Length != 2) return false;
+
+            if (string.IsNullOrEmpty(CredentialsParts[0]) || string.IsNullOrEmpty(CredentialsParts[1])) return false;
+
+            credentials = new Credentials(CredentialsParts[0], CredentialsParts[1]);
+            return true;
+        }
+        public readonly struct EncodedPassword
+        {
+            public readonly string PasswordAfterEncoding { get; }
+            public EncodedPassword(string inputPassword)
+            {
+                var unicode = new UnicodeEncoding();
+                byte[] passwordInByteArray = unicode.GetBytes(inputPassword);
+                PasswordAfterEncoding = string.Join(" ", passwordInByteArray);
+            }
+        }
+
+        public string GetCredentials()
+        {
+            Console.WriteLine($"string password: {Password}");
+            return $"{Username},{Password}";
+        }
+
+        public bool Equals(Credentials credentialsToCompare)
+        {
+            return (Username.Equals(credentialsToCompare.Username) && Password.Equals(credentialsToCompare.Password));
         }
     }
 }
--- Src/BootCamp.Chapter/CredentialsManager.cs
+++ Src/BootCamp.Chapter/CredentialsManager.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.IO;
+
+namespace BootCamp.Chapter
 {
     public class CredentialsManager
     {
@@ -6,19 +9,45 @@
 
         public CredentialsManager(string credentialsFile)
         {
+            if (string.IsNullOrEmpty(credentialsFile))
+            {
+                throw new ArgumentException("File path cannot be empty");
+            }
             _credentialsFile = credentialsFile;
         }
 
         // TODO: load credentials and check for equality.
         public bool Login(Credentials credentials)
         {
+            string credentialsToFind = credentials.GetCredentials();
+
+            string fileContent = File.ReadAllText(_credentialsFile);
+            string[] credentialsInFile = fileContent.Split(Environment.NewLine);
+
+            foreach (var credential in credentialsInFile)
+            {
+                if (credential.Equals(credentialsToFind)) return true;
+            }
+
             return false;
         }
 
         // TODO: store credentials in credentials file.
         public void Register(Credentials credentials)
         {
+            string credentialsToAppend = credentials.GetCredentials();
 
+            if (File.Exists(_credentialsFile) && !String.IsNullOrEmpty(File.ReadAllText(_credentialsFile)))
+            {
+                credentialsToAppend = $"{Environment.NewLine}{credentialsToAppend}";
+            }
+
+            File.AppendAllText(_credentialsFile, credentialsToAppend);
+        }
+
+        private bool CheckIfCredentialsExistInFile()
+        {
+            return false;
         }
     }
 }
