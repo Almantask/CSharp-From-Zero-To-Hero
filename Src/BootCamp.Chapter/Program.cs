771e4f0c8897eb9662f17c06b2935495a23222e9
claytonellison <44485854+claytonellison@users.noreply.github.com>
2020-08-04 15:51:26 -0500

    Created some additional variables and added them to the sentence.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,16 +7,20 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.");
+            string TomsName = "Tom Jefferson";
+            int TomsAge = 19;
             int TomsWeight = 50;
             float TomsHeight = 156.5f;
             float TomsBMI = ((TomsWeight / TomsHeight)/TomsHeight)*10000;
-            Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. His BMI is " + TomsBMI + ".");
+            Console.WriteLine(TomsName + " is " + TomsAge + " years old, his weight is " + TomsWeight + " kg, his height is " + TomsHeight + " cm, and his BMI is " + TomsBMI + ".");
 
             Console.WriteLine("Samson is 30 years old, his weight is 200 lbs and his height is 69 inches.");
+            string SamsonsName = "Samson";
+            int SamsonsAge = 30;
             float SamsonsWeight = 200;
             float SamsonsHeight = 69;
             float SamsonsBMI = ((SamsonsWeight / SamsonsHeight)/SamsonsHeight)*703;
-            Console.WriteLine("Samson is 30 years old, his weight is 200 lbs and his height is 69 inches. His BMI is " + SamsonsBMI + ".");
+            Console.WriteLine(SamsonsName + " is " + SamsonsAge + " years old, his weight is " + SamsonsWeight + " lbs, his height is " + SamsonsHeight + " inches, and his BMI is " + SamsonsBMI + ".");
         }
     }
 }
