f3812c0ccb237f47ab58fa45f1594e68f44b0ac7
mayank2441 <40947269+mayank2441@users.noreply.github.com>
2020-08-06 20:00:26 +0530

    Homework 1 and 2 with Softcoding. I hope i didn't merge into the master file again.

--- 1and2/1and2/Program.cs
+++ 1and2/1and2/Program.cs
@@ -11,17 +11,29 @@ namespace _1and2
         static void Main(string[] args)
         { // PRess enter or type anything and the the code will appear.
             string name = Console.ReadLine();
-            Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.");
 
+            String TomName = "Tom Jefferson";
+             int TomAge = 19;
+            int TomWeight = 50;
+            float TomHeight = 156.5f;
+
+            Console.WriteLine(TomName + "is " + TomAge + " years old , his weight is "+TomWeight+ "kg and his height is " +TomHeight+ "cm" + ".");
             var TomBMI = (50 / (1.565*1.565));
             Console.WriteLine("Tom's Body mass index is " + TomBMI);
 
-            string Rahul = Console.ReadLine();
+            
 
-            Console.WriteLine("Rahul is 25 years old, his weight is 80kg and his height is 180 cm");
+            string RahulName = "Rahul Reddy";
+                int RahulAge = 25;
+            int RahulWeight = 80;
+            int RahulHeight = 180;
+
+            Console.WriteLine(RahulName + "is " + RahulAge + "years old , his weight is" + RahulWeight + "kg and his height is " + RahulHeight + "cm" + ".");
             var RahulBMI = (80 / (1.80 * 1.80));
-            Console.WriteLine("Rahul Body mass index is " + RahulBMI );
+            Console.WriteLine("Rahul Body Mass Index is "+ RahulBMI);
+             
 
+            
           
         }
     }
