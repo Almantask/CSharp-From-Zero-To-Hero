058ac6aff9efe86a06268c209a12b705d0a26aec
mayank2441 <40947269+mayank2441@users.noreply.github.com>
2020-08-06 14:25:58 +0530

    I did the homework of chapter 1 and 2 but still having difficulty with some part. Please check how do i improve myself.

---
+++ 1and2/1and2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1and2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // PRess enter or type anything and the the code will appear.
+            string name = Console.ReadLine();
+            Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.");
+
+            var TomBMI = (50 / (1.565*1.565));
+            Console.WriteLine("Tom's Body mass index is " + TomBMI);
+
+            string Rahul = Console.ReadLine();
+
+            Console.WriteLine("Rahul is 25 years old, his weight is 80kg and his height is 180 cm");
+            var RahulBMI = (80 / (1.80 * 1.80));
+            Console.WriteLine("Rahul Body mass index is " + RahulBMI );
+
+          
+        }
+    }
+}
