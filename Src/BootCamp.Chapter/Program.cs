570140bfa8c374184149cbb80c7b6cc06a62f833
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-08-08 20:46:17 +0200

    Homework 1-1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,37 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name 1:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Surname 1:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Age 1:");
+            var age = Console.ReadLine();
+            Console.WriteLine("Weight 1 in kg:");
+            var weight = Console.ReadLine();
+            Console.WriteLine("Height 1 in cm:");
+            var height = Console.ReadLine();
+            Console.WriteLine($"{name} {surname} is {age} old, his weight is {weight} kg and his height is {height} cm.");
+            double weightNr = Convert.ToDouble(weight);
+            double heightNr = Convert.ToDouble(height) * .01;
+            double BMI = weightNr / (heightNr * heightNr);
+            Console.WriteLine($"{name}'s BMI: {BMI}");
+
+            Console.WriteLine("Name 2:");
+            name = Console.ReadLine();
+            Console.WriteLine("Surname 2:");
+            surname = Console.ReadLine();
+            Console.WriteLine("Age 2:");
+            age = Console.ReadLine();
+            Console.WriteLine("Weight 2 in kg:");
+            weight = Console.ReadLine();
+            Console.WriteLine("Height 2 in cm:");
+            height = Console.ReadLine();
+            Console.WriteLine($"{name} {surname} is {age} old, his weight is {weight} kg and his height is {height} cm.");
+            weightNr = Convert.ToDouble(weight);
+            heightNr = Convert.ToDouble(height) * .01;
+            BMI = weightNr / (heightNr * heightNr);
+            Console.WriteLine($"{name}'s BMI: {BMI}");
         }
     }
 }
