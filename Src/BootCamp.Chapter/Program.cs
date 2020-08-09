579ad1e9b9fd675a8cab6c6c4947918ad14fe661
malamunyx <68112724+malamunyx@users.noreply.github.com>
2020-08-08 19:31:30 -0700

    Add Files and replace original Program.cs
    Homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,43 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name, surname, ageInput, weightInput, heightInput;
+            int age;
+            double weight, height, bodyMassIndex;
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Surname: ");
+            surname = Console.ReadLine();
+
+            Console.Write("Age: ");
+            ageInput = Console.ReadLine();
+
+            Console.Write("Weight: ");
+            weightInput = Console.ReadLine();
+
+            Console.Write("Height: ");
+            heightInput = Console.ReadLine();
+
+            age = int.Parse(ageInput);
+            // Convert age string input weight into int
+
+            weight = double.Parse(weightInput);
+            // Convert weight string input weight into double
+
+            height = double.Parse(heightInput);
+            // Convert height string input into a double
+
+            double heightInMetres = height / 100;
+            // Height is measured in cm, but BMI requires height to be in metres
+
+            bodyMassIndex = (weight) / (heightInMetres * heightInMetres);
+
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm");
+            Console.WriteLine($"Body Mass Index: {bodyMassIndex}");
+
+            Console.ReadKey();
         }
     }
 }
