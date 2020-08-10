1bed67e49718b0ab456cc9ec1536a099d63413e6
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-08-10 10:58:48 +0200

    Working functions

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,93 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            var i = Console.ReadLine();
+            var isNumeric = int.TryParse(i, out int number);
+            if (String.IsNullOrEmpty(i))
+            {
+                return 0;
+            }
+            else if (!isNumeric)
+            {
+                Console.Write($"\"{i}\" is not a valid number.");
+                return -1;
+            }
+            else if (isNumeric && number == 0)
+            {
+                Console.Write($"\"{i}\" is not a valid number.");
+                return 0;
+            }
+            return number;
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
-        }
+            Console.WriteLine(message);
+            var s = Console.ReadLine();
+            if (String.IsNullOrEmpty(s))
+            {
+                Console.Write("Name cannot be empty.");
+                return "-";
 
+            }
+            else
+            {
+                return s;
+            }
+        }
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            var f = Console.ReadLine();
+            var isNumeric = float.TryParse(f, System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.InvariantCulture, out float number);
+            if (String.IsNullOrEmpty(f))
+            {
+                return 0;
+            }
+            else if (!isNumeric && number != 0)
+            {
+                Console.Write($"\"{f}\" is not a valid number.");
+                return -1;
+            }
+            else if (!isNumeric)
+            {
+                Console.Write($"\"{f}\" is not a valid number.");
+                return -1;
+            }
+            else if (isNumeric && number == 0)
+            {
+                //Console.WriteLine($"\"{f}\" is not a valid number.");
+                return 0f;
+            }
+            return number;
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            if (height <= 0 && weight <= 0)
+            {
+                Console.WriteLine("Failed calculating BMI. Reason:");
+                Console.WriteLine($"Weight cannot be equal or less than zero, but was {weight}.");
+                Console.WriteLine($"Height cannot be less than zero, but was {height}.");
+                return -1;
+            }
+            else if (height <= 0)
+            {
+                Console.WriteLine("Failed calculating BMI. Reason:");
+                Console.WriteLine($"Height cannot be equal or less than zero, but was {height}.");
+                return -1;
+            }
+            else if (weight <= 0)
+            {
+                Console.WriteLine("Failed calculating BMI. Reason:");
+                Console.WriteLine($"Weight cannot be equal or less than zero, but was {weight}.");
+                return -1;
+            }
+            else
+            {
+                return weight / (height * height);
+            }
         }
     }
 }
