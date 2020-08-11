e5f5c5627f38524066ef867f452b710443f5090f
acupadhy <achyutpdh249@gmail.com>
2020-08-11 03:20:43 -0400

    Complete code to Add user data

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,26 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("Enter your First Name:");
+            string firstname = Console.ReadLine();
+
+            System.Console.Write("Enter your Last Name: ");
+            string lastname = Console.ReadLine();
+
+            System.Console.Write("Enter our age:");
+            int age = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Enter your weight(kg):");
+            float weight = float.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Enter your height(cm)");
+            float height = float.Parse(Console.ReadLine());
+
+            System.Console.WriteLine($"{firstname} {lastname} is {age} years old, his weight is {weight} kg and his height is {height} cm");
+
+
+
+
         }
     }
 }
