fc1ee68cbd3bbe1567b23adf575193c0218b8b28
Yossie21 <janis.jaunzems2005@gmail.com>
2020-08-17 22:41:27 +0300

    Finally it is working!

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -66,7 +66,7 @@ namespace BootCamp.Chapter
         public static float AskingAndConvertingFLOAT(string message)
         {
             Console.WriteLine(message);
-            float theFloat = float.Parse(Console.ReadLine());
+            float theFloat = float.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
             return theFloat;
         }
     }
