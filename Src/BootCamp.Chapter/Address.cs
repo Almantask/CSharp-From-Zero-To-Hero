b1e9d653452bafd0ea00c0086d68b08174f43367
Matthew Hole <matthew.hole23@gmail.com>
2020-08-18 14:12:34 +0100

    First Commit - All test passing and operator overloading demonstrated.

--- Src/BootCamp.Chapter/Address.cs
+++ Src/BootCamp.Chapter/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -8,6 +9,7 @@ namespace BootCamp.Chapter
     {
         public string Recipient { get; }
         public string Street { get; }
+        private const int ExpectedLengthOfAddress = 7;
 
         public Address(string recipient, string street, string town, string county, string postalCode, string country, string houseNumber = "")
         {
@@ -18,7 +20,23 @@ namespace BootCamp.Chapter
         public static bool TryParse(string addressString, out Address address)
         {
             address = default;
-            return false;
+            string[] tempString = addressString?.Split(Environment.NewLine);
+            // Can refine to only check if key elements are missing i.e PostCode, Streetname and Building Number/Name
+            // Unsure what the is the best action. Either throw a new exception and capture the error or just return false? Will return false for benefit of tests
+            if (tempString?.Length != ExpectedLengthOfAddress) return false;
+
+            address = new Address(tempString[0], tempString[1], tempString[2], tempString[3], tempString[4], tempString[5]);
+            return true;
+        }
+
+        public static bool operator ==(Address address1, Address address2)
+        {
+            return address1?.Recipient == address2?.Recipient && address1?.Street == address2?.Street;
+        }
+
+        public static bool operator !=(Address address1, Address address2)
+        {
+            return address1?.Recipient != address2?.Recipient && address1?.Street != address2?.Street;
         }
     }
 }
