8edf8ab8cd305feb4bb6d85d223b8232d7935c95
Tailor <kralle37@gmail.com>
2020-09-02 20:19:58 +0200

    First draft of homework 10.

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -11,12 +12,46 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            return string.IsNullOrEmpty(message) ? message : EncryptedMessage(message,shift); 
+        }
+
+        private static string EncryptedMessage(string message, byte shift)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(message);
+
+            for (int i = 0; i < asciiBytes.Length; i++)
+            {
+                int newAsciiNumber = (asciiBytes[i] + shift) % 255;
+                asciiBytes[i] = (byte)newAsciiNumber;
+            }
+
+            char[] asciiChars = Encoding.ASCII.GetChars(asciiBytes);
+            return new string(asciiChars);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            return string.IsNullOrEmpty(message) ? message : DecryptedMessage(message, shift);
+        }
+
+        private static string DecryptedMessage(string message, byte shift)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(message);
+
+            for (int i = 0; i < asciiBytes.Length; i++)
+            {
+                int newAsciiNumber = (asciiBytes[i] - shift);
+                
+                if (newAsciiNumber < 0)
+                {
+                    newAsciiNumber = 255 + newAsciiNumber;
+                }
+
+                asciiBytes[i] = (byte)newAsciiNumber;
+            }
+
+            char[] asciiChars = Encoding.ASCII.GetChars(asciiBytes);
+            return new string(asciiChars);
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
