1d01bec51bbd5d30fa4bc797b8a01eb231f0ba95
GitSaranga <bsaranga@gmail.com>
2020-09-11 15:32:07 +0530

    Completed homework 1and2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,39 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numPeople = 2;
+
+            while(numPeople > 0)
+            {
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter weight (in kg): ");
+                double weight = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter height (in cm): ");
+                double height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+
+                BMIPrinter(weight, height);
+
+                numPeople--;
+            }
+
+            Console.ReadKey();
+        }
+
+        static void BMIPrinter(double weight, double height)
+        {
+            double BMI = weight / ((height / 100) * (height / 100));
+            Console.WriteLine($"BMI = {BMI}");
         }
     }
 }
