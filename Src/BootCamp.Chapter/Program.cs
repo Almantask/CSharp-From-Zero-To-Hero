d0b4a25e72d339094b0eae8bdeacdb4d54e08a03
Abhay Aysola <abhay.ayyasola@gmail.com>
2020-09-12 18:47:31 +0530

    Homework 1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,20 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Surname: ");
+            var surName = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            var age = Console.ReadLine();
+            Console.WriteLine("Weight");
+            var weight = Console.ReadLine();
+            Console.WriteLine("height");
+            var height = Console.ReadLine();
+
+            var print = name + " " + surName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.";
+
+            Console.WriteLine(print);
         }
     }
 }
