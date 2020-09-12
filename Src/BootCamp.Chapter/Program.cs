fca36aea8a941df1f118f32aec3a270c25156a82
Abhay Aysola <abhay.ayyasola@gmail.com>
2020-09-12 19:09:02 +0530

    Chapter1/Homework/1and2 Completed!

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace BootCamp.Chapter
 {
@@ -6,20 +7,31 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Name: ");
-            var name = Console.ReadLine();
-            Console.WriteLine("Surname: ");
-            var surName = Console.ReadLine();
-            Console.WriteLine("Age: ");
-            var age = Console.ReadLine();
-            Console.WriteLine("Weight");
-            var weight = Console.ReadLine();
-            Console.WriteLine("height");
-            var height = Console.ReadLine();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Surname: ");
+                var surName = Console.ReadLine();
+                Console.WriteLine("Age: ");
+                var age = Console.ReadLine();
+                Console.WriteLine("Weight");
+                var weight = Console.ReadLine();
+                Console.WriteLine("height");
+                var height = Console.ReadLine();
 
-            var print = name + " " + surName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.";
+                var statement = name + " " + surName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.";
 
-            Console.WriteLine(print);
+                Console.WriteLine(statement);
+
+                Console.WriteLine("His BMI is " + Bmi(double.Parse(weight), double.Parse(height)) + ".");
+            }
+        }
+
+        private static double Bmi(double weight, double height)
+        {
+            var BMI = weight / ((height / 100) * (height / 100));
+            return BMI;
         }
     }
 }
