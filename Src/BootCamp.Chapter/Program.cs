c373866d75525b3e2143b163198aec15ac72f250
Vytautas Šablinskas <vytsab2@ktu.lt>
2020-09-18 13:32:42 +0300

    I've watched Lesson 3 and I decided to make code more readable, while using everything that was presented in Lesson 1.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,9 +6,10 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello first person, what's your first name?"); //First person information.
+            // Reading first person information: first name, second name, age, weight, height.
+            Console.WriteLine("Hello first person, what's your first name?");
             string firstName = Console.ReadLine();
-            Console.WriteLine("What's your surname");
+            Console.WriteLine("What's your second name?");
             string secondName = Console.ReadLine();
             Console.WriteLine("What's your age?");
             int age = int.Parse(Console.ReadLine());
@@ -16,16 +17,20 @@ namespace BootCamp.Chapter
             double weight = double.Parse(Console.ReadLine());
             Console.WriteLine("What's your height? (In cm)");
             int height = int.Parse(Console.ReadLine());
-            string fullName = (firstName + " " + secondName);
+
+            // Making BMI formula.
             double heightInMeters = height / 100.0;
             double heightInSquareMeters = heightInMeters * heightInMeters;
             double Bmi = weight / heightInSquareMeters;
-            Console.WriteLine(fullName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            // Making output of his first name, second name, age, weight, height and BMI. 
+            Console.WriteLine(firstName + " " + secondName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
             Console.WriteLine("Your body-mass index (BMI) is: " + Bmi);
 
-            Console.WriteLine("\nHello second person, what's your first name?"); //Second person information.
+            // Reading second person information: first name, second name, age, weight, height. (I add _2 to every variable)
+            Console.WriteLine("\nHello first person, what's your first name?");
             string firstName_2 = Console.ReadLine();
-            Console.WriteLine("What's your surname");
+            Console.WriteLine("What's your second name?");
             string secondName_2 = Console.ReadLine();
             Console.WriteLine("What's your age?");
             int age_2 = int.Parse(Console.ReadLine());
@@ -33,11 +38,14 @@ namespace BootCamp.Chapter
             double weight_2 = double.Parse(Console.ReadLine());
             Console.WriteLine("What's your height? (In cm)");
             int height_2 = int.Parse(Console.ReadLine());
-            string fullName_2 = (firstName_2 + " " + secondName_2);
+
+            // Making BMI formula.
             double heightInMeters_2 = height_2 / 100.0;
             double heightInSquareMeters_2 = heightInMeters_2 * heightInMeters_2;
             double Bmi_2 = weight_2 / heightInSquareMeters_2;
-            Console.WriteLine(fullName_2 + " is " + age_2 + " years old, his weight is " + weight_2 + " kg and his height is " + height_2 + " cm.");
+
+            // Making output of his first name, second name, age, weight, height and BMI. 
+            Console.WriteLine(firstName_2 + " " + secondName_2 + " is " + age_2 + " years old, his weight is " + weight_2 + " kg and his height is " + height_2 + " cm.");
             Console.WriteLine("Your body-mass index (BMI) is: " + Bmi_2);
         }
     }
