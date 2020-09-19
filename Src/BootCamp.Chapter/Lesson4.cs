33213a962d423ed7f8067f4cfa9e351079762d32
Vytautas Šablinskas <vytsab2@ktu.lt>
2020-09-19 13:29:21 +0300

    First addition to homework 4.

---
+++ Src/BootCamp.Chapter/Lesson4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson4
+    {
+        public static void Demo()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Hello first person, what's your first name?");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("What's your second name?");
+                string secondName = Console.ReadLine();
+                Console.WriteLine("What's your age?");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("What's your weight? (In kg)");
+                float weight = float.Parse(Console.ReadLine());
+                Console.WriteLine("What's your height? (In cm)");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine(firstName + " " + secondName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+                Console.WriteLine("Your body-mass index (BMI) is: " + (weight / ((height / 100) * (height / 100))));
+            }
+        }
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int number);
+
+            if (!isNumber && !string.IsNullOrEmpty(input))
+            {
+                Console.Write("{input} is not a valid number.");
+                return -1;
+            }
+            return number;
+        }
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+        public static float CalculateBmi(float weight, float height)
+        {
+            return (weight / height / height);
+        }
+    }
+}
