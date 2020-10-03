e4c81115d4afb5281a7ffe40ad5ccda10f54237f
Max <hellhunter15@hotmail.com>
2020-10-03 05:48:48 +0200

    Created MaxBuildingHeight Tests;

--- Tests/BootCamp.Chapter.Tests/CityTest.cs
+++ Tests/BootCamp.Chapter.Tests/CityTest.cs
@@ -38,6 +38,41 @@ namespace BootCamp.Chapter.Tests
             Assert.Equal(correctView, view);
         }
 
+        [Theory]
+        [InlineData(0, 3, 1, 5)]
+        [InlineData(1, 3, 1, 5)]
+        [InlineData(2, 3, 1, 5)]
+        public void City_Given_Buildinging_Returns_Correct_MaxBuildingHeight(int offPlace, int citySize, int buildingHeight, int offNumber)
+        {
+            //Arrange
+            int[][] buildings = BuildBuildingsArray(citySize, buildingHeight);
+            buildings[offPlace][offPlace] = offNumber;
+            int[][] correctMaxBuildingHeight = GetMaxBuildingsArrArr(citySize, buildingHeight, offNumber, offPlace);
+
+            //Act
+            City city = new City(buildings);
+            int[][] MaxHeightBuildings = city.MaxBuildingHeight;
+
+            //Assert
+            Assert.Equal(correctMaxBuildingHeight, MaxHeightBuildings);
+        }
+
+        private int[][] GetMaxBuildingsArrArr(int citySize, int buildingHeight, int offNumber, int offPlace)
+        {
+            int[][] buildings = new int[citySize][];
+
+            for (int i = 0; i < citySize; i++)
+            {
+                buildings[i] = new int[citySize];
+                for (int k = 0; k < citySize; k++)
+                {
+                    bool isOffPlace = (i == offPlace || k == offPlace);
+                    buildings[i][k] = isOffPlace ? offNumber : buildingHeight;
+                }
+            }
+            return buildings;
+        }
+
         private int[][] BuildBuildingsArray(int citySize, int buildingHeight)
         {
             int[][] buildings = new int[citySize][];
