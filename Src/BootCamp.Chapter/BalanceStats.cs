ed7cf053a80b7630f1be5b2761e0aa2ac2a63f5c
Eddie Finlaw <edrfinlaw@gmail.com>
2020-10-06 15:36:11 -0700

    Inverted if statements to achieve fail early principle

--- Src/BootCamp.Chapter/BalanceStats.cs
+++ Src/BootCamp.Chapter/BalanceStats.cs
@@ -15,43 +15,42 @@ namespace BootCamp.Chapter
         /// </summary>
         public static string FindHighestBalanceEver(string[] peopleAndBalances)
         {
-            if (peopleAndBalances != null && peopleAndBalances.Length != 0)
+            if (peopleAndBalances == null && peopleAndBalances.Length == 0) return defaultReturn;
+
+            var highNames = new List<string>();
+            int highBal = 0;
+
+            foreach (var str in peopleAndBalances)
             {
-                var highNames = new List<string>();
-                int highBal = 0;
-
-                foreach (var str in peopleAndBalances)
+                var arr = str.Split(", ");
+                var name = arr[0];
+                int balance = 0;
+                for (int i = 1; i < arr.Length; i++)
                 {
-                    var arr = str.Split(", ");
-                    var name = arr[0];
-                    int balance = 0;
-                    for (int i = 1; i < arr.Length; i++)
-                    {
-                        var ok = int.TryParse(arr[i], out int number);
-                        if (ok)
-                            balance = Math.Max(balance, number);
-                    }
-                    if (balance > highBal)
-                    {
-                        highNames.Clear();
-                        highNames.Add(name);
-                        highBal = balance;
-                    }
-                    else if (balance == highBal)
-                    {
-                        highNames.Add(name);
-                    }
+                    var ok = int.TryParse(arr[i], out int number);
+                    if (ok)
+                        balance = Math.Max(balance, number);
                 }
-
-                StringBuilder sb = new StringBuilder();
-                var highBalStr = FormatValue(highBal);
-
-                BuildNamesString(sb, highNames);
-                sb.Append($" had the most money ever. {highBalStr}.");
-
-                return sb.ToString();
+                if (balance > highBal)
+                {
+                    highNames.Clear();
+                    highNames.Add(name);
+                    highBal = balance;
+                }
+                else if (balance == highBal)
+                {
+                    highNames.Add(name);
+                }
             }
-            return defaultReturn;
+
+            StringBuilder sb = new StringBuilder();
+            var highBalStr = FormatValue(highBal);
+
+            BuildNamesString(sb, highNames);
+            sb.Append($" had the most money ever. {highBalStr}.");
+
+            return sb.ToString();
+
         }
 
         /// <summary>
@@ -59,50 +58,49 @@ namespace BootCamp.Chapter
         /// </summary>
         public static string FindPersonWithBiggestLoss(string[] peopleAndBalances)
         {
-            if (peopleAndBalances != null && peopleAndBalances.Length != 0)
-            {
-                var lowNames = new List<string>();
-                int biggestLoss = 0;
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0) return defaultReturn;
 
-                foreach (var str in peopleAndBalances)
+            var lowNames = new List<string>();
+            int biggestLoss = 0;
+
+            foreach (var str in peopleAndBalances)
+            {
+                var arr = str.Split(", ");
+                var name = arr[0];
+                int balance = 0;
+                int difference = 0;
+                for (int i = 1; i < arr.Length; i++)
                 {
-                    var arr = str.Split(", ");
-                    var name = arr[0];
-                    int balance = 0;
-                    int difference = 0;
-                    for (int i = 1; i < arr.Length; i++)
+                    var ok = int.TryParse(arr[i], out int number);
+                    if (ok)
                     {
-                        var ok = int.TryParse(arr[i], out int number);
-                        if (ok)
-                        {
-                            balance = Math.Max(balance, number);
-                            difference = balance - number;
-                        }
-                    }
-                    if (difference > biggestLoss)
-                    {
-                        lowNames.Clear();
-                        lowNames.Add(name);
-                        biggestLoss = difference;
-                    }
-                    else if (difference == biggestLoss)
-                    {
-                        lowNames.Add(name);
+                        balance = Math.Max(balance, number);
+                        difference = balance - number;
                     }
                 }
-
-                if (biggestLoss == 0) return defaultReturn;
-
-                var sb = new StringBuilder();
-
-                var biggestLossStr = FormatValue(biggestLoss);
-
-                BuildNamesString(sb, lowNames);
-                sb.Append($" lost the most money. -{biggestLossStr}.");
-
-                return sb.ToString();
+                if (difference > biggestLoss)
+                {
+                    lowNames.Clear();
+                    lowNames.Add(name);
+                    biggestLoss = difference;
+                }
+                else if (difference == biggestLoss)
+                {
+                    lowNames.Add(name);
+                }
             }
-            return defaultReturn;
+
+            if (biggestLoss == 0) return defaultReturn;
+
+            var sb = new StringBuilder();
+
+            var biggestLossStr = FormatValue(biggestLoss);
+
+            BuildNamesString(sb, lowNames);
+            sb.Append($" lost the most money. -{biggestLossStr}.");
+
+            return sb.ToString();
+
         }
 
         /// <summary>
@@ -110,44 +108,42 @@ namespace BootCamp.Chapter
         /// </summary>
         public static string FindRichestPerson(string[] peopleAndBalances)
         {
-            if (peopleAndBalances != null && peopleAndBalances.Length != 0)
+            if (peopleAndBalances == null && peopleAndBalances.Length == 0) return defaultReturn;
+
+            var richNames = new List<string>();
+            int biggestBalance = 0;
+
+            foreach (var str in peopleAndBalances)
             {
-                var richNames = new List<string>();
-                int biggestBalance = 0;
+                var arr = str.Split(", ");
+                var name = arr[0];
 
-                foreach (var str in peopleAndBalances)
+                var ok = int.TryParse(arr[arr.Length - 1], out int number);
+
+                if (ok)
                 {
-                    var arr = str.Split(", ");
-                    var name = arr[0];
-
-                    var ok = int.TryParse(arr[arr.Length-1], out int number);
-
-                    if (ok)
+                    if (number > biggestBalance)
                     {
-                        if (number > biggestBalance)
-                        {
-                            richNames.Clear();
-                            richNames.Add(name);
-                            biggestBalance = number;
-                        }
-                        else if (number == biggestBalance)
-                        {
-                            richNames.Add(name);
-                        }
+                        richNames.Clear();
+                        richNames.Add(name);
+                        biggestBalance = number;
+                    }
+                    else if (number == biggestBalance)
+                    {
+                        richNames.Add(name);
                     }
                 }
-
-                var sb = new StringBuilder();
-
-                var biggestBalanceStr = FormatValue(biggestBalance);
-
-                BuildNamesString(sb, richNames);
-                var pluralOrSingle = (richNames.Count == 1 ? "is the richest person." : "are the richest people.");
-                sb.Append($" {pluralOrSingle} {biggestBalanceStr}.");
-
-                return sb.ToString();
             }
-            return defaultReturn;
+
+            var sb = new StringBuilder();
+
+            var biggestBalanceStr = FormatValue(biggestBalance);
+
+            BuildNamesString(sb, richNames);
+            var pluralOrSingle = (richNames.Count == 1 ? "is the richest person." : "are the richest people.");
+            sb.Append($" {pluralOrSingle} {biggestBalanceStr}.");
+
+            return sb.ToString();
         }
 
         /// <summary>
@@ -155,44 +151,43 @@ namespace BootCamp.Chapter
         /// </summary>
         public static string FindMostPoorPerson(string[] peopleAndBalances)
         {
-            if (peopleAndBalances != null && peopleAndBalances.Length != 0)
+            if (peopleAndBalances == null && peopleAndBalances.Length == 0) return defaultReturn;
+
+            var poorNames = new List<string>();
+            int lowestBalance = int.MaxValue;
+
+            foreach (var str in peopleAndBalances)
             {
-                var poorNames = new List<string>();
-                int lowestBalance = int.MaxValue;
+                var arr = str.Split(", ");
+                var name = arr[0];
 
-                foreach (var str in peopleAndBalances)
+                var ok = int.TryParse(arr[arr.Length - 1], out int number);
+
+                if (ok)
                 {
-                    var arr = str.Split(", ");
-                    var name = arr[0];
-
-                    var ok = int.TryParse(arr[arr.Length - 1], out int number);
-
-                    if (ok)
+                    if (number < lowestBalance)
                     {
-                        if (number < lowestBalance)
-                        {
-                            poorNames.Clear();
-                            poorNames.Add(name);
-                            lowestBalance = number;
-                        }
-                        else if (number == lowestBalance)
-                        {
-                            poorNames.Add(name);
-                        }
+                        poorNames.Clear();
+                        poorNames.Add(name);
+                        lowestBalance = number;
+                    }
+                    else if (number == lowestBalance)
+                    {
+                        poorNames.Add(name);
                     }
                 }
-
-                var sb = new StringBuilder();
-
-                var lowestBalanceStr = FormatValue(lowestBalance);
-
-                BuildNamesString(sb, poorNames);
-                var pluralOrSingle = (poorNames.Count == 1 ? "has" : "have");
-                sb.Append($" {pluralOrSingle} the least money. {lowestBalanceStr}.");
-
-                return sb.ToString();
             }
-            return defaultReturn;
+
+            var sb = new StringBuilder();
+
+            var lowestBalanceStr = FormatValue(lowestBalance);
+
+            BuildNamesString(sb, poorNames);
+            var pluralOrSingle = (poorNames.Count == 1 ? "has" : "have");
+            sb.Append($" {pluralOrSingle} the least money. {lowestBalanceStr}.");
+
+            return sb.ToString();
+
         }
 
         public static string FormatValue(int value)
