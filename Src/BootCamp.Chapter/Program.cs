cb1ab20a82a2aece1b08b6b6abd02491096f7797
Eddie Finlaw <edrfinlaw@gmail.com>
2020-10-06 15:32:15 -0700

    Removed comments from main

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,15 +6,12 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            
-            // Print each of the statistical output using Text Table with padding 3:
-            // - FindHighestBalanceEver
             BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances);
-            // - FindPersonWithBiggestLoss
+
             BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances);
-            // - FindRichestPerson
+
             BalanceStats.FindRichestPerson(PeoplesBalances.Balances);
-            // - FindMostPoorPerson
+
             BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances);
         }
     }
