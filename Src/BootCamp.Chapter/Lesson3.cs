18897686c0f059c8fb9f25309f532a028337612b
Abubakar Ahmed <abubakarahmed4479@outlook.com>
2020-10-20 03:02:58 +0900

    Changed To Verb after Review

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -6,43 +6,43 @@ namespace BootCamp.Chapter
 {
     class Lesson3
     {
-        public static string NameofPerson()
+        public static string GetNameofPerson()
         {
             string personName = Checks.PromptString("Enter your name: ");
             return personName;
         }
 
-        public static string SurnameofPerson()
+        public static string GetSurnameofPerson()
         {
             string personSurName = Checks.PromptString("Enter your Surname: ");
             return personSurName;
         }
 
-        public static int AgeofPerson()
+        public static int GetAgeofPerson()
         {
             int personAge = Checks.PromptInt("Enter your Age: ");
             return personAge;
         }
 
-        public static float WeightofPerson()
+        public static float GetWeightofPerson()
         {
             float personWeight = Checks.PromptFloat("Enter your Weight: ");
             return personWeight;
         }
 
-        public static float HeightofPerson()
+        public static float GetHeightofPerson()
         {
             float personHeight = Checks.PromptFloat("Enter your Height: ");
             return personHeight;
         }
 
-        public static float BMIofPerson(float personWeight, float personHight)
+        public static float GetBMIofPerson(float personWeight, float personHight)
         {
             float personBIM = Checks.CalculateBmi(personWeight, personHight);
             return personBIM;
         }
 
-        public static void InfoPerson(string name, string surname, int age, float weight, float height, float bmi)
+        public static void PrintInfoPerson(string name, string surname, int age, float weight, float height, float bmi)
         {
             Checks.PrintInfo(name, surname, age, weight, height, bmi);
         }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -11,23 +11,23 @@ namespace BootCamp.Chapter
         {
             Console.WriteLine("Hello This is for homework 3");
 
-            string personName = Lesson3.NameofPerson();
-            string personSurname = Lesson3.SurnameofPerson();
-            int personAge = Lesson3.AgeofPerson();
-            float personWeight = Lesson3.WeightofPerson();
-            float personHeight = Lesson3.HeightofPerson();
-            float personBMI = Lesson3.BMIofPerson(personWeight, personHeight);
-            Lesson3.InfoPerson(personName, personSurname, personAge, personWeight, personHeight, personBMI);
+            string personName = Lesson3.GetNameofPerson();
+            string personSurname = Lesson3.GetSurnameofPerson();
+            int personAge = Lesson3.GetAgeofPerson();
+            float personWeight = Lesson3.GetWeightofPerson();
+            float personHeight = Lesson3.GetHeightofPerson();
+            float personBMI = Lesson3.GetBMIofPerson(personWeight, personHeight);
+            Lesson3.PrintInfoPerson(personName, personSurname, personAge, personWeight, personHeight, personBMI);
 
 
 
-             personName = Lesson3.NameofPerson();
-             personSurname = Lesson3.SurnameofPerson();
-             personAge = Lesson3.AgeofPerson();
-             personWeight = Lesson3.WeightofPerson();
-             personHeight = Lesson3.HeightofPerson();
-             personBMI = Lesson3.BMIofPerson(personWeight, personHeight);
-            Lesson3.InfoPerson(personName, personSurname, personAge, personWeight, personHeight, personBMI);
+             personName = Lesson3.GetNameofPerson();
+             personSurname = Lesson3.GetSurnameofPerson();
+             personAge = Lesson3.GetAgeofPerson();
+             personWeight = Lesson3.GetWeightofPerson();
+             personHeight = Lesson3.GetHeightofPerson();
+             personBMI = Lesson3.GetBMIofPerson(personWeight, personHeight);
+            Lesson3.PrintInfoPerson(personName, personSurname, personAge, personWeight, personHeight, personBMI);
         }
 
     }
