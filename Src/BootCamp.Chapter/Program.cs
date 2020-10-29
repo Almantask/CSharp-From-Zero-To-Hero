31a752725bd210fc4fa6ae3f968732236cc1abc9
vManono <62712893+vManono@users.noreply.github.com>
2020-10-29 19:12:21 +0100

    Finished homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input your first name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Input your surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Input your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Input your weight in kg: ");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Input your height in cm: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Console.WriteLine();
+
+            double heightInMeters = height / 100;
+            double bmiResult = weight / Math.Pow(heightInMeters, 2);
+
+            Console.WriteLine(bmiResult);
         }
     }
 }
