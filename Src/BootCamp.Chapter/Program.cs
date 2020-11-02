7413259457260bebf9d5e38ee5ff7c306804c095
TEEFlike <60589842+TEEFlike@users.noreply.github.com>
2020-11-02 13:31:37 +0800

    commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
-namespace BootCamp.Chapter
+namespace Homework
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person like = new Person("like", 27, 81, 181.5);
+            Console.WriteLine($"{like.Name} is {like.Age} years old, his weight is {like.Weight} kg, his height is {like.Height} cm.");
+            Console.WriteLine(like.doBMI());
+        }
+    }
+    class Person
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public double Weight { get; }
+        public double Height { get; }
+        public Person(string name, int age, double weight, double height)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+            Height = height;
+        }
+        public double doBMI()
+        {
+            double result = this.Weight / ((this.Height / 100) * (this.Height / 100));
+            return result;
+
         }
     }
 }
