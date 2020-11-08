1e8baee587139ed73227932cd5af7ee452226b46
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-11-08 10:47:30 -0800

    Tests passed. Unsure about keeping TryParse idea

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Net.Http.Headers;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace BootCamp.Chapter
 {
@@ -18,49 +20,79 @@ namespace BootCamp.Chapter
         public static float PromptFloat(string message)
         {
             Console.WriteLine(message);
-            float number = 0.0f;
-            bool convertedSuccessfully = false;
-            while (!convertedSuccessfully)
+            string userInput = Console.ReadLine();
+            float result = -1f;
+            if (string.IsNullOrEmpty(userInput) || !float.TryParse(
+                userInput,
+                NumberStyles.AllowDecimalPoint,
+                CultureInfo.InvariantCulture,
+                out result
+            ))
             {
-                convertedSuccessfully = float.TryParse(
-                    Console.ReadLine(),
-                    NumberStyles.AllowDecimalPoint,
-                    CultureInfo.InvariantCulture,
-                    out number
-                );
-                if (!convertedSuccessfully)
-                {
-                    Console.WriteLine("Please enter a number.");
-                }
+                Console.WriteLine("\"{0}\" is not a valid number.", userInput);
+                result = -1f; // reset by TryParse
             }
-            return number;
+            return result;
         }
 
         public static int PromptInt(string message)
         {
             Console.WriteLine(message);
-            int number = 0;
-            bool convertedSuccessfully = false;
-            while (!convertedSuccessfully)
+            string userInput = Console.ReadLine();
+            int result = -1;
+            if (string.IsNullOrEmpty(userInput) || !Int32.TryParse(userInput, out result))
             {
-                convertedSuccessfully = Int32.TryParse(Console.ReadLine(), out number);
-                if (!convertedSuccessfully)
-                {
-                    Console.WriteLine("Please enter a number.");
-                }
+                Console.WriteLine("\"{0}\" is not a valid number.", userInput);
+                result = -1; // reset by TryParse
             }
-            return number;
+            return result;
         }
 
         public static string PromptString(string message)
         {
             Console.WriteLine(message);
-            return Console.ReadLine();
+            string userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("Name cannot be empty.");
+                userInput = "-";
+            }
+            return userInput;
         }
 
         public static float CalculateBmi(float weightKg, float heightM)
         {
-            return weightKg / (heightM * heightM);
+            List<string> errorMessages = new List<string>();
+            if (heightM <= 0)
+            {
+                errorMessages.Add(
+                    String.Format(
+                    "Height cannot be equal or less than zero, but was {0}.",
+                    heightM
+                ));
+            }
+            if (weightKg <= 0)
+            {
+                errorMessages.Add(
+                    String.Format(
+                    "Weight cannot be equal or less than zero, but was {0}.",
+                    weightKg
+                ));
+            }
+            
+            if (errorMessages.Count == 0)
+            {
+                return weightKg / (heightM * heightM);
+            }
+            else
+            {
+                Console.WriteLine(
+                    String.Format(
+                        "Failed calculating BMI. Reason: {0}", 
+                        String.Join("", errorMessages)
+                ));
+                return -1f;
+            }
         }
     }
 }
