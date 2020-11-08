e0fe79c6fb60306b7623f613d86210f1b4fc2f74
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-11-08 08:18:22 -0800

    Added back running the code three times inside the demo.

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -9,26 +9,31 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            var firstName = Checks.PromptString("What is their first name?");
-            var lastName = Checks.PromptString("What is their last name?");
-            var age = Checks.PromptInt("What is their age in years?");
-            var weightKg = Checks.PromptFloat("What is their weight in kilograms?");
-            var heightM = Checks.PromptFloat("What is their height in meters?");
-            float bmi = Checks.CalculateBmi(weightKg, heightM);
-            Console.WriteLine(
-                String.Format("{0} {1} is {2} {3} old, their weight is {4} kilograms, and their height is {5} meters.",
-                                firstName,
-                                lastName,
-                                age,
-                                Pluralize("year", age, "s"),
-                                weightKg.ToString("F1", CultureInfo.CurrentCulture),
-                                heightM.ToString("F1", CultureInfo.CurrentCulture)
+            for (int i = 0; i < 3; i++)
+            {
+                var firstName = Checks.PromptString("What is their first name?");
+                var lastName = Checks.PromptString("What is their last name?");
+                var age = Checks.PromptInt("What is their age in years?");
+                var weightKg = Checks.PromptFloat("What is their weight in kilograms?");
+                var heightM = Checks.PromptFloat("What is their height in meters?");
+                float bmi = Checks.CalculateBmi(weightKg, heightM);
+                Console.WriteLine(
+                    String.Format("{0} {1} is {2} {3} old, their weight is {4} kilograms, and their height is {5} meters.",
+                                    firstName,
+                                    lastName,
+                                    age,
+                                    Pluralize("year", age, "s"),
+                                    weightKg.ToString("F1", CultureInfo.CurrentCulture),
+                                    heightM.ToString("F1", CultureInfo.CurrentCulture)
+                    ));
+                Console.WriteLine(
+                    String.Format(
+                        "Their BMI is {0}.",
+                        bmi.ToString("F1", CultureInfo.CurrentCulture)
                 ));
-            Console.WriteLine(
-                String.Format(
-                    "Their BMI is {0}.",
-                    bmi.ToString("F1", CultureInfo.CurrentCulture)
-            ));
+                Console.WriteLine();
+            }
+
         }
 
         private static string Pluralize(string word, int count, string suffix)
