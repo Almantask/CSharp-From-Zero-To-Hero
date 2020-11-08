23c8579edaef64a71cf76f667c210095d2c37383
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-11-08 09:25:49 -0800

    Checks.cs set up with Homework 3 code

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace BootCamp.Chapter
@@ -14,24 +15,52 @@ namespace BootCamp.Chapter
     /// </summary>
     public static class Checks
     {
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            float number = 0.0f;
+            bool convertedSuccessfully = false;
+            while (!convertedSuccessfully)
+            {
+                convertedSuccessfully = float.TryParse(
+                    Console.ReadLine(),
+                    NumberStyles.AllowDecimalPoint,
+                    CultureInfo.InvariantCulture,
+                    out number
+                );
+                if (!convertedSuccessfully)
+                {
+                    Console.WriteLine("Please enter a number.");
+                }
+            }
+            return number;
+        }
+
         public static int PromptInt(string message)
         {
-            return Lesson4.PromptInt(message);
+            Console.WriteLine(message);
+            int number = 0;
+            bool convertedSuccessfully = false;
+            while (!convertedSuccessfully)
+            {
+                convertedSuccessfully = Int32.TryParse(Console.ReadLine(), out number);
+                if (!convertedSuccessfully)
+                {
+                    Console.WriteLine("Please enter a number.");
+                }
+            }
+            return number;
         }
 
         public static string PromptString(string message)
         {
-            return Lesson4.PromptString(message);
+            Console.WriteLine(message);
+            return Console.ReadLine();
         }
 
-        public static float PromptFloat(string message)
+        public static float CalculateBmi(float weightKg, float heightM)
         {
-            return Lesson4.PromptFloat(message);
-        }
-
-        public static float CalculateBmi(float weight, float height)
-        {
-            return Lesson4.CalculateBmi(weight, height);
+            return weightKg / (heightM * heightM);
         }
     }
 }
