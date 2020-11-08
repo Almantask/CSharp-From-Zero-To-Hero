ddf1c9f14be6bf30635d8e90a4c47736b4da5ce4
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-11-08 09:12:44 -0800

    Allowed Pluralize to optionally slice input word and add suffix at end. The intention is to have this work for meter/meters or meter/metres.

--- Src/BootCamp.Chapter/Lesson4.cs
+++ Src/BootCamp.Chapter/Lesson4.cs
@@ -9,29 +9,32 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            var firstName = PromptString("What is their first name?");
-            var lastName = PromptString("What is their last name?");
-            var age = PromptInt("What is their age in years?");
-            var weightKg = PromptFloat("What is their weight in kilograms?");
-            var heightM = PromptFloat("What is their height in meters?");
-            float bmi = CalculateBmi(weightKg, heightM);
-            Console.WriteLine(
-                String.Format("{0} {1} is {2} {3} old, their weight is {4} kilograms, and their height is {5} meters.",
-                                firstName,
-                                lastName,
-                                age,
-                                Pluralize("year", age, "s"),
-                                weightKg.ToString("F1", CultureInfo.CurrentCulture),
-                                heightM.ToString("F1", CultureInfo.CurrentCulture)
+            for (int i = 0; i < 3; i++)
+            {
+                var firstName = Checks.PromptString("What is their first name?");
+                var lastName = Checks.PromptString("What is their last name?");
+                var age = Checks.PromptInt("What is their age in years?");
+                var weightKg = Checks.PromptFloat("What is their weight in kilograms?");
+                var heightM = Checks.PromptFloat("What is their height in meters?");
+                float bmi = Checks.CalculateBmi(weightKg, heightM);
+                Console.WriteLine(
+                    String.Format("{0} {1} is {2} {3} old, their weight is {4} kilograms, and their height is {5} meters.",
+                                    firstName,
+                                    lastName,
+                                    age,
+                                    Pluralize("year", age, "s"),
+                                    weightKg.ToString("F1", CultureInfo.CurrentCulture),
+                                    heightM.ToString("F1", CultureInfo.CurrentCulture)
+                    ));
+                Console.WriteLine(
+                    String.Format(
+                        "Their BMI is {0}.",
+                        bmi.ToString("F1", CultureInfo.CurrentCulture)
                 ));
-            Console.WriteLine(
-                String.Format(
-                    "Their BMI is {0}.",
-                    bmi.ToString("F1", CultureInfo.CurrentCulture)
-            ));
+            }
         }
 
-        private static string Pluralize(string word, int count, string suffix)
+        private static string Pluralize(string word, int count, string suffix, int? index = null)
         {
             if (count == 1)
             {
@@ -39,57 +42,14 @@ namespace BootCamp.Chapter
             }
             else
             {
-                return word + suffix;
-            }
-        }
-
-        public static float PromptFloat(string message)
-        {
-            Console.WriteLine(message);
-            float number = 0.0f;
-            bool convertedSuccessfully = false;
-            while (!convertedSuccessfully)
-            {
-                convertedSuccessfully = float.TryParse(
-                    Console.ReadLine(),
-                    NumberStyles.AllowDecimalPoint,
-                    CultureInfo.InvariantCulture,
-                    out number
-                );
-                if (!convertedSuccessfully)
+                if (index.HasValue)
                 {
-                    Console.WriteLine("Please enter a number.");
+                    return word.Substring(0, (int)index) + suffix;
+                } else
+                {
+                    return word + suffix;
                 }
             }
-            return number;
         }
-
-        public static int PromptInt(string message)
-        {
-            Console.WriteLine(message);
-            int number = 0;
-            bool convertedSuccessfully = false;
-            while (!convertedSuccessfully)
-            {
-                convertedSuccessfully = Int32.TryParse(Console.ReadLine(), out number);
-                if (!convertedSuccessfully)
-                {
-                    Console.WriteLine("Please enter a number.");
-                }
-            }
-            return number;
-        }
-
-        public static string PromptString(string message)
-        {
-            Console.WriteLine(message);
-            return Console.ReadLine();
-        }
-
-        public static float CalculateBmi(float weightKg, float heightM)
-        {
-            return weightKg / (heightM * heightM);
-        }
-
     }
 }
