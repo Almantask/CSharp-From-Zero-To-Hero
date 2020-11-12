c65d66d5b759eccc6856fb8601794d11bfe8f9a5
Sofiane Boumelit <boumelitsofiane@gmail.com>
2020-11-12 18:17:22 +0100

    i did the tasked asked in the homework .

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            string surname;
+            int age;
+            int weight;
+            int height;
+            int BMI;
+            int compter = 0;
+
+            while (compter < 2)
+            {
+                compter++;
+                Console.WriteLine("\nEnter the information below : \n");
+                Console.WriteLine("name :");
+                name = Console.ReadLine();
+                Console.WriteLine("surname :");
+                surname = Console.ReadLine();
+                Console.WriteLine("age :");
+                age = int.Parse(Console.ReadLine());
+                Console.WriteLine("weight :");
+                weight = int.Parse(Console.ReadLine());
+                Console.WriteLine("height :");
+                height = int.Parse(Console.ReadLine());
+                BMI = weight / (height*height);
+                Console.WriteLine(name +" "+ surname +" "+ "is" + " " + +age+ " " + ",his weight is" + " " + +weight+ " " + "and his height is " +height+"cm");
+                Console.WriteLine("BMI ="+BMI);
+            }
+
         }
     }
 }
