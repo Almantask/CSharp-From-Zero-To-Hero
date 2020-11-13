ba639d2303118f3bb9989aa7eda796b16bc60c41
Sofiane Boumelit <boumelitsofiane@gmail.com>
2020-11-13 19:45:16 +0100

    add the changes to my code

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,28 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            string name;
-            string surname;
-            int age;
-            int weight;
-            int height;
-            int BMI;
-            int compter = 0;
+            int peopleCount = 0;
 
-            while (compter < 2)
+            while (peopleCount < 2)
             {
-                compter++;
+                peopleCount++;
                 Console.WriteLine("\nEnter the information below : \n");
+
+                string name;
                 Console.WriteLine("name :");
                 name = Console.ReadLine();
+
+                string surname;
                 Console.WriteLine("surname :");
                 surname = Console.ReadLine();
+
+                int age;
                 Console.WriteLine("age :");
                 age = int.Parse(Console.ReadLine());
+
+                int weight;
                 Console.WriteLine("weight :");
                 weight = int.Parse(Console.ReadLine());
+
+                int height;
                 Console.WriteLine("height :");
                 height = int.Parse(Console.ReadLine());
+
+                int BMI;
                 BMI = weight / (height*height);
                 Console.WriteLine(name +" "+ surname +" "+ "is" + " " + +age+ " " + ",his weight is" + " " + +weight+ " " + "and his height is " +height+"cm");
                 Console.WriteLine("BMI ="+BMI);
