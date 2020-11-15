abba4aafc8a1e7ef24e1fb5a93679bf60a13af50
TEEFlike <60589842+TEEFlike@users.noreply.github.com>
2020-11-15 19:52:49 +0800

    Update FileCleaner.cs

--- Src/BootCamp.Chapter/FileCleaner.cs
+++ Src/BootCamp.Chapter/FileCleaner.cs
@@ -14,7 +14,64 @@ namespace BootCamp.Chapter
         /// <param name="cleanedFile">Cleaned up file without any "_".</param>
         public static void Clean(string dirtyFile, string cleanedFile)
         {
-            File.WriteAllText(cleanedFile, "a");
+            try
+            {
+                if (IsNullOrEmpty(dirtyFile) || IsNullOrEmpty(cleanedFile))
+                {
+                    throw new ArgumentException();
+                }
+                using (StreamReader sr = new StreamReader(dirtyFile))
+                {
+                    if (!sr.EndOfStream)
+                    {
+                        string raw = sr.ReadLine();
+                        string preProcess = raw.Replace("_", "");
+                        string[] balance = preProcess.Split(",");
+                        ValidatePerson(balance);
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(preProcess + $"{Environment.NewLine}");
+
+                        File.WriteAllText(cleanedFile, sb.ToString());
+                    }
+                }
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+        
+        private static bool IsNullOrEmpty(string s)
+        {
+            if (s == null || s == string.Empty)
+                return true;
+            return false;
+        }
+        private static void ValidatePerson(string[]data)
+        {
+            string name = data[0];
+            foreach(char c in data[0])
+            {
+                if(Char.IsDigit(c)||Char.IsPunctuation(c))
+                {
+                    throw new InvalidBalancesException($"{nameof(name)} is invalid ", new ArgumentException());
+                }
+            }
+            if(data.Length > 1)
+            {
+                for(int i = 1; i < data.Length; i++)
+                {
+                    var isNumber = int.TryParse(data[1],out _);
+                    if(!isNumber)
+                    {
+                        throw new InvalidBalancesException($"{data[i]} is not a number ", new ArgumentException());
+                    }
+                }
+            }
+            
+
         }
     }
 }
