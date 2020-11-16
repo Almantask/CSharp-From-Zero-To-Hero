3c86782a2e54a4fe8dd561a4510dd17adea9ec1e
Sofiane Boumelit <boumelitsofiane@gmail.com>
2020-11-16 21:28:07 +0100

    my homework 4 commit

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,57 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            int Number;
+            bool success = int.TryParse(input, out Number);
+
+            if (!success)
+            {
+                Console.WriteLine($"\"{input}\" is not a valid number.");
+                return -1;
+            }
+            return Number;
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            if (input == string.Empty || input == null)
+            {
+                Console.WriteLine("Name cannot be empty");
+                return "-";
+            }
+            return input;
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            float Number;
+            bool success = float.TryParse(input, System.Globalization.NumberStyles.Float,
+    System.Globalization.CultureInfo.InvariantCulture, out Number);
+
+            if (!success)
+            {
+                Console.WriteLine($"\"{input}\" is not a valid number.");
+                return -1;
+            }
+            return Number;
+
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            if (weight <= 0 || height <= 0)
+            {
+                Console.WriteLine("Failed calculating BMI. Reason:" + Environment.NewLine + $"Height cannot be equal or less than zero, but was {height}" + Environment.NewLine + $"Weight cannot be equal or less than zero, but was {weight}");
+                return -1;
+            }
+            float bmi = weight / height / height;
+            return bmi;
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson_4
+    {
+        public static void Demo()
+        {
+            string name = Checks.PromptString("Enter your name: ");
+            int age = Checks.PromptInt("Enter your age: ");
+            float weight = Checks.PromptFloat("Enter your weight,unit is kg: ");
+            float height = Checks.PromptFloat("Enter your height,unit is meters: ");
+
+            float bmi = Checks.CalculateBmi(weight, height);
+            if (bmi == (-1) || age == (-1) || name == "-")
+                Console.WriteLine("Input error, please check again.");
+            else
+            {
+                Console.WriteLine($"{name} is {age} years old, weight is {weight} kg, height is {height} meters, BMI is {bmi}");
+            }
+        }
+    }
+}
