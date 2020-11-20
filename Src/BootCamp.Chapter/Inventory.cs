aaaa10dc34b1f07904717ebfe070670fb9d2a4bd
TEEFlike <60589842+TEEFlike@users.noreply.github.com>
2020-11-20 09:13:32 +0800

    Update Inventory.cs

--- Src/BootCamp.Chapter/Inventory.cs
+++ Src/BootCamp.Chapter/Inventory.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections;
+
+namespace BootCamp.Chapter
 {
     public class Inventory
     {
@@ -15,12 +18,28 @@
 
         public Item[] GetItems(string name)
         {
-            return new Item[0];
+            ArrayList list = new ArrayList();
+            foreach (Item item1 in _items)
+            {
+                if (item1.GetName() == name)
+                    list.Add(item1);
+            }
+            return (Item[])list.ToArray();
         }
 
-        public void AddItem(Item item)
+        public void AddItem(Item item)                  
         {
-
+            foreach(Item item2 in _items)
+            {
+                if (item2 == item)
+                    return;
+                else
+                {
+                    _items = new Item[_items.Length + 1];
+                    _items[_items.Length] = item;
+                }
+            }
+            
         }
 
         /// <summary>
@@ -29,7 +48,40 @@
         /// </summary>
         public void RemoveItem(Item item)
         {
-
+            if (_items.Length == 0)
+                return ;
+            for(int i = 0; i < _items.Length; i++)
+            {
+                if(_items[i] == item)
+                {                   
+                    var temp = new Item[_items.Length - 1];
+                   for(int j = 0; j <_items.Length -1;j++)
+                    {
+                        if (j < i)
+                            temp[j] = _items[j];
+                        else
+                            temp[j] = _items[j + 1];
+                    }
+                    _items = new Item[_items.Length - 1];
+                    _items = temp;                    
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
