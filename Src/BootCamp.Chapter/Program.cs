d11b43702a41135841dd7ab53d80c5800824c3f0
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-11-22 21:09:56 -0800

    Program.cs implemented.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+namespace BootCamp.Chapter
 {
     class Program
     {
+
+        private static int padding = 3;
         static void Main(string[] args)
         {
             // Print each of the statistical output using Text Table with padding 3:
@@ -9,6 +12,19 @@
             // - FindPersonWithBiggestLoss
             // - FindRichestPerson
             // - FindMostPoorPerson
+
+            Console.WriteLine(
+                TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances), padding)
+                );
+            Console.WriteLine(
+                TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances), padding)
+                );
+            Console.WriteLine(
+                TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances), padding)
+                );
+            Console.WriteLine(
+                TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances), padding)
+                );
         }
     }
 }
