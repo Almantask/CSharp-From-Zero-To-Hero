c182421cafc1abf82d89fe43d978dce0857a53cb
TEEFlike <60589842+TEEFlike@users.noreply.github.com>
2020-12-01 16:51:18 +0800

    update item folder

--- Src/BootCamp.Chapter/Items/Armpiece.cs
+++ Src/BootCamp.Chapter/Items/Armpiece.cs
@@ -4,7 +4,7 @@
     {
         private string _name;
         private decimal _price;
-        private float _weight;
+        private float _weight;       
 
         public Armpiece(string name, decimal price, float weight)
         {
@@ -17,5 +17,24 @@
             get =>  _name;
             set => _name = value;
         }
+        public decimal Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+        public float Weight
+        {
+            get => _weight;
+            set => _weight = value;
+        }
+
+        private float _defense;
+        public float Defense
+        {
+            get => _defense;
+            set => _defense = value;
+        }
+
+
     }
 }
--- Src/BootCamp.Chapter/Items/Chestpiece.cs
+++ Src/BootCamp.Chapter/Items/Chestpiece.cs
@@ -12,5 +12,27 @@
             _price = price;
             _weight = weight;
         }
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public decimal Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+        public float Weight
+        {
+            get => _weight;
+            set => _weight = value;
+        }
+
+        private float _defense;
+        public float Defense
+        {
+            get => _defense;
+            set => _defense = value;
+        }
     }
 }
