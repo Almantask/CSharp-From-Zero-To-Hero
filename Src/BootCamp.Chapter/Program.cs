e95e2150c1b9becae5ac9c2ced0cf35104ab198a
TEEFlike <60589842+TEEFlike@users.noreply.github.com>
2020-12-08 18:31:45 +0800

    commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -14,6 +14,8 @@ namespace BootCamp.Chapter
             const string EmptyFile = @"EmptyCredentials.txt";
             var credentialsManager = new CredentialsManager(EmptyFile);
             credentialsManager.Register(credentials);
+            var credentials1 = new Credentials("Tom", "123455");
+            credentialsManager.Register(credentials1);
             bool isLogin = credentialsManager.Login(credentials);
             Console.WriteLine(isLogin);
         }
