5ff2d1c500b7c6d447041a061e741c315b0a00da
Joshhortt <joshhortt@yahoo.com>
2020-12-21 16:26:26 +0000

    Added variables & functions to prompt user input,  partly refactored homework1AND12 in Lesson.cs

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -8,6 +8,35 @@ namespace BootCamp.Chapter
     class Lesson3
     {
 
-        // Body
+        public static void Demo()
+        {
+            string name = PromptString("Name: ");
+            string surName = PromptString("Surname: ");
+            int age = PromptInt("Age: ");
+            float weight = PromptFloat("Weight(Kg): ");
+            float height = PromptFloat("Height(m): ");
+
+            Console.Write("\n");
+            Console.WriteLine($"{name} {surName} you are {age} years old, your weight is {weight} Kg and your height is {height} cm\n");
+
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            return float.Parse(Console.ReadLine());
+        }
     }
 }
