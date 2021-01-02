a32d20f1a7533ef91de026cea6976c58d5dceb73
Joshhortt <joshhortt@yahoo.com>
2021-01-02 21:30:43 +0000

    First Commit Part 1 of Homework 7. Passed Initial Tests.

--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -1,4 +1,7 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     /// <summary>
     /// Part 1.
@@ -6,7 +9,6 @@
     public static class TextTable
     {
         /*
-
          Input: "Hello", 0
            +-----+
            |Hello|
@@ -24,7 +26,6 @@
            | Hello |
            |       |
            +-------+
-
          */
 
         /// <summary>
@@ -32,9 +33,55 @@
         /// Padding means how many spaces will a message be wrapped with.
         /// Table itself is made of: "+-" symbols. 
         /// </summary>
+        
         public static string Build(string message, int padding)
         {
-            return "";
+            if (string.IsNullOrEmpty(message))
+            {
+                return "";
+            }
+            string[] textSplit = message.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+            StringBuilder textTable = new StringBuilder();
+
+                int maxIndex = GetMaxIndex(textSplit);
+                int iOne;
+
+            textTable.Append("+").Append('-', textSplit[maxIndex].Length + padding * 2).AppendLine("+");
+            for (iOne = 0; iOne < padding; iOne++)
+            {
+                textTable.Append("|").Append(' ', textSplit[maxIndex].Length + padding * 2).AppendLine("|");
+            }
+            for (iOne = 0; iOne < textSplit.Length; iOne++)
+            {
+                textTable.Append("|").Append(' ', padding).Append(textSplit[iOne]);
+                textTable.Append(' ', textSplit[maxIndex].Length - textSplit[iOne].Length);
+                textTable.Append(' ', padding).AppendLine("|");
+            }
+            for (iOne = 0; iOne < padding; iOne++)
+            {
+                textTable.Append("|").Append(' ', textSplit[maxIndex].Length + padding * 2).AppendLine("|");
+            }
+            textTable.Append("+").Append('-', textSplit[maxIndex].Length + padding * 2).AppendLine("+");
+
+                return textTable.ToString();
+        }
+        public static int GetMaxIndex(string[] message)
+        {
+                int iTwo, Max, GetIndex;
+                GetIndex = 0;
+                Max = 0;
+
+            if (message.Length == 1) return 0;
+                
+            for (iTwo = 0; iTwo < message.Length; iTwo++)
+            {
+                if (message[iTwo].Length > Max)
+                {
+                    Max = message[iTwo].Length;
+                    GetIndex = iTwo;
+                }
+            }
+            return GetIndex;
         }
     }
 }
