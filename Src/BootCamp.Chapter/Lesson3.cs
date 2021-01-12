62d397fd5c9285d4ac39ba32d9da7e988d1e53c4
ltolentino <ltolentino@cdeee.gob.do>
2021-01-12 10:59:19 -0400

    refactor lesson3 using functions

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -8,27 +8,40 @@ namespace BootCamp.Chapter
     {
         public static void Demo()
         {
-            var name = GetInput("Name");
-            var lastname = GetInput("Surname");
-            var age = int.Parse(GetInput("Age"));
-            var weight = double.Parse(GetInput("Weight (in kg)"));
-            var height = double.Parse(GetInput("Height (in cm)"));
+            var name = GetInputString("Name");
+            var lastname = GetInputString("Surname");
+            var age = GetInputInt("Age");
+            var weight = GetInputFloat("Weight (in kg)");
+            var height = GetInputFloat("Height (in cm)");
 
             var bmi = CalculateBodyMaxIndex(weight, height);
 
-            Console.WriteLine($"{name} {lastname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
-            Console.WriteLine($"Your body mass index is {bmi:0.00}");
+            Console.WriteLine(
+                $"{name} {lastname} is {age} years old, his weight is {weight} kg and his height is {height} cm.{Environment.NewLine}" +
+                $"Your body mass index is {bmi:0.00}"
+                );
         }
 
-        private static string GetInput(string question)
+        public static string GetInputString(string question)
         {
             Console.Write("Enter your " + question + ": ");
             return Console.ReadLine();
         }
 
+        public static int GetInputInt(string question)
+        {
+            Console.Write("Enter your " + question + ": ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static float GetInputFloat(string question)
+        {
+            Console.Write("Enter your " + question + ": ");
+            return float.Parse(Console.ReadLine());
+        }
 
         //height in cm
-        private static double CalculateBodyMaxIndex(double weight, double height)
+        public static float CalculateBodyMaxIndex(float weight, float height)
         {
             var heightInMeter = height / 100;
             return weight / (heightInMeter * heightInMeter);
