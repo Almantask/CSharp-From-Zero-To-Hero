6599979ecfd7ec0256bd4c4dcb8ce05bd4802721
ltolentino <ltolentino@cdeee.gob.do>
2021-01-12 10:29:55 -0400

    adding homework 2 code

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Console = System.Console;
 
 namespace BootCamp.Chapter
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var name = GetInput("Name");
+            var lastname = GetInput("Surname");
+            var age = int.Parse(GetInput("Age"));
+            var weight = double.Parse(GetInput("Weight (in kg)"));
+            var height = double.Parse(GetInput("Height (in cm)"));
+
+            var bmi = CalculateBodyMaxIndex(weight, height);
+
+            Console.WriteLine($"{name} {lastname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Console.WriteLine($"Your body mass index is {bmi:0.00}");
+        }
+
+        private static string GetInput(string question)
+        {
+            Console.Write("Enter your " + question + ": ");
+            return Console.ReadLine();
+        }
+
+
+        //height in cm
+        private static double CalculateBodyMaxIndex(double weight, double height)
+        {
+            var heightInMeter = height / 100;
+            return weight / (heightInMeter * heightInMeter);
         }
     }
 }
