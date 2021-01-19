5e3f72e8052b17ec09f58014252b556ac9084a03
Pommpeii401 <72211336+Pommpeii401@users.noreply.github.com>
2021-01-19 15:30:43 -0600

    did the homework for chap 1&2 :>

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.");
         }
     }
 }
