7081ea4826441dc94ccd42f55072e148abb0d6cf
Vanessa Marie Delfin <vmdelfin@csu.fullerton.edu>
2021-01-20 14:42:02 -0800

    Completed BMI calculation

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,27 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter person information:");
+            Console.WriteLine("First name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Weight(kg): ");
+            string weight = Console.ReadLine();
+            Console.WriteLine("Height(cm): ");
+            string height = Console.ReadLine();
+
+            double parseWeight = double.Parse(weight);
+            double parseHeight = double.Parse(height);
+            double heightSquared = Math.Pow(parseHeight, 2);
+
+            //Calculate BMI 
+            double bmi = (parseWeight / heightSquared);
+
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Console.WriteLine($"BMI is: {bmi}");
         }
     }
 }
