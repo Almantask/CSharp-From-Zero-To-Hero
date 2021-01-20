2945f47d2c08e24cc09f288f95177e0c76467cca
Pommpeii401 <72211336+Pommpeii401@users.noreply.github.com>
2021-01-20 12:22:34 -0600

    for some reason it gives me this error:
    An unhandled exception of type 'System.IO.FileNotFoundException' occurred in Unknown Module.
    Could not load file or assembly 'System.Runtime, Version=4.2.1.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a' or one of its dependencies. The system cannot find the file specified
    
    people on the discord say its a module error
    i have no idea what that is

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.");
+            string name = Console.ReadLine();
+            string surName = Console.ReadLine();
+            string age = Console.ReadLine();
+            string weight = Console.ReadLine();
+            string height = Console.ReadLine();
+
+            float weightFloat = float.Parse(weight);
+            float heightFloat = float.Parse(height);
+            float ageFloat = float.Parse(age);
+
+
+            Console.WriteLine($"{name} {surName} is {ageFloat} years old, weighs {weightFloat} kilograms, and is {heightFloat}cm tall.");
+
+            float heightSquared = MathF.Sqrt(heightFloat);
+            float bodyMassIndex = weightFloat / heightSquared;
+
+            Console.WriteLine($"{name}'s BMI(Body Mass Index) is {bodyMassIndex}");
+
         }
     }
 }
