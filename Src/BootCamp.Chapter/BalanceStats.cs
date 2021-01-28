0622663f7af9d18192d655c8c853765de5543dee
ltolentino <ltolentino@cdeee.gob.do>
2021-01-28 13:00:39 -0400

    implementing FindHighestBalanceEver; missing when last balance is 0

--- Src/BootCamp.Chapter/BalanceStats.cs
+++ Src/BootCamp.Chapter/BalanceStats.cs
@@ -7,7 +7,58 @@
         /// </summary>
         public static string FindHighestBalanceEver(string[] peopleAndBalances)
         {
-            return "";
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0) return "N/A.";
+
+            var names = "";
+            var highestBalance = 0f;
+
+            for (int i = 0; i < peopleAndBalances.Length; i++)
+            {
+                var balance = peopleAndBalances[i].Split(",");
+                var lastBalance = float.Parse(balance[balance.Length - 1].Trim());
+                if(lastBalance > highestBalance)
+                {
+                    highestBalance = lastBalance;
+                }
+            }
+
+            for (int i = 0; i < peopleAndBalances.Length; i++)
+            {
+                var balance = peopleAndBalances[i].Split(",");
+                var name = balance[0];
+                var lastBalance = float.Parse(balance[balance.Length - 1].Trim());
+                if (lastBalance == highestBalance)
+                {
+                    if(names != "")
+                    {
+                        names += $", {name}";
+                    } 
+                    else
+                    {
+                        names += $"{name}";
+                    }
+                }
+            }
+
+            var arrayNames = names.Split(",");
+            names = "";
+            for (int i = 0; i < arrayNames.Length; i++)
+            {
+                if (i == 0)
+                {
+                    names += arrayNames[i];
+                }
+                else if(i == arrayNames.Length - 1)
+                {
+                    names += $" and{arrayNames[i]}";
+                } 
+                else
+                {
+                    names += $",{arrayNames[i]}";
+                }
+            }
+
+            return $"{names} had the most money ever. ¤{highestBalance}.";
         }
 
         /// <summary>
