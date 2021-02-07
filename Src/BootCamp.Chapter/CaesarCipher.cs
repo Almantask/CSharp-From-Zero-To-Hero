79002e45c7d2022f02f21db26e66e5bdad92c881
Joshhortt <joshhortt@yahoo.com>
2021-02-07 19:11:03 +0000

    Homework Assignment 10 /chapter 1. Done. Passed all tests sucessfully.

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -4,19 +4,54 @@ using System.Text;
 
 namespace BootCamp.Chapter
 {
+    // ToDo: Change code so it only allows letters from an alphabet.
+
     /// <summary>
     /// Caesar cipher which supports all ASCII character (256 in total)
     /// </summary>
     public static class CaesarCipher
     {
+        // Constant for the 26 ASCII character keys
+        private const int MOD26 = 26;
+
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            return CipherText(message, shift);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            return CipherText(message, MOD26 - shift);
+        }
+
+        private static string CipherText(string inputText, int shift)
+        {
+            if (string.IsNullOrEmpty(inputText)) return inputText;
+
+            var text = new StringBuilder();
+
+            foreach (var characterNumber in inputText)
+            {
+                text.Append(PlainChar(characterNumber, shift));
+            }
+            return text.ToString();
+        }
+
+        // Constant for Upper Alphabet keys
+        private const char UPPER = 'A';
+
+        // Constant for the Lower Alphabet keys
+        private const char LOWER = 'a';
+
+        private static char PlainChar(char character, int digitKey)
+        {
+            if (!char.IsLetter(character)) return character;
+
+            char counterBalance = char.IsUpper(character) ? UPPER : LOWER;
+
+            return (char)
+
+                ((((character + digitKey) - counterBalance) % MOD26) + counterBalance);
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("");
+            // Symmetric key encription
+
+            // Encrypt right-shift of three
+            var encryptString = CaesarCipher.Encrypt("Because decryption is the inverse operation of encryption.", 3);
+            Console.WriteLine(encryptString);
+
+            // Decrypt Encripted Message left-shift of three
+            var decryptString = CaesarCipher.Decrypt(encryptString, 3);
+            Console.WriteLine(decryptString);
         }
     }
 }
