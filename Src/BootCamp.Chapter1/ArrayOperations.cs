309bd279e2fd1e1f90e91ca99c4e5fddb085e9b4
Vanessa Marie Delfin <vanessa.delfin12@yahoo.com>
2021-02-08 15:17:13 -0800

    Completed HW5

--- Src/BootCamp.Chapter1/ArrayOperations.cs
+++ Src/BootCamp.Chapter1/ArrayOperations.cs
@@ -3,23 +3,66 @@
     public static class ArrayOperations
     {
         /// <summary>
+        /// Checks if array is null or empty
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns>true or false</returns>
+        public static bool IsNullOrEmpty(int[] array)
+        {
+            if(array == null || array.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
         /// Sort the array in ascending order.
         /// If array empty or null- don't do anything.
         /// </summary>
-        /// <param name="array">Input array in a random order.</param>
+        /// <param name="array">Input array in a sorted order.</param>
         public static void Sort(int[] array)
         {
-            // ToDo: implement.
+            if(IsNullOrEmpty(array))
+            {
+                return;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+        
+            }
         }
 
         /// <summary>
         /// Reverse the array elements, first being last and so on.
         /// If array empty or null- don't do anything.
         /// </summary>
-        /// <param name="array">Input array in a random order.</param>
+        /// <param name="array">Input array in a reversed order.</param>
         public static void Reverse(int[] array)
         {
-            // ToDo: implement.
+            if (IsNullOrEmpty(array))
+            {
+                return;
+            }
+
+            for (int i = 0; i < array.Length/2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
         }
 
         /// <summary>
@@ -29,8 +72,12 @@
         /// <returns>A new array with the last element removed. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveLast(int[] array)
         {
-            // ToDo: implement.
-            return array;
+            if (IsNullOrEmpty(array))
+            {
+                return array;
+            }
+
+            return RemoveAt(array, array.Length - 1);
         }
 
         /// <summary>
@@ -39,8 +86,12 @@
         /// <returns>A new array with the first element removed. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveFirst(int[] array)
         {
-            // ToDo: implement.
-            return array;
+            if (IsNullOrEmpty(array))
+            {
+                return array;
+            }
+
+            return RemoveAt(array, 0);
         }
 
         /// <summary>
@@ -51,8 +102,29 @@
         /// <returns>A new array with element removed at a given index. If an array is empty or null, returns input array.</returns>
         public static int[] RemoveAt(int[] array, int index)
         {
-            // ToDo: implement.
-            return array;
+            if (IsNullOrEmpty(array))
+            {
+                return array;
+            }
+            // Check if index is within length of array
+            if (index < 0 || index >= array.Length)
+            {
+                return array;
+            }
+
+            var new_array = new int[array.Length - 1];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i < index)
+                {
+                    new_array[i] = array[i];
+                }
+                else
+                {
+                    new_array[i] = array[i + 1];
+                }
+            }
+            return new_array;
         }
 
         /// <summary>
@@ -63,8 +135,7 @@
         /// <returns>A new array with element added at a given index. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertFirst(int[] array, int number)
         {
-            // ToDo: implement.
-            return array;
+            return InsertAt(array, number, 0);
         }
 
         /// <summary>
@@ -75,8 +146,7 @@
         /// <returns>A new array with element added in the end of array. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertLast(int[] array, int number)
         {
-            // ToDo: implement.
-            return array;
+            return InsertAt(array, number, array.Length);
         }
 
         /// <summary>
@@ -88,8 +158,40 @@
         /// <returns>A new array with element inserted at a given index. If an array is empty or null, returns new array with number in it.</returns>
         public static int[] InsertAt(int[] array, int number, int index)
         {
-            // ToDo: implement.
-            return array;
+            if (IsNullOrEmpty(array))
+            {
+                if (index == 0)
+                {
+                    array = new int[] { number };
+                    return array;
+
+                }
+                else
+                {
+                    return array;
+                }
+            }
+
+            if (index < 0 || index > array.Length)
+            {
+                return array;
+            }
+
+            var new_array = new int[array.Length + 1];
+            new_array[index] = number;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < index)
+                {
+                    new_array[i] = array[i];
+                }
+                else
+                {
+                    new_array[i + 1] = array[i];
+                }
+            }
+            return new_array;
+
         }
     }
 }
