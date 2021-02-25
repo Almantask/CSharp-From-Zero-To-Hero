3a78f91fad366c7bb0c06e8ed95f47900424d69d
dt1307 <dominiq.tudor@live.com>
2021-02-25 14:41:11 +0000

    Solution for homework 1 and 2.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Hello Friend!");
+
+                Console.WriteLine("Please enter your name!");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Please enter your surname!");
+                string surname = Console.ReadLine();
+
+                Console.WriteLine("Please tell me your age!");
+                string age = Console.ReadLine();
+
+                Console.WriteLine("How much do you weight in kg?");
+                string kg = Console.ReadLine();
+                int weight = int.Parse(kg);
+
+                Console.WriteLine("What is your height in cm?");
+                string cm = Console.ReadLine();
+                double height = double.Parse(cm);
+
+                double bmi = (double)(weight / Math.Pow(height / 100.0, 2));
+                bmi = Math.Round(bmi, 2);
+
+                Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+                Console.WriteLine($"{name} {surname} BMI is {bmi}.");
+            }
         }
     }
 }
