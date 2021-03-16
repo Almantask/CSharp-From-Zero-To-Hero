f77ad81bae6220ab5170b5eac96c8110276c27c4
gary djordjevic <garyainen85@hotmail.com>
2021-03-16 03:07:23 +0100

    Homework 1and2 first solutopn

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +6,47 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name,surname;
+            int years;
+            double weight, height;
+
+            // Input user info
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Please enter your ages: ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your weigth in kg: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter your height in cm: ");
+            height = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.WriteLine($"{name} {surname} is {years} years old,his weight is {weight} weight and his height is {height}m");
+
+            double bmi = weight / Math.Pow(height, height);
+
+            Console.WriteLine($"Your BMI is {bmi:N2}");
+
+
+            // Input user info
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Please enter your ages: ");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your weigth in kg: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter your height in cm: ");
+            height = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.WriteLine($"{name} {surname} is {years} years old,his weight is {weight} weight and his height is {height}m");
+
+            bmi = weight / Math.Pow(height, height);
+
+            Console.WriteLine($"Your BMI is {bmi:N2}");
+
         }
     }
 }
