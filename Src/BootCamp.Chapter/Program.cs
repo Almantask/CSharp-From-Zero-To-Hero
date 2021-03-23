885c9f47b6d22be9d29ddad84cc4a3edcaa8cc35
Raja Y <illayarajah@hotmail.com>
2021-03-23 14:12:18 -0400

    Complete with 2nd person

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -31,8 +31,34 @@ namespace BootCamp.Chapter
             //Output BMI
             Console.WriteLine("Your BMI is " + bmi);
 
+            Console.WriteLine();
+            Console.WriteLine("Please enter the required information below for another person");
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your weight in KG: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your height in CM: ");
+            height = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Branch Test");
+            //Output all relavant information. 
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " KG and his height is " + height + " CM.");
+
+            //Convert CM to M
+            heightInM = height / 100;
+
+            //Calculate BMI, route to two digits
+            bmi = Math.Round(weight / (heightInM * heightInM), 2);
+
+            //Output BMI
+            Console.WriteLine("Your BMI is " + bmi);
+
+
+
+
         }
     }
 }
