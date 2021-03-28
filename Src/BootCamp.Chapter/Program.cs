13f9b340d6db64ee84a199bdfc354d89388697f9
kubusiaq <kubusiaq@DESKTOP-PR6QHBH>
2021-03-28 23:23:08 +0200

    homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,14 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            var age = Console.ReadLine();
+            var weight = Convert.ToInt32(Console.ReadLine());
+            var height = Convert.ToInt32(Console.ReadLine());
+            var bmi = (weight / ((height / 100.0) * (height / 100.0)));
+            Console.WriteLine(name + " " + surname + " is " + age + " years  old, his weight is " + weight + " kg and his height is " + height + " cm.");
+            //System.Console.WriteLine("your bmi is " + bmi);
         }
     }
 }
