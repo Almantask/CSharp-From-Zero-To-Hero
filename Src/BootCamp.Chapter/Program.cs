1aacabbaa65df682a7ebcc06fe6175c3981c972d
deempa <62642034+deempa@users.noreply.github.com>
2021-04-06 11:05:40 +0300

    created a new bmi Calculator

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("First name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Sure Name: ");
+                string sureName = Console.ReadLine();
+                Console.WriteLine("Age: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Weight: ");
+                double weight = double.Parse(Console.ReadLine());
+                Console.WriteLine("Height: ");
+                double height = double.Parse(Console.ReadLine());
+
+                var bmi = weight / ((height / 100) * (height / 100));
+
+                Console.WriteLine($"{name} {sureName} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+
+                Console.WriteLine("Your BMI - " + bmi);
+            }
         }
     }
 }
