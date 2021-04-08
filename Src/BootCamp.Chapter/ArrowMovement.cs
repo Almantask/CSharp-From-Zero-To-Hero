91944fac87e9d2709ed81ac7e16b138d2025b813
Raja Y <illayarajah@hotmail.com>
2021-04-08 12:58:17 -0400

    Encoding HW

--- Src/BootCamp.Chapter/ArrowMovement.cs
+++ Src/BootCamp.Chapter/ArrowMovement.cs
@@ -18,7 +18,30 @@ namespace BootCamp.Chapter
         /// <returns>One of the arrow characters. '↥' by default.</returns>
         public static char GetIndicator(char symbol)
         {
-            return '-';
+            char result = Convert.ToChar('\u21A5');
+            string acceptedChars = "wasdWASD";
+            if (acceptedChars.Contains(symbol))
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                if(symbol.Equals('w') || symbol.Equals('W'))
+                {  
+                    result = (char)'\u21A5';
+                }
+                else if (symbol.Equals('a') || symbol.Equals('A'))
+                {
+                    result = (char)'\u21A4';
+                }
+                else if (symbol.Equals('s') || symbol.Equals('S'))
+                {
+                    result = (char)'\u21A7';
+                }
+                else if (symbol.Equals('d') || symbol.Equals('D'))
+                {
+                    result = (char)'\u21A6';
+                }
+            }
+
+            return result;
         }
     }
 }
--- Src/BootCamp.Chapter/BinaryConverter.cs
+++ Src/BootCamp.Chapter/BinaryConverter.cs
@@ -2,18 +2,68 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace BootCamp.Chapter
 {
     public static class BinaryConverter
     {
         public static long ToInteger(string binary)
         {
+            if (!(binary==null || binary == ""))
+            {
+                char[] binaryArray = binary.ToCharArray();
+                long value = 0;
+                int bit = 0;
+                for (var i = 0; i < binary.Length; i++)
+                {
+                 
+                    if (binary[i] == '0' || binary[i] == '1')
+                    {
+                        int.TryParse(binary[i].ToString(), out bit);
+
+                        value += (bit * (long)Math.Pow(2, binary.Length - i - 1));
+                    }
+                    else
+                        throw new InvalidBinaryNumberException(binary);
+                    
+                }
+                return value;
+            }
             return 0;
+
         }
 
         public static string ToBinary(long number)
         {
-            return "";
+            string binary = "";
+            bool complete = true;
+            
+            while (complete)
+            {
+                binary += number % 2;
+                if ((number > 1))
+                {
+                    number = ( number / 2); 
+                }
+                else
+                {
+                    complete = false;
+                }
+            }
+
+            if(!complete)
+            {
+                char[] result = binary.ToCharArray();
+                char[] binaryResult = new char[result.Length];
+                Console.WriteLine(result);
+                for (var i=0;i<result.Length;i++)
+                {
+                    binaryResult[i] = result[result.Length - i - 1 ];
+                }
+                binary = new string( binaryResult);
+            }
+
+            return binary;
         }
     }
 }
