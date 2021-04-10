6b341122a80effa4322224e3c72f7bdf209093f0
deempa <62642034+deempa@users.noreply.github.com>
2021-04-10 18:28:44 +0300

    added ILogger interface, and implemented it on the program class.

---
+++ Src/BootCamp.Chapter/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public interface ILogger
+    {
+        void LogToConsole(string text);
+
+        void LogToFile(string text);
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
 
 namespace BootCamp.Chapter
 {
-    class Program
+    class Program : ILogger
     {
         static void Main(string[] args)
         {
+            var p = new Program();
+            p.LogToFile("Again");
+            p.LogToConsole("We logged to file.");
+        }
 
+        public void LogToConsole(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void LogToFile(string text)
+        {
+            string path = @"C:\Users\Rabanian\Desktop\C# Bootcamp\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Logs.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+            else
+            {
+                using(StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
         }
     }
 }
