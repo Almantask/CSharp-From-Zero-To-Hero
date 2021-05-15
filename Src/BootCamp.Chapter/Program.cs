3d2bf0ba94a1db7561b0b9fe7a31ca377a9898e2
Tailor <kralle37@gmail.com>
2021-05-15 15:58:01 +0200

    Refactored and fixed empty collection exception.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -25,27 +25,40 @@ namespace BootCamp.Chapter
             var list1 = LinqMethods.SnapFingers(intCollection, x => x.ToString().StartsWith('1'));
             var list2 = LinqMethods.SnapFingers(intCollection, x => x % 2 == 0);
 
-            Console.WriteLine($"Task 1 and 2 on collection:{ListToString(intCollection)}");
+            Console.WriteLine($"Task 1 and 2 on collection A:{EnumerableToString(intCollection)}");
+            Console.WriteLine("Collection B:");
             foreach (var item in list1)
             {
                 Console.WriteLine(item);
             }
 
             Console.WriteLine();
-
+            Console.WriteLine("Collection C:");
             foreach (var item in list2)
             {
                 Console.WriteLine(item);
             }
 
             //Task 3
-
-            
+            Console.WriteLine();
+            Console.WriteLine("Task 3. LINQ.");
+            Console.WriteLine();
+            Console.WriteLine($"Is there any number in collection C that equals 4: {list2.Any(x => x == 4)}");
+            Console.WriteLine($"Count number of elements in collection B: {list1.Count()}");
+            Console.WriteLine($"Order the collection B: {EnumerableToString(list1.OrderBy(x => x))}");
+            Console.WriteLine($"Union the set of collection B with C: {EnumerableToString(list1.Union(list2))}");
+            Console.WriteLine($"Intersection the set of collection B with C: {EnumerableToString(list1.Intersect(list2))}");
+            Console.WriteLine($"Subtraction the set of collection B with C: {EnumerableToString(list1.Except(list2))}");
         }
 
-        internal static string ListToString<T>(IEnumerable<T> collection)
+        internal static string EnumerableToString<T>(IEnumerable<T> collection)
         {
             string result = "";
+
+            if (collection.Count() == 0)
+            {
+                return result;
+            }
 
             foreach (var item in collection)
             {
