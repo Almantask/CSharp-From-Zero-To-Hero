09a984b0e1a6d7fed90c48e677ae1c935fcdc18e
RyanBinkley <binkley62918@gmail.com>
2021-06-03 21:09:45 +0200

    Homework_1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 
-namespace BootCamp.Chapter
+namespace ConsoleApp
 {
     class Program
     {
+        // Running with VS menu will put application in debugging mode.
+        // Running with "ctrl + F5" will put it in release mode.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello! \nWhat is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("And what is your height in Inches?");
+            var heightinches = Console.ReadLine();
+            Console.WriteLine("Your age?");
+            var age = Console.ReadLine();
+            Console.WriteLine("Finally, what is your weight in pounds?");
+            var weight = Console.ReadLine();
 
-            Console.ReadLine();
+            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight + " lbs, and his height is " + heightinches + " inches.");
+
+            var bmi_1 = Convert.ToInt16(heightinches) * Convert.ToInt16(heightinches);
+            var bmi_2 = float.Parse(weight) / bmi_1;
+            var bmi_3 = bmi_2 * 703;
+            Console.WriteLine(name + "'s Body Mass Index is also " + bmi_3 + "!");
         }
     }
 }
