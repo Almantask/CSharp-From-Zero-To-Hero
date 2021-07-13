7225533fafae32bcc8b77b452fdd298340c84e56
Spark <piotr.rac@gmail.com>
2021-07-13 17:25:38 +0200

    Test

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
