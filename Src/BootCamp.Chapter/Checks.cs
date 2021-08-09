c0923d1f0c6da45510e25d8737eed56f98559828
iamzumie <cuypers.mic@gmail.com>
2021-08-09 17:34:31 +0200

    finished lesson3 & checks

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,23 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            return Lesson3.promptInt(message);
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            return Lesson3.promptString(message);
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            return Lesson3.promptFloat(message);
         }
 
         public static float CalculateBmi(float weight, float height)
         {
             // To do: call your implementation. 
-            return 0;
+            return Lesson3.calculateBmi(weight, height);
         }
     }
 }
---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            string name = promptString("Please enter your name:");
+            int age = promptInt("Please tell me how old you are:");
+            int weight = promptInt("What is your weight?");
+            float height = promptInt("What is your height?");
+
+            float bmi = calculateBmi(weight, height) * 10000;
+
+            Console.WriteLine($"Welcome {name}, you are {age} years old and have a BMI of {bmi:F2}.");
+        }
+
+        public static float calculateBmi(float weight, float height)
+        {
+            float bmi = weight / (height * height);
+            return bmi;
+        }
+
+        public static int promptInt(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static string promptString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static float promptFloat(string message)
+        {
+            Console.WriteLine(message);
+            float number = float.Parse(Console.ReadLine());
+            return number / 10;
+        }
+    }
+}
