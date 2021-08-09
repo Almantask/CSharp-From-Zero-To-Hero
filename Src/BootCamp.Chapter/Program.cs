0e4cfc3b23a4ebf9e4a03888d2511963de284c01
iamzumie <cuypers.mic@gmail.com>
2021-08-09 11:27:48 +0200

    Homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,55 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fullName = askFullName();
+            int age = askAge();
+            int weight = askWeight();
+            double height = askHeight();
+
+            Console.WriteLine($"{fullName} is {age} years old, his weight is {weight} kg and his height is {height:F1} cm.");
+
+            CalculateBMI(weight, height);
+        }
+
+        static string askFullName()
+        {
+            Console.WriteLine("What is your first name?");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("What is your family name?");
+            string surname = Console.ReadLine();
+
+            return firstname + " " + surname;
+        }
+
+        static int askAge()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            return age;
+        }
+
+        static int askWeight()
+        {
+            Console.WriteLine("How much do you weigh?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            return weight;
+        }
+
+        static double askHeight()
+        {
+            Console.WriteLine("How tall are you?");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            return height;
+        }
+
+        static void CalculateBMI(int weight, double height)
+        {
+            double bmi = (weight / (height * height) * 10000);
+
+            Console.WriteLine($"Your BMI is {bmi:F1}");
         }
     }
 }
