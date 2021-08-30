925f0e0bec871e539ea7991941fa9d2aa0fb0e3e
LunchboxCC <LunchboxCC@gmail.com>
2021-08-30 17:42:57 +0200

    input queries and basic write

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,18 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Name: "); 
+            string name = Console.ReadLine();
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Height: ");
+            var height = Console.ReadLine();
+            Console.Write("Weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + height + " cm tall and weighs " + weight*2 + " kg.");
         }
     }
 }
