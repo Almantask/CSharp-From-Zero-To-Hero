7d0f0ee42fdf6ade4574a53225bffb0ed98418a2
Spark <piotr.rac@gmail.com>
2021-10-08 21:55:00 +0200

    Test

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Stack test");
         }
     }
 }
