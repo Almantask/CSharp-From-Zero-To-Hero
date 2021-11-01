190c2fedb5a7516aa7a12b7fd442be82c8b93efd
Spark <piotr.rac@gmail.com>
2021-11-01 20:13:57 +0100

    Implemented simple logger

---
+++ Src/BootCamp.Chapter/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Logger : ILogBase
+    {
+        private string CurrentDirectory { get; set; }
+
+        private string FileName { get; set; }
+
+        private string FilePath { get; set; }
+
+        public Logger()
+        {
+            this.CurrentDirectory = Directory.GetCurrentDirectory();
+            this.FileName = "Log.txt";
+            this.FilePath = this.CurrentDirectory + @"/" + this.FileName;
+        }
+
+        public void Log(string message)
+        {
+            using (StreamWriter sw = System.IO.File.AppendText(this.FilePath))
+            {
+                sw.Write("\r\nLog entry : ");
+                sw.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                sw.WriteLine(" : {0}", message);
+                sw.WriteLine("--------------------------------------------------------------------");
+            }
+        }
+    }
+}
