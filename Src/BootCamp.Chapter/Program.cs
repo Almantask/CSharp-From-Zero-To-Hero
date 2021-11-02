52432d4cea41509e0abaf276a47655f5e6ad5208
Spark <piotr.rac@gmail.com>
2021-11-02 20:32:13 +0100

    Moved part 1 test to demo class

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,59 +6,10 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            var logger = new Logger();
-            logger.Log("Start of program");
-
-            try
-            {
-                string[] arrayS = new string[5];
-
-                int[] arrayInt = new int[5];
-
-                for (int i = 0; i < arrayInt.Length; i++)
-                {
-                    arrayInt[i] = i;
-                }
-
-                foreach (var item in arrayInt)
-                {
-                    logger.Log(item.ToString());
-                }
-
-                foreach (var test in arrayS)
-                {
-                    Console.WriteLine(test);
-                }
-
-                updateArray(arrayS);
-
-                foreach (var test in arrayS)
-                {
-                    logger.Log($"{test}");
-                }
-
-                ///<summary>
-                /// checking logging of error messages
-                ///</summary>
-                arrayInt[6] = 2;
-
-                Console.ReadKey();
-                logger.Log("End of program");
-            }
-            catch (Exception ex)
-            {
-                string error = ("Program fail, reason: \n" + ex.ToString());
-                logger.Log(error);
-            }
+            Demo.PartOne();
         }
 
-        public static void updateArray(string[] s)
-        {
-            for (int i = 0; i < s.Length; i++)
-            {
-                s[i] = "test " + i;
-            }
-        }
+      
 
 
     }
