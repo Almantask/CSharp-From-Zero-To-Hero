3f1b1c5c8c90cc989406c830f527b7fe8d650050
Javad Shafiei <jshafiey@yahoo.com>
2021-11-05 12:55:01 +0330

    Frisrt homework done!

---
+++ Src/BootCamp.Chapter/info.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+   public static class info
+    {
+        public static string name { get; set; }
+        public static string surename { get; set; }
+        public static int age { get; set; }
+        public static float weight { get; set; }
+        public static float height { get; set; }
+
+    }
+}
