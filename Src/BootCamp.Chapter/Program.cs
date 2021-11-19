121a4b954c54feee4b91256db4dd49ed5a6a2a1a
Spark <piotr.rac@gmail.com>
2021-11-19 20:32:18 +0100

    Updated GetAmountOfRows() method

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,79 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            ///<summary>
+            ///     Get amount of rows from user
+            ///</summary>
+            int rowsAmount = GetAmountOfRows();
 
+            if (rowsAmount != -1)
+            {
+                ///<summary>
+                ///     Get length of each grind from a user, and store input in array
+                /// </summary>
+                int[] gridLength = new int[rowsAmount];
+                GetLengthOfRows(rowsAmount, gridLength);
+
+                ///<summary>
+                ///     Create jagged array, based on user input
+                /// </summary>
+                bool[][] jaggedArray = new bool[rowsAmount][];
+                CreateJaggedArray(rowsAmount, jaggedArray, gridLength);
+
+                //IGridClearer gridClearer = null;
+                ToggleableGridJagged test = new ToggleableGridJagged(jaggedArray, null);
+
+                (string tupleTest, int value) = TuplesTest("test", 4);
+                Console.WriteLine($"{tupleTest}, {value}");
+
+                test.Toggle(2, 1);
+            }
+           
+
+
+            Console.ReadKey();
+        }
+
+        public static int GetAmountOfRows()
+        {
+            int result;
+            bool convertSuccess = int.TryParse(PrintMessage("What's the amount of rows in the grid"), out result);
+
+            return (convertSuccess == true && result > 0) ? result : -1;
+        }
+
+        public static void GetLengthOfRows(int rowsAmount, int[] gridLength)
+        {
+            for (int i = 0; i < gridLength.Length; i++)
+            {
+                bool success = int.TryParse(PrintMessage($"Enter length of {i} grid"), out rowsAmount);
+                if (success)
+                {
+                    gridLength[i] = rowsAmount;
+                }
+            }
+        }
+
+        public static void CreateJaggedArray(int rowsAmount, bool[][] jaggedArray, int[] gridLength)
+        {
+            for (int i = 0; i < rowsAmount; i++)
+            {
+                jaggedArray[i] = new bool[gridLength[i]];
+            }
+        }
+
+        public static string PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static (string test, int numberOne) TuplesTest(string message, int numberOne)
+        {
+            numberOne += 100;
+            message += numberOne;
+
+            return (message, numberOne);
         }
     }
 }
--- Src/BootCamp.Chapter/ToggleableGridJagged.cs
+++ Src/BootCamp.Chapter/ToggleableGridJagged.cs
@@ -5,14 +5,25 @@ namespace BootCamp.Chapter
     public class ToggleableGridJagged : IToggleableGrid
     {
         private readonly IGridClearer _gridClearer;
+        private readonly bool[][] _jaggedArray;
 
         public ToggleableGridJagged(bool[][] toggles, IGridClearer gridClearer)
         {
             _gridClearer = gridClearer;
+            _jaggedArray = toggles;
         }
 
         public void Toggle(int x, int y)
         {
+            try
+            {
+                _jaggedArray[x][y] = true;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new ArgumentOutOfRangeException("Index was out of range", ex);
+            }
         }
     }
 }
