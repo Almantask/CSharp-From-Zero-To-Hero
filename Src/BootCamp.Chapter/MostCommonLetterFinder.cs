6c8d98344500d330a8a81e6614d30445bc1b0d49
Spark <piotr.rac@gmail.com>
2021-11-21 11:18:41 +0100

    Implemented MostCommonLetterFinder

--- Src/BootCamp.Chapter/MostCommonLetterFinder.cs
+++ Src/BootCamp.Chapter/MostCommonLetterFinder.cs
@@ -1,10 +1,29 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+
+namespace BootCamp.Chapter
 {
     public static class MostCommonLetterFinder
     {
         public static char Find(string sentence)
         {
-            return 'z';
+            if (sentence == "" || sentence == " " || sentence == null) throw new ArgumentNullException();
+
+            int[] count = new int[256];
+            int max = 0;
+            Char result = Char.MinValue;
+
+            Array.Clear(count, 0, count.Length - 1);
+
+            foreach (Char c in sentence)
+            {
+                if (++count[c] > max)
+                {
+                    max = count[c];
+                    result = c;
+                }
+            }
+
+            return result;
         }
     }
 }
