b53c06fee4f232a38d1948c6c55b9b242550d473
Spark <piotr.rac@gmail.com>
2021-11-24 20:37:19 +0100

    Added IListExtenstions class

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,16 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            List<int> lista = new List<int> { 1, 2, 3, 4, 5, 6 };
+            List<int> lista2 = new List<int> { 1, 2, 3, 4, 5, 6 };
+            lista2.ShuffleListIList();
 
+            lista.SnapFingers();
+
+            int[] arrayList = new int[] { 1, 2, 3, 4, 5 };
+            arrayList.ShuffleListIList();
+
+            Console.ReadKey();
         }
     }
 }
