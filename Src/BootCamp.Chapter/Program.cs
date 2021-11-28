862abbdb0fe36c4968173e3a00921a0f3a517a39
Spark <piotr.rac@gmail.com>
2021-11-28 17:28:26 +0100

    Added Transactions.cs to save data from file

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,7 @@ namespace BootCamp.Chapter
     {
         public static void Main(string[] args)
         {
+            
 
         }
     }
---
+++ Src/BootCamp.Chapter/Transactions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Transactions
+    {
+        //Shop,City,Street,Item,DateTime,Price
+        private string shop;
+        public string Shop
+        {
+            get { return this.shop; }
+        }
+
+        private string city;
+        public string City
+        {
+            get { return this.city; }
+        }
+
+        private string street;
+        public string Street
+        {
+            get { return this.street; }
+        }
+
+        private string item;
+        public string Item
+        {
+            get { return this.item; }
+        }
+
+        private DateTime time;
+        public DateTime Time
+        {
+            get { return this.time;}
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get { return this.price; }
+        }
+
+        private List<Transactions> transactionList;
+        public List<Transactions> TransactionList
+        {
+            get { return this.transactionList; }
+            //set { this.transactionList = value; }
+        }
+
+        public Transactions(string shop, string city, string street, string item, string dateTime,  string price)
+        {
+            this.shop = shop;
+            this.city = city;
+            this.street = street;
+            this.item = item;
+
+            CultureInfo culture = new CultureInfo("en-US");
+            DateTime tempDate = Convert.ToDateTime(dateTime, culture);
+            this.time = tempDate;
+
+            bool ok = decimal.TryParse(price, out decimal result);
+            this.price = ok ? result : -1;
+
+            AddTransaction(this);
+        }
+
+        public void AddTransaction(Transactions items)
+        {
+            TransactionList.Add(items);
+        }
+    }
+}
