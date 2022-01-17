9fd848999c9e96f952d8486c540a30a619e7f16d
LebenaNtsime <kea@axxess.co.za>
2022-01-17 14:58:21 +0200

    Added another "Hello world" line

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,7 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
