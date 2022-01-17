dc92b393fcd52421b4dbd51b6d17beb609ec4800
LebenaNtsime <97886711+LebenaNtsime@users.noreply.github.com>
2022-01-17 16:15:25 +0200

    Update Program.cs

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,12 +7,6 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("There's are the official changes");
-<<<<<<< HEAD
-
-
-            //Mofufu
-=======
->>>>>>> 8f6cb64a565e31030ba04a52b540b7797923abe7
         }
     }
 }
