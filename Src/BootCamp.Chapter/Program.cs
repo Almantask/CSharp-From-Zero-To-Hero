f273669439fe4d69c82d3b66574aacc94ae58b22
Yaffles <89337312+Yaffles@users.noreply.github.com>
2022-04-15 19:17:39 +1000

    homework 1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,22 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Weight (kg): ");
+            float weight = float.Parse(Console.ReadLine());
+            Console.Write("Height (cm): ");
+            float height = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height + " cm.");
+
+            float heightMetres = height / 100;
+            float bmi = weight / heightMetres * heightMetres;
+            Console.WriteLine("BMI: " + bmi);
         }
     }
 }
