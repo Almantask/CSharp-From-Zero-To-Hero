ed4ca553d759caaf5c5ae0fc14438a33f82a380c
XwSeifer <70734223+XwSeifer@users.noreply.github.com>
2022-04-19 22:14:21 -0400

    Completed Chapter 1 homework.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,32 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Enter your given name: ");
+                string givenName = Console.ReadLine();
+                Console.Write("Enter your surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                short age = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Enter your weight (kg): ");
+                short weight = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Enter your height (cm): ");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                double bmi = Math.Round(weight / height * 100, 2);
+
+                Console.WriteLine("\n" + givenName + " " + surname + " is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.\nTheir BMI is " + bmi + ".\n");
+
+                if (i == 0)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
