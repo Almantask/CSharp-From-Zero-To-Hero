106bf51b1cf75ad85cb5b835a17462288b5bb389
Jan Nikolov <nikolovjan@gmail.com>
2022-04-21 13:51:35 +0200

    Created Lesson3 class. Created void Demo function.

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    internal class Lesson3
+    {
+        public static void Demo() { }
+
+
+    }
+}
