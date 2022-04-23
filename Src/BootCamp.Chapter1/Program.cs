e2f5efdb12da155e2c0a3c338993864c09fe0633
Jan Nikolov <nikolovjan@gmail.com>
2022-04-23 14:42:04 +0200

    Deleted test code in Program class.

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -4,12 +4,6 @@
     {
         static void Main(string[] args)
         {
-            int[] cisla = new int[] { 6, 2, 1, 4, 5 };
-            cisla = ArrayOperations.InsertAt(cisla, 4, 0);
-            foreach (var cislo in cisla)
-            {
-                System.Console.Write($"{cislo} ");
-            }
         }
     }
 }
