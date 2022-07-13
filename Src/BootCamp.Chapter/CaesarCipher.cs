f824b62e9381483ce78d2d3542ffbb901c0a0a69
Yaffles <jimmyfurini@gmail.com>
2022-07-13 19:55:17 +1000

    Complete

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,30 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (message == null) return null;
+            
+            char[] array = message.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                var letter = Convert.ToChar(array[i] + shift);
+                if (Char.IsUpper(letter) && letter > 'Z' || Char.IsLower(letter) && letter > 'z') letter = Convert.ToChar(letter - 26);
+                array[i] = letter;
+            }
+            return new string(array);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+            if (message == null) return null;
+            
+            char[] array = message.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                var letter = Convert.ToChar(array[i] - shift);
+                if (Char.IsUpper(letter) && letter < 'A' || Char.IsLower(letter) && letter < 'a') letter = Convert.ToChar(letter + 26);
+                array[i] = letter;
+            }
+            return new string(array);
         }
     }
 }
