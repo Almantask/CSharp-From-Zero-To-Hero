bcdfb9293a6fe4654601349d030c95ca646c77fc
Yaffles <jimmyfurini@gmail.com>
2022-07-17 19:54:10 +1000

    Completed

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
-namespace BootCamp.Chapter
+﻿namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            ILogger[] loggers = { new ConsoleLog(), new FileLog(@"C:\Users\FURI01\Documents\log.log") };
+            foreach (var logger in loggers)
+            {
+                logger.Message("Log message #1");
+                logger.Error("oh no an error");
+                logger.Message("Error fixed");
+                logger.Error("Another error");
+                logger.Error("Error is worse");
+                logger.Error("System is crashing");
+                logger.Shutdown();
+            }
         }
     }
 }
