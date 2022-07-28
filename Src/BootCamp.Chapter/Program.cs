116af9adcaed43b17cc03e4e4aea627f910ca77f
Joseph Cerdan <joseph.cerdan@hotmail.com>
2022-07-28 22:48:07 +1000

    BMI Calculator

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("BMI Calculator by Joseph Cerdan");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight (kg): ");
+            int weight = int.Parse(Console.ReadLine());
+
+            Console.Write("Height (cm): ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, their weight is " + weight + " kg and their height is " + height + " cm.");
+
+            double bmi = weight/Math.Pow((height/100), 2);
+
+            Console.WriteLine("Their calculated BMI is: " + bmi);
+
+
         }
     }
 }
