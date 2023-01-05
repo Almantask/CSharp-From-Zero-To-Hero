c92723f7e1bc6a0f960cffd7805b8ba3570b8d98
Phil Ho <10146268+kungfuphil@users.noreply.github.com>
2023-01-05 10:31:48 -0500

    Add homework.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("What is your name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your weight (kg)? ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("What is your height (cm)? ");
+            double height = double.Parse(Console.ReadLine());
+
+            double bmi = weight / Math.Pow((height / 100), 2);
+
+            Console.WriteLine($"\n{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height} cm. BMI is {bmi}.");
         }
     }
 }
