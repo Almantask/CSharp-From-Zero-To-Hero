123d6a62c5f936f2f197f57d9d13e5868db6a742
IamNEO <Llauderes@c1@gmail.com>
2023-02-14 20:45:13 +0800

    My first commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -8,6 +8,7 @@ namespace BootCamp.Chapter
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
