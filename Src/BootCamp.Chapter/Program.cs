9f28262ee57fa20bb61468e76e922ab2db81d581
IamNEO <Llauderes@c1@gmail.com>
2023-02-14 22:35:49 +0800

    Added another write line

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,7 @@ namespace BootCamp.Chapter
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
