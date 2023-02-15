cf7144d54d887808d4f4344879421381f97bb7ca
IamNEO <Llauderes@c1@gmail.com>
2023-02-15 14:33:38 +0800

    Finish Chapter1-Homework/1and2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,9 +6,35 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age = int.Parse((Console.ReadLine()));
+            Console.Write("Enter your weight in (kg): ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Enter your height in (cm): ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg and his height is {height} cm");
+            double BMI = (weight/height/height) * 10000;
+            Console.WriteLine($"{name}, BMI is {BMI}");
+
+
+            Console.Write("\nEnter your name: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age2 = int.Parse((Console.ReadLine()));
+            Console.Write("Enter your weight in (kg): ");
+            double weight2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter your height in (cm): ");
+            double height2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{name2} is {age2} years old, his weight is {weight2} kg and his height is {height2} cm");
+
+            double BMI2 = (weight2/height2/height2) * 10000;
+            Console.WriteLine($"{name2} BMI is {BMI2}");
+            Console.WriteLine("Press any key to exit the program...");
+            Console.ReadKey();
         }
     }
 }
