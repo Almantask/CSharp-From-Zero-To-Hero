be53b33426ae4fe73308a70637db3d3af8c0cadf
IamNEO <Llauderes@c1@gmail.com>
2023-02-15 12:26:21 +0800

    Test

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
