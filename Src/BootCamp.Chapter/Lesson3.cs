0fa17be95f6f6c39524bdadf24ce415e1b631cae
IamNEO <Llauderes@c1@gmail.com>
2023-02-16 19:34:56 +0800

    Done Chapter 1/Homework3

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -1,11 +1,21 @@
 using System;
+  
   public class Lesson3
   {
       public static void Demo(){
-        var name = PromptString("Please enter a name: ");
-        PromptInt("Please enter a age: ");
-        Console.WriteLine($"Hi ! {name} your BMI is, {CalculateBmi(PromptFloat("Please enter a weight in (kg): "), PromptFloat("Please enter a height in (cm): "))}");        
+        PromptDetails();
+        Console.WriteLine();
+        PromptDetails();
+        Console.Write("Press any key to continue...");
+        Console.ReadKey();       
+      }
+      public static void PromptDetails(){
+        var name = PromptString("Please enter your name: ");
+        var age = PromptInt("Please enter your age: ");
+        var weight = PromptFloat("Please enter your weight: ");
+        var height = PromptFloat("Please enter your height: ");
 
+        toString(name, age, weight, height);
       }
       public static int PromptInt(string message){
         Console.Write(message);
@@ -25,9 +35,10 @@ using System;
     
         return float.Parse(height_weight);
       }
-        public static float CalculateBmi(float weight, float height)
-        {
-            if(weight <= 0 || height <= 0) return -1;
-            return weight/height/height * 10000;
-        }
+
+      public static float CalculateBmi(float weight, float height){
+        if(weight <= 0 || height <= 0) return -1;
+        return weight/height/height;
+      }
+      public static void toString(string name, int age, float weight, float height) => Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg and his height is {height} is cm. \nBMI is: {CalculateBmi(weight, height)}");  
   }
