6c65e634191d1c28e315ee7f02b4f1fe2c2d6f8d
Pedro Gómez <pedroxgomez@hotmail.com>
2023-03-02 00:51:30 -0300

    bla

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,30 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetPersonData();
+            GetPersonData();
+
+
+            void GetPersonData()
+            {
+                Console.WriteLine("Enter person name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter person surename:");
+                string surename = Console.ReadLine();
+                Console.WriteLine("Enter person age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter person weight in kg:");
+                double weight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter person height in cm:");
+                double height = Convert.ToDouble(Console.ReadLine());
+                double heightMt = height / 100.0;
+                Console.WriteLine($"{name} {surename} is {age}, weight is {weight} kg and his height is {height} cm.");
+                var BMI = weight / (heightMt * heightMt);
+                Console.WriteLine($"BMI is {BMI:f2}");
+
+            }
+
+
         }
     }
 }
