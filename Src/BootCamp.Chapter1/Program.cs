d3081d39105f7973c3531a2831a69e7d4ebec52e
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-03-12 19:31:42 -0300

    Just Sort

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -2,8 +2,21 @@
 {
     class Program
     {
+        static var array = new { 4, 3, 1, 2, 5 };
+
         static void Main(string[] args)
         {
+            PrintArray(array);
+            array.Sort();
+            PrintArray(array);
+        }
+
+        static void PrintArray(int[] ints)
+        {
+            foreach (int i in ints)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
