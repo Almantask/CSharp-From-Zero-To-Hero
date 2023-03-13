8705d524a9c921cbcb8cb8d3c29d6cd3cf1da27f
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-03-12 21:53:31 -0300

    Homework 5 till all remove methods

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -1,15 +1,34 @@
-﻿namespace BootCamp.Chapter1
+﻿using System;
+
+namespace BootCamp.Chapter1
 {
     class Program
     {
-        static var array = new { 4, 3, 1, 2, 5 };
 
         static void Main(string[] args)
         {
+            var array = new[] { 3, 2, 1, 4, 5 };
+
+            Console.WriteLine("Before sort: ");
             PrintArray(array);
-            array.Sort();
+            Array.Sort(array);
+            Console.WriteLine("\nAfter sort: ");
             PrintArray(array);
+            array = ReverseArray(array);
+            Console.WriteLine("\nAfter reverse: ");
+            PrintArray(array);
+            var arrayWithoutFirst = RemoveFirst(array);
+            Console.WriteLine("\nAfter RemoveFirst: ");
+            PrintArray(arrayWithoutFirst);
+            var arrayWithoutLast = RemoveLast(array);
+            Console.WriteLine("\nAfter RemoveFirst: ");
+            PrintArray(arrayWithoutLast);
+            var arrayWithoutNdx = RemoveOnIndex(array, 4);
+            Console.WriteLine("\nAfter RemoveOnIndex: ");
+            PrintArray(arrayWithoutNdx);
+
         }
+
 
         static void PrintArray(int[] ints)
         {
@@ -17,6 +36,51 @@
             {
                 Console.Write(i + " ");
             }
+        }
+
+        static int[] ReverseArray(int[] ints)
+        {
+            var reversedArray = new int[ints.Length];
+            for(int i = 0; i < ints.Length; i++)
+            {
+                reversedArray[reversedArray.Length-1-i] = ints[i];
+            }
+            return reversedArray;
+        }
+        static int[] RemoveFirst(int[] ints)
+        {
+            var newArray = new int[ints.Length-1];
+
+            for(int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = ints[i+1];
+            }
+            return newArray;
+        }
+        static int[] RemoveLast(int[] ints)
+        {
+            var newArray = new int[ints.Length - 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = ints[i];
+            }
+            return newArray;
+        }
+
+        static int[] RemoveOnIndex(int[] ints, int index)
+        {
+            var newArray = new int[ints.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = ints[i];
+            }
+            for (int i = index; i < newArray.Length; i++)
+            {
+                newArray[i] = ints[i+1];
+            }
+            return newArray;
         }
     }
 }
