363e9128eec96e4d52367406de6c7f35c562ed86
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-03-14 00:38:49 +0900

    Homework done commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,41 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int age;
+            double weight;
+            double height;
+            Console.Write("Entern name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter weight: ");
+            weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter height: ");
+            height = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg & his height is {height} cm");
+            Console.WriteLine($"his bmi is {weight/Math.Pow(height/100.0,2):N}");
+
+            Console.Write("Entern name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter weight: ");
+            weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter height: ");
+            height = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} kg & his height is {height} cm");
+            Console.WriteLine($"his bmi is {weight / Math.Pow(height / 100.0, 2):N}");
         }
     }
 }
