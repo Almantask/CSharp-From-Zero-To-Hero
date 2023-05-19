622bede433be135c63c8f242eaf4146a135e0f84
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-05-20 05:04:45 +0900

    Chapter1/HW/7

--- Src/BootCamp.Chapter/BalanceStats.cs
+++ Src/BootCamp.Chapter/BalanceStats.cs
@@ -13,31 +13,29 @@ namespace BootCamp.Chapter
         /// </summary>
         public static string FindHighestBalanceEver(string[] peopleAndBalances)
         {
-            if (peopleAndBalances == null)
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0)
                 return "N/A.";
 
 
             List<string> names = new List<string>();
-            double maxNum = 0;
+            decimal maxNum = 0;
 
-            
 
-            for (int i = 0; i < peopleAndBalances.Length; i++)
+            foreach (string person in peopleAndBalances)
             {
-                var items = GetCleanData(peopleAndBalances, i);
+                var items = GetCleanData(person);
                 var currentName = items[0];
-                double currentMax = 0;
+                decimal currentMax = 0;
 
                 for (int j = 1; j < items.Length; j++)
                 {
-                    var isParse = double.TryParse(items[j], out double item);
+                    var isParse = decimal.TryParse(items[j], out decimal item);
                     if (isParse && item > currentMax)
                     {
                         currentMax = item;
                     }
                 }
 
-                //THIS IS WHERE CURRENTMAX NUMBER & OWNER OF THAT NUMBER a
 
                 if (currentMax >= maxNum)
                 {
@@ -46,7 +44,106 @@ namespace BootCamp.Chapter
                         maxNum = currentMax;
                         names.Clear();
                         names.Add(currentName);
+                    }
+                    else
+                    {
+                        names.Add(currentName);
+                    }
+                }
+            }
+            
+            string nameInput = GetDisplayName(names);
+            return $"{nameInput} had the most money ever. ¤{maxNum}.";
+        }
 
+        
+
+        /// <summary>
+        /// Return name and loss of a person with a biggest loss (balance change negative).
+        /// </summary>
+        public static string FindPersonWithBiggestLoss(string[] peopleAndBalances)
+        {
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0)
+                return "N/A.";
+            
+            List<string> names = new List<string>();
+            decimal mostLoss = decimal.Zero;
+
+            foreach (var person in peopleAndBalances)
+            {
+                var items = GetCleanData(person);
+
+                if (items.Length <= 2)
+                {
+                    return "N/A.";
+                }
+
+                var currentName = items[0];
+                var currentLoss = decimal.Zero;
+
+                for (int j = 1; j < items.Length - 1; j++)
+                {
+                    var currentBalanceParse = decimal.TryParse(items[j], out decimal currentBalance);
+                    var nextBalanceParse = decimal.TryParse(items[j + 1], out decimal nextBalance);
+
+                    if (currentBalanceParse && nextBalanceParse && currentBalance - nextBalance > currentLoss)
+                    {
+                        currentLoss = currentBalance - nextBalance;
+                    }
+                }
+
+
+                if (currentLoss >= mostLoss)
+                {
+                    if (currentLoss > mostLoss)
+                    {
+                        mostLoss = currentLoss;
+                        names.Clear();
+                        names.Add(currentName);
+                    }
+                    else
+                    {
+                        names.Add(currentName);
+                    }
+                }
+            }
+
+
+            string nameInput = GetDisplayName(names);
+            return $"{nameInput} lost the most money. -¤{Math.Abs(mostLoss)}.";
+        }
+
+        /// <summary>
+        /// Return name and current money of the richest person.
+        /// </summary>
+        public static string FindRichestPerson(string[] peopleAndBalances)
+        {
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0)
+            {
+                return "N/A.";
+            }
+
+            List<string> names = new List<string>();
+            var max = decimal.Zero;
+
+            foreach (var person in peopleAndBalances)
+            {
+                var items = GetCleanData(person);
+                if (items.Length < 2)
+                {
+                    return "N/A.";
+                }
+
+                var currentName = items[0];
+
+                var isParse = decimal.TryParse(items[items.Length-1], out decimal item);
+                if (isParse && item >= max)
+                {
+                    if (item > max)
+                    {
+                        max = item;
+                        names.Clear();
+                        names.Add(currentName);
                     }
                     else
                     {
@@ -56,9 +153,63 @@ namespace BootCamp.Chapter
             }
 
             string nameInput = GetDisplayName(names);
-
-            return $"{nameInput} had the most money ever. ¤{maxNum}.";
+            if (!nameInput.Contains("and"))
+            {
+                return $"{nameInput} is the richest person. ¤{max}.";
+            }
+            return $"{nameInput} are the richest people. ¤{max}.";
         }
+
+        /// <summary>
+        /// Return name and current money of the most poor person.
+        /// </summary>
+        public static string FindMostPoorPerson(string[] peopleAndBalances)
+        {
+            if (peopleAndBalances == null || peopleAndBalances.Length == 0)
+            {
+                return "N/A.";
+            }
+
+            List<string> names = new List<string>();
+            var min = decimal.MaxValue;
+
+            foreach (var person in peopleAndBalances)
+            {
+                var items = GetCleanData(person);
+                if (items.Length < 2)
+                {
+                    return "N/A.";
+                }
+
+                var currentName = items[0];
+
+                var isParse = decimal.TryParse(items[items.Length - 1], out decimal item);
+                if (isParse && item <= min)
+                {
+                    if (item < min)
+                    {
+                        min = item;
+                        names.Clear();
+                        names.Add(currentName);
+                    }
+                    else
+                    {
+                        names.Add(currentName);
+                    }
+                }
+            }
+
+            string nameInput = GetDisplayName(names);
+            string finalPart = (min < decimal.Zero) ? $"-¤{Math.Abs(min)}" : $"¤{Math.Abs(min)}";
+
+            if (!nameInput.Contains("and"))
+            {
+                return $"{nameInput} has the least money. {finalPart}.";
+            }
+            return $"{nameInput} have the least money. {finalPart}.";
+        }
+
+        public static string[] GetCleanData(string inputValue) => inputValue.Split(',').Select(a => a.Trim()).ToArray();
 
         private static string GetDisplayName(List<string> names)
         {
@@ -80,40 +231,10 @@ namespace BootCamp.Chapter
             }
             else
             {
-                    nameInput.Append(names[0]);
+                nameInput.Append(names[0]);
             }
 
             return nameInput.ToString();
-        }
-
-        /// <summary>
-        /// Return name and loss of a person with a biggest loss (balance change negative).
-        /// </summary>
-        public static string FindPersonWithBiggestLoss(string[] peopleAndBalances)
-        {
-            return "";
-        }
-
-        /// <summary>
-        /// Return name and current money of the richest person.
-        /// </summary>
-        public static string FindRichestPerson(string[] peopleAndBalances)
-        {
-            return "";
-        }
-
-        /// <summary>
-        /// Return name and current money of the most poor person.
-        /// </summary>
-        public static string FindMostPoorPerson(string[] peopleAndBalances)
-        {
-            return "";
-        }
-
-        public static string[] GetCleanData(string[] peopleAndBalances, int index)
-        {
-            var item = peopleAndBalances[index];
-            return item.Split(", ");
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -10,8 +10,11 @@ namespace BootCamp.Chapter
             // - FindHighestBalanceEver
             Console.WriteLine(TextTable.Build(BalanceStats.FindHighestBalanceEver(PeoplesBalances.Balances), 3));
             // - FindPersonWithBiggestLoss
+            Console.WriteLine(TextTable.Build(BalanceStats.FindPersonWithBiggestLoss(PeoplesBalances.Balances), 3));
             // - FindRichestPerson
+            Console.WriteLine(TextTable.Build(BalanceStats.FindRichestPerson(PeoplesBalances.Balances), 3));
             // - FindMostPoorPerson
+            Console.WriteLine(TextTable.Build(BalanceStats.FindMostPoorPerson(PeoplesBalances.Balances), 3));
         }
     }
 }
