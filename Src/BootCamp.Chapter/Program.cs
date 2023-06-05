0c3372f56820a0f921f6d09eb892674e8379b804
Mostafa Ashraf <61520377+MostafaA1s@users.noreply.github.com>
2023-06-05 17:36:01 +0300

    I collect user data and calculatr BMI Then display it back to the user

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +7,39 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name, surname;
+            int age;
+            float weight, height;
+            int counter = 0;
+            while (counter < 3)
+            {
+                Console.WriteLine("Enter Name:");
+                name = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Enter SurName:");
+                surname = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Enter age:");
+                age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter weight:");
+                weight = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter height:");
+                height = float.Parse(Console.ReadLine());
+
+
+
+                Console.WriteLine("{0} {1} is {2} years old his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
+
+                float bmi = (weight / height / height) * 10000f;
+                Console.WriteLine("your BMI: {0} ", bmi);
+
+
+
+
+                counter++;
+            }
         }
     }
 }
