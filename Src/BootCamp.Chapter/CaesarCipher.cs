6f45660b99d44578ba2183c1d7bb4ae557b62b01
TMason11095 <tmason11095@gmail.com>
2023-06-17 01:50:07 -0500

    Submit Ch1 HW 10: Implemented Caesar cipher fo encrypting and decrypting functions.

--- Src/BootCamp.Chapter/CaesarCipher.cs
+++ Src/BootCamp.Chapter/CaesarCipher.cs
@@ -11,12 +11,42 @@ namespace BootCamp.Chapter
     {
         public static string Encrypt(string message, byte shift)
         {
-            return message;
+            if (message == null || message.Equals(""))
+            {
+                return message;
+            }
+
+            //Convert to char array to change chars
+            char[] charMessage = message.ToCharArray();
+            for (int i = 0; i < charMessage.Length; i++)
+            {
+                //Shift
+                charMessage[i] += Convert.ToChar(shift);
+                //Rollover
+                charMessage[i] %= (char)255;
+            }
+
+            return string.Join("", charMessage);
         }
 
         public static string Decrypt(string message, byte shift)
         {
-            return message;
+			if (message == null || message.Equals(""))
+			{
+				return message;
+			}
+
+			//Convert to char array to change chars
+			char[] charMessage = message.ToCharArray();
+			for (int i = 0; i < charMessage.Length; i++)
+			{
+				//Shift
+				charMessage[i] = (char)(charMessage[i] - Convert.ToChar(shift));
+				//Rollover
+				charMessage[i] %= (char)255;
+			}
+
+			return string.Join("", charMessage);
         }
     }
 }
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Text;
 
 namespace BootCamp.Chapter
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Make a random word
+			Random random = new Random();
+			byte[] rndBytes = new byte[16];
+			random.NextBytes(rndBytes);
+			string rndText = Encoding.ASCII.GetString(rndBytes);
+
+			Console.WriteLine($"Random text: {rndText}");
+
+			//Encrypt
+			Byte shift = 4;
+			string encryptedText = CaesarCipher.Encrypt(rndText, shift);
+			Console.WriteLine($"Encrypted: {encryptedText}");
+
+			//Decrypt
+			string decryptedText = CaesarCipher.Decrypt(encryptedText, shift);
+			Console.WriteLine($"Decrypted: {decryptedText}");
+
+			Console.ReadLine();
+		}
+	}
 }
