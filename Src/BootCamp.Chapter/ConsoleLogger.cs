9885b8a167f7f6e0ddbbf9d13dd8aeb2be12369d
TMason11095 <tmason11095@gmail.com>
2023-06-22 20:16:28 -0500

    Submit Ch2 HW 4: -Created IOuputLogger interface that requires methods WriteLine(string), Write(string), WriteBootMessage(), WriteShutdownMessage(), and WriteErrorMessage(string). -Created ConsoleLogger class that implements the IOutputLogger interface and outputs everything to the console window. -Refactored the code from HW 1And2 and replaced all Console.WriteLine() and Console.Write() calls with the IOutputLogger calls.

---
+++ Src/BootCamp.Chapter/ConsoleLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+	public class ConsoleLogger : IOutputLogger
+	{
+		public string BootMessage { get; }
+		public string ShutdownMessage { get; }
+		//public string ErrorMessage { get; }
+
+		public ConsoleLogger()
+		{
+			this.BootMessage = "Program has booted.";
+			this.ShutdownMessage = "Program has shut down.";
+			//this.ErrorMessage = "An error has occured.";
+		}
+
+		public void WriteLine(string message)
+		{
+			Console.WriteLine(message);
+		}
+		public void Write(string message)
+		{
+			Console.Write(message);
+		}
+		public void WriteBootMessage()
+		{
+			this.WriteLine(this.BootMessage);
+		}
+		public void WriteShutdownMessage()
+		{
+			this.WriteLine(this.ShutdownMessage);
+		}
+		public void WriteErrorMessage(string errorMessage)
+		{
+			this.WriteLine(errorMessage);
+		}
+	}
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -2,11 +2,76 @@
 
 namespace BootCamp.Chapter
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create output logger
+			IOutputLogger outLogger = new ConsoleLogger();
 
-        }
-    }
+			//Print boot message
+			outLogger.WriteBootMessage();
+
+			//Get 2 sets of inputs
+			for (int i = 0; i < 2; i++)
+			{
+				//Get user inputs
+				//First name
+				outLogger.Write("First Name: ");
+				string firstName = Console.ReadLine();
+
+				//Last name
+				outLogger.Write("Last Name: ");
+				string lastName = Console.ReadLine();
+
+				//Age
+				int age = (int)ReadLineFloat("Age: ", outLogger);
+
+				//Weight
+				float kgWeight = ReadLineFloat("Weight(kg): ", outLogger);
+
+				//Height
+				float cmHeight = ReadLineFloat("Height(cm): ", outLogger);
+
+				//Log all variables in one line
+				outLogger.WriteLine($"{firstName} {lastName} is {age} years old, their weight is {kgWeight} kg and their height is {cmHeight} cm.");
+
+				//Log BMI
+				float bmi = GetBmi(kgWeight, cmHeight);
+				outLogger.WriteLine($"BMI: {bmi}");
+			}
+
+			//Print shutdown message
+			outLogger.WriteShutdownMessage();
+		}
+
+		private static float ReadLineFloat(string prompt, IOutputLogger outLogger)
+		{
+			bool isNumber = false;
+			string input;
+			float num = 0;
+
+			while (!isNumber)
+			{
+				outLogger.Write(prompt);
+				input = Console.ReadLine();
+				isNumber = Single.TryParse(input, out num);
+
+				//Log error if didn't enter a number
+				if (!isNumber)
+				{
+					outLogger.WriteErrorMessage($"Error: {input} is not a number.");
+				}
+			}
+
+			return num;
+		}
+
+		private static float GetBmi(float kgWeight, float cmHeight)
+		{
+			decimal mHeight = (decimal)cmHeight / 100;
+
+			return (float)Math.Round((decimal)kgWeight / (mHeight * mHeight), 1);
+		}
+	}
 }
