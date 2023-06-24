ea550a1a48a5649089e3b1fe90bef1a508154377
TMason11095 <tmason11095@gmail.com>
2023-06-24 17:32:21 -0500

    -Changed Credentials from a class to a struct. -Created a private Credentials constructor to choose whether to encode the password or not. Default construction always encodes and internal methods, like TryParse(), don't encode. -Implemented Credentials.TryParse(). -Overrode Credentials.Equals() and Credentials.ToString().
    -Implemented CredentialsManager.Login() and CredentialsManager.Register().
    
    -Created example program to register logins from the user, store them in a txt file, have the user try to login, compare against the logins stored in the txt file, and display if the login was correct or not.

--- Src/BootCamp.Chapter/Credentials.cs
+++ Src/BootCamp.Chapter/Credentials.cs
@@ -1,22 +1,81 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
-    // TODO: make a struct and add validation and other needed methods (if needed)
-    public class Credentials
-    {
-        public string Username;
-        public string Password;
+	public readonly struct Credentials
+	{
+		public readonly string Username;
+		private readonly string Password;
 
-        public Credentials(string username, string password)
-        {
-            Username = username;
-            Password = password;
-        }
+		private Credentials(string username, string password, bool isEncoded) : this()
+		{
+			//Throw argument exception if username or password are null or blank
+			if (username == null || username.Equals("") || password == null || password.Equals(""))
+			{
+				throw new ArgumentException();
+			}
 
-        // TODO: Implement properly.
-        public static bool TryParse(string input, out Credentials credentials)
-        {
-            credentials = default;
-            return false;
-        }
-    }
+			Username = username;
+
+			if (isEncoded)
+			{
+				Password = password;
+			}
+			else//Encode it
+			{
+				Password = EncodePassword(password);
+			}
+		}
+		//Takes an uncoded username and password
+		public Credentials(string username, string password) : this(username, password, isEncoded: false)//Encode the password
+		{
+		}
+
+		public static bool TryParse(string input, out Credentials credentials)
+		{
+			//Input should only be 2 strings seperated by ,
+			string[] inputs = input.Split(',');
+			if (inputs.Length != 2)
+			{
+				credentials = default;
+				return false;
+			}
+
+			//Create new credentials
+			credentials = new Credentials(inputs[0], inputs[1], isEncoded: true);
+			
+			return true;
+		}
+		public override bool Equals(object obj)
+		{
+			//Return false if null or not Credentials
+			if (obj == null || !obj.GetType().Equals(typeof(Credentials)))
+			{
+				return false;
+			}
+
+			//Return false if username or password don't match
+			Credentials credObj = (Credentials)obj;
+			if (!credObj.Username.Equals(Username) || !credObj.Password.Equals(Password))
+			{
+				return false;
+			}
+			
+			return true;
+		}
+
+		public override string ToString()
+		{
+			return $"{Username},{Password}";
+		}
+
+		private string EncodePassword(string password)
+		{
+			byte[] passwordBytes = Encoding.Unicode.GetBytes(password, 0, password.Length);
+			return string.Join(" ", passwordBytes);
+		}
+	}
 }
--- Src/BootCamp.Chapter/CredentialsManager.cs
+++ Src/BootCamp.Chapter/CredentialsManager.cs
@@ -1,24 +1,92 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BootCamp.Chapter
 {
-    public class CredentialsManager
-    {
-        private readonly string _credentialsFile;
+	public class CredentialsManager
+	{
+		private string CredentialsFile { get; }
 
-        public CredentialsManager(string credentialsFile)
-        {
-            _credentialsFile = credentialsFile;
-        }
+		public CredentialsManager(string credentialsFile)
+		{
+			//Throw argument exception if string is null or empty
+			if (credentialsFile == null || credentialsFile.Equals(""))
+			{
+				throw new ArgumentException();
+			}
 
-        // TODO: load credentials and check for equality.
-        public bool Login(Credentials credentials)
-        {
-            return false;
-        }
+			CredentialsFile = credentialsFile;
+		}
 
-        // TODO: store credentials in credentials file.
-        public void Register(Credentials credentials)
-        {
+		public bool Login(Credentials credentials)
+		{
+			return ValidateLogin(CredentialsFile, credentials);
+		}
 
-        }
-    }
+		public void Register(Credentials credentials)
+		{
+			StoreCredentials(CredentialsFile, credentials);
+		}
+
+		private bool ValidateLogin(string filePath, Credentials credential)
+		{
+			//Get list of credentials from file
+			List<Credentials> credentials;
+			if (!TryGetCredentials(filePath, out credentials))
+			{
+				//Conversion failed
+				return false;
+			}
+
+			//Return if this instance is in the file
+			return credentials.Contains(credential);
+		}
+
+		private bool TryGetCredentials(string filePath, out List<Credentials> credentials)
+		{
+			//Read the file
+			string fileText;
+			using (StreamReader sr = new StreamReader(filePath))
+			{
+				fileText = sr.ReadToEnd();
+			}
+
+			//Parse lines
+			string[] entries = fileText.Split(Environment.NewLine);
+
+			//Fill the list
+			credentials = new List<Credentials>();
+			foreach (string entry in entries)
+			{
+				//Convert to Credentials and return false if any fail
+				Credentials credential;
+				if (!Credentials.TryParse(entry, out credential))
+				{
+					return false;
+				}
+
+				credentials.Add(credential);
+			}
+
+			return true;
+		}
+
+		private void StoreCredentials(string filePath, Credentials credentials)
+		{
+			//Check if file has any existing entries or not
+			bool hasEntries = File.ReadAllText(filePath).Length > 0;
+
+			//Open the file to write
+			using (StreamWriter sw = new StreamWriter(filePath, append: true))
+			{
+				//If there's already entries then add a newline before adding the next entry
+				if (hasEntries)
+				{
+					sw.Write(Environment.NewLine);
+				}
+				sw.Write(credentials.ToString());
+			}
+		}
+	}
 }
