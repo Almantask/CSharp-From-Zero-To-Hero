37578fc1e4d98b3fe9765e63b55cb184bd7fc9ce
TMason11095 <tmason11095@gmail.com>
2023-06-24 22:13:00 -0500

    Convert to tabs

--- Src/BootCamp.Chapter/Inventory.cs
+++ Src/BootCamp.Chapter/Inventory.cs
@@ -1,35 +1,35 @@
 ﻿namespace BootCamp.Chapter
 {
-    public class Inventory
-    {
-        private Item[] _items;
-        public Item[] GetItems()
-        {
-            return new Item[0];
-        }
+	public class Inventory
+	{
+		private Item[] _items;
+		public Item[] GetItems()
+		{
+			return new Item[0];
+		}
 
-        public Inventory()
-        {
-            _items = new Item[0];
-        }
+		public Inventory()
+		{
+			_items = new Item[0];
+		}
 
-        public Item[] GetItems(string name)
-        {
-            return new Item[0];
-        }
+		public Item[] GetItems(string name)
+		{
+			return new Item[0];
+		}
 
-        public void AddItem(Item item)
-        {
+		public void AddItem(Item item)
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Removes item matching criteria by item.
-        /// Does nothing if no such item exists
-        /// </summary>
-        public void RemoveItem(Item item)
-        {
+		/// <summary>
+		/// Removes item matching criteria by item.
+		/// Does nothing if no such item exists
+		/// </summary>
+		public void RemoveItem(Item item)
+		{
 
-        }
-    }
+		}
+	}
 }
--- Src/BootCamp.Chapter/Shop.cs
+++ Src/BootCamp.Chapter/Shop.cs
@@ -1,70 +1,70 @@
 ﻿namespace BootCamp.Chapter
 {
-    public class Shop
-    {
-        private decimal _money;
-        public decimal GetMoney()
-        {
-            return _money;
-        }
+	public class Shop
+	{
+		private decimal _money;
+		public decimal GetMoney()
+		{
+			return _money;
+		}
 
-        private Inventory _inventory;
+		private Inventory _inventory;
 
-        public Shop()
-        {
+		public Shop()
+		{
 
-        }
+		}
 
-        public Shop(decimal money)
-        {
-            _money = money;
-        }
+		public Shop(decimal money)
+		{
+			_money = money;
+		}
 
-        public Item[] GetItems()
-        {
-            return _inventory.GetItems();
-        }
+		public Item[] GetItems()
+		{
+			return _inventory.GetItems();
+		}
 
-        /// <summary>
-        /// Adds item to the stock.
-        /// If item of same name exists, does nothing.
-        /// </summary>
-        public void Add(Item item)
-        {
-        }
+		/// <summary>
+		/// Adds item to the stock.
+		/// If item of same name exists, does nothing.
+		/// </summary>
+		public void Add(Item item)
+		{
+		}
 
-        /// <summary>
-        /// Removes item from the stock.
-        /// If item doesn't exist, does nothing.
-        /// </summary>
-        /// <param name="name"></param>
-        public void Remove(string name)
-        {
-        }
+		/// <summary>
+		/// Removes item from the stock.
+		/// If item doesn't exist, does nothing.
+		/// </summary>
+		/// <param name="name"></param>
+		public void Remove(string name)
+		{
+		}
 
-        /// <summary>
-        /// Player can sell items to a shop.
-        /// All items can be sold.
-        /// Shop looses money.
-        /// </summary>
-        /// <returns>Price of an item.</returns>
-        public decimal Buy(Item item)
-        {
-            return 0;
-        }
+		/// <summary>
+		/// Player can sell items to a shop.
+		/// All items can be sold.
+		/// Shop looses money.
+		/// </summary>
+		/// <returns>Price of an item.</returns>
+		public decimal Buy(Item item)
+		{
+			return 0;
+		}
 
-        /// <summary>
-        /// Sell item from a shop.
-        /// Shop increases it's money.
-        /// No money is increased if item does not exist.
-        /// </summary>
-        /// <returns>
-        /// Item sold.
-        /// Null, if no item is sold.
-        /// </returns>
-        public Item Sell(string item)
-        {
-            return null;
-        }
-    }
+		/// <summary>
+		/// Sell item from a shop.
+		/// Shop increases it's money.
+		/// No money is increased if item does not exist.
+		/// </summary>
+		/// <returns>
+		/// Item sold.
+		/// Null, if no item is sold.
+		/// </returns>
+		public Item Sell(string item)
+		{
+			return null;
+		}
+	}
 }
