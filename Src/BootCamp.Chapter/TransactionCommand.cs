182ace33eacd020c5340910363e749447175da95
TMason11095 <tmason11095@gmail.com>
2023-07-31 06:37:09 -0500

    Submit CH3 HW 11: Updated the following for Tests: ProgramTests.Main_When_Valid_MinMax_Command_With_Returns_Expected_Cities_With_Min_Max: -Added "Expected/" to all the expectedFile parameter InlineData entries, as that's where the files were being stored. -Updated all the Tests/Expected files to have the property, Copy to Output Directory, set to Copy if newer, as the files weren't being copied over during testing. -Swapped around the expectedFile parameter InlineData entries to match their respective cmd parameter, as the -money and -items were flipped compared to their expectedFile (cmd "city -money -max" had the expectedFile "CityItemsMax" and cmd "city -items -max" had the expectedFile "CityMoneyMax").
    ProgramTests.Main_When_Valid_Time_Command_Creates_File_And_Writes_Stats_For_Every_Hour:
    -AssertMatchingContents was missing "+ extension" on the OutputFile variable.
    
    ProgramTests.Main_When_Valid_Time_Command_With_Range_Creates_File_And_Writes_Stats_For_Every_Hour_Belonging_To_Range:
    -Main() call was missing "+ extension" on the ValidTransactionsFile variable.
    
    ProgramTests.Main_When_Transactions_File_Empty_Or_Not_Found_Throws_NoTransactionsException:
    -No changes made, but this test will never pass, as the call to Main() only includes the inputFile parameter and doesn't include the command nor the outputFile. So InvalidCommandException will always be thrown before before the inputFile is checked to be able to throw NoTransactionsFoundException.
    
    Expected/FullDay.json and .xml:
    -Hour 23's Earned field is "€10.05". Looking at the Input/Transactions.json and .xml files, the 2 entries for that hour are "€9" and "€12", which average to "€10.5" not "€10.05". Fixed this in both files.
    
    Expected/FullDay.json:
    -"RushHour" has extra spacing before and after the following ":" as well as at the end of the line, which isn't common JSON formatting. Changed ""RushHour" :  22 " to ""RushHour": 22".
    
    Expected/FullDay.xml:
    -Missing Time entry for Hour 0 that the .json version of the file has.
    -Hour 22 is set as just 2.
    
    Expected/Night.json and .xml:
    -Results in both files are only the Times and don't include RushHour. HW 9 includes RushHour and HW 11 makes no mention that this functionality should be removed. Both the Night and FullDay files also come from the same "time" command. Updated both Night.json and .xml to include RushHour = 22 and follow the formatting from FullDay.json and .xml respectively.
    
    Expected/Night.xml:
    -Hour 22 is set as just 2.
    -Hour 23's Earned field is "€10.05". The .json version has "€10.5".
    
    --------------------------------------------------------------------
    -Cloned files over from CH3 HW 9.
    -Created TransactionDTO and EarningsDTO classes.
    -Created TransactionSerializer class to serialize/deserialize json/xml files using the new DTO classes.
    -Updated TransactionCommand class to have command functions return object instead of string to be serialized.
    -Updated Transaction class to include the string version of the Price property to simplify mapping to the DTO version.
    -Updated Main() to utilize these new changes.

---
+++ Src/BootCamp.Chapter/TransactionCommand.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace BootCamp.Chapter
+{
+	public class TransactionCommand
+	{
+		public static readonly string Time = "time";
+		public static readonly string City = "city";
+
+		//Key: string cmd
+		//Value: Action that takes string cmdParameters
+		public Dictionary<string, Func<IEnumerable<Transaction>, string, object>> cmds = new Dictionary<string, Func<IEnumerable<Transaction>, string, object>>();
+
+		public TransactionCommand()
+		{
+			//Setup default command options
+			cmds.Add(TransactionCommand.Time, TransactionCommand.TimeCmd);
+			cmds.Add(TransactionCommand.City, TransactionCommand.CityCmd);
+		}
+
+		public object RunCmd(IEnumerable<Transaction> transactions, string cmdAndArgs)
+		{
+			//Error if nothing was given
+			if (string.IsNullOrWhiteSpace(cmdAndArgs)) throw new InvalidCommandException();
+
+			//Pull command out
+			string[] splitCmd = cmdAndArgs.Split(' ');
+			string cmd = splitCmd[0];
+			//Put args back together
+			string args = splitCmd.Length > 1 ? string.Join(" ", splitCmd[1..]) : string.Empty;
+
+			//Check if cmd is valid
+			if (!cmds.ContainsKey(cmd)) throw new InvalidCommandException();
+
+			//Run cmd
+			return cmds[cmd](transactions, args);
+		}
+
+		public static object TimeCmd(IEnumerable<Transaction> transactions, string parameters)
+		{
+			bool hasParameters = !string.IsNullOrEmpty(parameters);
+			DateTime startTime = default;
+			DateTime endTime = default;
+
+			//Parameters should be a time range "20:00-00:00"
+			if (hasParameters)
+			{
+				string formatErrorMessage = $"Command parameter, \"{parameters}\", is not formatted correctly.";
+				string[] splitTimes = parameters.Split('-');
+				//Error if we don't have 2 fields
+				if (splitTimes.Length != 2) throw new FormatException(formatErrorMessage);
+				//Get start time
+				if (!DateTime.TryParseExact(splitTimes[0], "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out startTime)) throw new FormatException(formatErrorMessage);
+				//Get end time
+				if (!DateTime.TryParseExact(splitTimes[1], "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out endTime)) throw new FormatException(formatErrorMessage);
+			}
+
+			//Group transactions by hours
+			var hoursEarned = from transaction in transactions
+							  group transaction by transaction.DateTime.Hour into hours
+							  select new
+							  {
+								  Hour = hours.Key,
+								  Count = hours.Count(),
+								  DayCount = (from hour in hours select hour.DateTime.Date).Distinct().Count(),
+								  TotalEarned = hours.Sum(transaction => transaction.PriceValue)
+							  } into hours
+							  select new
+							  {
+								  hours.Hour,
+								  hours.Count,
+								  Earned = (hours.TotalEarned / hours.DayCount)
+							  };
+
+			//Left join to all hours to get a full day list of values
+			var allHoursEarned = from hour in Enumerable.Range(0, 24).ToList()
+								 join hourEarned in hoursEarned
+								 on hour equals hourEarned.Hour into allHours
+								 from allHour in allHours.DefaultIfEmpty(new
+								 {
+									 Hour = hour,
+									 Count = 0,
+									 Earned = 0m
+								 })
+								 select allHour;
+
+			//Filter time range if passed in
+			if (hasParameters)
+			{
+				allHoursEarned = from hourEarned in allHoursEarned
+								 where hourEarned.Hour >= startTime.Hour
+								 where hourEarned.Hour <= (endTime.Hour == 0 ? 24 : endTime.Hour)//Change 00:00 end times to 24:00 for range check
+								 select hourEarned;
+			}
+
+			//Get rush hour (highest earned hour)
+			int rushHour = (from hourEarned in allHoursEarned
+							orderby hourEarned.Earned descending
+							select hourEarned.Hour).First();
+
+			//Convert to DTO and return
+			return new EarningsDTO
+			{
+				Times = allHoursEarned.Select(h => new TimeDTO
+				{
+					Hour = h.Hour,
+					Count = h.Count,
+					Earned = string.Format($"€{h.Earned}")
+				}).ToList(),
+				RushHour = rushHour
+			};
+		}
+		public static object CityCmd(IEnumerable<Transaction> transactions, string parameters)
+		{
+			//2 parameters required
+			//Split parameters
+			string formatErrorMessage = $"Command parameters, \"{parameters}\", are not formatted correctly.";
+			string[] splitParams = parameters.Split(' ');
+			//Error if we don't have 2 fields
+			if (splitParams.Length != 2) throw new FormatException(formatErrorMessage);
+			//Get what field we're ordering by
+			CityFilterField filterField;
+			if (!Enum.TryParse(splitParams[0][1..], out filterField)) throw new FormatException(formatErrorMessage);//Ignore first char as it's '-'
+																													//Get how we're ordering
+			CityFilterType filterType;
+			if (!Enum.TryParse(splitParams[1][1..], out filterType)) throw new FormatException(formatErrorMessage);//Ignore first char as it's '-'
+
+			//Group each city
+			var results = from transaction in transactions
+						  group transaction by transaction.City into cities
+						  select new
+						  {
+							  City = cities.Key,
+							  ItemCount = cities.Count(),
+							  TotalPrice = cities.Sum(c => c.PriceValue)
+						  };
+
+			//Apply filters
+			switch (filterField)
+			{
+				case CityFilterField.items:
+					//Order
+					results = results.OrderBy(c => c.ItemCount);
+					//Get value from min/max to check for ties
+					int minMaxCount = GetMinMaxValue<int>(results.Select(r => r.ItemCount), filterType);
+					//Filter results where only that count matches
+					results = results.Where(r => r.ItemCount ==  minMaxCount);
+					break;
+				case CityFilterField.money:
+					//Order
+					results = results.OrderBy(c => c.TotalPrice);
+					//Get value from min/max to check for ties
+					decimal minMaxPrice = GetMinMaxValue<decimal>(results.Select(r => r.TotalPrice), filterType);
+					//Filter results where only that count matches
+					results = results.Where(r => r.TotalPrice == minMaxPrice);
+					break;
+			}
+
+			//Combine cities if needed and return
+			return String.Join(", ", results.Select(r => r.City));
+		}
+
+		private static T GetMinMaxValue<T>(IEnumerable<T> values, CityFilterType filterType)
+		{
+			return filterType switch
+			{
+				CityFilterType.min => values.First(),
+				CityFilterType.max => values.Last(),
+				_ => throw new ArgumentException()
+			};
+		}
+
+		private enum CityFilterField
+		{
+			items,
+			money
+		}
+		private enum CityFilterType
+		{
+			min,
+			max
+		}
+	}
+
+
+}
---
+++ Src/BootCamp.Chapter/TransactionSerializer.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BootCamp.Chapter
+{
+	public class TransactionSerializer
+	{
+		private readonly IMapper _mapper = SetupMapper();
+		private static IMapper SetupMapper()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				cfg.CreateMap<Transaction, TransactionDTO>();
+				cfg.CreateMap<TransactionDTO, Transaction>();
+			});
+
+			return new Mapper(config);
+		}
+
+		public IEnumerable<Transaction> DeserializeFile(string inputFile)
+		{
+			//Validate input file
+			if (!File.Exists(inputFile)) throw new NoTransactionsFoundException();
+			//Prepare DTO list variable
+			List<TransactionDTO> transactionDTOs = null;
+			//Get text from file
+			string fileText = File.ReadAllText(inputFile);
+			//Check if file is XML or JSON
+			switch (Path.GetExtension(inputFile))
+			{
+				case ".json":
+					transactionDTOs = JsonConvert.DeserializeObject<List<TransactionDTO>>(fileText);
+					break;
+				case ".xml":
+					//Convert to Json first as it's easier to handle (Just needs additional wrapper classes)
+					XmlDocument xmlDocument = new XmlDocument();
+					xmlDocument.LoadXml(fileText);
+					string jsonFileText = JsonConvert.SerializeXmlNode(xmlDocument);
+					transactionDTOs = JsonConvert.DeserializeObject<JsonTransactionsDTO>(jsonFileText).TransactionsDTO.Transactions;
+
+					//Couldn't figure out how to deserialize a standard XML file
+					//XmlSerializer xmlSerializer = new XmlSerializer(typeof(TransactionsDTO));
+					//using (TextReader reader = new StringReader(fileText))
+					//{
+					//	var test = (TransactionsDTO)xmlSerializer.Deserialize(reader);
+					//}
+					break;
+			}
+
+			//Convert DTO to Transaction obj
+			return transactionDTOs.Select(dto => _mapper.Map<Transaction>(dto)).ToList();
+		}
+
+		public void SerializeFile(string outputFile, object dto)
+		{
+			//Serialize based on output file extension type
+			string resultFileText = Path.GetExtension(outputFile) switch
+			{
+				".json" => JsonConvert.SerializeObject(dto, Newtonsoft.Json.Formatting.Indented),
+				".xml" => SerializeXml(dto),
+				_ => dto.ToString()
+			};
+
+			File.WriteAllText(outputFile, resultFileText);
+		}
+
+		private string SerializeXml(object dto)
+		{
+			//Disable xmlns text from file
+			XmlSerializerNamespaces xmlns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+			//Create serializer
+			XmlSerializer serializer = new XmlSerializer(dto.GetType());
+			//Disable xml declarations
+			XmlWriterSettings settings = new XmlWriterSettings()
+			{
+				Indent = true,
+				OmitXmlDeclaration = true
+			};
+
+			string resultText;
+			using (StringWriter stringWriter = new StringWriter())
+			{
+				using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, settings))
+				{
+					serializer.Serialize(xmlWriter, dto, xmlns);
+					resultText = stringWriter.ToString();
+				}
+			}
+
+			return resultText;
+		}
+	}
+}
