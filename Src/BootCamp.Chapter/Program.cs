a61beb20cbc72e181cd832ebb8466780eb033a68
TMason11095 <tmason11095@gmail.com>
2023-08-01 01:48:20 -0500

    Submit CH3 HW 12: Tests/AddressTests: Address1_EqualityOperator_On_Address2_When_Both_Have_Same_Values_Returns_True and Address1_EqualityOperator_On_Address2_When_Both_Have_Different_Values_Returns_False are comparing "address2 == address2". Changed to "address1 == address2" to match methods' naming. -------------------- -Updated Addresses.txt's "Copy to Output Directory" property to be "Copy if newer" as the file wasn't showing up during debugging. -Implemented Address.TryParse(). -Implemented Address's == and != overloads. -Implemented Address's Equals() and GetHashCode() to utilize the new == overload within LINQ. -Updated Main() to read the given text file, find postal codes with the most duplicate address entries, and then display the results to the console.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace BootCamp.Chapter
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		private static readonly string FILE_PATH = "Addresses.txt";
 
-        }
-    }
+		static void Main(string[] args)
+		{
+			//Read Address file
+			string fileText = File.ReadAllText(FILE_PATH);
+			//Split into each address entry
+			//Each address entry is seperated by a new line (1st newline is the end of the entry, 2nd newline is the line between entries)
+			string[] addressTexts = fileText.Split(Environment.NewLine + Environment.NewLine);
+			//Parse into address objects
+			List<Address> addresses = new List<Address>();
+			foreach (string addressText in addressTexts)
+			{
+				Address address;
+				bool isAddress = Address.TryParse(addressText, out address);
+				if (isAddress)
+				{
+					addresses.Add(address);
+				}
+			}
+
+			//Get duplicate addresses
+			var dupAddresses = from address in addresses
+							   group address by address into addressGroup
+							   select new
+							   {
+								   DuplicateAddress = addressGroup.Key,
+								   DuplicateCount = addressGroup.Count()
+							   };
+			//Get total count per postal code
+			var postalGroupDups = from address in dupAddresses
+								  group address by address.DuplicateAddress.PostalCode into postalGroup
+								  select new
+								  {
+									  PostalCode = postalGroup.Key,
+									  DupCount = postalGroup.Sum(p => p.DuplicateCount)
+								  };
+			//Get highest dup count
+			int biggestDupCount = postalGroupDups.Max(p => p.DupCount);
+			//Get all postal codes with highest dup count (if more than 1)
+			var biggestDupCountPostals = from postalCode in postalGroupDups
+										 where postalCode.DupCount == biggestDupCount
+										 select postalCode.PostalCode;
+			string resultPostals = String.Join(", ", biggestDupCountPostals);
+
+			//Write results to console
+			Console.WriteLine($"Postal code(s) with the most duplicate addresses: {resultPostals}");
+			Console.ReadLine();
+		}
+	}
 }
