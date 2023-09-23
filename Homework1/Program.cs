ddd081921aa82a6b6faccb19f37bd57bf0b078b3
7IronSnow7 <divan.debruin25@gmail.com>
2023-09-22 22:02:17 -0400

    Homework1And2/Divan.

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person One
+            Console.WriteLine(PersonDetails("Tom", "Jefferson", 19, 50, 156.65));
+            Console.WriteLine($"BMI = {Math.Round(BodyMassIndex(50, 156.65), 1)}");
+            // Person Two
+            Console.WriteLine($"\n{PersonDetails("Tommy", "Shelby", 47, 70, 173.00)}");
+            Console.WriteLine($"BMI = {Math.Round(BodyMassIndex(70, 173.00), 1)}");
+        }
+        public static string PersonDetails(string firstName, string lastName, int age, int weight, double height)
+        {
+            var userDetails = string.Empty;
+
+            userDetails = $"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is ";
+
+            return userDetails; 
+        }
+        public static double BodyMassIndex(int weight, double height)
+        {
+            var bmi = weight / (Math.Pow((height / 100), 2));
+
+            return bmi;
+        }
+    }
+}
