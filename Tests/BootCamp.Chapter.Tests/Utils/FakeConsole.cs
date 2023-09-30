341d21550148a3cb3dbeb391652a9e2780f4e5fb
LostCSharp7 <avinash.kurma@gmail.com>
2023-10-01 05:19:35 +0530

    Chapter 4 homework

--- Tests/BootCamp.Chapter.Tests/Utils/FakeConsole.cs
+++ Tests/BootCamp.Chapter.Tests/Utils/FakeConsole.cs
@@ -30,4 +30,20 @@ namespace BootCamp.Chapter.Tests.Utils
             File.Delete($"{testKey}.{TestFileExtension}");
         }
     }
+
+
+    public static class ConsoleStub
+    {
+        public static StringWriter StubConsole(string readLineReturn)
+            {
+                var output = new StringWriter();
+                Console.SetOut(output);
+
+                var input = new StringReader(readLineReturn);
+                Console.SetIn(input);
+
+                return output;
+            }
+    }
+    
 }
