5739710eff367e4ae44b02588a534149e750faa0
LostCSharp7 <avinash.kurma@gmail.com>
2023-10-02 13:09:24 +0530

    Chapter 5 Homework

--- Src/BootCamp.Chapter1/Program.cs
+++ Src/BootCamp.Chapter1/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace BootCamp.Chapter1
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace BootCamp.Chapter1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] array = new int[] {1};
+            foreach (var num in array)
+            {
+                Console.Write(num + ",");
+            }
+            Console.WriteLine(Environment.NewLine);
+           ArrayOperations.Reverse(array);
+
+            foreach(var num in array)
+            {
+                Console.Write(num + ",");
+            }
+
         }
     }
 }
