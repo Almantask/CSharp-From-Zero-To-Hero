7a3a13430bfc7348988bc74d8d4aeee0e26caeb1
emailaddressforpublicusage@gmail.com <emailaddressforpublicusage@gmail.com>
2023-10-14 11:34:13 -0400

    Homework 1 and 2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,20 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Please state your full name:");
+                string fullname = Console.ReadLine();
+                Console.WriteLine("Please state your age in years:");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please state your weight in kilograms:");
+                double weight = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please state your height in meters:");
+                double height = double.Parse(Console.ReadLine());
+                double bmi = weight / (height * height);
+                Console.WriteLine(fullname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height * 100 + " cm.");
+                Console.WriteLine(fullname + " has a BMI of " + bmi);
+            }
         }
     }
 }
