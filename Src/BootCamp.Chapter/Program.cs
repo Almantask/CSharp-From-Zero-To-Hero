41d48c5ea93e18bcbc9c9bc6840c976aa1eabc98
Fowsia Anzal <fowsianzal@gmail.com>
2023-12-18 20:27:34 +0800

    Homework1And2 is done.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,17 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Enter the first name:");
+            string firstName=Console.ReadLine();
+            Console.WriteLine("Enter the last name:");
+            string lastName=Console.ReadLine();
+            Console.WriteLine("Enter the age:");
+            int age=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the weight:");
+            float weight=float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            float height=float.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight}kg and his height is {height}cm.");
         }
     }
 }
