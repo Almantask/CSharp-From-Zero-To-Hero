a737d1ddad986416f87f7a77e1abc9b0cb7a4a12
Fowsia Anzal <fowsianzal@gmail.com>
2023-12-18 21:27:53 +0800

    bmi calculated and do the same for second person.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,6 +6,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            double bmi,bmi1 = 0;
             Console.WriteLine("Enter the first name:");
             string firstName=Console.ReadLine();
             Console.WriteLine("Enter the last name:");
@@ -17,6 +18,22 @@ namespace BootCamp.Chapter
             Console.WriteLine("Enter the height:");
             float height=float.Parse(Console.ReadLine());
             Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight}kg and his height is {height}cm.");
+            bmi = weight / ((height/100) * (height/100));
+            Console.WriteLine($"BMI of {firstName} {lastName} is {bmi}");
+            Console.WriteLine("Enter the first name:");
+            string firstName1 = Console.ReadLine();
+            Console.WriteLine("Enter the last name:");
+            string lastName1 = Console.ReadLine();
+            Console.WriteLine("Enter the age:");
+            int age1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the weight:");
+            float weight1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            float height1 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstName1} {lastName1} is {age1} years old, his weight is {weight1}kg and his height is {height1}cm.");
+            bmi1 = weight1 / ((height1 / 100) * (height1 / 100));
+            Console.WriteLine($"BMI of {firstName1} {lastName1} is {bmi1}");
+
         }
     }
 }
