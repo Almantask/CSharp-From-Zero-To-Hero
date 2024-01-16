51788a7d453f910d30179f315b72f459925a06ec
a-goben <agoben@telzio.com>
2024-01-16 10:04:36 -0500

    add in ch1 homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+        
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+        
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+        
+            Console.Write("Please enter your weight in lbs: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+        
+            Console.Write("Please enter your height in inches: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            double bmi = weight / Math.Pow(height, 2) * 703;
+        
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, their weight is {weight} lbs and their height is {height} inches. Their BMI is {bmi}.");
         }
     }
 }
