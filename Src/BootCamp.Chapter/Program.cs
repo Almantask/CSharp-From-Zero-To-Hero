8cb00a49cfa0705cb16df894950278d2ee126146
fowsianzal <fowsianzal@gmail.com>
2024-02-21 14:07:45 +0800

    Homework2 done

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,6 +9,33 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            ReadPersonDetails();
+             
+            ReadPersonDetails();
+
+        }
+         static void ReadPersonDetails()
+        {
+            Console.WriteLine("\nEnter the First name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the Surname:");
+            string surName = Console.ReadLine();
+            Console.WriteLine("Enter the age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the weight(kg):");
+            decimal weight = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height(cm):");
+            decimal height = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"\n{firstName} {surName} is {age} years old, his weight is {weight} kg and his height is {height} cm");
+            decimal Bmi = CalculateBMI(weight, height);
+            Console.WriteLine($"\nBMI of {firstName} {surName} is {Bmi}");
+        }
+
+        static decimal CalculateBMI(decimal weight, decimal height)
+        {
+            decimal high = (height / 100) * (height / 100);
+            decimal bmi = weight / high;
+            return bmi;
         }
     }
 }
