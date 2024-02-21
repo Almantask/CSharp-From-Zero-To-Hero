e1a47630acc4dad10c89517ffc933f6594cd8ff8
fowsianzal <fowsianzal@gmail.com>
2024-02-21 18:51:35 +0800

    changed metre to centimeter in program.cs

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -21,9 +21,10 @@ namespace BootCamp.Chapter
             string surName=Lesson3.PromptString("Enter the Surname:");
             int age=Lesson3.PromptInt("Enter the age:");
             float weight=Lesson3.PromptFloat("Enter the weight(kg):");
-            float height=Lesson3.PromptFloat("Enter the height(m):");
-            float bmi=Lesson3.CalculateBmi(weight,height);
-            Console.WriteLine($"\n{firstName} {surName} is {age} years old, his weight is {weight} kg and his height is {height} m ");
+            float height=Lesson3.PromptFloat("Enter the height(cm):");
+            float heightInMeters=height/100;
+            float bmi=Lesson3.CalculateBmi(weight, heightInMeters);
+            Console.WriteLine($"\n{firstName} {surName} is {age} years old, his weight is {weight} kg and his height is {height} cm ");
             Console.WriteLine($"His BMI is {bmi}");
         }
          
