503ebb9b705341afccab82a592f610be1bbb12f1
fowsianzal <fowsianzal@gmail.com>
2024-02-22 14:12:41 +0800

    Homework4 done

--- Src/BootCamp.Chapter/Checks.cs
+++ Src/BootCamp.Chapter/Checks.cs
@@ -16,26 +16,81 @@ namespace BootCamp.Chapter
     {
         public static int PromptInt(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            if(string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input cannot be empty");
+                return -1;
+            }
+            else if (!int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"\"{input}\" is not a valid number.");
+                return -1;
+            }
+            else
+            {
+                return number;
+            }
+
         }
 
         public static string PromptString(string message)
         {
-            // To do: call your implementation. 
-            return "";
+            Console.WriteLine(message);
+            var name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name cannot be empty");
+                return "-";
+            }
+            else
+            {
+                return name;
+            }
         }
 
         public static float PromptFloat(string message)
         {
-            // To do: call your implementation. 
-            return 0;
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input cannot be empty");
+                return -1;
+            }
+            else if (!float.TryParse(input, out float number))
+            {
+                Console.WriteLine($"\"{input}\" is not a valid number.");
+                return -1;
+            }
+            else
+            {
+                return number;
+            }
         }
 
         public static float CalculateBmi(float weight, float height)
         {
-            // To do: call your implementation. 
-            return 0;
+            if (weight == 0 && height == 0)
+            {
+                Console.WriteLine("Failed calculating BMI. Reason:Weight cannot be equal or less than zero, but was 0.\n Height cannot be equal or less than zero, but was 0");
+                return -1;
+            }
+            else if (weight <= 0)
+            {
+                Console.WriteLine($"Failed calculating BMI. Reason:Weight cannot be equal or less than zero, but was {weight}.");
+                return -1;
+            }
+            else if (height <= 0)
+            {
+                Console.WriteLine($"Failed calculating BMI. Reason:Height cannot be equal or less than zero, but was {height}");
+                return -1;
+            }   
+            else
+            {
+                return weight / (height * height);
+            }
         }
     }
 }
